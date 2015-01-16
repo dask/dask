4d0de04cda71e0480d06fdc14041f6358a0fce59
Wesley Emeneker <Wesley.Emeneker@continuum.io>
2015-01-16 15:29:56 -0500

    Bugfix on block step offsets. Added comments and doctests. Adding comments and doctests revealed some bugs in the previous code that didn't properly handle steps with offsets. The updated code fixes these errors, but I'm not happy with it. I think that the code can be significantly simpler and clearer, but I don't understand it well-enough yet. As it stands, steps and starting offsets should be properly handled now.

--- dask/tests/test_slicing.py
+++ dask/tests/test_slicing.py
@@ -1,96 +1,64 @@
 #!/usr/bin/env py.test
 
 import dask
-from dask.array import dask_1d_slice, _dask_slice
+from dask.array import _block_slice_step_start, _index_in_block, dask_slice, _slice_1d
 import operator
+import numpy
 
-def test_1d_left_slicing():
-    expected = {('y', 0): (operator.getitem, ('x',0), (slice(5,20))),
-                ('y', 1): (operator.getitem, ('x',1), (slice(0,3)))}
-    result = dask_1d_slice('y', 'x', slice(5,23), (100,), (20,))
-    assert expected == result
+def test_block_slice_step_start():
+    arr25 = numpy.arange(25)
+    arr27 = numpy.arange(27)
 
-def test_1d_inner_slicing():
-    expected = {('y', 0): (operator.getitem, ('x',0), (slice(3,19)))}
-    result = dask_1d_slice('y', 'x', slice(3,19), (100,), (20,))
-    assert expected == result
+    assert all(arr25[::2] == arr25[_block_slice_step_start(25, 0, 0, 25, 2)])
+    assert all(arr25[1::2] == arr25[_block_slice_step_start(25, 1, 0, 50, 2)])
+    assert all(arr25[::2] == arr25[_block_slice_step_start(25, 2, 0, 100, 2)])
+    assert all(arr25[1::2] == arr25[_block_slice_step_start(25, 3, 0, 150, 2)])
 
-def test_1d_full_inner_slicing():
-    expected = {('y', 0): (operator.getitem, ('x',2), (slice(5,20))),
-                ('y', 1): (operator.getitem, ('x',3), (slice(0,20))),
-                ('y', 2): (operator.getitem, ('x',4), (slice(0,12)))}
-    result = dask_1d_slice('y', 'x', slice(45,92), (100,), (20,))
-    assert expected == result
+    assert all(arr25[::3] == arr25[_block_slice_step_start(25, 0, 0, 25, 3)])
+    assert all(arr25[::10] == arr25[_block_slice_step_start(25, 0, 0, 25, 10)])
+    assert all(arr25[::20] == arr25[_block_slice_step_start(25, 0, 0, 25, 20)])
+    assert all(arr25[::32] == arr25[_block_slice_step_start(25, 0, 0, 25, 32)])
     
+
+def test_slice_1d():
+    expected = {0: slice(10, 25, None), 1: slice(0, 25, None), 2: slice(0, 1, None)}
+    result = _slice_1d(100, 25, slice(10,51,None))
+
+    assert expected == result
+
+    expected = {0: slice(None, None, None), 1: slice(None, None, None), 2: slice(None, None, None), 3: slice(None, None, None)}
+    result = _slice_1d(100,25,slice(None,None,None))
+
+    assert expected == result
+
+
+def test_dask_slice_1d():
+    expected = {('y', 0): (operator.getitem, ('x', 0), (slice(24, 25, 2),)),
+                ('y', 1): (operator.getitem, ('x', 1), (slice(1, 25, 2),)),
+                ('y', 2): (operator.getitem, ('x', 2), (slice(0, 25, 2),)),
+                ('y', 3): (operator.getitem, ('x', 3), (slice(1, 25, 2),))}
+    result = dask_slice('x', 'y', [100], [25], [slice(24,None,2)])
+
+    assert expected == result
+
+    expected = {('y', 0): (operator.getitem, ('x', 1), (slice(1, 25, 2),)),
+                ('y', 1): (operator.getitem, ('x', 2), (slice(0, 25, 2),)),
+                ('y', 2): (operator.getitem, ('x', 3), (slice(1, 25, 2),))}
         
-def test_1d_right_border_slicing():
-    #The first test slices up to the right-most border
-    #  of the array
-    expected = {('y', 0): (operator.getitem, ('x',2), (slice(5,20))),
-                ('y', 1): (operator.getitem, ('x',3), (slice(0,20))),
-                ('y', 2): (operator.getitem, ('x',4), (slice(0,20)))}
-    result = dask_1d_slice('y', 'x', slice(45,100), (100,), (20,))
+    result = dask_slice('x', 'y', [100], [25], [slice(26,None,2)])    
     assert expected == result
 
-    #The first test slices past the right-most border
-    #  of the array
-    expected = {('y', 0): (operator.getitem, ('x',2), (slice(5,20))),
-                ('y', 1): (operator.getitem, ('x',3), (slice(0,20))),
-                ('y', 2): (operator.getitem, ('x',4), (slice(0,20)))}
-    result = dask_1d_slice('y', 'x', slice(45,200), (100,), (20,))
-    assert expected == result
-    
-
-def test_1d_negative_slicing():
-    expected = {('y', 0): (operator.getitem, ('x',2), (slice(15,20))),
-                ('y', 1): (operator.getitem, ('x',3), (slice(0,20))),
-                ('y', 2): (operator.getitem, ('x',4), (slice(0,19)))}
-    result = dask_1d_slice('y', 'x', slice(-45,-1), (100,), (20,))
-    assert expected == result
-
-def test_1d_bad_slices():
-    expected = {}
-    result = dask_1d_slice('y', 'x', slice(100,200), (100,), (20,))
-    assert expected == result
-
-    
-
-def test_priv_dask_slice_1d():
-    expected = {('y', 0):(operator.getitem, ('x', 0), (slice(5,20),)),
-                ('y', 1):(operator.getitem, ('x', 1), (slice(0,10),)),
-                }
-    result = _dask_slice('x', 'y', [0], [2], [5], [10], [20])
+def test_dask_slice_2d():
+    expected = {('y', 0, 0): (operator.getitem,
+                               ('x', 0, 0),
+                               (slice(13, 20, 2), slice(10, 20, None))),
+                 ('y', 0, 1): (operator.getitem,
+                               ('x', 0, 1),
+                               (slice(13, 20, 2), slice(0, 20, None))),
+                 ('y', 0, 2): (operator.getitem,
+                               ('x', 0, 2),
+                               (slice(13, 20, 2), slice(0, 5, None)))}
+        
+    result = dask_slice('x', 'y', [20, 45], [25, 20], [slice(13,None,2), slice(10, None, None)])
 
     assert expected == result
-    
-
-def test_priv_dask_slice_2d():
-    expected = {('y', 0, 0):(operator.getitem, ('x', 1, 0),
-                             (slice(5,20), slice(10,22))),
-                ('y', 1, 0):(operator.getitem, ('x', 2, 0),
-                             (slice(0,10), slice(10,22))),
-                }
-    result = _dask_slice('x', 'y', [1, 0], [3, 1], [5, 10], [10, 22], [20, 30])
-
-    assert expected == result
-    
-    expected = {('y', 0, 0):(operator.getitem, ('x', 1, 0),
-                             (slice(5,20), slice(10,30))),
-                ('y', 0, 1):(operator.getitem, ('x', 1, 1),
-                             (slice(5,20), slice(0,15))),
-
-                ('y', 1, 0):(operator.getitem, ('x', 2, 0),
-                             (slice(0,20), slice(10,30))),
-                ('y', 1, 1):(operator.getitem, ('x', 2, 1),
-                             (slice(0,20), slice(0,15))),
-
-                ('y', 2, 0):(operator.getitem, ('x', 3, 0),
-                             (slice(0,10), slice(10,30))),
-                ('y', 2, 1):(operator.getitem, ('x', 3, 1),
-                             (slice(0,10), slice(0,15)))
-                }
-
-    result = _dask_slice('x', 'y', [1, 0], [4, 2], [5, 10], [10, 15], [20, 30])
-
-    assert expected == result
-    
