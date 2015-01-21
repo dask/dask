01f66830f5c76836d3da27c3dd408a41b9573d94
Matthew Rocklin <mrocklin@gmail.com>
2015-01-20 17:45:29 -0800

    guard md imports

--- dask/__init__.py
+++ dask/__init__.py
@@ -1,3 +1,8 @@
 from .core import istask, get, set
+from multipledispatch import halt_ordering, restart_ordering
+
+halt_ordering()
+from .obj import Array
+restart_ordering()
 
 __version__ = '0.1.0'
