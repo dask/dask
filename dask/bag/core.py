6ebc86ca227593fb8cfad88f37f2a5d5560d05b5
Matthew Rocklin <mrocklin@gmail.com>
2015-01-30 18:25:59 -0800

    Add first draft of bag

---
+++ dask/bag/core.py
@@ -0,0 +1,68 @@
+import itertools
+from toolz import merge, concat
+from ..threaded import get # TODO: get better get
+
+names = ('bag-%d' % i for i in itertools.count(1))
+
+
+class Item(object):
+    def __init__(self, dsk, key):
+        self.dask = dsk
+        self.key = key
+
+    def compute(self):
+        return get(self.dask, self.key)
+
+    __int__ = __float__ = __complex__ = __bool__ = compute
+
+
+class Bag(object):
+    """ Unordered collection with repeats
+
+    Computed in paritions with dask
+
+    >>> dsk = {('x', 0): (range, 5),
+    ...        ('x', 1): (range, 5),
+    ...        ('x', 2): (range, 5)}
+    >>> b = Bag(dsk, 'x', npartitions=3)
+
+    >>> sorted(b.map(lambda x: x * 10))
+    [0, 0, 0, 10, 10, 10, 20, 20, 20, 30, 30, 30, 40, 40, 40]
+
+    >>> int(b.fold(lambda x, y: x + y))
+    30
+    """
+    def __init__(self, dsk, name, npartitions):
+        self.dask = dsk
+        self.name = name
+        self.npartitions = npartitions
+
+    def map(self, func):
+        name = next(names)
+        dsk = dict(((name, i), (map, func, (self.name, i)))
+                        for i in range(self.npartitions))
+        return Bag(merge(self.dask, dsk), name, self.npartitions)
+
+    def filter(self, predicate):
+        name = next(names)
+        dsk = dict(((name, i), (filter, predicate, (self.name, i)))
+                        for i in range(self.npartitions))
+        return Bag(merge(self.dask, dsk), name, self.npartitions)
+
+    def fold(self, binop, combine=None, initial=None):
+        a = next(names)
+        b = next(names)
+        if initial:
+            dsk = dict(((a, i), (reduce, binop, (self.name, i), initial))
+                            for i in range(self.npartitions))
+        else:
+            dsk = dict(((a, i), (reduce, binop, (self.name, i)))
+                            for i in range(self.npartitions))
+        dsk2 = {b: (reduce, combine or binop, list(dsk.keys()))}
+        return Item(merge(self.dask, dsk, dsk2), b)
+
+    def keys(self):
+        return [(self.name, i) for i in range(self.npartitions)]
+
+    def __iter__(self):
+        return concat(get(self.dask, self.keys()))
---
+++ dask/bag/tests/test_core.py
@@ -0,0 +1,51 @@
+from toolz import merge
+from dask.bag.core import *
+
+dsk = {('x', i): (range, 5) for i in range(3)}
+b = Bag(dsk, 'x', 3)
+
+def inc(x):
+    return x + 1
+
+def iseven(x):
+    return x % 2 == 0
+
+def add(x, y):
+    return x + y
+
+
+def test_Bag():
+    assert b.name == 'x'
+    assert b.npartitions == 3
+
+
+def test_keys():
+    assert sorted(b.keys()) == sorted(dsk.keys())
+
+
+def test_map():
+    c = b.map(inc)
+    expected = merge(dsk, {(c.name, i): (map, inc, (b.name, i))
+                           for i in range(b.npartitions)})
+    assert c.dask == expected
+
+def test_filter():
+    c = b.filter(iseven)
+    expected = merge(dsk, {(c.name, i): (filter, iseven, (b.name, i))
+                           for i in range(b.npartitions)})
+    assert c.dask == expected
+
+def dont_test_fold():
+    c = b.fold(add)
+    expected = merge(dsk, {(c.name, i): (reduce, add, (b.name, i))
+                           for i in range(b.npartitions)})
+    assert c.dask == expected
+
+
+def test_iter():
+    assert sorted(list(b)) == sorted(list(range(5)) * 3)
+    assert sorted(list(b.map(inc))) == sorted(list(range(1, 6)) * 3)
+
+
+def test_fold_computation():
+    assert int(b.fold(add)) == sum(range(5)) * 3
