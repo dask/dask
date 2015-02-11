d34f5d6de997d40f92c52e623a1b32c05ac760b5
Matthew Rocklin <mrocklin@gmail.com>
2015-02-11 14:02:27 -0800

    deepmap supports multiple arg inputs

--- dask/utils.py
+++ dask/utils.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, division, print_function
+from collections import Iterator
 
 def raises(err, lamda):
     try:
@@ -8,14 +9,18 @@ def raises(err, lamda):
         return True
 
 
-def deepmap(func, seq):
+def deepmap(func, *seqs):
     """ Apply function inside nested lists
 
     >>> inc = lambda x: x + 1
     >>> deepmap(inc, [[1, 2], [3, 4]])
     [[2, 3], [4, 5]]
+
+    >>> add = lambda x, y: x + y
+    >>> deepmap(add, [[1, 2], [3, 4]], [[10, 20], [30, 40]])
+    [[11, 22], [33, 44]]
     """
-    if isinstance(seq, list):
-        return [deepmap(func, item) for item in seq]
+    if isinstance(seqs[0], (list, Iterator)):
+        return [deepmap(func, *items) for items in zip(*seqs)]
     else:
-        return func(seq)
+        return func(*seqs)
