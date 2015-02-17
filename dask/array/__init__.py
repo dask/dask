6d4e08e77782ad3bade6b3df5ddc1d372df88a50
Matthew Rocklin <mrocklin@gmail.com>
2015-02-17 11:32:45 -0800

    remove multipledispatch and into as dependencies

--- dask/array/__init__.py
+++ dask/array/__init__.py
@@ -1,9 +1,6 @@
 from __future__ import absolute_import, division, print_function
-from multipledispatch import halt_ordering, restart_ordering
 
-halt_ordering()
 from .core import Array, stack, concatenate, tensordot, transpose, from_array
-
 from .core import (arccos, arcsin, arctan, arctanh, arccosh, arcsinh, arctan2,
         ceil, copysign, cos, cosh, degrees, exp, expm1, fabs, floor, fmod,
         frexp, hypot, isinf, isnan, ldexp, log, log10, log1p, modf, radians,
@@ -11,4 +8,3 @@ from .core import (arccos, arcsin, arctan, arctanh, arccosh, arcsinh, arctan2,
 from .reductions import (sum, mean, std, var, any, all, min, max, vnorm)
 from . import random
 from .wrap import ones, zeros, empty
-restart_ordering()
