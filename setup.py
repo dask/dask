f5a06a337845f062d0dd0bc694feba84ca58601a
Matthew Rocklin <mrocklin@gmail.com>
2015-02-16 16:56:41 -0800

    add base to setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='dask',
       maintainer_email='mrocklin@gmail.com',
       license='BSD',
       keywords='task-scheduling parallelism',
-      packages=['dask', 'dask.array', 'dask.bag'],
+      packages=['dask', 'dask.array', 'dask.bag', 'dask.base'],
       long_description=(open('README.rst').read() if exists('README.rst')
                         else ''),
       install_requires=(list(open('requirements.txt').read().strip().split('\n'))),
