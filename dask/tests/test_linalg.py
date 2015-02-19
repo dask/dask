b75350a64c0494dc5de49152590431d7377eaa82
Mariano <marianotepper@gmail.com>
2015-02-18 23:40:17 -0500

    Basic test for QR properties: - A - QR must be small - Q^T * Q must be close to the identity matrix - R must be upper triangular

---
+++ dask/tests/test_linalg.py
@@ -0,0 +1,17 @@
+import numpy as np
+from dask.array import from_array
+from dask.array.linalg import tsqr
+
+
+def test_tsqr():
+    m, n = 1000, 20
+    mat = np.random.rand(m, n)
+    data = from_array(mat, blockshape=(200, n), name='A')
+
+    q, r = tsqr(data)
+    q = np.array(q)
+    r = np.array(r)
+
+    assert np.allclose(mat, np.dot(q, r))
+    assert np.allclose(np.eye(n, n), np.dot(q.T, q))
+    assert np.all(r == np.triu(r))
