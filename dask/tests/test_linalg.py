b26ded6d90dd2d405446c9bed110477f3cf8302a
Mariano <marianotepper@gmail.com>
2015-02-20 08:49:11 -0500

    Added a test for irregular blocks

--- dask/tests/test_linalg.py
+++ dask/tests/test_linalg.py
@@ -4,15 +4,30 @@ from dask.array import from_array
 from dask.array.linalg import tsqr
 
 
-def test_tsqr():
-    m, n = 1000, 20
+def test_tsqr_regular_blocks():
+    m, n = 20, 10
     mat = np.random.rand(m, n)
-    data = from_array(mat, blockshape=(200, n), name='A')
+    data = from_array(mat, blockshape=(10, n), name='A')
 
     q, r = tsqr(data)
     q = np.array(q)
     r = np.array(r)
 
     assert np.allclose(mat, np.dot(q, r))  # accuracy check
+    assert np.allclose(np.eye(n, n), np.dot(q.T, q))  # q must be orthonormal
+    assert np.all(r == np.triu(r))  # r must be upper triangular
+
+
+def test_tsqr_irregular_blocks():
+    m, n = 20, 10
+    mat = np.random.rand(m, n)
+    data = from_array(mat, blockshape=(3, n), name='A')[1:]
+    mat2 = mat[1:, :]
+
+    q, r = tsqr(data)
+    q = np.array(q)
+    r = np.array(r)
+
+    assert np.allclose(mat2, np.dot(q, r))  # accuracy check
     assert np.allclose(np.eye(n, n), np.dot(q.T, q))  # q must be orthonormal
     assert np.all(r == np.triu(r))  # r must be upper triangular
