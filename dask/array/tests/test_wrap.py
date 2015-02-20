d3950bf6e916e5f0d0f6cf041db4c4850d53d7fc
Matthew Rocklin <mrocklin@gmail.com>
2015-02-20 07:04:25 -0800

    add missing test_wrap file

---
+++ dask/array/tests/test_wrap.py
@@ -0,0 +1,17 @@
+from dask.array.wrap import ones
+import numpy as np
+
+def test_ones():
+    a = ones((10, 10), 'i4', blockshape=(4, 4))
+    x = np.array(a)
+    assert (x == np.ones((10, 10), 'i4')).all()
+
+def test_singleton_size():
+    a = ones(10, 'i4', blockshape=(4,))
+    x = np.array(a)
+    assert (x == np.ones(10, 'i4')).all()
+
+def test_kwargs():
+    a = ones(10, dtype='i4', blockshape=(4,))
+    x = np.array(a)
+    assert (x == np.ones(10, 'i4')).all()
