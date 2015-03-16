e5ee6afbbeafdc0c10badd00bcfdf523a14a0a86
Matthew Rocklin <mrocklin@gmail.com>
2015-03-16 15:09:35 -0700

    categorical stripping functions

--- dask/frame/shuffle.py
+++ dask/frame/shuffle.py
@@ -461,3 +461,72 @@ def consistent_until(x, start):
         if val != start_val:
             return start + i - 1, val
     return None, None
+
+
+""" Strip and re-apply category information
+
+Categoricals are great for storage as long as we don't repeatedly store the
+categories themselves many thousands of times.  Lets collect category
+information once, strip it from all of the shards, store, then re-apply as we
+load them back in
+"""
+
+def strip_categories(df):
+    """ Strip category information from dataframe
+
+    Operates in place.
+
+    >>> df = pd.DataFrame({'a': pd.Categorical(['Alice', 'Bob', 'Alice'])})
+    >>> df
+           a
+    0  Alice
+    1    Bob
+    2  Alice
+    >>> strip_categories(df)
+       a
+    0  0
+    1  1
+    2  0
+    """
+    for name in df.columns:
+        if df.dtypes[name] == 'category':
+            df[name] = df[name].cat.codes
+    return df
+
+
+def categorical_metadata(df):
+    """ Collects category metadata
+
+    >>> df = pd.DataFrame({'a': pd.Categorical(['Alice', 'Bob', 'Alice'])})
+    >>> categorical_metadata(df)
+    {'a': {'ordered': True, 'categories': Index([u'Alice', u'Bob'], dtype='object')}}
+
+    """
+    result = dict()
+    for name, in df.columns:
+        if df.dtypes[name] == 'category':
+            result[name] = {'categories': df[name].cat.categories,
+                            'ordered': df[name].cat.ordered}
+    return result
+
+
+def reapply_categories(df, metadata):
+    """ Reapply stripped off categories
+
+    Operates in place
+
+    >>> df = pd.DataFrame({'a': [0, 1, 0]})
+    >>> metadata = {'a': {'ordered': True,
+    ...                   'categories': pd.Index(['Alice', 'Bob'], dtype='object')}}
+
+    >>> reapply_categories(df, metadata)
+           a
+    0  Alice
+    1    Bob
+    2  Alice
+
+    """
+    for name, d in metadata.items():
+        df[name] = pd.Series(pd.Categorical.from_codes(df[name],
+                             d['categories'], d['ordered']), name=name)
+    return df
