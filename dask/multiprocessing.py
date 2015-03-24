bcb71cda5da5b26bd2bfffea693dd9207d22d4d3
Matthew Rocklin <mrocklin@gmail.com>
2015-03-24 10:48:00 -0700

    reused pools clean up properly

--- dask/multiprocessing.py
+++ dask/multiprocessing.py
@@ -13,7 +13,13 @@ cpu_count = psutil.cpu_count()
 
 def get(dsk, keys, optimizations=[fuse], num_workers=cpu_count):
     """ Multiprocessed get function appropriate for Bags """
-    pool = _globals['pool'] or multiprocessing.Pool(psutil.cpu_count())
+    pool = _globals['pool']
+    if pool is None:
+        pool = multiprocessing.Pool(psutil.cpu_count())
+        cleanup = True
+    else:
+        cleanup = False
+
     manager = multiprocessing.Manager()
     queue = manager.Queue()
 
@@ -27,7 +33,8 @@ def get(dsk, keys, optimizations=[fuse], num_workers=cpu_count):
         result = get_async(apply_async, cpu_count, dsk2, keys,
                            queue=queue)
     finally:
-        pool.close()
+        if cleanup:
+            pool.close()
     return result
 
 
--- dask/threaded.py
+++ dask/threaded.py
@@ -41,14 +41,22 @@ def get(dsk, result, nthreads=NUM_CPUS, cache=None, debug_counts=None, **kwargs)
     >>> get(dsk, ['w', 'y'])
     (4, 2)
     """
-    pool = _globals['pool'] or ThreadPool(nthreads)
+    pool = _globals['pool']
+
+    if pool is None:
+        pool = ThreadPool(nthreads)
+        cleanup = True
+    else:
+        cleanup = False
+
     queue = Queue()
     try:
         results = get_async(pool.apply_async, nthreads, dsk, result,
                             cache=cache, debug_counts=debug_counts,
                             queue=queue, **kwargs)
     finally:
-        pool.close()
-        pool.join()
+        if cleanup:
+            pool.close()
+            pool.join()
 
     return results
