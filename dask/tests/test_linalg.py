145a919fddf01a9bba9d5a5b4d58c75c71ae84df
Mariano <marianotepper@gmail.com>
2015-03-26 15:13:55 -0400

    tests for the SVD for tall-and-skinny matrices

--- dask/tests/test_linalg.py
+++ dask/tests/test_linalg.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 import numpy as np
 from dask.array import from_array
 from dask.array.linalg import tsqr
+from dask.async import get_sync
 
 
 def test_tsqr_regular_blocks():
@@ -31,3 +32,38 @@ def test_tsqr_irregular_blocks():
     assert np.allclose(mat2, np.dot(q, r))  # accuracy check
     assert np.allclose(np.eye(n, n), np.dot(q.T, q))  # q must be orthonormal
     assert np.all(r == np.triu(r))  # r must be upper triangular
+
+
+def test_tsqr_svd_regular_blocks():
+    m, n = 20, 10
+    mat = np.random.rand(m, n)
+    data = from_array(mat, blockshape=(10, n), name='A')
+
+    u, s, v = tsqr(data, compute_svd=True)
+    u = np.array(u)
+    s = np.array(s)
+    v = np.array(v)
+
+    assert np.allclose(mat, np.dot(u, np.dot(np.diag(s), v)))  # accuracy check
+    assert np.allclose(np.eye(n, n), np.dot(u.T, u))  # u must be orthonormal
+    assert np.allclose(np.eye(n, n), np.dot(v.T, v))  # v must be orthonormal
+    assert np.allclose(s, np.linalg.svd(mat)[1])  # s must contain the singular
+                                                  # values
+
+
+def test_tsqr_svd_irregular_blocks():
+    m, n = 20, 10
+    mat = np.random.rand(m, n)
+    data = from_array(mat, blockshape=(3, n), name='A')[1:]
+    mat2 = mat[1:, :]
+
+    u, s, v = tsqr(data, compute_svd=True)
+    u = np.array(u)
+    s = np.array(s)
+    v = np.array(v)
+
+    assert np.allclose(mat2, np.dot(u, np.dot(np.diag(s), v)))  # accuracy check
+    assert np.allclose(np.eye(n, n), np.dot(u.T, u))  # u must be orthonormal
+    assert np.allclose(np.eye(n, n), np.dot(v.T, v))  # v must be orthonormal
+    assert np.allclose(s, np.linalg.svd(mat2)[1])  # s must contain the singular
+                                                   # values
