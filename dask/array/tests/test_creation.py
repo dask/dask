47ec5e8b3e3a188b926068a8380fa3432ae0c50d
Blake Griffith <blake.a.griffith@gmail.com>
2015-04-02 14:55:24 -0500

    Fix tests. Mark arange failure cases with xfail.

--- dask/array/tests/test_creation.py
+++ dask/array/tests/test_creation.py
@@ -1,26 +1,18 @@
 import numpy as np
+from numpy.testing import assert_array_almost_equal
+import pytest
 
-import dask
 import dask.array as da
 
 
 def eq(a, b):
-    if isinstance(a, da.Array):
-        adt = a._dtype
-        a = a.compute(get=dask.get)
-    if isinstance(b, da.Array):
-        bdt = b._dtype
-        b = b.compute(get=dask.get)
-    else:
-        bdt = getattr(b, 'dtype', None)
-
-    if not str(adt) == str(bdt):
-        return False
-
-    c = a == b
-    if isinstance(c, np.ndarray):
-        c = c.all()
-    return c
+    """a is a dask array, b is a numpy array. Checks dtype, shape and value. No
+    `assert` needed.
+    """
+    a = np.array(a)
+    assert a.shape == b.shape
+    assert a.dtype == b.dtype
+    assert_array_almost_equal(a, b)
 
 
 def test_linspace():
@@ -55,25 +47,10 @@ def test_arange():
     eq(darr, nparr)
 
     # negative steps
-    darr = da.arange(5, 53, -3, blocksize=5)
-    nparr = np.arange(5, 53, -3)
+    darr = da.arange(53, 5, -3, blocksize=5)
+    nparr = np.arange(53, 5, -3)
     eq(darr, nparr)
 
-    # non-integer steps
-    darr = da.arange(2, 13, .3, blocksize=4)
-    nparr = np.arange(2, 13, .3)
-    eq(darr, nparr)
-
-    # both non-integer and negative
-    darr = da.arange(1.5, 7.7, -.8, blocksize=3)
-    nparr = np.arange(1.5, 7.7, -.8)
-    eq(darr, nparr)
-
-    darr = da.arange(-9.1, 3.3, -.25, blocksize=3)
-    nparr = np.arange(-9.1, 3.3, -.25)
-    eq(darr, nparr)
-
-    # dtypes
     darr = da.arange(77, blocksize=13, dtype=float)
     nparr = np.arange(77, dtype=float)
     eq(darr, nparr)
@@ -82,10 +59,31 @@ def test_arange():
     nparr = np.arange(2, 13, dtype=int)
     eq(darr, nparr)
 
-    darr = da.arange(1.5, 7.7, -.8, blocksize=3, dtype='f8')
-    nparr = np.arange(1.5, 7.7, -.8, dtype='f8')
+
+def test_arange_working_float_step():
+    """Sometimes floating point step arguments work, but this could be platform
+    dependent.
+    """
+    darr = da.arange(3.3, -9.1, -.25, blocksize=3)
+    nparr = np.arange(3.3, -9.1, -.25)
     eq(darr, nparr)
 
-    darr = da.arange(-9.1, 3.3, -.25, blocksize=3, dtype='i8')
-    nparr = np.arange(-9.1, 3.3, -.25, dtype='i8')
+
+@pytest.mark.xfail(reason="Casting floats to ints is not supported since edge"
+                          "behavior is not specified or guaranteed by NumPy.")
+def test_arange_cast_float_int_step():
+    darr = da.arange(3.3, -9.1, -.25, blocksize=3, dtype='i8')
+    nparr = np.arange(3.3, -9.1, -.25, dtype='i8')
+    eq(darr, nparr)
+
+
+@pytest.mark.xfail(reason="arange with a floating point step value can fail"
+                          "due to numerical instability.")
+def test_arange_float_step():
+    darr = da.arange(2., 13., .3, blocksize=4)
+    nparr = np.arange(2., 13., .3)
+    eq(darr, nparr)
+
+    darr = da.arange(7.7, 1.5, -.8, blocksize=3)
+    nparr = np.arange(7.7, 1.5, -.8)
     eq(darr, nparr)
