9f7822eab2530577d788b999edc6755955405fe9
Matthew Rocklin <mrocklin@gmail.com>
2015-04-19 11:14:24 -0700

    add filetexts to utils.py
    stolen from odo

--- dask/utils.py
+++ dask/utils.py
@@ -97,3 +97,27 @@ class IndexCallable(object):
 
     def __getitem__(self, key):
         return self.fn(key)
+
+
+@contextmanager
+def filetexts(d, open=open):
+    """ Dumps a number of textfiles to disk
+
+    d - dict
+        a mapping from filename to text like {'a.csv': '1,1\n2,2'}
+    """
+    for filename, text in d.items():
+        f = open(filename, 'wt')
+        try:
+            f.write(text)
+        finally:
+            try:
+                f.close()
+            except AttributeError:
+                pass
+
+    yield list(d)
+
+    for filename in d:
+        if os.path.exists(filename):
+            os.remove(filename)
