5fb7e0eb076ffd07573d104765e4b136f852d29a
Matthew Rocklin <mrocklin@gmail.com>
2015-04-20 11:54:04 -0700

    dataframe warning on import

--- dask/dataframe/__init__.py
+++ dask/dataframe/__init__.py
@@ -1,3 +1,15 @@
+from warnings import warn
+
+warn('''
+
+Please pardon our dust.
+Dask.dataframe is experimental and not ready for public use.
+Expect errors today and breaks in the APIs in the future.
+
+Please report issues to https://github.com/ContinuumIO/dask/issues/new
+''')
+
+
 from .core import (DataFrame, Series, Index, _Frame, read_csv, from_array,
         from_bcolz)
 from .shuffle import from_pframe
