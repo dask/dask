259e86d08cb32130b13be35615bd4f03fe5ba739
Blake Griffith <blake.a.griffith@gmail.com>
2015-04-21 16:06:26 -0500

    Add dask.array.image in __init__.py

--- dask/array/__init__.py
+++ dask/array/__init__.py
@@ -19,7 +19,7 @@ from .reductions import (sum, prod, mean, std, var, any, all, min, max, vnorm,
 from .percentile import percentile
 with ignoring(ImportError):
     from .reductions import nanprod
-from . import random, linalg, ghost, learn
+from . import random, linalg, ghost, learn, image
 from .wrap import ones, zeros, empty, full
 from .rechunk import rechunk
 from ..context import set_options
