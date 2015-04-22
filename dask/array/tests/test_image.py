3fc4f6d4ee446022a83d57d491b3c594c6f97e29
Blake Griffith <blake.a.griffith@gmail.com>
2015-04-22 10:20:04 -0500

    Test gaussian_filter wrapping

--- dask/array/tests/test_image.py
+++ dask/array/tests/test_image.py
@@ -4,7 +4,6 @@ from numpy.testing import assert_array_almost_equal
 import pytest
 
 import dask.array as da
-from dask.array.image import filter_
 
 filters = pytest.importorskip('scipy.ndimage.filters')
 
@@ -12,12 +11,12 @@ from scipy.ndimage.filters import (gaussian_filter)
 
 def test_filters():
     # make data
-    a = np.random.random((10, 10))
-    d = da.from_array(a, chunks=(2, 5))
+    a = np.random.random((50, 50))
+    d = da.from_array(a, chunks=(10, 20))
 
     # apply filter
     sigma = 2
-    res = filter_(gaussian_filter, d, sigma=sigma)
+    res = da.image.filter_(gaussian_filter, d, sigma=sigma)
     exp = gaussian_filter(a, sigma=sigma)
 
     assert_array_almost_equal(np.array(res), exp)
