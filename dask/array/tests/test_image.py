6569b63da305292d34854514ba0d8a182d8f3a39
Blake Griffith <blake.a.griffith@gmail.com>
2015-04-23 12:51:43 -0500

    Tests for new ndimage stuff

--- dask/array/tests/test_image.py
+++ dask/array/tests/test_image.py
@@ -7,16 +7,42 @@ import dask.array as da
 
 filters = pytest.importorskip('scipy.ndimage.filters')
 
-from scipy.ndimage.filters import (gaussian_filter, gaussian_filter1d)
+from scipy.ndimage.filters import (gaussian_filter, gaussian_filter1d,
+                                   correlate1d, convolve1d)
 
-def test_filters():
-    # make data
-    a = np.random.random((20, 20))
-    d = da.from_array(a, chunks=(10, 10))
 
+# make data
+a = np.random.random((20, 20))
+d = da.from_array(a, chunks=(10, 10))
+
+
+def test_gaussian_filter():
     # apply filter
     sigma = 1
     res = da.image.filter_(gaussian_filter, d, sigma=sigma)
     exp = gaussian_filter(a, sigma=sigma)
+
+    assert_array_almost_equal(np.array(res), exp)
+
+
+def test_gaussian_filter1d():
+    # apply filter
+    sigma = 1
+    res = da.image.filter_(gaussian_filter1d, d, sigma=sigma)
+    exp = gaussian_filter1d(a, sigma=sigma)
+
+    assert_array_almost_equal(np.array(res), exp)
+
+def test_correlate1d():
+    weights = list(range(5))
+    res = da.image.filter_(correlate1d, d, weights=weights)
+    exp = correlate1d(a, weights=weights)
+
+    assert_array_almost_equal(np.array(res), exp)
+
+def test_convolve1d():
+    weights = list(range(5))
+    res = da.image.filter_(convolve1d, d, weights=weights)
+    exp = convolve1d(a, weights=weights)
 
     assert_array_almost_equal(np.array(res), exp)
