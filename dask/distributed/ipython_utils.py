f3397e5c7152c11b24cc563c970b7bd4da11b8a9
Blake Griffith <blake.a.griffith@gmail.com>
2015-05-13 12:37:33 -0500

    WIP on client_from_ipclient.

--- dask/distributed/ipython_utils.py
+++ dask/distributed/ipython_utils.py
@@ -6,9 +6,31 @@ from .client import Client
 def dask_client_from_ipclient(client):
     """
     Construct a scheduler from an ipython client.
+
+    1. Run a command on the first engine to start a scheduler. This should
+       return an address for clients and an address for workers.
+    2. Run some start worker command on the rest of the engines, giving them
+       the scheduler_to_engines address.
+    3. Start a client here with the scheduler to client address.
     """
-    zmq_context = client._context
-    scheduler = Scheduler(context=zmq_context)
-    workers = [Worker(scheduler.address_to_workers) for i in range(len(client))]
+
+    view = client[:]
+    def start_scheduler():
+        from dask.distributed import Scheduler
+        scheduler = Scheduler()
+        return scheduler.port_to_client, scheduler.port_to_workers
+
+    def start_worker(scheduler_port):
+        from dask.distributed import Worker
+        worker = Worker(scheduler_port)
+
+    # start sched
+    # TODO which view method to use apply sync or apply asyc? Depends on if I
+    # want to choose my addresses ahead of time.
+
+    # start workers
+    # TODO
+
+    # start client
     dask_client = Client(scheduler.address_to_clients)
     return dask_client
