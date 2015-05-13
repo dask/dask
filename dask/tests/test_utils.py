eb26bbd06b719afad6de9ecc6da12e934e327ba6
Matthew Rocklin <mrocklin@gmail.com>
2015-05-13 13:41:37 -0700

    textblock reads in binary mode, uses readline

--- dask/tests/test_utils.py
+++ dask/tests/test_utils.py
@@ -1,11 +1,16 @@
 from dask.utils import textblock
 from dask.utils import filetext
+import os
 
 def test_textblock():
-    with filetext('123\n456\n789\nabc\ndef\nghi') as fn:
-        with open(fn):
+    text = '123 456 789 abc def ghi'.replace(' ', os.linesep)
+    with filetext(text) as fn:
+        with open(fn, 'rb') as f:
             text = textblock(f, 1, 10)
-            assert text == '456\n789\n'
+            assert text == '456 789 '.replace(' ', os.linesep)
             assert set(map(len, text.split())) == set([3])
 
             assert textblock(f, 1, 10) == textblock(fn, 1, 10)
+
+            assert textblock(f, 0, 3) == '123' + os.linesep
+            assert textblock(f, 3 + len(os.linesep), 6) == '456' + os.linesep
