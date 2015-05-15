611c34eee4b5aa263669f1b7321b97fab9a98b5e
Blake Griffith <blake.a.griffith@gmail.com>
2015-05-15 14:48:41 -0500

    Remove lambda test. Add dask array tests.

--- dask/distributed/tests/test_ipython_utils.py
+++ dask/distributed/tests/test_ipython_utils.py
@@ -1,8 +1,29 @@
 from dask.distributed import dask_client_from_ipclient
+import numpy as np
+from numpy.testing import assert_array_almost_equal
+import dask.array as da
+
 
 def test_dask_client_from_ipclient():
     from IPython.parallel import Client
     c = Client()
-    dc = dask_client_from_ipclient(c)
-    assert 2 == dc.get({'a': 1, 'b': (lambda x: x + 1, 'a')}, 'b')
-    dc.close(close_workers=True, close_scheduler=True)
+    dask_client = dask_client_from_ipclient(c)
+
+    # data
+    a = np.arange(100).reshape(10, 10)
+    d = da.from_array(a, ((5, 5), (5, 5)))
+
+    # test array.mean
+    expected = a.mean(axis=0)
+    d1 = d.mean(axis=0)
+    result = d1.compute(get=dask_client.get)
+    assert_array_almost_equal(result, expected)
+
+    # test ghosting
+    d2 = da.ghost.ghost(d, depth=1, boundary='reflect')
+    d3 = da.ghost.trim_internal(d2, {0: 1, 1: 1})
+    result1 = d3.compute(get=dask_client.get)
+    assert_array_almost_equal(result1, a)
+
+    # close the workers
+    dask_client.close(close_workers=True, close_scheduler=True)
