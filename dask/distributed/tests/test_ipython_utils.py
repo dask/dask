b698dc387291899d978404a5fbc17c6d0c18358e
Blake Griffith <blake.a.griffith@gmail.com>
2015-05-17 15:03:41 -0500

    setup/teardown ipcluster in test_ipython_utils

--- dask/distributed/tests/test_ipython_utils.py
+++ dask/distributed/tests/test_ipython_utils.py
@@ -1,3 +1,6 @@
+import time
+from subprocess import Popen, PIPE
+
 import pytest
 
 np = pytest.importorskip('numpy')
@@ -6,12 +9,42 @@ pytest.importorskip('IPython.parallel')
 import numpy as np
 from numpy.testing import assert_array_almost_equal
 
+from IPython.parallel import Client
+
 from dask.distributed import dask_client_from_ipclient
 import dask.array as da
 
 
+def setup_module():
+    start_cmd = ['ipcluster', 'start', '-n', '4']
+    cp = Popen(start_cmd, stdout=PIPE, stderr=PIPE)
+    tic = time.time()
+    while True:
+        line = cp.stderr.readline().decode()
+        if 'Engines appear to have started' in line:
+            break
+        elif 'CRITICAL | Cluster is already running' in line:
+            raise RuntimeWarning("Cluster is already running")
+        elif time.time() - tic > 50:
+            raise RuntimeError("Timeout waiting for cluster to start.")
+        time.sleep(0.1)
+
+
+def teardown_module():
+    kill_cmd = ['ipcluster', 'stop']
+    cp = Popen(kill_cmd, stdout=PIPE, stderr=PIPE)
+    tic = time.time()
+    while True:
+        line = cp.stderr.readline().decode()
+        if "Stopping cluster" in line:
+            break
+        elif time.time()-tic > 15:
+            # probably stopped or something went wrong
+            break
+        time.sleep(0.1)
+
+
 def test_dask_client_from_ipclient():
-    from IPython.parallel import Client
     c = Client()
     dask_client = dask_client_from_ipclient(c)
 
