955ba5bc4c6265ce69db6d82635f990a401ea163
Blake Griffith <blake.a.griffith@gmail.com>
2015-06-02 15:04:16 -0500

    closures -> module level, list(key) -> key.read()

--- dask/bag/core.py
+++ dask/bag/core.py
@@ -779,6 +779,52 @@ def write(data, filename):
         f.close()
 
 
+# private functions for from_s3
+def _get_s3_bucket(bucket_name, aws_access_key, aws_secret_key, connection,
+                   anon):
+    """Connect to s3 and return a bucket"""
+    import boto
+    if anon is True:
+        connection = boto.connect_s3(anon=anon)
+    elif connection is None:
+        connection = boto.connect_s3(aws_access_key, aws_secret_key)
+    return connection.get_bucket(bucket_name)
+
+
+def _from_s3(bucket_name, matches, conn_args):
+    """
+    Takes an s3 bucket name, list of bucket key, and connection info.
+    Returns a Bag.
+    """
+    name = next(load_names)
+    dsk = {}
+    for i, m in enumerate(matches):
+        aak, ask, conn, anon = conn_args
+        # get the bucket
+        get_bucket = (_get_s3_bucket, bucket_name, aak, ask, conn, anon)
+        # get a key from the bucket
+        get_key = (lambda b, k: b.get_key(k), get_bucket, m)
+        # read the key
+        dsk[(name, i)] = (list, get_key)
+    return Bag(dsk, name, len(matches))
+
+
+def _parse_s3_URI(bucket_name, paths):
+    # FIXME
+    # DIRTY HACK: so that urlparse won't think '?' in the URI path is for a
+    # query. We replace '?' with 'QUESTIONMARK' so it stays in the path
+    # then after urlpars gives us the path replace 'QUESTIONMARK' with '?'.
+    # So this can be made to fail if a path has 'QUESTIONMARK' in it.
+    if '?' in bucket_name:
+        bucket_name = bucket_name.replace('?', 'QUESTIONMARK')
+    o = urlparse(bucket_name)
+    # if path is specified
+    if (paths == '*') and (o.path != '' and o.path != '/'):
+        paths = o.path[1:].replace('QUESTIONMARK', '?')
+    bucket_name = o.hostname
+    return bucket_name, paths
+
+
 def from_s3(bucket_name, paths='*', aws_access_key=None, aws_secret_key=None,
             connection=None, anon=False):
     """ Create a dask.bag by loading files from s3.
@@ -798,51 +844,13 @@ def from_s3(bucket_name, paths='*', aws_access_key=None, aws_secret_key=None,
     2
 
     """
-    def _connect_s3(aws_access_key, aws_secret_key, connection, anon):
-        """Connect to s3"""
-        import boto
-        if anon is True:
-            connection = boto.connect_s3(anon=anon)
-        elif connection is None:
-            connection = boto.connect_s3(aws_access_key, aws_secret_key)
-        return connection
-
-    def _from_s3(bucket_name, matches, conn_args):
-        """
-        takes an s3 bucket object and a list of bucket strings
-        """
-        dsk = {}
-        for i, m in enumerate(matches):
-            # connect to s3
-            a, b, c, d = conn_args
-            get_connection = (_connect_s3, a, b, c, d)
-            # get the bucket with the connection
-            get_bucket = (lambda x: x.get_bucket(bucket_name), get_connection)
-            # get a key from the bucket
-            get_key = (lambda b, k: b.get_key(k), get_bucket, m)
-            # read the key
-            read_key = lambda k: [getattr(k, 'read')()]
-            dsk[('load', i)] = (read_key, get_key)
-        return Bag(dsk, 'load', len(matches))
-
     conn_args = (aws_access_key, aws_secret_key, connection, anon)
 
     if bucket_name.startswith('s3://'):
-        # FIXME
-        # DIRTY HACK: so that urlparse won't think '?' in the URI path is for a
-        # query. We replace '?' with 'QUESTIONMARK' so it stays in the path
-        # then after urlpars gives us the path replace 'QUESTIONMARK' with '?'.
-        if '?' in bucket_name:
-            bucket_name = bucket_name.replace('?', 'QUESTIONMARK')
-        o = urlparse(bucket_name)
-        # if path is specified
-        if (paths == '*') and (o.path != '' and o.path != '/'):
-            paths = o.path[1:].replace('QUESTIONMARK', '?')
-        bucket_name = o.hostname
+        bucket_name, paths = _parse_s3_URI(bucket_name, paths)
 
     # get the bucket
-    conn = _connect_s3(*conn_args)
-    bucket = conn.get_bucket(bucket_name)
+    bucket = _get_s3_bucket(bucket_name, *conn_args)
 
     # sing bucket key, or pattern to expand
     if isinstance(paths, str):
