b3474f88734c09ff426eafbb532ee33a5d17839a
Phillip Cloud <cpcloud@gmail.com>
2015-06-17 09:15:21 -0400

    Export from_dataframe

--- dask/dataframe/__init__.py
+++ dask/dataframe/__init__.py
@@ -1,5 +1,5 @@
-from .core import (DataFrame, Series, Index, _Frame, concat, map_blocks)
+from .core import DataFrame, Series, Index, _Frame, concat, map_blocks
 from .shuffle import from_pframe
 from .io import (read_csv, from_array, from_bcolz, from_array,
-        from_bcolz)
+                 from_bcolz, from_dataframe)
 from .optimize import optimize
