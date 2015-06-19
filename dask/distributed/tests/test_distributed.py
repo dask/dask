4cc038487fbe76e42d74c80cda6fd96e42c8432a
Matthew Rocklin <mrocklin@gmail.com>
2015-06-19 12:17:24 -0700

    replace module-level ipcluster with contextmanager

--- dask/distributed/tests/test_distributed.py
+++ dask/distributed/tests/test_distributed.py
@@ -12,13 +12,14 @@ import numpy as np
 from numpy.testing import assert_array_almost_equal
 
 from IPython.parallel import Client
+from contextlib import contextmanager
 
 from dask.distributed import dask_client_from_ipclient
 import dask.array as da
 import dask.bag as db
 
 
-def setup_module():
+def setup_cluster():
     start_cmd = ['ipcluster', 'start', '-n', '4']
     cp = Popen(start_cmd, stdout=PIPE, stderr=PIPE)
     tic = time.time()
@@ -33,7 +34,7 @@ def setup_module():
         time.sleep(0.1)
 
 
-def teardown_module():
+def teardown_cluster():
     kill_cmd = ['ipcluster', 'stop']
     cp = Popen(kill_cmd, stdout=PIPE, stderr=PIPE)
     tic = time.time()
@@ -46,31 +47,39 @@ def teardown_module():
             break
         time.sleep(0.1)
 
+@contextmanager
+def ipcluster():
+    setup_cluster()
+    try:
+        c = Client()
+        yield c
+    finally:
+        teardown_cluster()
 
 def test_dask_client_from_ipclient():
-    c = Client()
-    dask_client = dask_client_from_ipclient(c)
+    with ipcluster() as c:
+        dask_client = dask_client_from_ipclient(c)
 
-    # data
-    a = np.arange(100).reshape(10, 10)
-    d = da.from_array(a, ((5, 5), (5, 5)))
+        # data
+        a = np.arange(100).reshape(10, 10)
+        d = da.from_array(a, ((5, 5), (5, 5)))
 
-    try:
-        # test array.mean
-        expected = a.mean(axis=0)
-        d1 = d.mean(axis=0)
-        result = d1.compute(get=dask_client.get)
-        assert_array_almost_equal(result, expected)
+        try:
+            # test array.mean
+            expected = a.mean(axis=0)
+            d1 = d.mean(axis=0)
+            result = d1.compute(get=dask_client.get)
+            assert_array_almost_equal(result, expected)
 
-        # test ghosting
-        d2 = da.ghost.ghost(d, depth=1, boundary='reflect')
-        d3 = da.ghost.trim_internal(d2, {0: 1, 1: 1})
-        result1 = d3.compute(get=dask_client.get)
-        assert_array_almost_equal(result1, a)
+            # test ghosting
+            d2 = da.ghost.ghost(d, depth=1, boundary='reflect')
+            d3 = da.ghost.trim_internal(d2, {0: 1, 1: 1})
+            result1 = d3.compute(get=dask_client.get)
+            assert_array_almost_equal(result1, a)
 
-    finally:
-        # close the workers
-        dask_client.close(close_scheduler=True)
+        finally:
+            # close the workers
+            dask_client.close(close_scheduler=True)
 
 
 def test_gh_248():
