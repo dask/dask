7e3829571d05f02488ed415b64b6febb0018ea24
Matthew Rocklin <mrocklin@gmail.com>
2015-06-23 15:18:20 -0700

    add align function

---
+++ dask/dataframe/multi.py
@@ -0,0 +1,53 @@
+from .core import repartition
+from bisect import bisect_left, bisect_right
+from toolz import merge_sorted, unique
+
+
+def bound(seq, left, right):
+    """ Bound sorted list by left and right values
+
+    >>> bound([1, 3, 4, 5, 8, 10, 12], 4, 10)
+    [4, 5, 8, 10]
+    """
+    return seq[bisect_left(seq, left): bisect_right(seq, right)]
+
+
+def align(*dfs):
+    """ Mutually partition and align DataFrame blocks
+
+    This serves as precursor to multi-dataframe operations like join, concat,
+    or merge.
+
+    Parameters
+    ----------
+    dfs: sequence of dd.DataFrames
+        Sequence of dataframes to be aligned on their index
+
+    Returns
+    -------
+    dfs: sequence of dd.DataFrames
+        These DataFrames have consistent divisions with each other
+    divisions: tuple
+        Full divisions sequence of the entire result
+    result: list
+        A list of lists of keys that show which dataframes exist on which
+        divisions
+
+    """
+    divisions = list(unique(merge_sorted(*[df.divisions for df in dfs])))
+    divisionss = [bound(divisions, df.divisions[0], df.divisions[-1])
+                  for df in dfs]
+    dfs2 = list(map(repartition, dfs, divisionss))
+
+    result = list()
+    inds = [0 for df in dfs]
+    for d in divisions[:-1]:
+        L = list()
+        for i in range(len(dfs)):
+            j = inds[i]
+            divs = dfs2[i].divisions
+            if j < len(divs) - 1 and divs[j] == d:
+                L.append((dfs2[i]._name, inds[i]))
+                inds[i] += 1
+        result.append(L)
+    return dfs2, tuple(divisions), result
---
+++ dask/dataframe/tests/test_multi.py
@@ -0,0 +1,28 @@
+import dask.dataframe as dd
+import pandas as pd
+from dask.dataframe.multi import align
+
+
+A = pd.DataFrame({'x': [1, 2, 3, 4, 5, 6], 'y': list('abdabd')},
+                 index=[10, 20, 30, 40, 50, 60])
+a = dd.repartition(A, [10, 40, 60])
+
+B = pd.DataFrame({'x': [1, 2, 3, 4], 'y': list('abda')},
+                 index=[30, 70, 80, 100])
+b = dd.repartition(B, [30, 80, 100])
+
+
+def test_align():
+    (aa, bb), divisions, L = align(a, b)
+    assert isinstance(a, dd.DataFrame)
+    assert isinstance(b, dd.DataFrame)
+    assert divisions == (10, 30, 40, 60, 80, 100)
+    assert isinstance(L, list)
+    assert len(divisions) == 1 + len(L)
+    assert L == [[(aa._name, 0)],
+                 [(aa._name, 1), (bb._name, 0)],
+                 [(aa._name, 2), (bb._name, 1)],
+                 [(bb._name, 2)],
+                 [(bb._name, 3)]]
+
+
