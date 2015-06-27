2586a2b3b84a675453e77579a4faf9e80c058b70
Phillip Cloud <cpcloud@gmail.com>
2015-06-27 17:17:12 -0400

    Hack that will eventually need to removed

--- dask/dataframe/core.py
+++ dask/dataframe/core.py
@@ -652,7 +652,18 @@ def _loc(df, start, stop, include_right_boundary=True):
         # result = df[df.index != stop]
         result = result.iloc[:result.index.get_slice_bound(stop, 'left',
                                                    result.index.inferred_type)]
-    return result
+    if not result.empty:
+        return result
+
+    # this is horrible
+    # i need to find a non-type-specific way to generate a range containing
+    # values that don't exist in the index and fill that with NaNs
+    d = np.diff(df.index.values)
+    assert (d > 0).all()
+    assert (d == d[0]).all()
+    freq = pd.datetools.to_offset(df.index[1] - df.index[0])
+    return pd.Series([np.nan],
+                     pd.date_range(start=start, end=stop, freq=freq))
 
 
 def _coerce_loc_index(divisions, o):
