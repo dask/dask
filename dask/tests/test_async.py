c627d5545ae096e79aa1ac4bdb1f19ce867eb444
Matthew Rocklin <mrocklin@gmail.com>
2015-07-08 15:53:42 +0200

    more ordering tests

--- dask/tests/test_async.py
+++ dask/tests/test_async.py
@@ -139,7 +139,7 @@ def issorted(L, reverse=False):
     return sorted(L, reverse=reverse) == L
 
 
-def test_ordering():
+def test_ordering_prefers_depth_first():
     a, b, c = 'abc'
     f = lambda *args: None
     d = {(a, 0): (f,), (b, 0): 0, (c, 0): (f,),
@@ -151,6 +151,44 @@ def test_ordering():
 
     assert issorted(map(o.get, [(a, i) for i in range(4)]), reverse=True)
     assert issorted(map(o.get, [(c, i) for i in range(4)]), reverse=True)
+
+
+def test_ordering_keeps_groups_together():
+    a, b, c = 'abc'
+    f = lambda *args: None
+    d = dict(((a, i), (f,)) for i in range(4))
+    d.update({(b, 0): (f, (a, 0), (a, 1)),
+              (b, 1): (f, (a, 2), (a, 3))})
+    o = order(d)
+
+    assert abs(o[(a, 0)] - o[(a, 1)]) == 1
+    assert abs(o[(a, 2)] - o[(a, 3)]) == 1
+
+    d = dict(((a, i), (f,)) for i in range(4))
+    d.update({(b, 0): (f, (a, 0), (a, 2)),
+              (b, 1): (f, (a, 1), (a, 3))})
+    o = order(d)
+
+    assert abs(o[(a, 0)] - o[(a, 2)]) == 1
+    assert abs(o[(a, 1)] - o[(a, 3)]) == 1
+
+
+def test_ordering_prefers_tasks_that_release_data():
+    a, b, c = 'abc'
+    f = lambda *args: None
+    d = {(a, 0): (f,), (a, 1): (f,),
+         (b, 0): (f, (a, 0)), (b, 1): (f, (a, 1)), (b, 2): (f, (a, 1))}
+
+    o = order(d)
+
+    assert o[(a, 1)] > o[(a, 0)]
+
+    d = {(a, 0): (f,), (a, 1): (f,),
+         (b, 0): (f, (a, 0)), (b, 1): (f, (a, 1)), (b, 2): (f, (a, 0))}
+
+    o = order(d)
+
+    assert o[(a, 1)] < o[(a, 0)]
 
 
 def test_order_of_startstate():
