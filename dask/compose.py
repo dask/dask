f7246759d34419a32451894bc845d3cbac9baeef
Jim Crist <crist042@umn.edu>
2015-07-13 16:35:08 -0500

    Add rudimentary docstrings

--- dask/compose.py
+++ dask/compose.py
@@ -9,14 +9,19 @@ from .optimize import cull
 from .context import _globals
 from . import threaded
 
+__all__ = ['do', 'value', 'Value']
+
 
 def get_dasks(task):
+    """Given a task, return a list of all unique dasks for any `Value` object
+    it's composed of"""
     dsks = chain.from_iterable(t._dasks for t in preorder_traversal(task)
                                if isinstance(t, Value))
     return list(dict((id(d), d) for d in dsks).values())
 
 
 def insert_lists(task):
+    """Insert calls to `list` in a task around all literal lists"""
     if isinstance(task, list):
         return (list, [insert_lists(i) for i in task])
     elif istask(task):
@@ -26,6 +31,8 @@ def insert_lists(task):
 
 
 def tokenize(v):
+    """Mapping function from task -> consistent name"""
+    # TODO: May have hash collisions...
     try:
         return str(hash(v))
     except TypeError:
@@ -34,6 +41,11 @@ def tokenize(v):
 
 
 def applyfunc(func, *args, **kwargs):
+    """Create a Value by applying a function to args.
+
+    Given a function and arguments, return a Value that represents the result
+    of that computation."""
+
     if kwargs:
         func = partial(func, **kwargs)
     task = insert_lists((func,) + args)
@@ -47,7 +59,30 @@ def applyfunc(func, *args, **kwargs):
 
 
 def do(func):
-    """Wraps func so that it outputs a ``Value``"""
+    """Wraps a function so that it outputs a ``Value``.
+
+    Examples
+    --------
+    Can be used as a decorator:
+
+    >>> @do
+    ... def add(a, b):
+    ...     return a + b
+    >>> res = add(1, 2)
+    >>> type(res)
+    Value
+    >>> res.compute()
+    3
+
+    For other cases, it may be cleaner to call ``do`` on a function at call
+    time:
+
+    >>> res2 = do(sum)([res, 2, 3])
+    >>> type(res2)
+    Value
+    >>> res2.compute()
+    8
+    """
     @wraps(func)
     def _dfunc(*args, **kwargs):
         return applyfunc(func, *args, **kwargs)
@@ -62,10 +97,12 @@ def get(dsk, keys, get=None, **kwargs):
 
 
 def methodcaller(self, method, *args, **kwargs):
+    """Calls method on self with args/kwargs"""
     return getattr(self, method)(*args, **kwargs)
 
 
 def right(method):
+    """Wrapper to create 'right' version of operator given left version"""
     def _inner(self, other):
         return method(other, self)
     return _inner
@@ -83,6 +120,7 @@ class Value(object):
         object.__setattr__(self, '_dasks', dasks)
 
     def compute(self, **kwargs):
+        """Compute the result."""
         dask1 = merge(*self._dasks)
         dask2 = cull(dask1, self)
         return get(dask2, self, **kwargs)
@@ -91,9 +129,10 @@ class Value(object):
     def dask(self):
         return merge(*self._dasks)
 
-    def visualize(self):
+    def visualize(self, **kwargs):
+        """Visualize the dask as a graph"""
         from dask.dot import dot_graph
-        dot_graph(self.dask)
+        dot_graph(self.dask, **kwargs)
 
     def __hash__(self):
         return hash(self._name)
@@ -105,7 +144,7 @@ class Value(object):
         return list(self.__dict__.keys())
 
     def __getattr__(self, attr):
-        if not attr.startswith('__'):
+        if not attr.startswith('_'):
             def _inner(*args, **kwargs):
                 return do(methodcaller)(self, attr, *args, **kwargs)
             return _inner
@@ -165,7 +204,41 @@ class Value(object):
 
 
 def value(val, name=None):
-    """Create a value from a python object"""
+    """Create a ``Value`` from a python object.
+
+    Parameters
+    ----------
+    val : object
+        Object to be wrapped.
+    name : string, optional
+        Name to be used in the resulting dask.
+
+    Examples
+    --------
+    >>> a = value([1, 2, 3])
+    >>> a.compute()
+    [1, 2, 3]
+
+    Values can act as a proxy to the underlying object. Many operators are
+    supported:
+
+    >>> (a + [1, 2]).compute()
+    [1, 2, 3, 1, 2]
+    >>> a[1].compute()
+    2
+
+    Method axis also works:
+
+    >>> a.count(2).compute()
+    1
+
+    Note that if a method doesn't exist, no error will be thrown until runtime:
+
+    >>> res = a.not_a_real_method()
+    >>> res.compute()  # doctest: +SKIP
+    AttributeError("'list' object has no attribute 'not_a_real_method'")
+    """
+
     name = name or tokenize(val)
     dsk = {}
     res = Value(name, [dsk])
