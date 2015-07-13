0a6045278c8e37fadecb249652c7092beba13819
Jim Crist <crist042@umn.edu>
2015-07-13 15:39:37 -0500

    Add more methods to Value & cleanups
    - Supports most magic methods
    - Explicitly forbid mutating values
    - Explicitly forbid iteration
    - Dynamically generate proxy methods on values
    - `__dir__` method to still allow for tab completion
    - Sort methods to make easier to read

--- dask/compose.py
+++ dask/compose.py
@@ -61,52 +61,26 @@ def get(dsk, keys, get=None, **kwargs):
     return get(dsk2, keys, **kwargs)
 
 
+def methodcaller(self, method, *args, **kwargs):
+    return getattr(self, method)(*args, **kwargs)
+
+
+def right(method):
+    def _inner(self, other):
+        return method(other, self)
+    return _inner
+
+
 class Value(object):
     """Represents a value to be computed by dask.
 
     Equivalent to the output from a single key in a dask graph.
     """
+    __slots__ = ('_name', '_dasks')
+
     def __init__(self, name, dasks):
-        self._name = name
-        self._dasks = dasks
-
-    def __hash__(self):
-        return hash(self._name)
-
-    def __repr__(self):
-        return "Value({0})".format(repr(self._name))
-
-    __lt__ = daskify(operator.lt)
-    __le__ = daskify(operator.le)
-    __eq__ = daskify(operator.eq)
-    __ne__ = daskify(operator.ne)
-    __ge__ = daskify(operator.ge)
-    __gt__ = daskify(operator.gt)
-    __not__ = daskify(operator.not_)
-    __abs__ = daskify(operator.abs)
-    __add__ = daskify(operator.add)
-    __and__ = daskify(operator.and_)
-    __div__ = daskify(operator.floordiv)
-    __floordiv__ = daskify(operator.floordiv)
-    __index__ = daskify(operator.index)
-    __inv__ = daskify(operator.inv)
-    __invert__ = daskify(operator.invert)
-    __lshift__ = daskify(operator.lshift)
-    __mod__ = daskify(operator.mod)
-    __mul__ = daskify(operator.mul)
-    __neg__ = daskify(operator.neg)
-    __or__ = daskify(operator.or_)
-    __pos__ = daskify(operator.pos)
-    __pow__ = daskify(operator.pow)
-    __rshift__ = daskify(operator.rshift)
-    __sub__ = daskify(operator.sub)
-    __truediv__ = daskify(operator.truediv)
-    __xor__ = daskify(operator.xor)
-    __concat__ = daskify(operator.concat)
-    __contains__ = daskify(operator.contains)
-    __delitem__ = daskify(operator.delitem)
-    __getitem__ = daskify(operator.getitem)
-    __setitem__ = daskify(operator.setitem)
+        object.__setattr__(self, '_name', name)
+        object.__setattr__(self, '_dasks', dasks)
 
     def compute(self, **kwargs):
         dask1 = merge(*self._dasks)
@@ -120,6 +94,74 @@ class Value(object):
     def visualize(self):
         from dask.dot import dot_graph
         dot_graph(self.dask)
+
+    def __hash__(self):
+        return hash(self._name)
+
+    def __repr__(self):
+        return "Value({0})".format(repr(self._name))
+
+    def __dir__(self):
+        return list(self.__dict__.keys())
+
+    def __getattr__(self, attr):
+        if not attr.startswith('__'):
+            def _inner(*args, **kwargs):
+                return daskify(methodcaller)(self, attr, *args, **kwargs)
+            return _inner
+        else:
+            raise AttributeError("Attribute {0} not found".format(attr))
+
+    def __setattr__(self, attr, val):
+        raise TypeError("Value objects are immutable")
+
+    def __iter__(self):
+        raise TypeError("Value objects are not iterable")
+
+    __abs__ = daskify(operator.abs)
+    __add__ = daskify(operator.add)
+    __and__ = daskify(operator.and_)
+    __concat__ = daskify(operator.concat)
+    __contains__ = daskify(operator.contains)
+    __delitem__ = daskify(operator.delitem)
+    __div__ = daskify(operator.floordiv)
+    __eq__ = daskify(operator.eq)
+    __floordiv__ = daskify(operator.floordiv)
+    __ge__ = daskify(operator.ge)
+    __getitem__ = daskify(operator.getitem)
+    __gt__ = daskify(operator.gt)
+    __index__ = daskify(operator.index)
+    __inv__ = daskify(operator.inv)
+    __invert__ = daskify(operator.invert)
+    __le__ = daskify(operator.le)
+    __lshift__ = daskify(operator.lshift)
+    __lt__ = daskify(operator.lt)
+    __mod__ = daskify(operator.mod)
+    __mul__ = daskify(operator.mul)
+    __ne__ = daskify(operator.ne)
+    __neg__ = daskify(operator.neg)
+    __not__ = daskify(operator.not_)
+    __or__ = daskify(operator.or_)
+    __pos__ = daskify(operator.pos)
+    __pow__ = daskify(operator.pow)
+    __radd__ = daskify(right(operator.add))
+    __rand__ = daskify(right(operator.and_))
+    __rdiv__ = daskify(right(operator.floordiv))
+    __rfloordiv__ = daskify(right(operator.floordiv))
+    __rlshift__ = daskify(right(operator.lshift))
+    __rmod__ = daskify(right(operator.mod))
+    __rmul__ = daskify(right(operator.mul))
+    __ror__ = daskify(right(operator.or_))
+    __rpow__ = daskify(right(operator.pow))
+    __rrshift__ = daskify(right(operator.rshift))
+    __rshift__ = daskify(operator.rshift)
+    __rsub__ = daskify(right(operator.sub))
+    __rtruediv__ = daskify(right(operator.truediv))
+    __rxor__ = daskify(right(operator.xor))
+    __setitem__ = daskify(operator.setitem)
+    __sub__ = daskify(operator.sub)
+    __truediv__ = daskify(operator.truediv)
+    __xor__ = daskify(operator.xor)
 
 
 def value(val, name=None):
