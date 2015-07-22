28f13398ee197d61419df631123729bc4dc50c74
Matthew Rocklin <mrocklin@gmail.com>
2015-07-21 17:43:29 -0700

    remove old wrap function

--- dask/array/wrap.py
+++ dask/array/wrap.py
@@ -24,42 +24,6 @@ def dims_from_size(size, blocksize):
     return result
 
 
-def wrap_func_size_as_kwarg(func, *args, **kwargs):
-    """
-    Transform np.random function into blocked version
-    """
-    if 'shape' in kwargs and 'size' not in kwargs:
-        kwargs['size'] = kwargs.pop('shape')
-    if 'size' not in kwargs:
-        args, size = args[:-1], args[-1]
-    else:
-        size = kwargs.pop('size')
-
-    if not isinstance(size, (tuple, list)):
-        size = (size,)
-
-    chunks = kwargs.pop('chunks', None)
-    chunks = normalize_chunks(chunks, size)
-    name = kwargs.pop('name', None)
-
-    dtype = kwargs.pop('dtype', None)
-    if dtype is None:
-        kw = kwargs.copy(); kw['size'] = (0,)
-        dtype = func(*args, **kw).dtype
-
-    name = name or next(names)
-
-    keys = product([name], *[range(len(bd)) for bd in chunks])
-    sizes = product(*chunks)
-    if not kwargs:
-        vals = ((func,) + args + (size,) for size in sizes)
-    else:
-        vals = ((partial(func, *args, size=size, **kwargs),) for size in sizes)
-
-    dsk = dict(zip(keys, vals))
-    return Array(dsk, name, chunks, dtype=dtype)
-
-
 def wrap_func_shape_as_first_arg(func, *args, **kwargs):
     """
     Transform np.random function into blocked version
