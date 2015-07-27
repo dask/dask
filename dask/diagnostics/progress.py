d77dfe696452b34793789ebca9d1f8e6012c4e82
Jim Crist <crist042@umn.edu>
2015-07-27 18:21:02 -0500

    First pass at a progress bar
    Works in both terminal and notebook.

---
+++ dask/diagnostics/progress.py
@@ -0,0 +1,43 @@
+from __future__ import division
+import sys
+
+from ..core import istask
+
+
+class ProgressBar(object):
+
+    def __init__(self, get, nbins=50):
+        self._get = get
+        self._nbins = nbins
+
+    def _end_callback(self, key, value, dsk, state, id):
+        if key is not None:
+            self._ndone += 1
+            if not self._ndone % self._update_rate:
+                self._update_bar()
+        else:
+            self._finalize_bar()
+
+    @staticmethod
+    def _draw_bar(ndone, ntasks, nbins):
+        tics = int(ndone * nbins / ntasks)
+        bar = '#' * tics
+        percent = (100 * ndone) // ntasks
+        msg = '\r[{0:<{1}}] | {2}% Completed'.format(bar, nbins, percent)
+        sys.stdout.write(msg)
+        sys.stdout.flush()
+
+    def _update_bar(self):
+        self._draw_bar(self._ndone, self._ntasks, self._nbins)
+
+    def _finalize_bar(self):
+        self._draw_bar(self._ndone, self._ntasks, self._nbins)
+        sys.stdout.write('\n')
+
+    def get(self, dsk, result, **kwargs):
+        self._ntasks = len([k for (k, v) in dsk.items() if istask(v)])
+        self._ndone = 0
+        self._update_rate = max(1, self._ntasks // self._nbins)
+        self._update_bar()
+        return self._get(dsk, result, end_callback=self._end_callback,
+                         **kwargs)
