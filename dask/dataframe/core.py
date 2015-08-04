0ce78199568eead43c8e459792a3df22c963d267
Blake Griffith <blake.a.griffith@gmail.com>
2015-08-04 12:59:38 -0500

    Make concat that works on both Frams and Indexs.
    closes gh-517

--- dask/dataframe/core.py
+++ dask/dataframe/core.py
@@ -2,7 +2,7 @@ from __future__ import division
 
 from itertools import count
 from math import sqrt
-from functools import wraps
+from functools import wraps, reduce
 import bisect
 import uuid
 from hashlib import md5
@@ -1068,6 +1068,13 @@ def apply_concat_apply(args, chunk=None, aggregate=None, columns=None,
     """
     if not isinstance(args, (tuple, list)):
         args = [args]
+
+    def concat(args):
+        if isinstance(args[0], pd.Index):
+            concat = lambda a: reduce(lambda b, c: b.append(c), a)
+        else:
+            concat = pd.concat
+        return concat(args)
     assert all(arg.npartitions == args[0].npartitions
                 for arg in args
                 if isinstance(arg, _Frame))
@@ -1085,7 +1092,7 @@ def apply_concat_apply(args, chunk=None, aggregate=None, columns=None,
 
     b = 'apply-concat-apply--second' + token
     dsk2 = {(b, 0): (aggregate,
-                      (pd.concat,
+                      (concat,
                         (list, [(a, i) for i in range(args[0].npartitions)])))}
 
     return type(args[0])(
