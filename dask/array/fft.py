5fa2ae1bea82f0bb0aa923722b5e13875fe3fee0
Blake Griffith <blake.a.griffith@gmail.com>
2015-08-05 14:38:08 -0500

    Add support for "n" fft kwarg. Fix out sizes.

--- dask/array/fft.py
+++ dask/array/fft.py
@@ -5,10 +5,6 @@ import numpy.fft as npfft
 
 from .core import map_blocks
 
-"""
-TODO: implement `n` kwarg of fft's. With the current approach this would mean
-specifying a how the chunks change to pass to map_blocks.
-"""
 
 chunk_error = ("Dask array only supports taking an FFT along an axis that \n"
                "has a single chunk. An FFT operation was tried on axis %s \n"
@@ -16,14 +12,60 @@ chunk_error = ("Dask array only supports taking an FFT along an axis that \n"
                "dask.Array.rechunk.")
 
 
-def _fft_wrap(fft_func, a, axis, dtype):
+def _fft_wrap(fft_func, a, n, axis, dtype, chunks):
     if len(a.chunks[axis]) != 1:
         raise ValueError(chunk_error % (axis, a.chunks[axis]))
 
-    return map_blocks(lambda x: fft_func(x, axis=axis), a, dtype=dtype)
+    return map_blocks(lambda x: fft_func(x, n=n, axis=axis), a, dtype=dtype,
+                      chunks=chunks)
 
 
-def fft(a, axis=-1):
+def _fft_out_chunks(a, n, axis):
+    """ For computing the output chunks of fft and ifft"""
+    if n is None:
+        return a.chunks
+    chunks = list(a.chunks)
+    chunks[axis] = (n,)
+    return chunks
+
+
+def _rfft_out_chunks(a, n, axis):
+    if n is None:
+        n = a.chunks[axis][0]
+    chunks = list(a.chunks)
+    chunks[axis] = (n//2 + 1,)
+    return chunks
+
+
+def _irfft_out_chunks(a, n, axis):
+    if n is None:
+        n = 2 * (a.chunks[axis][0] - 1)
+    chunks = list(a.chunks)
+    chunks[axis] = (n,)
+    return chunks
+
+
+def _hfft_out_chunks(a, n, axis):
+    if n is None:
+        n = 2 * (a.chunks[axis][0] - 1)
+    chunks = list(a.chunks)
+    chunks[axis] = (n,)
+    return chunks
+
+
+def _ihfft_out_chunks(a, n, axis):
+    if n is None:
+        n = a.chunks[axis][0]
+    chunks = list(a.chunks)
+    if n % 2 == 0:
+        m = (n//2) + 1
+    else:
+        m = (n + 1)//2
+    chunks[axis] = (m,)
+    return chunks
+
+
+def fft(a, n=None, axis=-1):
     """
     Compute the one-dimensional discrete Fourier Transform along an axis that
     only has one chunk.
@@ -36,10 +78,12 @@ def fft(a, axis=-1):
         Axis over which to compute the FFT.  If not given, the last
         axis is used.
     """
-    return _fft_wrap(npfft.fft, a, axis, np.complex_)
+    chunks = _fft_out_chunks(a, n, axis)
+
+    return _fft_wrap(npfft.fft, a, n, axis, np.complex_, chunks)
 
 
-def ifft(a, axis=-1):
+def ifft(a, n=None, axis=-1):
     """
     Compute the one-dimensional inverse discrete Fourier Transform along an
     axis that only has one chunk.
@@ -52,10 +96,12 @@ def ifft(a, axis=-1):
         Axis over which to compute the inverse FFT.  If not given, the last
         axis is used.
     """
-    return _fft_wrap(npfft.ifft, a, axis, np.complex_)
+    chunks = _fft_out_chunks(a, n, axis)
+
+    return _fft_wrap(npfft.ifft, a, n, axis, np.complex_, chunks)
 
 
-def rfft(a, axis=-1):
+def rfft(a, n=None, axis=-1):
     """
     Compute the one-dimensional discrete Fourier Transform for real input,
     along an axis that has only one chunk.
@@ -72,10 +118,12 @@ def rfft(a, axis=-1):
         Axis over which to compute the FFT. If not given, the last axis is
         used.
     """
-    return _fft_wrap(npfft.rfft, a, axis, np.complex_)
+    chunks = _rfft_out_chunks(a, n=n, axis=axis)
+
+    return _fft_wrap(npfft.rfft, a, n, axis, np.complex_, chunks)
 
 
-def irfft(a, axis=-1):
+def irfft(a, n=None, axis=-1):
     """
     Compute the inverse of the n-point DFT for real input, along an axis that
     has only one chunk.
@@ -99,10 +147,12 @@ def irfft(a, axis=-1):
         Axis over which to compute the inverse FFT. If not given, the last
         axis is used.
     """
-    return _fft_wrap(npfft.irfft, a, axis, np.float_)
+    chunks = _irfft_out_chunks(a, n=n, axis=axis)
+
+    return _fft_wrap(npfft.irfft, a, n, axis, np.float_, chunks)
 
 
-def hfft(a, axis=-1):
+def hfft(a, n=None, axis=-1):
     """
     Compute the FFT of a signal which has Hermitian symmetry (real spectrum),
     along an axis that has only one chunk.
@@ -115,10 +165,12 @@ def hfft(a, axis=-1):
         Axis over which to compute the FFT. If not given, the last
         axis is used.
     """
-    return _fft_wrap(npfft.hfft, a, axis, np.float_)
+    chunks = _hfft_out_chunks(a, n=n, axis=axis)
+
+    return _fft_wrap(npfft.hfft, a, n, axis, np.float_, chunks)
 
 
-def ihfft(a, axis=-1):
+def ihfft(a, n=None, axis=-1):
     """
     Compute the inverse FFT of a signal which has Hermitian symmetry, along
     an axis that has only one chunk.
@@ -131,4 +183,6 @@ def ihfft(a, axis=-1):
         Axis over which to compute the inverse FFT. If not given, the last
         axis is used.
     """
-    return _fft_wrap(npfft.ihfft, a, axis, np.complex_)
+    chunks = _ihfft_out_chunks(a, n=n, axis=axis)
+
+    return _fft_wrap(npfft.ihfft, a, n, axis, np.complex_, chunks)
