3b8c224cff65412adf2acfd5b412ad16552a9b22
Jim Crist <crist042@umn.edu>
2015-08-06 17:37:13 -0500

    Use only deterministic names in da.percentile

--- dask/array/percentile.py
+++ dask/array/percentile.py
@@ -4,7 +4,8 @@ from functools import wraps
 from collections import Iterator
 import numpy as np
 
-from .core import Array
+from ..base import tokenize
+from .core import Array, tokenize_array
 
 
 @wraps(np.percentile)
@@ -37,11 +38,12 @@ def percentile(a, q, interpolation='linear'):
         raise NotImplementedError(
             "Percentiles only implemented for 1-d arrays")
     q = np.array(q)
-    name = next(names)
+    token = tokenize((a.name, tokenize_array(q), interpolation))
+    name = 'percentile_chunk-' + token
     dsk = dict(((name, i), (_percentile, (key), q, interpolation))
             for i, key in enumerate(a._keys()))
 
-    name2 = next(names)
+    name2 = 'percentile-' + token
     dsk2 = {(name2, 0): (merge_percentiles, q, [q] * len(a.chunks[0]),
                          sorted(dsk), a.chunks[0], interpolation)}
 
