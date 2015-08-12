daf8fce3c0f67a8e122c737e26c4deb3ad2e3ca8
Matthew Rocklin <mrocklin@gmail.com>
2015-08-12 13:53:27 -0700

    mark read_csv getmtime test as slow

--- dask/dataframe/tests/test_io.py
+++ dask/dataframe/tests/test_io.py
@@ -549,15 +549,15 @@ def test_multiple_read_csv_has_deterministic_name():
         os.remove('_foo.2.csv')
 
 
+@pytest.mark.slow
 def test_read_csv_of_modified_file_has_different_name():
     with filetext(text) as fn:
         mtime = os.path.getmtime(fn)
+        sleep(1)
         a = read_csv(fn)
         with open(fn, 'a') as f:
             f.write('\nGeorge,700')
             os.fsync(f)
-        while mtime == os.path.getmtime(fn):
-            sleep(0.01)
         b = read_csv(fn)
 
         assert sorted(a.dask) != sorted(b.dask)
