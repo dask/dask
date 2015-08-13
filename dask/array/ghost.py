5c1910ce57021895b235399e019cfef3a5850fcc
Blake Griffith <blake.a.griffith@gmail.com>
2015-08-13 10:43:38 -0500

    docfix in dask.array.ghost

--- dask/array/ghost.py
+++ dask/array/ghost.py
@@ -293,7 +293,7 @@ def ghost(x, depth, boundary):
     depth: dict
         The size of the shared boundary per axis
     boundary: dict
-        The boundary of boundary condition on each axis
+        The boundary condition on each axis
 
     The axes dict informs how many cells to overlap between neighboring blocks
     {0: 2, 2: 5} means share two cells in 0 axis, 5 cells in 2 axis
