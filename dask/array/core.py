BUG: fix da.sign
This was a copy & paste error...

dask/array/core.py
@@ -1701,7 +1701,7 @@ imag = wrap_elemwise(np.imag)
 
 clip = wrap_elemwise(np.clip)
 fabs = wrap_elemwise(np.fabs)
-sign = wrap_elemwise(np.fabs)
+sign = wrap_elemwise(np.sign)
 
 
 def frexp(x):

dask/array/tests/test_array_core.py
@@ -883,7 +883,7 @@ def test_arithmetic():
 
     assert eq(da.clip(b, 1, 4), np.clip(y, 1, 4))
     assert eq(da.fabs(b), np.fabs(y))
-    assert eq(da.sign(b - 2), np.fabs(y - 2))
+    assert eq(da.sign(b - 2), np.sign(y - 2))
 
     l1, l2 = da.frexp(a)
     r1, r2 = np.frexp(x)