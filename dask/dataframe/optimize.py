d5f0fe64a8a3222b6485bae36b3b3f969ada26d2
Jim Crist <crist042@umn.edu>
2015-09-14 22:51:17 -0500

    Move index fusing to separate function

--- dask/dataframe/optimize.py
+++ dask/dataframe/optimize.py
@@ -5,6 +5,13 @@ from ..optimize import cull, fuse_getitem, fuse_selections
 from .. import core
 
 
+def fuse_castra_index(dsk):
+    from castra import Castra
+    def merge(a, b):
+        return (Castra.load_index, b[1], b[2]) if a[2] == 'index' else a
+    return fuse_selections(dsk, getattr, Castra.load_partition, merge)
+
+
 def optimize(dsk, keys, **kwargs):
     if isinstance(keys, list):
         dsk2 = cull(dsk, list(core.flatten(keys)))
@@ -13,9 +20,7 @@ def optimize(dsk, keys, **kwargs):
     try:
         from castra import Castra
         dsk3 = fuse_getitem(dsk2, Castra.load_partition, 3)
-        def merge(a, b):
-            return (Castra.load_index, b[1], b[2]) if a[2] == 'index' else a
-        dsk4 = fuse_selections(dsk3, getattr, Castra.load_partition, merge)
+        dsk4 = fuse_castra_index(dsk3)
     except ImportError:
         dsk4 = dsk2
     dsk5 = fuse_getitem(dsk4, dataframe_from_ctable, 3)
