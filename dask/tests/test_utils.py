242d3328e17f8cd287c698ed0e7318b975dcb51d
Blake Griffith <blake.a.griffith@gmail.com>
2015-09-17 14:40:43 -0500

    Add regression test for textblock, next_newline.

--- dask/tests/test_utils.py
+++ dask/tests/test_utils.py
@@ -1,6 +1,8 @@
 import os
 
-from dask.utils import textblock, filetext, takes_multiple_arguments, Dispatch
+from dask.utils import (textblock, filetext, takes_multiple_arguments,
+                        Dispatch, tmpfile, next_newline)
+
 
 def test_textblock():
     text = b'123 456 789 abc def ghi'.replace(b' ', os.linesep.encode())
@@ -50,3 +52,54 @@ def test_dispatch():
     assert foo(1.0) == 0.0
     assert foo(b) == b
     assert foo((1, 2.0, b)) == (2, 1.0, b)
+
+
+def test_nextnewline():
+    encoding = 'utf-16-le'
+
+    euro = u'\u20ac'
+    yen = u'\u00a5'
+
+    bin_euro = u'\u20ac'.encode(encoding)
+    bin_yen = u'\u00a5'.encode(encoding)
+    bin_newline = '\n'.encode(encoding)
+
+    data = (euro * 10) + '\n' + (yen * 10) + '\n' + (euro * 10)
+    bin_data = data.encode(encoding)
+
+    with tmpfile() as fn:
+        with open(fn, 'w+b') as f:
+            f.write(bin_data)
+            f.seek(0)
+
+            start, stop = next_newline(f, 2, encoding)
+            assert start == len(bin_euro) * 10
+            assert stop == len(bin_euro) * 10 + len('\n'.encode(encoding))
+
+            start, stop = next_newline(f, len(bin_euro)*11, encoding)
+            assert start == len(bin_euro) * 20 + len(bin_newline)
+            assert stop == len(bin_euro) * 20 + len(bin_newline) * 2
+
+
+def test_gh606():
+    encoding = 'utf-16-le'
+    euro = u'\u20ac'
+    yen = u'\u00a5'
+    bin_euro = u'\u20ac'.encode(encoding)
+    bin_yen = u'\u00a5'.encode(encoding)
+
+    data = (euro * 10) + '\n' + (yen * 10) + '\n' + (euro * 10)
+    bin_data = data.encode(encoding)
+
+    with tmpfile() as fn:
+        with open(fn, 'w+b') as f:
+            f.write(bin_data)
+            f.seek(0)
+
+            stop = len(bin_euro) * 10 + len('\n'.encode(encoding))
+            res = textblock(f, 1, stop, encoding=encoding)
+            assert res == ((yen * 10) + '\n').encode(encoding)
+
+            stop = len(bin_euro) * 10 + len('\n'.encode(encoding))
+            res = textblock(f, 0, stop, encoding=encoding)
+            assert res == ((euro * 10) + '\n' + (yen * 10) + '\n').encode(encoding)
