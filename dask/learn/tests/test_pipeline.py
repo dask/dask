2961b8612804a73fefd240b68a550b16683de148
Matthew Rocklin <mrocklin@gmail.com>
2015-09-21 16:22:20 -0700

    ensure that pipelines share structure

--- dask/learn/tests/test_pipeline.py
+++ dask/learn/tests/test_pipeline.py
@@ -6,6 +6,7 @@ import sklearn.pipeline
 import dask
 import dask.learn as dl
 import dask.imperative as di
+from toolz import merge
 
 X, y = make_blobs()
 
@@ -38,3 +39,19 @@ def test_pipeline():
     sk_y2 = skpipeline.predict(X)
     sk_score = skpipeline.score(X, y)
     assert sk_score == score.compute()
+
+
+def test_pipeline_shares_structure():
+    pipeline = dl.Pipeline([("scale", StandardScaler()),
+                            ("fdr", SelectFdr()),
+                            ("svm", LinearSVC())])
+
+    pipeline.fit(X, y)
+    score = pipeline.score(X, y)
+
+    pipeline.set_params(svm__C=0.1)
+    pipeline.fit(X, y)
+    score2 = pipeline.score(X, y)
+
+    assert len(merge(score.dask, score2.dask)) <= len(score.dask) + 3
+    assert score.key != score2.key
