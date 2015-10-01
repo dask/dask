d9f623baaa8e1d1075f9132108ed7bb11eea39b0
Matthew Rocklin <mrocklin@gmail.com>
2015-09-30 17:08:42 -0700

    Replace dask.get from core.get to async.get_sync
    We really shouldn't publish core.get anywhere, particularly in the
    top level API.

--- dask/__init__.py
+++ dask/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import, division, print_function
 
-from .core import istask, get
+from .core import istask
 from .context import set_options
-
+from .async import get_sync as get
 try:
     from .imperative import do, value
 except ImportError:
