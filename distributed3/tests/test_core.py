8971a2aa1360dd216538552ac2df57c8bf3eb182
Matthew Rocklin <mrocklin@gmail.com>
2015-10-01 17:07:44 -0700

    add test for multi-stream rpc

--- distributed3/tests/test_core.py
+++ distributed3/tests/test_core.py
@@ -45,6 +45,29 @@ def test_rpc():
     ioloop.IOLoop.current().run_sync(f)
 
 
+def test_rpc_with_many_connections():
+    remote = rpc(ip='127.0.0.1', port=8887)
+
+    @gen.coroutine
+    def g():
+        for i in range(10):
+            yield remote.ping()
+
+    @gen.coroutine
+    def f():
+        server = Server({'ping': pingpong})
+        server.listen(8887)
+
+        yield [g() for i in range(10)]
+
+        server.stop()
+
+        remote.close_streams()
+        assert all(stream.closed() for stream in remote.streams)
+
+    ioloop.IOLoop.current().run_sync(f)
+
+
 def test_sync():
     def f():
         from distributed3.core import Server
