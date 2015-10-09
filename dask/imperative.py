cb8e13ace5f7dc16e1ddc9c14110204626059b2d
Matthew Rocklin <mrocklin@gmail.com>
2015-10-09 09:29:40 -0700

    A few imperative fixes
    - Key names in do prepend function name
    - Value keys use type name as prelude
    - Correct intend of tokenize(..., True) to pure=True

--- dask/imperative.py
+++ dask/imperative.py
@@ -6,7 +6,7 @@ from collections import Iterator
 from toolz import merge, unique, curry
 
 from .optimize import cull, fuse
-from .utils import concrete
+from .utils import concrete, funcname
 from . import base
 from .compatibility import apply
 from . import threaded
@@ -65,7 +65,7 @@ def to_task_dasks(expr):
     if isinstance(expr, Value):
         return expr.key, expr._dasks
     if isinstance(expr, base.Base):
-        name = tokenize(expr, True)
+        name = tokenize(expr, pure=True)
         keys = expr._keys()
         dsk = expr._optimize(expr.dask, keys)
         dsk[name] = (expr._finalize, expr, (concrete, keys))
@@ -120,7 +120,7 @@ def applyfunc(func, args, kwargs, pure=False):
         task = (apply, func, (list, list(args)), dask_kwargs)
     else:
         task = (func,) + args
-    name = tokenize(*task, pure=pure)
+    name = funcname(func) + '-' + tokenize(*task, pure=pure)
     dasks = flat_unique(dasks)
     dasks.append({name: task})
     return Value(name, dasks)
@@ -248,7 +248,7 @@ class Value(base.Base):
 
     def __getattr__(self, attr):
         if not attr.startswith('_'):
-            return do(getattr, True)(self, attr)
+            return do(getattr, pure=True)(self, attr)
         else:
             raise AttributeError("Attribute {0} not found".format(attr))
 
@@ -347,10 +347,9 @@ def value(val, name=None):
     >>> res.compute()  # doctest: +SKIP
     AttributeError("'list' object has no attribute 'not_a_real_method'")
     """
-
     if isinstance(val, Value):
         return val
-    name = name or tokenize(val, True)
+    name = name or (type(val).__name__ + '-' + tokenize(val, pure=True))
     task, dasks = to_task_dasks(val)
     dasks.append({name: task})
     return Value(name, dasks)
