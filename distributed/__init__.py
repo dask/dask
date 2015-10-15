b97cadc183689e46e6cb8a74647521fe8f61762a
Matthew Rocklin <mrocklin@gmail.com>
2015-10-15 14:03:29 -0700

    remove pool

--- distributed/__init__.py
+++ distributed/__init__.py
@@ -1,7 +1,6 @@
 from .center import Center
 from .worker import Worker
 from .client import scatter, gather, delete, clear
-from .pool import Pool
 from .dask import get
 from .executor import Executor
 
