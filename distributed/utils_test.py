0de39cfac5b36d865f3c2e68c75486441a6a318d
Matthew Rocklin <mrocklin@gmail.com>
2015-10-19 16:53:53 -0700

    increment port numbers in cluster context manager

--- distributed/utils_test.py
+++ distributed/utils_test.py
@@ -32,17 +32,25 @@ def run_worker(port, center_port, **kwargs):
     IOLoop.current().close()  # Never reached. TODO: clean shutdown of IOLoop
 
 
+_port = [8010]
+
 @contextmanager
 def cluster():
-    center = Process(target=run_center, args=(8010,))
-    a = Process(target=run_worker, args=(8011, 8010), kwargs={'ncores': 1})
-    b = Process(target=run_worker, args=(8012, 8010), kwargs={'ncores': 1})
+    _port[0] += 1
+    cport = _port[0]
+    center = Process(target=run_center, args=(cport,))
+    _port[0] += 1
+    aport = _port[0]
+    a = Process(target=run_worker, args=(aport, cport), kwargs={'ncores': 1})
+    _port[0] += 1
+    bport = _port[0]
+    b = Process(target=run_worker, args=(bport, cport), kwargs={'ncores': 1})
 
     center.start()
     a.start()
     b.start()
 
-    sock = connect_sync('127.0.0.1', 8010)
+    sock = connect_sync('127.0.0.1', cport)
     while True:
         write_sync(sock, {'op': 'ncores'})
         ncores = read_sync(sock)
@@ -50,10 +58,10 @@ def cluster():
             break
 
     try:
-        yield {'proc': center, 'port': 8010}, [{'proc': a, 'port': 8011},
-                                               {'proc': b, 'port': 8012}]
+        yield {'proc': center, 'port': cport}, [{'proc': a, 'port': aport},
+                                                {'proc': b, 'port': bport}]
     finally:
-        for port in [8011, 8012, 8010]:
+        for port in [aport, bport, cport]:
             with ignoring(socket.error):
                 sock = connect_sync('127.0.0.1', port)
                 write_sync(sock, dict(op='terminate', close=True))
@@ -62,5 +70,3 @@ def cluster():
         for proc in [a, b, center]:
             with ignoring(Exception):
                 proc.terminate()
-
-
