58981e5613043211c4da39eae24f08dec47d75f5
Matthew Rocklin <mrocklin@gmail.com>
2015-10-19 18:09:54 -0700

    add wait and as_completed to top level api

--- distributed/__init__.py
+++ distributed/__init__.py
@@ -2,6 +2,6 @@ from .center import Center
 from .worker import Worker
 from .client import scatter, gather, delete, clear
 from .dask import get
-from .executor import Executor
+from .executor import Executor, wait, as_completed
 
 __version__ = '1.1.0'
