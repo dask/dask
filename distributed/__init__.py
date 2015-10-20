68cbf837d12ae63642e37ad0464826f7e25f3307
Matthew Rocklin <mrocklin@gmail.com>
2015-10-20 13:22:34 -0700

    bump version to 1.2.0

--- distributed/__init__.py
+++ distributed/__init__.py
@@ -4,4 +4,4 @@ from .client import scatter, gather, delete, clear
 from .dask import get
 from .executor import Executor, wait, as_completed
 
-__version__ = '1.1.0'
+__version__ = '1.2.0'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup
 
 setup(name='distributed',
-      version='1.1.0',
+      version='1.2.0',
       description='Distributed computing',
       url='http://distributed.readthedocs.org/en/latest/',
       maintainer='Matthew Rocklin',
