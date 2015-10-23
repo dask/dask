f0680d852728d1a56e39b6225c715dd945d2ce5e
Matthew Rocklin <mrocklin@gmail.com>
2015-10-22 19:49:05 -0700

    cluster supports multiple workers

--- distributed/utils_test.py
+++ distributed/utils_test.py
@@ -35,39 +35,38 @@ def run_worker(port, center_port, **kwargs):
 _port = [8010]
 
 @contextmanager
-def cluster():
+def cluster(nworkers=2):
     _port[0] += 1
     cport = _port[0]
     center = Process(target=run_center, args=(cport,))
-    _port[0] += 1
-    aport = _port[0]
-    a = Process(target=run_worker, args=(aport, cport), kwargs={'ncores': 1})
-    _port[0] += 1
-    bport = _port[0]
-    b = Process(target=run_worker, args=(bport, cport), kwargs={'ncores': 1})
+    workers = []
+    for i in range(nworkers):
+        _port[0] += 1
+        port = _port[0]
+        proc = Process(target=run_worker, args=(port, cport), kwargs={'ncores': 1})
+        workers.append({'port': port, 'proc': proc})
 
     center.start()
-    a.start()
-    b.start()
+    for worker in workers:
+        worker['proc'].start()
 
     sock = connect_sync('127.0.0.1', cport)
     while True:
         write_sync(sock, {'op': 'ncores'})
         ncores = read_sync(sock)
-        if len(ncores) == 2:
+        if len(ncores) == nworkers:
             break
 
     try:
-        yield {'proc': center, 'port': cport}, [{'proc': a, 'port': aport},
-                                                {'proc': b, 'port': bport}]
+        yield {'proc': center, 'port': cport}, workers
     finally:
-        for port in [aport, bport, cport]:
+        for port in [cport] + [w['port'] for w in workers]:
             with ignoring(socket.error):
                 sock = connect_sync('127.0.0.1', port)
                 write_sync(sock, dict(op='terminate', close=True))
                 response = read_sync(sock)
                 sock.close()
-        for proc in [a, b, center]:
+        for proc in [center] + [w['proc'] for w in workers]:
             with ignoring(Exception):
                 proc.terminate()
 
