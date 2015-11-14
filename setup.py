73844261ee798b1ce9b7b11053ebcc695342a1ab
Matthew Rocklin <mrocklin@gmail.com>
2015-11-14 10:31:21 -0800

    bump version to 1.3.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info[0] < 3:
     requires.append('futures')
 
 setup(name='distributed',
-      version='1.3.1',
+      version='1.3.2',
       description='Distributed computing',
       url='http://distributed.readthedocs.org/en/latest/',
       maintainer='Matthew Rocklin',
