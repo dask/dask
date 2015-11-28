83ebcf300221b690256de00ec488580eeaa47739
Matthew Rocklin <mrocklin@gmail.com>
2015-11-27 17:12:29 -0800

    remove dask module

--- distributed/__init__.py
+++ distributed/__init__.py
@@ -3,7 +3,6 @@ from .worker import Worker
 from .client import scatter, gather, delete, clear, rpc
 from .utils import sync
 from .nanny import Nanny
-from .dask import get
 from .executor import Executor, wait, as_completed
 
 __version__ = '1.4.0'
