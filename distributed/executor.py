f443ca0c2f9b11d69c42b6922db62b49507c3b1d
Matthew Rocklin <mrocklin@gmail.com>
2015-12-11 13:21:26 -0800

    update see also api sections

--- distributed/executor.py
+++ distributed/executor.py
@@ -343,7 +343,7 @@ class Executor(object):
 
         See Also
         --------
-        distributed.executor.Executor.map:
+        distributed.executor.Executor.map: Submit on many arguments at once
         """
         if not callable(func):
             raise TypeError("First input to submit must be a callable function")
@@ -407,7 +407,7 @@ class Executor(object):
 
         See also
         --------
-        distributed.executor.Executor.submit
+        distributed.executor.Executor.submit: Submit a single function
         """
         pure = kwargs.pop('pure', True)
         workers = kwargs.pop('workers', None)
@@ -497,7 +497,7 @@ class Executor(object):
 
         See Also
         --------
-        distributed.executor.gather
+        distributed.executor.Executor.scatter: Send data out to cluster
         """
         return sync(self.loop, self._gather, futures)
 
@@ -542,7 +542,7 @@ class Executor(object):
 
         See Also
         --------
-        distributed.executor.gather
+        distributed.executor.Executor.gather: Gather data back to local process
         """
         return sync(self.loop, self._scatter, data, workers=workers)
 
@@ -589,7 +589,7 @@ class Executor(object):
 
         See Also
         --------
-        distributed.executor.compute
+        distributed.executor.Executor.compute: Compute asynchronous collections
         """
         status, result = sync(self.loop, self._get, dsk, keys,
                               raise_on_error=False, **kwargs)
@@ -630,7 +630,7 @@ class Executor(object):
 
         See Also
         --------
-        distributed.executor.get
+        distributed.executor.Executor.get: Normal synchronous dask.get function
         """
         sync = kwargs.pop('sync', False)
         assert not kwargs
