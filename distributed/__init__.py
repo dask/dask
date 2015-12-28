f56cfc71d244fe6d8f3bd6dc5c3ed2eb45842bd3
Matthew Rocklin <mrocklin@gmail.com>
2015-12-28 09:00:01 -0800

    add default executor to top level imports

--- distributed/__init__.py
+++ distributed/__init__.py
@@ -5,7 +5,7 @@ from .client import scatter, gather, delete, clear, rpc
 from .diagnostics import progress
 from .utils import sync
 from .nanny import Nanny
-from .executor import Executor, wait, as_completed
+from .executor import Executor, wait, as_completed, default_executor
 from .scheduler import Scheduler
 
 __version__ = '1.6.1'
