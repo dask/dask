dc1430119da8a1bb6dee657389e7d0d69ee496b8
Matthew Rocklin <mrocklin@gmail.com>
2016-01-05 12:54:08 -0800

    refactor test_nanny to use gen_test

--- distributed/tests/test_nanny.py
+++ distributed/tests/test_nanny.py
@@ -11,119 +11,109 @@ from tornado import gen
 from distributed import Nanny, Center, rpc
 from distributed.core import connect, read, write
 from distributed.utils import ignoring
-from distributed.utils_test import loop
+from distributed.utils_test import gen_test
 
 
-def test_nanny(loop):
+@gen_test()
+def test_nanny():
     c = Center('127.0.0.1')
     c.listen(0)
     n = Nanny(c.ip, c.port, ncores=2, ip='127.0.0.1')
 
-    @gen.coroutine
-    def f():
-        yield n._start(0)
-        nn = rpc(ip=n.ip, port=n.port)
-        assert n.process.is_alive()
-        assert c.ncores[n.worker_address] == 2
-        assert c.nannies[n.worker_address] > 1024
+    yield n._start(0)
+    nn = rpc(ip=n.ip, port=n.port)
+    assert n.process.is_alive()
+    assert c.ncores[n.worker_address] == 2
+    assert c.nannies[n.worker_address] > 1024
 
-        yield nn.kill()
-        assert n.worker_address not in c.ncores
-        assert n.worker_address not in c.nannies
-        assert not n.process
+    yield nn.kill()
+    assert n.worker_address not in c.ncores
+    assert n.worker_address not in c.nannies
+    assert not n.process
 
-        yield nn.kill()
-        assert n.worker_address not in c.ncores
-        assert n.worker_address not in c.nannies
-        assert not n.process
+    yield nn.kill()
+    assert n.worker_address not in c.ncores
+    assert n.worker_address not in c.nannies
+    assert not n.process
 
-        yield nn.instantiate()
-        assert n.process.is_alive()
-        assert c.ncores[n.worker_address] == 2
-        assert c.nannies[n.worker_address] > 1024
+    yield nn.instantiate()
+    assert n.process.is_alive()
+    assert c.ncores[n.worker_address] == 2
+    assert c.nannies[n.worker_address] > 1024
 
-        yield nn.terminate()
-        assert not n.process
+    yield nn.terminate()
+    assert not n.process
 
-        if n.process:
-            n.process.terminate()
+    if n.process:
+        n.process.terminate()
 
-        yield n._close()
-        c.stop()
-
-    loop.run_sync(f)
+    yield n._close()
+    c.stop()
 
 
-def test_nanny_process_failure(loop):
+@gen_test()
+def test_nanny_process_failure():
+    c = Center('127.0.0.1')
+    c.listen(0)
+    n = Nanny(c.ip, c.port, ncores=2, ip='127.0.0.1')
+    yield n._start()
+    nn = rpc(ip=n.ip, port=n.port)
+    first_dir = n.worker_dir
 
-    @gen.coroutine
-    def f():
-        c = Center('127.0.0.1')
-        c.listen(0)
-        n = Nanny(c.ip, c.port, ncores=2, ip='127.0.0.1')
-        yield n._start()
-        nn = rpc(ip=n.ip, port=n.port)
-        first_dir = n.worker_dir
+    assert os.path.exists(first_dir)
 
-        assert os.path.exists(first_dir)
+    ww = rpc(ip=n.ip, port=n.worker_port)
+    yield ww.update_data(data={'x': 1, 'y': 2})
+    with ignoring(StreamClosedError):
+        yield ww.compute(function=sys.exit, args=(0,), key='z')
 
-        ww = rpc(ip=n.ip, port=n.worker_port)
-        yield ww.update_data(data={'x': 1, 'y': 2})
-        with ignoring(StreamClosedError):
-            yield ww.compute(function=sys.exit, args=(0,), key='z')
+    start = time()
+    while n.process.is_alive():  # wait while process dies
+        yield gen.sleep(0.01)
+        assert time() - start < 2
 
-        start = time()
-        while n.process.is_alive():  # wait while process dies
-            yield gen.sleep(0.01)
-            assert time() - start < 2
+    start = time()
+    while not n.process.is_alive():  # wait while process comes back
+        yield gen.sleep(0.01)
+        assert time() - start < 2
 
-        start = time()
-        while not n.process.is_alive():  # wait while process comes back
-            yield gen.sleep(0.01)
-            assert time() - start < 2
+    start = time()
+    while n.worker_address not in c.ncores or n.worker_dir is None:
+        yield gen.sleep(0.01)
+        assert time() - start < 2
 
-        start = time()
-        while n.worker_address not in c.ncores or n.worker_dir is None:
-            yield gen.sleep(0.01)
-            assert time() - start < 2
+    second_dir = n.worker_dir
 
-        second_dir = n.worker_dir
-
-        yield n._close()
-        assert not os.path.exists(second_dir)
-        assert not os.path.exists(first_dir)
-        assert first_dir != n.worker_dir
-        c.stop()
-
-    loop.run_sync(f)
+    yield n._close()
+    assert not os.path.exists(second_dir)
+    assert not os.path.exists(first_dir)
+    assert first_dir != n.worker_dir
+    c.stop()
 
 
-def test_monitor_resources(loop):
+@gen_test()
+def test_monitor_resources():
     pytest.importorskip('psutil')
     c = Center(ip='127.0.0.1')
     c.listen(0)
     n = Nanny(c.ip, c.port, ncores=2, ip='127.0.0.1')
 
-    @gen.coroutine
-    def f():
-        yield n._start()
-        nn = rpc(ip=n.ip, port=n.port)
-        assert n.process.is_alive()
-        d = n.resource_collect()
-        assert {'cpu_percent', 'memory_percent'}.issubset(d)
+    yield n._start()
+    nn = rpc(ip=n.ip, port=n.port)
+    assert n.process.is_alive()
+    d = n.resource_collect()
+    assert {'cpu_percent', 'memory_percent'}.issubset(d)
 
-        assert isinstance(d['timestamp'], datetime)
+    assert isinstance(d['timestamp'], datetime)
 
-        stream = yield connect(ip=n.ip, port=n.port)
-        yield write(stream, {'op': 'monitor_resources', 'interval': 0.01})
+    stream = yield connect(ip=n.ip, port=n.port)
+    yield write(stream, {'op': 'monitor_resources', 'interval': 0.01})
 
-        for i in range(3):
-            msg = yield read(stream)
-            assert isinstance(msg, dict)
-            assert {'cpu_percent', 'memory_percent'}.issubset(msg)
+    for i in range(3):
+        msg = yield read(stream)
+        assert isinstance(msg, dict)
+        assert {'cpu_percent', 'memory_percent'}.issubset(msg)
 
-        stream.close()
-        yield n._close()
-        c.stop()
-
-    loop.run_sync(f)
+    stream.close()
+    yield n._close()
+    c.stop()
