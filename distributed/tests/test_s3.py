4a9ff39f2f79a23cb4c92210897f1c72a9e65f1b
Hussain Sultan <hussainz@gmail.com>
2016-01-16 21:54:55 -0500

    add test_bucket_name as a variable

--- distributed/tests/test_s3.py
+++ distributed/tests/test_s3.py
@@ -9,7 +9,7 @@ from distributed.utils import get_ip
 from distributed.utils_test import gen_cluster
 
 ip = get_ip()
-
+test_bucket_name = 'distributed-test'
 
 @contextmanager
 def make_s3(bucket_name):
@@ -25,7 +25,7 @@ def make_s3(bucket_name):
 
 
 def test_get_list_of_summary_objects():
-    with make_s3('distributed-test1') as bucket:
+    with make_s3(test_bucket_name) as bucket:
         data = b'a' * int(10)
         fn_1 = 'tmp/test/file1'
         fn_2 = 'tmp/test/file2'
@@ -33,27 +33,27 @@ def test_get_list_of_summary_objects():
         bucket.put_object(Key=fn_1, Body=data)
         bucket.put_object(Key=fn_2, Body=data)
 
-        L = get_list_of_summary_objects('distributed-test1', prefix='tmp')
+        L = get_list_of_summary_objects(test_bucket_name, prefix='tmp')
 
         assert len(L) == 2
         assert map(lambda o: o.key, L) == ['tmp/test/file1', 'tmp/test/file2']
 
 
 def test_read_keys_from_bucket():
-    with make_s3('distributed-test1') as bucket:
+    with make_s3(test_bucket_name) as bucket:
         data = b'a' * int(10)
         fn_1 = 'tmp/test/file1'
 
         bucket.put_object(Key=fn_1, Body=data)
 
-        file_contents = read_content_from_keys('distributed-test1', fn_1)
+        file_contents = read_content_from_keys(test_bucket_name, fn_1)
 
         assert file_contents == data
 
 
 @gen_cluster([(ip, 1), (ip, 2)], timeout=60)
 def test_read_contents(s, a, b):
-    with make_s3('distributed-test1') as bucket:
+    with make_s3(test_bucket_name) as bucket:
         data = b'a'
 
         for i in range(3):
@@ -63,7 +63,7 @@ def test_read_contents(s, a, b):
 
         e = Executor((s.ip, s.port), start=False)
         yield e._start()
-        futures = read_contents('distributed-test1', prefix='tmp/test/data')
+        futures = read_contents(test_bucket_name, prefix='tmp/test/data')
         assert len(futures) == 6
         results = yield e._gather(futures)
         assert len(results) == 6
@@ -72,7 +72,7 @@ def test_read_contents(s, a, b):
 
 @gen_cluster([(ip, 1), (ip, 2)], timeout=60)
 def test_read_contents_lazy(s, a, b):
-    with make_s3('distributed-test1') as bucket:
+    with make_s3(test_bucket_name) as bucket:
         data = b'a'
 
         for i in range(3):
@@ -82,7 +82,7 @@ def test_read_contents_lazy(s, a, b):
 
         e = Executor((s.ip, s.port), start=False)
         yield e._start()
-        values = read_contents('distributed-test1', prefix='tmp/test/data', lazy=True)
+        values = read_contents(test_bucket_name, prefix='tmp/test/data', lazy=True)
         assert all(isinstance(v, Value) for v in values)
         results = e.compute(*values, sync=False)
         yield _wait(results)
