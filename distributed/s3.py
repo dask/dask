123fa4877a68e8ed0c09f14326228df166ce7754
Hussain Sultan <hussainz@gmail.com>
2016-01-16 19:21:28 -0500

    Add get_objects_from_bucket to get_list_of_summary_objects - Add page_size parameter to get_list_of_summary_objects - Add prefix parameter - Remove get_objects_from_buckets function

--- distributed/s3.py
+++ distributed/s3.py
@@ -3,10 +3,13 @@ from dask.imperative import Value
 from distributed import default_executor
 from toolz import curry
 
+DEFAULT_PAGE_LENGHT = 1000
 
-def get_objects_from_bucket(bucket_name):
+
+def get_list_of_summary_objects(bucket_name, prefix='', page_size=DEFAULT_PAGE_LENGHT):
     s3 = boto3.resource('s3')
-    return [s3_object.key for s3_object in s3.Bucket(bucket_name).objects.all()]
+    return [summary_object for summary_object in
+            s3.Bucket(bucket_name).objects.filter(Prefix=prefix).page_size(page_size)]
 
 
 def read_content_from_keys(bucket, key):
@@ -15,18 +18,17 @@ def read_content_from_keys(bucket, key):
     return s3.Object(bucket, key).get()['Body'].read()
 
 
-def read_contents(bucket_name, executor=None, lazy=False):
+def read_contents(bucket_name, executor=None, prefix='', lazy=False):
     executor = default_executor(executor)
-    s3_objects = get_objects_from_bucket(bucket_name)
+    s3_objects = get_list_of_summary_objects(bucket_name, prefix=prefix)
+    keys = [obj.key for obj in s3_objects]
 
     curried_read_content_from_keys = curry(read_content_from_keys, bucket_name)
-    names = ['read-contents-{0}'.format(s3_object) for s3_object in s3_objects]
+    names = ['read-contents-{0}'.format(key) for key in keys]
 
     if lazy:
-        values = [Value(name, [{name: (curried_read_content_from_keys, fn)}])
-                  for name, fn in zip(names, s3_objects)]
-
+        values = [Value(name, [{name: (curried_read_content_from_keys, key)}])
+                  for name, key in zip(names, keys)]
         return values
     else:
-
-        return executor.map(curried_read_content_from_keys, s3_objects)
+        return executor.map(curried_read_content_from_keys, keys)
--- distributed/tests/test_s3.py
+++ distributed/tests/test_s3.py
@@ -4,7 +4,7 @@ import boto3
 from dask.imperative import Value
 from distributed import Executor
 from distributed.executor import _wait
-from distributed.s3 import get_objects_from_bucket, read_content_from_keys, read_contents
+from distributed.s3 import read_contents, get_list_of_summary_objects, read_content_from_keys
 from distributed.utils import get_ip
 from distributed.utils_test import gen_cluster
 
@@ -24,37 +24,36 @@ def make_s3(bucket_name):
         bucket.delete()
 
 
-def test_get_objects_from_bucket():
+def test_get_list_of_summary_objects():
     with make_s3('distributed-test1') as bucket:
-        data = b'a' * int(1e6)
+        data = b'a' * int(10)
         fn_1 = 'tmp/test/file1'
         fn_2 = 'tmp/test/file2'
 
         bucket.put_object(Key=fn_1, Body=data)
         bucket.put_object(Key=fn_2, Body=data)
 
-        L = get_objects_from_bucket('distributed-test1')
+        L = get_list_of_summary_objects('distributed-test1', prefix='tmp')
 
-        assert L == ['tmp/test/file1', 'tmp/test/file2']
+        assert len(L) == 2
+        assert map(lambda o: o.key, L) == ['tmp/test/file1', 'tmp/test/file2']
+
 
 def test_read_keys_from_bucket():
     with make_s3('distributed-test1') as bucket:
-        data = b'a' * int(1e6)
+        data = b'a' * int(10)
         fn_1 = 'tmp/test/file1'
-        fn_2 = 'tmp/test/file2'
 
         bucket.put_object(Key=fn_1, Body=data)
-        bucket.put_object(Key=fn_2, Body=data)
 
-        file_contents = read_content_from_keys('distributed-test1',fn_1)
+        file_contents = read_content_from_keys('distributed-test1', fn_1)
 
         assert file_contents == data
 
 
-
 @gen_cluster([(ip, 1), (ip, 2)], timeout=60)
-def test_read_contents(s,a,e):
-    with make_s3('distributed-test') as bucket:
+def test_read_contents(s, a, b):
+    with make_s3('distributed-test1') as bucket:
         data = b'a'
 
         for i in range(3):
@@ -64,7 +63,7 @@ def test_read_contents(s,a,e):
 
         e = Executor((s.ip, s.port), start=False)
         yield e._start()
-        futures = read_contents('distributed-test')
+        futures = read_contents('distributed-test1', prefix='tmp/test/data')
         assert len(futures) == 6
         results = yield e._gather(futures)
         assert len(results) == 6
@@ -72,8 +71,8 @@ def test_read_contents(s,a,e):
 
 
 @gen_cluster([(ip, 1), (ip, 2)], timeout=60)
-def test_read_contents_lazy(s,a,e):
-    with make_s3('distributed-test') as bucket:
+def test_read_contents_lazy(s, a, b):
+    with make_s3('distributed-test1') as bucket:
         data = b'a'
 
         for i in range(3):
@@ -83,7 +82,7 @@ def test_read_contents_lazy(s,a,e):
 
         e = Executor((s.ip, s.port), start=False)
         yield e._start()
-        values = read_contents('distributed-test', lazy=True)
+        values = read_contents('distributed-test1', prefix='tmp/test/data', lazy=True)
         assert all(isinstance(v, Value) for v in values)
         results = e.compute(*values, sync=False)
         yield _wait(results)
