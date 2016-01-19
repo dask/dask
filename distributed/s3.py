953f3df2d7453428b24bbb79950bcd95233b7af9
Matthew Rocklin <mrocklin@gmail.com>
2016-01-18 18:53:19 -0800

    Use anonymous connections in tests

--- distributed/s3.py
+++ distributed/s3.py
@@ -1,34 +1,48 @@
 import boto3
 from dask.imperative import Value
 from distributed import default_executor
+from botocore.handlers import disable_signing
+
 
 DEFAULT_PAGE_LENGTH = 1000
 
 
-def get_list_of_summary_objects(bucket_name, prefix='', delimiter='', page_size=DEFAULT_PAGE_LENGTH):
+def get_list_of_summary_objects(bucket_name, prefix='', delimiter='',
+        page_size=DEFAULT_PAGE_LENGTH, anon=False):
     s3 = boto3.resource('s3')
+    if anon:
+        s3.meta.client.meta.events.register('choose-signer.s3.*',
+                disable_signing)
     L = list(s3.Bucket(bucket_name)
                .objects.filter(Prefix=prefix, Delimiter=delimiter)
                .page_size(page_size))
     return [s for s in L if s.key[-1] != '/']
 
 
-def read_content_from_keys(bucket, key):
+def read_content_from_keys(bucket, key, anon=False):
     import boto3
     s3 = boto3.resource('s3')
+    if anon:
+        s3.meta.client.meta.events.register('choose-signer.s3.*',
+                disable_signing)
+
     return s3.Object(bucket, key).get()['Body'].read()
 
 
-def read_bytes(bucket_name, prefix='', delimiter='', executor=None, lazy=False):
+def read_bytes(bucket_name, prefix='', delimiter='', executor=None, lazy=False,
+               anon=False):
     executor = default_executor(executor)
-    s3_objects = get_list_of_summary_objects(bucket_name, prefix=prefix, delimiter=delimiter)
+    s3_objects = get_list_of_summary_objects(bucket_name, prefix, delimiter,
+                                             anon=anon)
     keys = [obj.key for obj in s3_objects]
 
     names = ['read-bytes-{0}'.format(key) for key in keys]
 
     if lazy:
-        values = [Value(name, [{name: (read_content_from_keys, bucket_name, key)}])
+        values = [Value(name, [{name: (read_content_from_keys, bucket_name,
+                                       key, anon)}])
                   for name, key in zip(names, keys)]
         return values
     else:
-        return executor.map(read_content_from_keys, [bucket_name] * len(keys), keys)
+        return executor.map(read_content_from_keys, [bucket_name] * len(keys),
+                keys, anon=anon)
--- distributed/tests/test_s3.py
+++ distributed/tests/test_s3.py
@@ -6,6 +6,7 @@ from distributed.s3 import read_bytes, get_list_of_summary_objects, read_content
 from distributed.utils import get_ip
 from distributed.utils_test import gen_cluster
 
+
 ip = get_ip()
 
 
@@ -23,7 +24,8 @@ files = {'test/accounts.1.json':  (b'{"amount": 100, "name": "Alice"}\n'
 
 
 def test_get_list_of_summary_objects():
-    L = get_list_of_summary_objects(test_bucket_name, prefix='test/accounts')
+    L = get_list_of_summary_objects(test_bucket_name, prefix='test/accounts',
+                                    anon=True)
 
     assert len(L) == 2
     assert list(map(lambda o: o.key, L)) == sorted(list(files))
@@ -31,20 +33,20 @@ def test_get_list_of_summary_objects():
 
 def test_read_keys_from_bucket():
     for k, data in files.items():
-        file_contents = read_content_from_keys('distributed-test', k)
+        file_contents = read_content_from_keys('distributed-test', k, anon=True)
 
         assert file_contents == data
 
 
 def test_list_summary_object_with_prefix_and_delimiter():
     keys = get_list_of_summary_objects(test_bucket_name, 'nested/nested2/',
-                                       delimiter='/')
+                                       delimiter='/', anon=True)
 
     assert len(keys) == 2
     assert [k.key for k in keys] == [u'nested/nested2/file1',
                                      u'nested/nested2/file2']
 
-    keys = get_list_of_summary_objects(test_bucket_name, prefix='nested/')
+    keys = get_list_of_summary_objects(test_bucket_name, 'nested/', anon=True)
 
     assert len(keys) == 4
     assert [k.key for k in keys] == [u'nested/file1',
@@ -53,23 +55,23 @@ def test_list_summary_object_with_prefix_and_delimiter():
                                      u'nested/nested2/file2']
 
 
-@gen_cluster()
+@gen_cluster(timeout=60)
 def test_read_bytes(s, a, b):
     e = Executor((s.ip, s.port), start=False)
     yield e._start()
 
-    futures = read_bytes(test_bucket_name, prefix='test/')
+    futures = read_bytes(test_bucket_name, prefix='test/', anon=True)
     assert len(futures) == len(files)
     results = yield e._gather(futures)
     assert set(results) == set(files.values())
 
 
-@gen_cluster()
+@gen_cluster(timeout=60)
 def test_read_bytes_lazy(s, a, b):
     e = Executor((s.ip, s.port), start=False)
     yield e._start()
 
-    values = read_bytes(test_bucket_name, prefix='test/', lazy=True)
+    values = read_bytes(test_bucket_name, prefix='test/', lazy=True, anon=True)
     assert all(isinstance(v, Value) for v in values)
 
     results = e.compute(*values, sync=False)
