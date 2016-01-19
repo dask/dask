9073ca0a09019eed7c0d87a02be706303522d868
Martin Durant <martin.durant@utoronto.ca>
2016-01-19 13:01:52 -0500

    Add simple test for avro files

--- distributed/hdfs.py
+++ distributed/hdfs.py
@@ -115,15 +115,17 @@ def avro_body(data, av):
     import fastavro._reader as fa
     sync = av._header['sync']
     if not data.endswith(sync):
-        ## Read delimited should keep end-of-block delimiter
+        # Read delimited should keep end-of-block delimiter
         data = data + sync
     stream = io.BytesIO(data)
     return list(fa._iter_avro(stream, av._header, av.codec, av.schema, None))
+
 
 def avro_to_df(b, av):
     """Parse avro binary data with header av into a pandas dataframe"""
     import pandas as pd
     return pd.DataFrame(data=avro_body(b, av))
+
 
 @gen.coroutine
 def _read_avro(fn, executor=None, hdfs=None, lazy=False, **kwargs):
@@ -135,12 +137,14 @@ def _read_avro(fn, executor=None, hdfs=None, lazy=False, **kwargs):
     hdfs = hdfs or HDFileSystem()
     executor = default_executor(executor)
     filenames = hdfs.glob(fn)
-    with hdfs.open(filenames[0], 'r') as f:
-        av = fastavro.reader(f)
-        sync = av._header['sync']
+    blockss = []
     filenames = hdfs.glob(fn)
-    blockss = [read_bytes(fn, executor, hdfs, lazy=True, delimiter=sync,
-                          not_zero=True) for fn in filenames]
+    for fn in filenames:
+        with hdfs.open(fn, 'r') as f:
+            av = fastavro.reader(f)
+            sync = av._header['sync']
+        blockss.extend([read_bytes(fn, executor, hdfs, lazy=True,
+                        delimiter=sync, not_zero=True) for fn in filenames])
 
     dfs1 = [do(avro_to_df)(b, av) for b in blockss]
     if lazy:
