177dd5eae5bd4a98b6bd25be22650f23de66d1eb
Matthew Rocklin <mrocklin@gmail.com>
2016-02-12 12:23:33 -0800

    reword cli python 3 encoding error

--- distributed/cli/utils.py
+++ distributed/cli/utils.py
@@ -1,10 +1,13 @@
-py3_err_msg = """You are using Python 3 and your system is not compatible with the Unicode text model in Python 3
-This usually gets solved by exporting this environment variables but it depends on your system:
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
+py3_err_msg = """
+Your terminal does not properly support unicode text required by command line
+utilities running Python 3.  This is commonly solved by specifying encoding
+environment variables, though exact solutions may depend on your system:
+
+    $ export LC_ALL=C.UTF-8
+    $ export LANG=C.UTF-8
 
 For more information see: http://click.pocoo.org/5/python3/
-"""
+""".strip()
 
 
 def check_python_3():
