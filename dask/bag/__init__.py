ee5b714e05fd0742d19168834341a33d8540f5c7
Matthew Rocklin <mrocklin@gmail.com>
2016-02-12 15:38:41 -0800

    add range to bag

--- dask/bag/__init__.py
+++ dask/bag/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 from .core import (Bag, Item, from_sequence, from_filenames, from_url,
-                   to_textfiles, concat, from_s3, from_castra, from_imperative)
+                   to_textfiles, concat, from_s3, from_castra, from_imperative,
+                   bag_range as range)
 from ..context import set_options
 from ..base import compute
