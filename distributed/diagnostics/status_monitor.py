8f50f546af8a112df3426c0d6ff13e83c790fea5
Luke Canavan <canavandl@gmail.com>
2016-03-17 15:33:36 -0500

    Add streaming memory alloc plot

--- distributed/diagnostics/status_monitor.py
+++ distributed/diagnostics/status_monitor.py
@@ -1,6 +1,8 @@
 from __future__ import print_function, division, absolute_import
 
+from collections import defaultdict
 import json
+import time
 from tornado import gen
 
 from ..core import rpc
@@ -12,9 +14,10 @@ from tornado.httpclient import AsyncHTTPClient
 from tornado.ioloop import IOLoop, PeriodicCallback
 
 with ignoring(ImportError):
+    from bokeh.palettes import Spectral11
     from bokeh.models import ColumnDataSource
     from bokeh.models.widgets import DataTable, TableColumn
-    from bokeh.plotting import vplot, output_notebook, show
+    from bokeh.plotting import vplot, output_notebook, show, figure
     from bokeh.io import curstate, push_notebook
 
 class Status_Monitor(object):
@@ -27,7 +30,7 @@ class Status_Monitor(object):
     interval: Number, optional
         Interval between updates.  Defaults to 1s
     """
-    def __init__(self, addr=None, interval=1000.00):
+    def __init__(self, addr=None, interval=500.00):
         if addr is None:
             scheduler = default_executor().scheduler
             if isinstance(scheduler, rpc):
@@ -56,6 +59,19 @@ class Status_Monitor(object):
         self.task_table = DataTable(source=self.task_source, columns=task_columns,
                                     row_headers=False, width=600, height=60)
 
+        ts_props = ['time','workers', 'nbytes']
+        self.ts_source = ColumnDataSource({k: [] for k in ts_props})
+        self.plot = figure(height=400, width=600, x_axis_type='datetime')
+        self.plot.multi_line(xs='time', ys='nbytes', color=Spectral11,
+                             source=self.ts_source)
+
+        # For managing monitor state
+        self.nbytes = defaultdict(list)
+        self.times = []
+        self.start = time.time()
+
+        self.output = vplot(self.task_table, self.worker_table, self.plot)
+
         self.client = AsyncHTTPClient()
 
         loop = IOLoop.current()
@@ -65,7 +81,7 @@ class Status_Monitor(object):
         pc.start()
 
     def _ipython_display_(self, **kwargs):
-        show(vplot(self.task_table, self.worker_table))
+        show(self.output)
         self.display_notebook = True
 
     @gen.coroutine
@@ -80,6 +96,7 @@ class Status_Monitor(object):
         response = yield self.client.fetch('http://%s:%d/status.json' % (ip, port))
         d = json.loads(response.body.decode())
 
+        self.wkr_source.data['time'] = [time.time()]
         self.wkr_source.data['workers'] = [k for k, v in sorted(d['ncores'].items(),
                                            key=lambda x: x[0], reverse=True)]
         self.wkr_source.data['ncores'] = [v for k, v in sorted(d['ncores'].items(),
@@ -94,6 +111,14 @@ class Status_Monitor(object):
         self.task_source.data['in-progress'] = [sum(v for vv in d['processing'].values() for v in vv.values())]
         self.task_source.data['in-memory'] = [d['in-memory']]
         self.task_source.data['total'] = [d['tasks']]
+
+        for k,v in d['bytes'].items():
+            self.nbytes[k].append(v)
+        self.times.append(time.time() * 1000 - self.start * 1000) #bokeh uses msse
+
+        self.ts_source.data['time'] = [self.times for k in self.nbytes.keys()]
+        self.ts_source.data['nbytes'] = [v for k, v in sorted(self.nbytes.items(),
+                                         key=lambda x: x[0], reverse=True)]
 
         if self.display_notebook:
             push_notebook()
