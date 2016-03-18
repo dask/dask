5b653cce5dc0b52025444dcfe2fe866ffac5ba25
Matthew Rocklin <mrocklin@gmail.com>
2016-03-18 14:40:53 -0700

    add worker processing route

--- distributed/http/worker.py
+++ distributed/http/worker.py
@@ -19,6 +19,15 @@ class Info(RequestHandler):
                 'status': self.server.status}
         self.write(resp)
 
+class Processing(RequestHandler):
+    """Basic info about the worker """
+    def get(self):
+        try:
+            resp = {'processing': [str(k) for k in self.server.active]}
+        except Exception as e:
+            resp = str(e)
+        self.write(resp)
+
 
 class LocalFiles(RequestHandler):
     """List the local spill directory"""
@@ -29,6 +38,7 @@ class LocalFiles(RequestHandler):
 def HTTPWorker(worker):
     application = MyApp(web.Application([
         (r'/info.json', Info, {'server': worker}),
+        (r'/processing.json', Processing, {'server': worker}),
         (r'/resources.json', Resources, {'server': worker}),
         (r'/files.json', LocalFiles, {'server': worker})
         ]))
