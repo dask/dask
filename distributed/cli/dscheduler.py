00dfb63059ef740dabc02bae97dbc3ce91d899d4
Matthew Rocklin <mrocklin@gmail.com>
2016-03-21 11:40:09 -0700

    move status_applicatio to new bokeh directory

--- distributed/cli/dscheduler.py
+++ distributed/cli/dscheduler.py
@@ -1,12 +1,15 @@
 from __future__ import print_function, division, absolute_import
 
 import logging
-from sys import argv, exit
+import os
 import socket
+import subprocess
+from sys import argv, exit
 from time import sleep
 
 import click
 
+import distributed
 from distributed import Scheduler
 from distributed.utils import get_ip
 from distributed.http import HTTPScheduler
@@ -31,9 +34,10 @@ signal.signal(signal.SIGTERM, handle_signal)
 @click.argument('center', type=str, default='')
 @click.option('--port', type=int, default=8786, help="Serving port")
 @click.option('--http-port', type=int, default=9786, help="HTTP port")
+@click.option('--bokeh-port', type=int, default=8787, help="HTTP port")
 @click.option('--host', type=str, default=ip,
               help="Serving host defaults to %s" % ip)
-def main(center, host, port, http_port):
+def main(center, host, port, http_port, bokeh_port):
     ip = socket.gethostbyname(host)
     loop = IOLoop.current()
     scheduler = Scheduler(center, ip=ip,
@@ -42,11 +46,27 @@ def main(center, host, port, http_port):
         loop.run_sync(scheduler.sync_center)
     scheduler.start(port)
 
+    try:
+        import bokeh
+    except ImportError:
+        pass
+    else:
+        hosts = ['%s:%d' % (h, bokeh_port) for h in
+                 ['localhost', '127.0.0.1', ip, socket.gethostname()]]
+        dirname = os.path.dirname(distributed.__file__)
+        path = os.path.join(dirname, 'diagnostics', 'bokeh', 'status.py')
+        proc = subprocess.Popen(['bokeh', 'serve', path,
+                                 '--log-level', 'warning',
+                                 '--port', str(bokeh_port)] +
+                                 sum([['--host', host] for host in hosts], []))
+        logger.info(" Start Bokeh UI at:        http://%s:%d/status/"
+                    % (ip, bokeh_port))
+
     loop.start()
     loop.close()
     scheduler.stop()
 
-    logging.info("End scheduler at %s:%d", ip, port)
+    logger.info("End scheduler at %s:%d", ip, port)
 
 
 def go():
--- distributed/cli/tests/test_dscheduler.py
+++ distributed/cli/tests/test_dscheduler.py
@@ -1,9 +1,14 @@
 from __future__ import print_function, division, absolute_import
 
+import socket
 from subprocess import Popen, PIPE
 import requests
 
+import pytest
+
 from distributed import Scheduler, Executor
+from distributed.utils import get_ip
+
 
 def test_defaults():
     try:
@@ -13,6 +18,21 @@ def test_defaults():
         response = requests.get('http://127.0.0.1:9786/info.json')
         assert response.ok
         assert response.json()['status'] == 'running'
+    finally:
+        e.shutdown()
+        proc.kill()
+
+
+def test_bokeh():
+    pytest.importorskip('bokeh')
+
+    try:
+        proc = Popen(['dscheduler'], stdout=PIPE, stderr=PIPE)
+        e = Executor('127.0.0.1:%d' % Scheduler.default_port)
+
+        for name in [socket.gethostname(), 'localhost', '127.0.0.1', get_ip()]:
+            response = requests.get('http://%s:8787/status/' % name)
+            assert response.ok
     finally:
         e.shutdown()
         proc.kill()
