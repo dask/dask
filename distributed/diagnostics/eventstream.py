4b22c60580001318b1d46d08964f72aa1db142c4
Matthew Rocklin <mrocklin@gmail.com>
2016-03-23 10:41:20 -0700

    add eventstream plugin

---
+++ distributed/diagnostics/eventstream.py
@@ -0,0 +1,74 @@
+from __future__ import print_function, division, absolute_import
+
+import logging
+
+from tornado import gen
+
+from .plugin import SchedulerPlugin
+
+from ..core import connect, write, coerce_to_address
+from ..worker import dumps_function
+
+
+logger = logging.getLogger(__name__)
+
+
+class EventStream(SchedulerPlugin):
+    """ Maintain a copy of worker events """
+    def __init__(self, scheduler=None):
+        self.buffer = []
+        if scheduler:
+            scheduler.add_plugin(self)
+
+    def task_finished(self, scheduler, **msg):
+        """ Run when a task is reported complete """
+        self.buffer.append(msg)
+
+    def task_erred(self, scheduler, **msg):
+        """ Run when a task is reported failed """
+        self.buffer.append(msg)
+
+
+def swap_buffer(scheduler, es):
+    es.buffer, buffer = [], es.buffer
+    return buffer
+
+
+def teardown(scheduler, es):
+    scheduler.remove_plugin(es)
+
+
+@gen.coroutine
+def eventstream(address, interval):
+    """ Open a TCP connection to scheduler, receive batched task messages
+
+    The messages coming back are lists of dicts.  Each dict is of the following
+    form::
+
+        {'key': 'mykey', 'worker': 'host:port', 'status': status,
+         'compute-start': time(), 'compute-stop': time(),
+         'transfer-start': time(), 'transfer-stop': time(),
+         'other': 'junk'}
+
+    Where ``status`` is either 'OK', or 'error'
+
+    Parameters
+    ----------
+    address: address of scheduler
+    interval: time between batches, in seconds
+
+    Examples
+    --------
+    >>> stream = yield eventstream('127.0.0.1:8786', 0.100)  # doctest: +SKIP
+    >>> print(yield read(stream))  # doctest: +SKIP
+    [{'key': 'x', 'status': 'OK', 'worker': '192.168.0.1:54684', ...},
+     {'key': 'y', 'status': 'error', 'worker': '192.168.0.1:54684', ...}]
+    """
+    ip, port = coerce_to_address(address, out=tuple)
+    stream = yield connect(ip, port)
+    yield write(stream, {'op': 'feed',
+                         'setup': dumps_function(EventStream),
+                         'function': dumps_function(swap_buffer),
+                         'interval': interval,
+                         'teardown': dumps_function(teardown)})
+    raise gen.Return(stream)
---
+++ distributed/diagnostics/tests/test_eventstream.py
@@ -0,0 +1,51 @@
+from __future__ import print_function, division, absolute_import
+
+import pytest
+
+from tornado import gen
+
+from distributed import Executor, Scheduler, Worker
+from distributed.core import read
+from distributed.executor import _wait
+from distributed.diagnostics.eventstream import EventStream, eventstream
+from distributed.utils_test import inc, div, dec, gen_cluster
+from distributed.worker import dumps_task
+from time import time, sleep
+
+
+@gen_cluster(executor=True)
+def test_eventstream(e, s, a, b):
+    es = EventStream()
+    s.add_plugin(es)
+    assert es.buffer == []
+
+    futures = e.map(div, [1] * 10, range(10))
+    yield _wait(futures)
+
+    assert len(es.buffer) == 10
+
+
+@gen_cluster(executor=True, timeout=None)
+def test_eventstream_remote(e, s, a, b):
+    stream = yield eventstream(s.address, interval=0.010)
+
+    start = time()
+    while not s.plugins:
+        yield gen.sleep(0.01)
+        assert time() < start + 5
+
+    futures = e.map(div, [1] * 10, range(10))
+
+    start = time()
+    total = []
+    while len(total) < 10:
+        msgs = yield read(stream)
+        assert isinstance(msgs, list)
+        total.extend(msgs)
+        assert time() < start + 5
+
+    stream.close()
+    start = time()
+    while s.plugins:
+        yield gen.sleep(0.01)
+        assert time() < start + 5
