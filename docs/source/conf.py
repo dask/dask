0cc8fe14bc402eaf7314289352ab17346f283a30
Kristopher Overholt <koverholt@gmail.com>
2016-04-14 00:38:55 -0500

    Update docs conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -116,9 +116,14 @@ todo_include_todos = True
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'default'
+# Taken from docs.readthedocs.org:
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
