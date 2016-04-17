f31f7e0d007049e1cba7a0772afd02c3368b32c4
Matthew Rocklin <mrocklin@gmail.com>
2016-04-17 11:42:40 -0700

    bump version to 1.9.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.version_info < (3, 4):
     requires.append('singledispatch')
 
 setup(name='distributed',
-      version='1.9.2',
+      version='1.9.3',
       description='Distributed computing',
       url='http://distributed.readthedocs.org/en/latest/',
       maintainer='Matthew Rocklin',
