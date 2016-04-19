c1c23e7a4e8263d2706181a256e7c16f8dc4d294
ofirr <ofir.raz@weizmann.ac.il>
2016-04-19 09:11:43 +0300

    Changed center_addr and center_port to scheduler
    Added missing loop
    Resolved variable name conflict

--- distributed/tests/test_cluster.py
+++ distributed/tests/test_cluster.py
@@ -3,21 +3,21 @@ from __future__ import print_function, division, absolute_import
 from distributed.cluster import Cluster
 from distributed.core import rpc
 from distributed.utils_test import slow
+from distributed.utils_test import loop
 import pytest
-
 
 @slow
 @pytest.mark.avoid_travis
-def test_cluster():
-    with Cluster(center_addr = '127.0.0.1',
-                 center_port = 8787,
+def test_cluster(loop):
+    with Cluster(scheduler_addr = '127.0.0.1',
+                 scheduler_port = 8787,
                  worker_addrs = ['127.0.0.1', '127.0.0.1']) as c:
-        c = rpc(ip='127.0.0.1', port=8787)
+        r = rpc(ip='127.0.0.1', port=8787)
         result = []
         while len(result) != 2:
-            result = loop.run_sync(c.ncores)
+            result = loop.run_sync(r.ncores)
 
         c.add_worker('127.0.0.1')
 
         while len(result) != 3:
-            result = loop.run_sync(c.ncores)
+            result = loop.run_sync(r.ncores)
