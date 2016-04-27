7b342dba42683a3a9f38eb45d06ed5222d4af9aa
Max Hutchinson <maxhutch@gmail.com>
2016-04-27 15:46:57 -0500

    Adding test of _start_state callback.

--- dask/tests/test_callbacks.py
+++ dask/tests/test_callbacks.py
@@ -1,11 +1,23 @@
 from dask.async import get_sync
 from dask.callbacks import Callback
 
-def test_callback():
+def test_start_callback():
     flag = [False]
 
     class MyCallback(Callback):
         def _start(self, dsk):
+            flag[0] = True
+
+    with MyCallback():
+        get_sync({'x': 1}, 'x')
+
+    assert flag[0] is True
+
+def test_start_state_callback():
+    flag = [False]
+
+    class MyCallback(Callback):
+        def _start_state(self, dsk, state):
             flag[0] = True
 
     with MyCallback():
