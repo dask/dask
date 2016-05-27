343885c2728d90aa176fe1474b9844ef22606ca1
Martin Durant <martin.durant@utoronto.ca>
2016-05-27 10:12:37 -0400

    Start attempt to use ntile instead of offsets
    This is untestable as ntile and row_number are not implemented
    in sqlite (but in the target sql servers, e.g., PG, they are).

--- dask/dataframe/sql.py
+++ dask/dataframe/sql.py
@@ -4,7 +4,7 @@ from dask.dataframe import from_delayed
 
 
 def read_sql_table(table, uri, npartitions=None, columns=None,
-                   chunkrowsize=1000000, **kwargs):
+                   index_col=None, chunkrowsize=1000000, **kwargs):
     """
     Create dataframe from an SQL table.
 
@@ -15,11 +15,16 @@ def read_sql_table(table, uri, npartitions=None, columns=None,
     uri : string
         Full sqlalchemy URI for the database connection
     npartitions : int or None
-        Number of partitions. If given, takes precedence over chunkrowsize
+        Number of partitions. If None, uses chunkrowsize.
+    chunkrowsize : int
+        If npartitions is None, use this to decide the sizes of the
+        partitions.
     columns : list of strings or None
         Which columns to select; if None, gets all
-    chunkrowsize : int
-        Number of rows in each partition
+    index_col : string
+        Column which becomes the index, and defines the partitioning. Should
+        be a indexed column in the SQL server. If None, uses row number (numerical
+        index).
     kwargs : dict
         Additional parameters to pass to `pd.read_sql()`
 
@@ -27,16 +32,19 @@ def read_sql_table(table, uri, npartitions=None, columns=None,
     -------
     dask.dataframe
     """
-    length = pd.read_sql('select count(1) from ' + table, uri).iloc[0, 0]
-    if npartitions is not None:
-        chunkrowsize = length // npartitions
+    if npartitions is None:
+        length = pd.read_sql('select count(1) from ' + table, uri).iloc[0, 0]
+        npartitions = length // chunkrowsize
     columns = " ".join(columns) if columns else "*"
     parts = []
-    for offset in range(0, length, chunkrowsize):
-        q = ('SELECT {columns} FROM {table} LIMIT {chunk} OFFSET {offset}'.format(
-                columns=columns, table=table, chunk=chunkrowsize, offset=offset
-            ))
-        parts.append(delayed(pd.read_sql_query)(q, uri, **kwargs))
+    if index_col:
+        for offset in range(0, length, chunkrowsize):
+            q = ('SELECT {columns} FROM {table} '
+                 'WHERE  NTILE({nparts}) OVER (ORDER BY {index}) = i'.format(
+                    columns=columns, table=table, nparts=npartitions,
+                    index=index_col
+                    ))
+            parts.append(delayed(pd.read_sql_query)(q, uri, **kwargs))
     head = pd.read_sql('SELECT {columns} FROM {table} LIMIT 5'.format(
         columns=columns, table=table
     ), uri, **kwargs)
