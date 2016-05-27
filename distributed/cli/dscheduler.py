841b7d4b137905cf7a17123db2e7fc473640ca5e
Dan Ring <dfring@gmail.com>
2016-05-27 13:20:08 -0700

    Better check for Windows platform when finding bokeh (#296)

--- distributed/cli/dscheduler.py
+++ distributed/cli/dscheduler.py
@@ -60,7 +60,7 @@ def main(center, host, port, http_port, bokeh_port, show, _bokeh, bokeh_whitelis
             dirname = os.path.dirname(distributed.__file__)
             paths = [os.path.join(dirname, 'bokeh', name)
                      for name in ['status', 'tasks']]
-            binname = 'bokeh.bat' if 'win' in sys.platform else 'bokeh'
+            binname = 'bokeh.bat' if sys.platform.startswith('win') else 'bokeh'
             binname = os.path.join(os.path.dirname(sys.argv[0]), binname)
             args = ([binname, 'serve'] + paths +
                     ['--log-level', 'warning',
