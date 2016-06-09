36c02853de8c3d482bf7ba0f07a487da43699de1
Erik Welch <erik.n.welch@gmail.com>
2016-06-09 12:01:07 -0500

    Add docstring.

--- dask/dataframe/core.py
+++ dask/dataframe/core.py
@@ -2336,8 +2336,63 @@ def quantile(df, q):
 
 
 def _sample_percentiles(num_old, num_new, total_length, chunk_length):
+    """Construct percentiles for a chunk for repartitioning.
+
+    Adapt the number of total percentiles calculated based on the number
+    of current and new partitions.  Returned percentiles include equally
+    spaced percentiles between [0, 100], and random percentiles.  See
+    detailed discussion below.
+
+    Parameters
+    ----------
+    num_old: int
+        Number of partitions of the current object
+    num_new: int
+        Number of partitions of the new object
+    total_length: int
+        Total number of rows of the object
+    chunk_length: int
+        Number of rows of a partition
+
+    Returns
+    -------
+    qs : numpy.ndarray of sorted percentiles between 0, 100
+
+    Constructing ordered (i.e., not hashed) partitions is hard.  Calculating
+    approximate percentiles for generic objects in an out-of-core fashion is
+    also hard.  Fortunately, partition boundaries don't need to be perfect
+    in order for partitioning to be effective, so we strive for a "good enough"
+    method that can scale to many partitions and is reasonably well-behaved for
+    a wide variety of scenarios.
+
+    Two similar approaches come to mind: (1) take a subsample of every
+    partition, then find the best new partitions for the combined subsamples;
+    and (2) calculate equally-spaced percentiles on every partition (a
+    relatively cheap operation), then merge the results.  We do both, but
+    instead of random samples, we use random percentiles.
+
+    If the number of partitions isn't changing, then the ratio of fixed
+    percentiles to random percentiles is 2 to 1.  If repartitioning goes from
+    a very high number of partitions to a very low number of partitions, then
+    we use more random percentiles, because a stochastic approach will be more
+    stable to potential correlations in the data that may cause a few equally-
+    spaced partitions to under-sample the data.
+
+    The more partitions there are, then the more total percentiles will get
+    calculated across all partitions.  Squaring the number of partitions
+    approximately doubles the number of total percentiles calculates, so
+    num_total_percentiles ~ sqrt(num_partitions).  The number of percentiles
+    calculated on a single partition is proportional to the relative length
+    of the partition.  This should provide adequate resolution and allow the
+    number of partitions to scale.
+
+    For numeric data, one could instead use T-Digest for floats and Q-Digest
+    for ints to calculate approximate percentiles.  Our current method works
+    for any dtype.
+
+    """
     # *waves hands*
-    random_percentage = 1 / (1 + (3.0 * num_new / num_old)**0.5)
+    random_percentage = 1 / (1 + (4.0 * num_new / num_old)**0.5)
     num_percentiles = num_new * (num_old + 22)**0.55 * chunk_length / total_length
     num_fixed = int(num_percentiles * (1 - random_percentage)) + 2
     num_random = int(num_percentiles * random_percentage) + 2
