7a941b8f99620f9543d6f3f9627bc1ff99c3f2ea
Erik Welch <erik.n.welch@gmail.com>
2016-06-20 12:06:56 -0500

    Add more docs, including a high level discussion in the module docstring.

--- dask/dataframe/partitionquantiles.py
+++ dask/dataframe/partitionquantiles.py
@@ -1,3 +1,72 @@
+"""Determine new partition divisions using approximate percentiles.
+
+We use a custom algorithm to calculate approximate, evenly-distributed
+percentiles of arbitrarily-ordered data for any dtype in a distributed
+fashion with one pass over the data.  This is used to determine new
+partition divisions when changing the index of a dask.dataframe.  We claim
+no statistical guarantees, but we use a variety of heuristics to try to
+provide reliable, robust results that are "good enough" and can scale to
+large number of partitions.
+
+Our approach is similar to standard approaches such as t- and q-digest,
+GK, and sampling-based algorithms, which consist of three parts:
+
+1. **Summarize:** create summaries of subsets of data
+2. **Merge:** combine summaries to make a new summary
+3. **Compress:** periodically compress a summary into a smaller summary
+
+We summarize the data in each partition by calculating several percentiles.
+The value at each percentile is given a weight proportional to the length
+of the partition and the differences between the current percentile and
+the adjacent percentiles.  Merging summaries is simply a ``merge_sorted``
+of the values and their weights, which we do with a reduction tree.
+
+Percentiles is a good choice for our case, because we are given a numpy
+array of the partition's data, and percentiles is a relatively cheap
+operation.  Moreover, percentiles are, by definition, much less
+susceptible to the underlying distribution of the data, so the weights
+given to each value--even across partitions--should be comparable.
+
+Let us describe this to a child of five.  We are given many small cubes
+(of equal size) with numbers on them.  Split these into many piles.  This
+is like the original data.  Let's sort and stack the cubes from one of the
+piles.  Next, we are given a bunch of unlabeled blocks of different sizes,
+and most are much larger than the the original cubes.  Stack these blocks
+until they're the same height as our first stack.  Let's write a number on
+each block of the new stack.  To do this, choose the number of the cube in
+the first stack that is located in the middle of an unlabeled block.  We
+are finished with this stack once all blocks have a number written on them.
+Repeat this for all the piles of cubes.  Finished already?  Great!  Now
+take all the stacks of the larger blocks you wrote on and throw them into
+a single pile.  We'll be sorting these blocks next, which may be easier if
+you carefully move the blocks over and organize... ah, nevermind--too late.
+Okay, sort and stack all the blocks from that amazing, disorganized pile
+you just made.  This will be very tall, so we had better stack it sideways
+on the floor like so.  This will also make it easier for us to split the
+stack into groups of approximately equal size, which is our final task...
+
+This, in a nutshell, is the algorithm we deploy.  The main difference
+is that we don't always assign a block the number at its median (ours
+fluctuates around the median).  The numbers at the edges of the final
+groups is what we use as divisions for repartitioning.  We also need
+the overall min and max, so we take the 0th and 100th percentile of
+each partition, and another sample near each edge so we don't give
+disproportionate weights to extreme values.
+
+Choosing appropriate percentiles to take in each partition is where things
+get interesting.  The data is arbitrarily ordered, which means it may be
+sorted, random, or follow some pathological distribution--who knows.  We
+hope all partitions are of similar length, but we ought to expect some
+variation in lengths.  The number of partitions may also be changing
+significantly, which could affect the optimal choice of percentiles.  For
+improved robustness, we use both evenly-distributed and random percentiles.
+If the number of partitions isn't changing, then the total number of
+percentiles across all partitions scales as ``npartitions**1.5``.  Although
+we do not yet have a compression operation (step 3 above), one could be
+added if needed, such as for extremely large ``npartitions`` or if we find
+we need to increase the sample size for each partition.
+
+"""
 from __future__ import absolute_import, division, print_function
 
 import math
@@ -89,6 +158,8 @@ def sample_percentiles(num_old, num_new, chunk_length, upsample=1.0, random_stat
 def tree_width(N, to_binary=False):
     """Generate tree width suitable for ``merge_sorted`` given N inputs
 
+    The larger N is, the more tasks are reduced in a single task.
+
     In theory, this is designed so all tasks are of comparable effort.
     """
     if N < 32:
@@ -142,7 +213,7 @@ def create_merge_tree(func, keys, token):
     has beneficial properties of trees.
 
     For reasonable numbers of keys, N < 1e5, the total number of nodes in the
-    tree is approximately N**0.78.
+    tree is roughly ``N**0.78``.  For 1e5 < N < 2e5, is it roughly ``N**0.8``.
     """
     level = 0
     prev_width = len(keys)
@@ -180,7 +251,6 @@ def prepare_percentile_merge(qs, vals, length):
 
     >>> 0.5 * length * (percentiles[2] - percentiles[0])
     250.0
-
     """
     if length == 0:
         return ()
@@ -190,6 +260,11 @@ def prepare_percentile_merge(qs, vals, length):
 
 
 def _merge_sorted(items):
+    """Merge and sort items that are already sorted.
+
+    Each item is a tuple like ``(vals, weights)`` where vals and weights
+    are lists.  We sort both by vals.
+    """
     items = [x for x in items if x]
     if not items:
         return ()
@@ -199,7 +274,17 @@ def _merge_sorted(items):
 
 
 def process_val_weights(vals_and_weights, finalq, dtype_info):
-    """Calculate final percentiles given weighted vals"""
+    """Calculate final approximate percentiles given weighted vals
+
+    ``vals_and_weights`` is assumed to be sorted.  We take a cumulative
+    sum of the weights, which makes them percentile-like (their scale is
+    [0, N] instead of [0, 100]).  Next we find the divisions that are
+    closest to the desired quantiles, ``finalq``.
+
+    It is possible for adjacent values of the result to be the same.  Since
+    these determine the divisions of the new partitions, some partitions
+    may be empty.
+    """
     vals, weights = vals_and_weights
     vals = np.array(vals)
     weights = np.array(weights)
@@ -225,10 +310,29 @@ def process_val_weights(vals_and_weights, finalq, dtype_info):
         rv = pd.Categorical.from_codes(rv, info[0], info[1])
     elif rv.dtype != dtype:
         rv = rv.astype(dtype)
+    # Should we try to fudge the result to avoid duplicates?  After all,
+    # two smaller partitions is probably better than having an empty one.
     return rv
 
 
-def weighted_percentiles(df, num_old, num_new, upsample=1.0, random_state=None):
+def percentiles_summary(df, num_old, num_new, upsample=1.0, random_state=None):
+    """Summarize data using percentiles and derived weights.
+
+    These summaries can be merged, compressed, and converted back into
+    approximate percentiles.
+
+    Parameters
+    ----------
+    df: pandas.Series
+        Data to summarize
+    num_old: int
+        Number of partitions of the current object
+    num_new: int
+        Number of partitions of the new object
+    upsample: float
+        Scale factor to increase the number of percentiles calculated in
+        each partition.  Use to improve accuracy.
+    """
     from dask.array.percentile import _percentile
     length = len(df)
     qs = sample_percentiles(num_old, num_new, length, upsample, random_state)
@@ -277,7 +381,7 @@ def partition_quantiles(df, npartitions, upsample=1.0, random_state=None):
     dtype_dsk = {(name0, 0): (dtype_info, df_keys[0])}
 
     name1 = 're-quantiles-1-' + token
-    val_dsk = dict(((name1, i), (weighted_percentiles, key, df.npartitions,
+    val_dsk = dict(((name1, i), (percentiles_summary, key, df.npartitions,
                                  npartitions, upsample, seeds[i]))
                    for i, key in enumerate(df_keys))
 
@@ -291,4 +395,3 @@ def partition_quantiles(df, npartitions, upsample=1.0, random_state=None):
 
     dsk = merge(df.dask, dtype_dsk, val_dsk, merge_dsk, last_dsk)
     return return_type(dsk, name3, df_name, new_divisions)
-
