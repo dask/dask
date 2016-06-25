6c8d703cc938d9263da292849c485f9f1daa4f40
Tomas Ostasevicius <to266@cam.ac.uk>
2016-06-25 13:37:51 +0100

    proper broadcasting and tests

--- dask/array/tests/test_random.py
+++ dask/array/tests/test_random.py
@@ -165,22 +165,12 @@ def test_array_broadcasting():
     arr = np.arange(6).reshape((2,3))
     assert da.random.poisson(arr, chunks=3).compute().shape == (2,3)
     assert da.random.normal(arr, 2, chunks=3).compute().shape == (2,3)
-    # multiple arrays of the same shape work
-    assert da.random.normal(arr, arr+2, chunks=2).compute().shape == (2,3)
-    # also works if the same shape is given by the user
-    assert da.random.normal(arr, arr+2,
-                            chunks=2, size=(2,3)).compute().shape == (2,3)
-    # works with kwargs
-    assert da.random.gumbel(loc=arr, scale=arr+2,
-                            chunks=2, size=(2,3)).compute().shape == (2,3)
-    # raises when ambiguous
+    assert da.random.normal(np.ones((1,4)), np.ones((3,1)),
+                            chunks=3).compute().shape == (3,4)
+    assert da.random.normal(np.ones((1,4)), np.ones((3,1)), size=(2, 3,4),
+                            chunks=3).compute().shape == (2,3,4)
     with pytest.raises(ValueError):
-        assert da.random.gumbel(loc=arr[:,:-1], scale=arr+2,
-                                chunks=2,)
-    # raises when does not match with user-given
-    with pytest.raises(ValueError):
-        assert da.random.gumbel(loc=arr, scale=arr+2, size=(3,4),
-                                chunks=2,)
+        da.random.normal(arr, np.ones((3,1)), size=(2,3,4), chunks=3)
 
 def test_multinomial():
     for size, chunks in [(5, 3), ((5, 4), (2, 3))]:
