1b8caf2e3df70307b593ae4bb0f4d86114fefb0c
limx0 <bradley.mcelroy@live.com>
2016-07-03 12:01:49 +1000

    Add tests to dask/utils.py

--- dask/utils.py
+++ dask/utils.py
@@ -736,7 +736,6 @@ def infer_storage_options(urlpath, inherit_storage_options=None):
     return inferred_storage_options
 
 
-<<<<<<< 3a043c2237cb23de2d523cdf1cdcd8179b8795eb
 def dependency_depth(dsk):
     import toolz
 
