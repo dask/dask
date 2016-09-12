09a1ac5962e7fc77d0deee8d1a559e0c73234520
Matthew Rocklin <mrocklin@gmail.com>
2016-09-12 08:24:41 -0400

    bump version to 1.13.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.version_info < (3, 4):
     requires.append('singledispatch')
 
 setup(name='distributed',
-      version='1.13.0',
+      version='1.13.1',
       description='Distributed computing',
       url='http://distributed.readthedocs.io/en/latest/',
       maintainer='Matthew Rocklin',
