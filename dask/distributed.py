546f78f89f316ce5f8ebc04c34d497f681212385
Matthew Rocklin <mrocklin@gmail.com>
2016-09-25 15:45:59 -0400

    expand imports for dask.distributed

--- dask/distributed.py
+++ dask/distributed.py
@@ -1,4 +1,6 @@
 # flake8: noqa
 from __future__ import absolute_import, division, print_function
 
-from distributed import Executor, progress, as_completed, wait, LocalCluster
+from distributed import (Client, Executor, progress, as_completed, wait,
+        LocalCluster, Scheduler, Worker, nanny, local_client, config,
+        CompatibleExecutor)
