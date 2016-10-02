df849e54ae1110811d66bba2c9029de46d0b0a8f
sinhrks <sinhrks@gmail.com>
2016-10-02 10:08:11 +0900

    Add DataFrame impl

--- dask/dataframe/reshape.py
+++ dask/dataframe/reshape.py
@@ -2,17 +2,19 @@ from __future__ import absolute_import, division, print_function
 
 import pandas as pd
 
-from .core import Series, map_partitions
+from .core import Series, DataFrame, map_partitions
+from .categorical import is_categorical_dtype, _get_categorical_columns
 
 
 def get_dummies(data, prefix=None, prefix_sep='_', dummy_na=False,
                 columns=None, sparse=False, drop_first=False):
     """
-    Convert categorical variable into dummy/indicator variables
+    Convert categorical variable into dummy/indicator variables. data must
+    have category dtype to infer result's column name
 
     Parameters
     ----------
-    data : Series with category dtype
+    data : Series or DataFrame with category dtype
     prefix : string, list of strings, or dict of strings, default None
         String to append DataFrame column names
         Pass a list with length equal to the number of columns
@@ -31,14 +33,20 @@ def get_dummies(data, prefix=None, prefix_sep='_', dummy_na=False,
     dummies : DataFrame
     """
 
-    if not isinstance(data, Series):
-        raise ValueError('data must be dd.Series')
-    if not pd.core.common.is_categorical_dtype(data):
-        raise ValueError('data must be category dtype')
+    if isinstance(data, (pd.Series, pd.DataFrame)):
+        return pd.get_dummies(data, prefix=prefix,
+                              prefix_sep=prefix_sep, dummy_na=dummy_na,
+                              columns=columns, sparse=sparse,
+                              drop_first=drop_first)
 
-    if columns is not None:
-        # columns kw is to handle DataFrame
-        raise NotImplementedError('columns keyword is not supported')
+    if isinstance(data, Series) and not is_categorical_dtype(data):
+        raise ValueError('data must have category dtype')
+    elif isinstance(data, DataFrame):
+        if columns is None:
+            columns = _get_categorical_columns(data)
+        else:
+            if not all(is_categorical_dtype(data[c]) for c in columns):
+                raise ValueError('target columns must have category dtype')
 
     if sparse:
         raise NotImplementedError('sparse=True is not supported')
