417a5f4fb7e9efb8a41f71c80bd94ea7949865a9
Jim Crist <crist042@umn.edu>
2016-11-04 14:21:27 -0400

    Add `dask.__git_revision__` (#1760)
    Contains full git-hash, matches convention in numpy. Fixes #1533.

--- dask/__init__.py
+++ dask/__init__.py
@@ -13,5 +13,7 @@ except ImportError:
     pass
 
 from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
