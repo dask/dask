a22650d7cb922619b2401c32fb71d33a613c9970
Antoine Pitrou <antoine@python.org>
2016-11-15 13:28:19 +0100

    Add a large traffic jam test

--- distributed/tests/test_batched.py
+++ distributed/tests/test_batched.py
@@ -197,11 +197,12 @@ def test_stress():
         stream.close()
 
 
-@gen_test()
-def test_sending_traffic_jam():
+@gen.coroutine
+def _run_traffic_jam(nsends, nbytes):
+    # This test eats `nsends * nbytes` bytes in RAM
     np = pytest.importorskip('numpy')
     from distributed.protocol import to_serialize
-    data = bytes(np.random.randint(0, 255, size=(300000,)).astype('u1').data)
+    data = bytes(np.random.randint(0, 255, size=(nbytes,)).astype('u1').data)
     with echo_server() as e:
         client = TCPClient()
         stream = yield client.connect('127.0.0.1', e.port)
@@ -209,27 +210,37 @@ def test_sending_traffic_jam():
         b = BatchedSend(interval=0.01)
         b.start(stream)
 
-        n = 50
-
         msg = {'x': to_serialize(data)}
-        for i in range(n):
+        for i in range(nsends):
             b.send(assoc(msg, 'i', i))
-            yield gen.sleep(0.001)
+            if np.random.random() > 0.5:
+                yield gen.sleep(0.001)
 
         results = []
         count = 0
-        while len(results) < n:
+        while len(results) < nsends:
             # If this times out then I think it's a backpressure issue
             # Somehow we're able to flood the socket so that the receiving end
             # loses some of our messages
             L = yield gen.with_timeout(timedelta(seconds=5), read(stream))
             count += 1
-            results.extend(L)
+            results.extend(r['i'] for r in L)
 
         assert count == b.batch_count == e.count
-        assert b.message_count == n
+        assert b.message_count == nsends
 
-        assert [r['i'] for r in results] == list(range(50))
+        assert results == list(range(nsends))
 
         stream.close()  # external closing
         yield b.close(ignore_closed=True)
+
+
+@gen_test()
+def test_sending_traffic_jam():
+    yield _run_traffic_jam(50, 300000)
+
+
+@slow
+@gen_test()
+def test_large_traffic_jam():
+    yield _run_traffic_jam(500, 1500000)
