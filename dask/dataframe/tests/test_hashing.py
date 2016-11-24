3f24308d1b1ddb34b37c5555d6c10bc91fccd938
Matthew Rocklin <mrocklin@gmail.com>
2016-11-24 08:57:34 -0500

    extend hashing tests

--- dask/dataframe/tests/test_hashing.py
+++ dask/dataframe/tests/test_hashing.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+import pytest
+
 from dask.dataframe.hashing import hash_array, hash_pandas_object
 
 df = pd.DataFrame({'i32': np.array([1, 2, 3] * 3, dtype='int32'),
@@ -19,5 +21,21 @@ def test_hash_array():
         np.testing.assert_equal(hash_array(a), hash_array(a))
 
 
-def test_hash_pandas_object():
-    np.testing.assert_equal(hash_pandas_object(df), hash_pandas_object(df))
+@pytest.mark.parametrize('obj', [
+    pd.Series([1, 2, 3]),
+    pd.Series([1.0, 1.5, 3.2]),
+    pd.Series([1.0, 1.5, 3.2], index=[1.5, 1.1, 3.3]),
+    pd.Series(['a', 'b', 'c']),
+    pd.Series([True, False, True]),
+    pd.Index([1, 2, 3]),
+    pd.Index([True, False, True]),
+    pd.DataFrame({'x': ['a', 'b', 'c'], 'y': [1, 2, 3]}),
+    pd.util.testing.makeMissingDataframe(),
+    pd.util.testing.makeMixedDataFrame(),
+    pd.util.testing.makeTimeDataFrame(),
+    pd.util.testing.makeTimeSeries(),
+    pd.util.testing.makeTimedeltaIndex()])
+def test_hash_pandas_object(obj):
+    a = hash_pandas_object(obj)
+    b = hash_pandas_object(obj)
+    np.testing.assert_equal(a, b)
