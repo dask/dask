f0fff5b239ab0991671efd675600b88b5346017a
Matthew Rocklin <mrocklin@gmail.com>
2016-11-28 22:53:14 -0500

    add split_every tests

--- dask/dataframe/tests/test_hyperloglog.py
+++ dask/dataframe/tests/test_hyperloglog.py
@@ -49,7 +49,17 @@ def test_basic(df, npartitions):
     assert abs(approx - exact) <= 2 or abs(approx - exact) / exact < 0.05
 
 
-@pytest.mark.xfail
+@pytest.mark.parametrize('split_every', [None, 2, 10])
+@pytest.mark.parametrize('npartitions', [2, 20])
+def test_split_every(split_every, npartitions):
+    df = pd.Series([1, 2, 3] * 1000)
+    ddf = dd.from_pandas(df, npartitions=npartitions)
+
+    approx = ddf.nunique_approx(split_every=split_every).compute(get=dask.async.get_sync)
+    exact = len(df.drop_duplicates())
+    assert abs(approx - exact) <= 2 or abs(approx - exact) / exact < 0.05
+
+
 def test_with_complex():
     df = pd.DataFrame({
         'a': [1, 2, 3] * 3,
