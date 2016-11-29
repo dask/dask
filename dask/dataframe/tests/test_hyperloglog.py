570d8e65018300fe0d0dd76a5091efcb1c6c792c
Mike Graham <mikegraham@gmail.com>
2016-11-29 10:12:05 -0500

    Remove stale comment and move complex test in with the other, normal tests. (Was previously separate because it was an xfail.)

--- dask/dataframe/tests/test_hyperloglog.py
+++ dask/dataframe/tests/test_hyperloglog.py
@@ -35,6 +35,11 @@ rs = np.random.RandomState(96)
         'z': np.zeros((20000,))}),
     pd.DataFrame({'x': [1, 2, 3] * 1000}),
     pd.DataFrame({'x': np.random.random(1000)}),
+    pd.DataFrame({
+        'a': [1, 2, 3] * 3,
+        'b': [1.2, 3.4, 5.6] * 3,
+        'c': [1 + 2j, 3 + 4j, 5 + 6j] * 3,
+        'd': -np.arange(9, dtype=np.int8)}),
     pd.Series([1, 2, 3] * 1000),
     pd.Series(np.random.random(1000)),
     pd.Series(np.random.random(1000), index=np.ones(1000)),
@@ -58,15 +63,3 @@ def test_split_every(split_every, npartitions):
     approx = ddf.nunique_approx(split_every=split_every).compute(get=dask.async.get_sync)
     exact = len(df.drop_duplicates())
     assert abs(approx - exact) <= 2 or abs(approx - exact) / exact < 0.05
-
-
-def test_with_complex():
-    df = pd.DataFrame({
-        'a': [1, 2, 3] * 3,
-        'b': [1.2, 3.4, 5.6] * 3,
-        'c': [1 + 2j, 3 + 4j, 5 + 6j] * 3, # complex not currently supported
-        'd': -np.arange(9, dtype=np.int8)})
-    ddf = dd.from_pandas(df, 2)
-
-    approx = ddf.nunique_approx().compute()
-    assert abs(approx - len(df.drop_duplicates())) < 0.1
