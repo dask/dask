c7dd9f8e82567ba8a4a7e5fde6843b8e8550f093
Matthew Rocklin <mrocklin@gmail.com>
2016-12-24 00:07:16 -0500

    Use empty index  in time slicing
    Previously pandas would reverse start and stop of a loc query.

--- dask/dataframe/indexing.py
+++ dask/dataframe/indexing.py
@@ -74,7 +74,7 @@ class _LocIndexer(object):
         Convert index-indexer for partial time string slicing
         if obj.index is DatetimeIndex / PeriodIndex
         """
-        iindexer = _maybe_partial_time_string(self.obj._meta_nonempty.index,
+        iindexer = _maybe_partial_time_string(self.obj._meta.index,
                                               iindexer, kind='loc')
         return iindexer
 
@@ -220,4 +220,5 @@ def _maybe_partial_time_string(index, indexer, kind):
         start = index._maybe_cast_slice_bound(indexer, 'left', 'loc')
         stop = index._maybe_cast_slice_bound(indexer, 'right', 'loc')
         return slice(start, stop)
+
     return indexer
