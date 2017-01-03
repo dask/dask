7589b92f4317a4ceb3da2ce920bed7eda77fa8bb
Min RK <benjaminrk@gmail.com>
2017-01-03 18:42:55 +0100

    use extras_require for conditional dependencies (#779)
    * universal wheel
    
    * use extras_require for conditional dependencies
    
    needed for wheels to properly embed conditional dependencies,
    since conditions are discarded on `install_requires`

--- setup.py
+++ setup.py
@@ -6,6 +6,18 @@ import sys
 import versioneer
 
 requires = open('requirements.txt').read().strip().split('\n')
+install_requires = []
+extras_require = {}
+for r in requires:
+    if ';' in r:
+        # requirements.txt conditional dependencies need to be reformatted for wheels
+        # to the form: `'[extra_name]:condition' : ['requirements']`
+        req, cond = r.split(';', 1)
+        cond = ':' + cond
+        cond_reqs = extras_require.setdefault(cond, [])
+        cond_reqs.append(req)
+    else:
+        install_requires.append(r)
 
 setup(name='distributed',
       version=versioneer.get_version(),
@@ -17,7 +29,8 @@ setup(name='distributed',
       license='BSD',
       package_data={ '': ['templates/index.html'], },
       include_package_data=True,
-      install_requires=requires,
+      install_requires=install_requires,
+      extras_require=extras_require,
       packages=['distributed',
                 'distributed.bokeh',
                 'distributed.bokeh.background',
