1a1e21a5abb143640f0dd780538b1adee503f8f5
Jim Crist <crist042@umn.edu>
2017-02-12 10:02:07 -0600

    Deprecate dask.delayed `compute` and `do` (#1976)
    ``dask.delayed.compute`` was removed in #1975, and was made a simple
    alias to ``dask.base.compute``. This deprecates the function found here,
    and redirects users to import from elsewhere.
    
    ``dask.delayed.do`` was renamed to ``dask.delayed.delayed`` in #1085,
    making ``dask.delayed.do`` a simple alias. This PR deprecates that
    alias. Users can always import ``dask.delayed`` as an alias if needed:
    
    ```
    from dask import delayed as do
    ```

--- dask/__init__.py
+++ dask/__init__.py
@@ -4,7 +4,7 @@ from .core import istask
 from .context import set_options
 from .async import get_sync as get
 try:
-    from .delayed import do, delayed, value
+    from .delayed import do, delayed
 except ImportError:
     pass
 try:
