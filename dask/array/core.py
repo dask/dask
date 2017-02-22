dask/dask

commit 5ff6821407df62f27814ea79b3d68e4f75df271a

Structural sharing between dask graphs (#1985)
* Add ShareDict draft implementation

This may eventually be used to create new dask graphs without excessive copies.

* update ShareDicts with ShareDicts

* use ShareDict in dask.array

* Demote ShareDict to Mapping

* flake8

* Remove ordering from ShareDict

* update docstring

* clean up delayed in dask.bag and dask.dataframe

* remove auto-cull in array slicing

* Remove special case for Delayed in base

* support backwards compatibility with Delayed(dsk])

* appease flake8

* rename update to update_with_key

* cleanup docstring and remove sorting

* assert key is None when updating with sharedict

* Ensure dask.array dicts are non-overlapping

* nonone -> non_one

* add disclaimer in docstring

* replace merge with sharedict.merge

dask/array/core.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 from bisect import bisect
-from collections import Iterable, MutableMapping
+from collections import Iterable, MutableMapping, Mapping
 from collections import Iterator
 from functools import partial, wraps
 import inspect
@@ -17,8 +17,9 @@ from threading import Lock
 import uuid
 import warnings
 
+import toolz
 from toolz.curried import (pipe, partition, concat, pluck, join, first, map,
-                           groupby, valmap, accumulate, merge, reduce,
+                           groupby, valmap, accumulate, reduce,
                            interleave, sliding_window, assoc)
 import numpy as np
 
@@ -31,8 +32,9 @@ from ..utils import (homogeneous_deepmap, ndeepmap, ignoring, concrete,
                      SerializableLock)
 from ..compatibility import unicode, long, getargspec, zip_longest, apply
 from ..delayed import to_task_dasks
-from ..optimize import cull
 from .. import threaded, core
+from .. import sharedict
+from ..sharedict import ShareDict
 
 
 def getarray(a, b, lock=None):
@@ -405,10 +407,9 @@ def top(func, output, out_indices, *arrind_pairs, **kwargs):
 
     # Unpack delayed objects in kwargs
     if kwargs:
-        task, dasks = to_task_dasks(kwargs)
-        if dasks:
-            for d in dasks:
-                dsk.update(d)
+        task, dsk2 = to_task_dasks(kwargs)
+        if dsk2:
+            dsk.update(dsk2)
             kwargs2 = task
         else:
             kwargs2 = kwargs
@@ -695,7 +696,8 @@ def map_blocks(func, *args, **kwargs):
 
     chunks = tuple(chunks2)
 
-    return Array(merge(dsk, *[a.dask for a in arrs]), name, chunks, dtype)
+    return Array(sharedict.merge((name, dsk), *[a.dask for a in arrs]),
+                 name, chunks, dtype)
 
 
 def broadcast_chunks(*chunkss):
@@ -748,15 +750,14 @@ def squeeze(a, axis=None):
         axis = tuple(i for i, d in enumerate(a.shape) if d == 1)
     b = a.map_blocks(partial(np.squeeze, axis=axis), dtype=a.dtype)
     chunks = tuple(bd for bd in b.chunks if bd != (1,))
+
+    name = 'squeeze-' + tokenize(a, axis)
     old_keys = list(product([b.name], *[range(len(bd)) for bd in b.chunks]))
-    new_keys = list(product([b.name], *[range(len(bd)) for bd in chunks]))
+    new_keys = list(product([name], *[range(len(bd)) for bd in chunks]))
 
-    dsk = b.dask.copy()
-    for o, n in zip(old_keys, new_keys):
-        dsk[n] = dsk[o]
-        del dsk[o]
+    dsk = {n: b.dask[o] for o, n in zip(old_keys, new_keys)}
 
-    return Array(dsk, b.name, chunks, dtype=a.dtype)
+    return Array(sharedict.merge(b.dask, (name, dsk)), name, chunks, dtype=a.dtype)
 
 
 def topk(k, x):
@@ -788,7 +789,7 @@ def topk(k, x):
                        slice(-1, -k - 1, -1))
     chunks = ((k,),)
 
-    return Array(merge(dsk, x.dask), name2, chunks, dtype=x.dtype)
+    return Array(sharedict.merge((name2, dsk), x.dask), name2, chunks, dtype=x.dtype)
 
 
 def store(sources, targets, lock=True, regions=None, compute=True, **kwargs):
@@ -858,15 +859,15 @@ def store(sources, targets, lock=True, regions=None, compute=True, **kwargs):
 
     updates = [insert_to_ooc(tgt, src, lock=lock, region=reg)
                for tgt, src, reg in zip(targets, sources, regions)]
-    dsk = merge([src.dask for src in sources] + updates)
     keys = [key for u in updates for key in u]
+    name = 'store-' + tokenize(*keys)
+    dsk = sharedict.merge((name, toolz.merge(updates)), *[src.dask for src in sources])
     if compute:
         Array._get(dsk, keys, **kwargs)
     else:
         from ..delayed import Delayed
-        name = 'store-' + tokenize(*keys)
-        dsk[name] = keys
-        return Delayed(name, [dsk])
+        dsk.update({name: keys})
+        return Delayed(name, dict(dsk))
 
 
 def blockdims_from_blockshape(shape, chunks):
@@ -938,6 +939,11 @@ class Array(Base):
     _finalize = staticmethod(finalize)
 
     def __init__(self, dask, name, chunks, dtype, shape=None):
+        assert isinstance(dask, Mapping)
+        if not isinstance(dask, ShareDict):
+            s = ShareDict()
+            s.update_with_key(dask, key=name)
+            dask = s
         self.dask = dask
         self.name = name
         self._chunks = normalize_chunks(chunks, shape)
@@ -1155,7 +1161,7 @@ class Array(Base):
             name = 'cache-' + tokenize(self)
             dsk = dict(((name, k[1:]), (operator.setitem, store, (tuple, list(k)), k))
                        for k in core.flatten(self._keys()))
-            Array._get(merge(dsk, self.dask), list(dsk.keys()), **kwargs)
+            Array._get(sharedict.merge(dsk, self.dask), list(dsk.keys()), **kwargs)
 
             dsk2 = dict((k, (operator.getitem, store, (tuple, list(k))))
                         for k in store)
@@ -1221,12 +1227,7 @@ class Array(Base):
 
         dsk, chunks = slice_array(out, self.name, self.chunks, index)
 
-        if len(dsk) < self.npartitions / 2:  # significant reduction in graph
-            needed = core.get_dependencies(self.dask, task=list(dsk.values()))
-            dsk2, _ = cull(self.dask, needed)
-            dsk2.update(dsk)
-        else:
-            dsk2 = merge(self.dask, dsk)
+        dsk2 = sharedict.merge(self.dask, (out, dsk))
 
         return Array(dsk2, out, chunks, dtype=self.dtype)
 
@@ -1712,7 +1713,7 @@ class Array(Base):
         dask.array.from_delayed
         """
         from ..delayed import Delayed
-        return np.array(ndeepmap(self.ndim, lambda k: Delayed(k, [self.dask]), self._keys()),
+        return np.array(ndeepmap(self.ndim, lambda k: Delayed(k, self.dask), self._keys()),
                         dtype=object)
 
     @wraps(np.repeat)
@@ -1829,7 +1830,8 @@ def from_array(x, chunks, name=None, lock=False, fancy=True):
     if lock is True:
         lock = SerializableLock()
     dsk = getem(original_name, chunks, out_name=name, fancy=fancy, lock=lock)
-    return Array(merge({original_name: x}, dsk), name, chunks, dtype=x.dtype)
+    dsk[original_name] = x
+    return Array(dsk, name, chunks, dtype=x.dtype)
 
 
 def from_delayed(value, shape, dtype, name=None):
@@ -1855,9 +1857,8 @@ def from_delayed(value, shape, dtype, name=None):
         value = delayed(value)
     name = name or 'from-value-' + tokenize(value, shape, dtype)
     dsk = {(name,) + (0,) * len(shape): value.key}
-    dsk.update(value.dask)
     chunks = tuple((d,) for d in shape)
-    return Array(dsk, name, chunks, dtype)
+    return Array(sharedict.merge(value.dask, (name, dsk)), name, chunks, dtype)
 
 
 def from_func(func, shape, dtype=None, name=None, args=(), kwargs={}):
@@ -2146,7 +2147,7 @@ def atop(func, out_ind, *args, **kwargs):
                         "adjust_chunks values must be callable, int, or tuple")
     chunks = tuple(chunks)
 
-    return Array(merge(dsk, *dsks), out, chunks, dtype=dtype)
+    return Array(sharedict.merge((out, dsk), *dsks), out, chunks, dtype=dtype)
 
 
 def unpack_singleton(x):
@@ -2226,7 +2227,7 @@ def stack(seq, axis=0):
               for inp in inputs]
 
     dsk = dict(zip(keys, values))
-    dsk2 = merge(dsk, *[a.dask for a in seq])
+    dsk2 = sharedict.merge((name, dsk), *[a.dask for a in seq])
 
     dt = reduce(np.promote_types, [a.dtype for a in seq])
 
@@ -2300,7 +2301,7 @@ def concatenate(seq, axis=0):
               key[axis + 2:] for key in keys]
 
     dsk = dict(zip(keys, values))
-    dsk2 = merge(dsk, * [a.dask for a in seq])
+    dsk2 = sharedict.merge((name, dsk), * [a.dask for a in seq])
 
     return Array(dsk2, name, chunks, dtype=dt)
 
@@ -2710,7 +2711,7 @@ def coarsen(reduction, x, axes, trim_excess=False):
                    for i, bds in enumerate(x.chunks))
 
     dt = reduction(np.empty((1,) * x.ndim, dtype=x.dtype)).dtype
-    return Array(merge(x.dask, dsk), name, chunks, dtype=dt)
+    return Array(sharedict.merge(x.dask, (name, dsk)), name, chunks, dtype=dt)
 
 
 def split_at_breaks(array, breaks, axis=0):
@@ -2795,7 +2796,7 @@ def broadcast_to(x, shape):
                 (chunk.broadcast_to, key, shape[:ndim_new] +
                  tuple(bd[i] for i, bd in zip(key[1:], chunks[ndim_new:]))))
                for key in core.flatten(x._keys()))
-    return Array(merge(dsk, x.dask), name, chunks, dtype=x.dtype)
+    return Array(sharedict.merge((name, dsk), x.dask), name, chunks, dtype=x.dtype)
 
 
 @wraps(np.ravel)
@@ -2883,7 +2884,8 @@ def reshape(array, shape):
                      shape[prev_index_count:])
         dsk[new_key] = (np.reshape, key, new_shape)
 
-    return Array(merge(dsk, array.dask), name, chunks, dtype=array.dtype)
+    return Array(sharedict.merge((name, dsk), array.dask), name, chunks,
+                 dtype=array.dtype)
 
 
 def offset_func(func, offset, *args):
@@ -2928,7 +2930,7 @@ def fromfunction(func, chunks=None, shape=None, dtype=None):
 def unique(x):
     name = 'unique-' + x.name
     dsk = dict(((name, i), (np.unique, key)) for i, key in enumerate(x._keys()))
-    parts = Array._get(merge(dsk, x.dask), list(dsk.keys()))
+    parts = Array._get(sharedict.merge((name, dsk), x.dask), list(dsk.keys()))
     return np.unique(np.concatenate(parts))
 
 
@@ -3048,12 +3050,13 @@ def histogram(a, bins=None, range=None, normed=False, weights=None, density=None
         w_keys = core.flatten(weights._keys())
         dsk = dict(((name, i, 0), (block_hist, k, w))
                    for i, (k, w) in enumerate(zip(a_keys, w_keys)))
-        dsk.update(weights.dask)
         dtype = weights.dtype
 
-    dsk.update(a.dask)
+    all_dsk = sharedict.merge(a.dask, (name, dsk))
+    if weights is not None:
+        all_dsk.update(weights.dask)
 
-    mapped = Array(dsk, name, chunks, dtype=dtype)
+    mapped = Array(all_dsk, name, chunks, dtype=dtype)
     n = mapped.sum(axis=0)
 
     # We need to replicate normed and density options from numpy
@@ -3144,14 +3147,13 @@ def diag(v):
         if v.chunks[0] == v.chunks[1]:
             dsk = dict(((name, i), (np.diag, row[i])) for (i, row)
                        in enumerate(v._keys()))
-            dsk.update(v.dask)
-            return Array(dsk, name, (v.chunks[0],), dtype=v.dtype)
+            return Array(sharedict.merge(v.dask, (name, dsk)), name, (v.chunks[0],), dtype=v.dtype)
         else:
             raise NotImplementedError("Extracting diagonals from non-square "
                                       "chunked arrays")
     chunks_1d = v.chunks[0]
     blocks = v._keys()
-    dsk = v.dask.copy()
+    dsk = {}
     for i, m in enumerate(chunks_1d):
         for j, n in enumerate(chunks_1d):
             key = (name, i, j)
@@ -3159,7 +3161,9 @@ def diag(v):
                 dsk[key] = (np.diag, blocks[i])
             else:
                 dsk[key] = (np.zeros, (m, n))
-    return Array(dsk, name, (chunks_1d, chunks_1d), dtype=v.dtype)
+
+    return Array(sharedict.merge(v.dask, (name, dsk)), name, (chunks_1d, chunks_1d),
+                 dtype=v.dtype)
 
 
 def triu(m, k=0):
@@ -3555,7 +3559,10 @@ def _vindex(x, *indexes):
     chunks.insert(0, (len(points),) if points else ())
     chunks = tuple(chunks)
 
-    return Array(merge(x.dask, dsk, dsk2), 'vindex-merge-' + token, chunks, x.dtype)
+    name = 'vindex-merge-' + token
+    dsk.update(dsk2)
+
+    return Array(sharedict.merge(x.dask, (name, dsk)), name, chunks, x.dtype)
 
 
 def _get_axis(indexes):
@@ -3743,7 +3750,7 @@ def to_npy_stack(dirname, x, axis=0):
     dsk = dict(((name, i), (np.save, os.path.join(dirname, '%d.npy' % i), key))
                for i, key in enumerate(core.flatten(xx._keys())))
 
-    Array._get(merge(dsk, xx.dask), list(dsk))
+    Array._get(sharedict.merge(dsk, xx.dask), list(dsk))
 
 
 def from_npy_stack(dirname, mmap_mode='r'):
@@ -3849,4 +3856,4 @@ def slice_with_dask_array(x, index):
            for i, k in enumerate(core.flatten(y._keys()))}
     chunks = ((np.nan,) * y.npartitions,)
 
-    return Array(merge(y.dask, dsk), name, chunks, x.dtype)
+    return Array(sharedict.merge(y.dask, (name, dsk)), name, chunks, x.dtype)

dask/delayed.py
@@ -6,12 +6,13 @@ import operator
 import uuid
 import warnings
 
-from toolz import merge, unique, curry, first
+from toolz import unique, curry, first
 
 from . import base, threaded
 from .compatibility import apply
 from .core import quote
 from .utils import concrete, funcname, methodcaller
+from . import sharedict
 
 __all__ = ['Delayed', 'delayed']
 
@@ -65,28 +66,28 @@ def to_task_dasks(expr):
     [{'a': 1}, {'b': 2}]
     """
     if isinstance(expr, Delayed):
-        return expr.key, expr._dasks
+        return expr.key, expr.dask
     if isinstance(expr, base.Base):
         name = 'finalize-' + tokenize(expr, pure=True)
         keys = expr._keys()
-        dsk = expr._optimize(expr.dask, keys)
+        dsk = expr._optimize(dict(expr.dask), keys)
         dsk[name] = (expr._finalize, (concrete, keys))
-        return name, [dsk]
+        return name, dsk
     if isinstance(expr, tuple) and type(expr) != tuple:
-        return expr, []
+        return expr, {}
     if isinstance(expr, (Iterator, list, tuple, set)):
         args, dasks = unzip((to_task_dasks(e) for e in expr), 2)
         args = list(args)
-        dasks = flat_unique(dasks)
+        dsk = sharedict.merge(*dasks)
         # Ensure output type matches input type
         if isinstance(expr, (tuple, set)):
-            return (type(expr), args), dasks
+            return (type(expr), args), dsk
         else:
-            return args, dasks
+            return args, dsk
     if isinstance(expr, dict):
-        args, dasks = to_task_dasks([[k, v] for k, v in expr.items()])
-        return (dict, args), dasks
-    return expr, []
+        args, dsk = to_task_dasks([[k, v] for k, v in expr.items()])
+        return (dict, args), dsk
+    return expr, {}
 
 
 def tokenize(*args, **kwargs):
@@ -255,10 +256,10 @@ def delayed(obj, name=None, pure=False, nout=None, traverse=True):
         return obj
 
     if isinstance(obj, base.Base) or traverse:
-        task, dasks = to_task_dasks(obj)
+        task, dsk = to_task_dasks(obj)
     else:
         task = quote(obj)
-        dasks = []
+        dsk = sharedict.ShareDict()
 
     if task is obj:
         if not (nout is None or (type(nout) is int and nout >= 0)):
@@ -275,8 +276,8 @@ def delayed(obj, name=None, pure=False, nout=None, traverse=True):
     else:
         if not name:
             name = '%s-%s' % (type(obj).__name__, tokenize(task, pure=pure))
-        dasks.append({name: task})
-        return Delayed(name, dasks)
+        dsk.update_with_key({name: task}, key=name)
+        return Delayed(name, dsk)
 
 
 do = delayed
@@ -308,14 +309,16 @@ class Delayed(base.Base):
 
     Equivalent to the output from a single key in a dask graph.
     """
-    __slots__ = ('_key', '_dasks', '_length')
+    __slots__ = ('_key', 'dask', '_length')
     _finalize = staticmethod(first)
     _default_get = staticmethod(threaded.get)
     _optimize = staticmethod(lambda d, k, **kwds: d)
 
-    def __init__(self, key, dasks, length=None):
+    def __init__(self, key, dsk, length=None):
         self._key = key
-        self._dasks = dasks
+        if type(dsk) is list:  # compatibility with older versions
+            dsk = sharedict.merge(*dsk)
+        self.dask = dsk
         self._length = length
 
     def __getstate__(self):
@@ -324,10 +327,6 @@ class Delayed(base.Base):
     def __setstate__(self, state):
         for k, v in zip(self.__slots__, state):
             setattr(self, k, v)
-
-    @property
-    def dask(self):
-        return merge(*self._dasks)
 
     @property
     def key(self):
@@ -405,16 +404,16 @@ def call_function(func, func_token, args, kwargs, pure=False, nout=None):
 
     args, dasks = unzip(map(to_task_dasks, args), 2)
     if kwargs:
-        dask_kwargs, dasks2 = to_task_dasks(kwargs)
-        dasks = dasks + (dasks2,)
+        dask_kwargs, dsk2 = to_task_dasks(kwargs)
+        dasks = dasks + (dsk2,)
         task = (apply, func, list(args), dask_kwargs)
     else:
         task = (func,) + args
 
-    dasks = flat_unique(dasks)
-    dasks.append({name: task})
+    dsk = sharedict.merge(*dasks)
+    dsk.update_with_key({name: task}, key=name)
     nout = nout if nout is not None else None
-    return Delayed(name, dasks, length=nout)
+    return Delayed(name, dsk, length=nout)
 
 
 class DelayedLeaf(Delayed):
@@ -430,10 +429,6 @@ class DelayedLeaf(Delayed):
     def dask(self):
         return {self._key: self._obj}
 
-    @property
-    def _dasks(self):
-        return [self.dask]
-
     def __call__(self, *args, **kwargs):
         return call_function(self._obj, self._key, args, kwargs,
                              pure=self._pure, nout=self._nout)
@@ -448,8 +443,9 @@ class DelayedAttr(Delayed):
         self._key = key
 
     @property
-    def _dasks(self):
-        return [{self._key: (getattr, self._obj._key, self._attr)}] + self._obj._dasks
+    def dask(self):
+        dsk = {self._key: (getattr, self._obj._key, self._attr)}
+        return sharedict.merge(self._obj.dask, (self._key, dsk))
 
     def __call__(self, *args, **kwargs):
         return call_function(methodcaller(self._attr), self._attr, (self._obj,) + args, kwargs)

dask/tests/test_delayed.py
@@ -10,6 +10,7 @@ from dask.compatibility import PY2, PY3
 from dask.delayed import delayed, to_task_dasks, compute, Delayed
 
 
+@pytest.mark.skip
 def test_to_task_dasks():
     a = delayed(1, name='a')
     b = delayed(2, name='b')
@@ -273,10 +274,9 @@ def test_array_delayed():
     assert val.sum().compute() == (arr + arr + 1).sum()
     assert val[0, 0].compute() == (arr + arr + 1)[0, 0]
 
-    task, dasks = to_task_dasks(darr)
-    assert len(dasks) == 1
+    task, dsk = to_task_dasks(darr)
     orig = set(darr.dask)
-    final = set(dasks[0])
+    final = set(dsk)
     assert orig.issubset(final)
     diff = final.difference(orig)
     assert len(diff) == 1

dask/bag/core.py
@@ -329,7 +329,7 @@ class Item(Base):
         Returns a single value.
         """
         from dask.delayed import Delayed
-        return Delayed(self.key, [self.dask])
+        return Delayed(self.key, self.dask)
 
 
 class Bag(Base):
@@ -1113,7 +1113,7 @@ class Bag(Base):
         Returns list of Delayed, one per partition.
         """
         from dask.delayed import Delayed
-        return [Delayed(k, [self.dask]) for k in self._keys()]
+        return [Delayed(k, self.dask) for k in self._keys()]
 
     def repartition(self, npartitions):
         """ Coalesce bag into fewer partitions

dask/dataframe/core.py
@@ -3742,7 +3742,7 @@ def to_delayed(df):
     >>> partitions = df.to_delayed()  # doctest: +SKIP
     """
     from ..delayed import Delayed
-    return [Delayed(k, [df.dask]) for k in df._keys()]
+    return [Delayed(k, df.dask) for k in df._keys()]
 
 
 def _repr_data_series(s, index):

commit 52799c5fd5efa0cc523a947c04fb231ca521c72f

delayed(dask_object) -> Delayed
This used to work, but a regression was introduced in #1985 where the
graph was mistakenly assumed to be a `sharedict.ShareDict`. This PR
fixes this, along with some general cleanups:

- Rename `to_task_dasks` to `to_task_dask`
- Update docstring to be consistent with new behavior introduced in
  #1985
- Fix bug when calling `delayed` on a Base object, and add test for it
- Unskip test for `to_task_dask`, and update to pass

dask/array/core.py
@@ -31,7 +31,7 @@ from ..utils import (homogeneous_deepmap, ndeepmap, ignoring, concrete,
                      is_integer, IndexCallable, funcname, derived_from,
                      SerializableLock)
 from ..compatibility import unicode, long, getargspec, zip_longest, apply
-from ..delayed import to_task_dasks
+from ..delayed import to_task_dask
 from .. import threaded, core
 from .. import sharedict
 from ..sharedict import ShareDict
@@ -407,7 +407,7 @@ def top(func, output, out_indices, *arrind_pairs, **kwargs):
 
     # Unpack delayed objects in kwargs
     if kwargs:
-        task, dsk2 = to_task_dasks(kwargs)
+        task, dsk2 = to_task_dask(kwargs)
         if dsk2:
             dsk.update(dsk2)
             kwargs2 = task

dask/delayed.py
@@ -30,12 +30,12 @@ def unzip(ls, nout):
     return out
 
 
-def to_task_dasks(expr):
-    """Normalize a python object and extract all sub-dasks.
+def to_task_dask(expr):
+    """Normalize a python object and merge all sub-graphs.
 
     - Replace ``Delayed`` with their keys
     - Convert literals to things the schedulers can handle
-    - Extract dasks from all enclosed values
+    - Extract dask graphs from all enclosed values
 
     Parameters
     ----------
@@ -46,24 +46,23 @@ def to_task_dasks(expr):
     Returns
     -------
     task : normalized task to be run
-    dasks : list of dasks that form the dag for this task
+    dask : a merged dask graph that forms the dag for this task
 
     Examples
     --------
-
     >>> a = delayed(1, 'a')
     >>> b = delayed(2, 'b')
-    >>> task, dasks = to_task_dasks([a, b, 3])
-    >>> task # doctest: +SKIP
+    >>> task, dask = to_task_dask([a, b, 3])
+    >>> task  # doctest: +SKIP
     ['a', 'b', 3]
-    >>> dasks # doctest: +SKIP
-    [{'a': 1}, {'b': 2}]
+    >>> dict(dask)  # doctest: +SKIP
+    {'a': 1, 'b': 2}
 
-    >>> task, dasks = to_task_dasks({a: 1, b: 2})
-    >>> task # doctest: +SKIP
+    >>> task, dasks = to_task_dask({a: 1, b: 2})
+    >>> task  # doctest: +SKIP
     (dict, [['a', 1], ['b', 2]])
-    >>> dasks # doctest: +SKIP
-    [{'a': 1}, {'b': 2}]
+    >>> dict(dask)  # doctest: +SKIP
+    {'a': 1, 'b': 2}
     """
     if isinstance(expr, Delayed):
         return expr.key, expr.dask
@@ -76,7 +75,7 @@ def to_task_dasks(expr):
     if isinstance(expr, tuple) and type(expr) != tuple:
         return expr, {}
     if isinstance(expr, (Iterator, list, tuple, set)):
-        args, dasks = unzip((to_task_dasks(e) for e in expr), 2)
+        args, dasks = unzip((to_task_dask(e) for e in expr), 2)
         args = list(args)
         dsk = sharedict.merge(*dasks)
         # Ensure output type matches input type
@@ -85,7 +84,7 @@ def to_task_dasks(expr):
         else:
             return args, dsk
     if isinstance(expr, dict):
-        args, dsk = to_task_dasks([[k, v] for k, v in expr.items()])
+        args, dsk = to_task_dask([[k, v] for k, v in expr.items()])
         return (dict, args), dsk
     return expr, {}
 
@@ -256,10 +255,10 @@ def delayed(obj, name=None, pure=False, nout=None, traverse=True):
         return obj
 
     if isinstance(obj, base.Base) or traverse:
-        task, dsk = to_task_dasks(obj)
+        task, dsk = to_task_dask(obj)
     else:
         task = quote(obj)
-        dsk = sharedict.ShareDict()
+        dsk = {}
 
     if task is obj:
         if not (nout is None or (type(nout) is int and nout >= 0)):
@@ -276,11 +275,8 @@ def delayed(obj, name=None, pure=False, nout=None, traverse=True):
     else:
         if not name:
             name = '%s-%s' % (type(obj).__name__, tokenize(task, pure=pure))
-        dsk.update_with_key({name: task}, key=name)
+        dsk = sharedict.merge(dsk, (name, {name: task}))
         return Delayed(name, dsk)
-
-
-do = delayed
 
 
 def do(*args, **kwargs):
@@ -402,15 +398,15 @@ def call_function(func, func_token, args, kwargs, pure=False, nout=None):
     else:
         name = dask_key_name
 
-    args, dasks = unzip(map(to_task_dasks, args), 2)
+    args, dasks = unzip(map(to_task_dask, args), 2)
+    dsk = sharedict.merge(*dasks)
     if kwargs:
-        dask_kwargs, dsk2 = to_task_dasks(kwargs)
-        dasks = dasks + (dsk2,)
+        dask_kwargs, dsk2 = to_task_dask(kwargs)
+        dsk.update(dsk2)
         task = (apply, func, list(args), dask_kwargs)
     else:
         task = (func,) + args
 
-    dsk = sharedict.merge(*dasks)
     dsk.update_with_key({name: task}, key=name)
     nout = nout if nout is not None else None
     return Delayed(name, dsk, length=nout)

dask/diagnostics/tests/test_profiler.py
@@ -144,8 +144,8 @@ def test_cache_profiler():
 @pytest.mark.skipif("not bokeh")
 def test_unquote():
     from dask.diagnostics.profile_visualize import unquote
-    from dask.delayed import to_task_dasks
-    f = lambda x: to_task_dasks(x)[0]
+    from dask.delayed import to_task_dask
+    f = lambda x: to_task_dask(x)[0]
     t = {'a': 1, 'b': 2, 'c': 3}
     assert unquote(f(t)) == t
     t = {'a': [1, 2, 3], 'b': 2, 'c': 3}

dask/tests/test_delayed.py
@@ -3,41 +3,33 @@ from operator import add, setitem
 import pickle
 from random import random
 
-from toolz import identity, partial
+from toolz import identity, partial, merge
 import pytest
 
 from dask.compatibility import PY2, PY3
-from dask.delayed import delayed, to_task_dasks, compute, Delayed
+from dask.delayed import delayed, to_task_dask, compute, Delayed
 
 
-@pytest.mark.skip
-def test_to_task_dasks():
+def test_to_task_dask():
     a = delayed(1, name='a')
     b = delayed(2, name='b')
-    task, dasks = to_task_dasks([a, b, 3])
+    task, dask = to_task_dask([a, b, 3])
     assert task == ['a', 'b', 3]
-    assert len(dasks) == 2
-    assert a.dask in dasks
-    assert b.dask in dasks
 
-    task, dasks = to_task_dasks((a, b, 3))
+    task, dask = to_task_dask((a, b, 3))
     assert task == (tuple, ['a', 'b', 3])
-    assert len(dasks) == 2
-    assert a.dask in dasks
-    assert b.dask in dasks
+    assert dict(dask) == merge(a.dask, b.dask)
 
-    task, dasks = to_task_dasks({a: 1, b: 2})
+    task, dask = to_task_dask({a: 1, b: 2})
     assert (task == (dict, [['b', 2], ['a', 1]]) or
             task == (dict, [['a', 1], ['b', 2]]))
-    assert len(dasks) == 2
-    assert a.dask in dasks
-    assert b.dask in dasks
+    assert dict(dask) == merge(a.dask, b.dask)
 
     f = namedtuple('f', ['x', 'y'])
     x = f(1, 2)
-    task, dasks = to_task_dasks(x)
+    task, dask = to_task_dask(x)
     assert task == x
-    assert dasks == []
+    assert dict(dask) == {}
 
 
 def test_delayed():
@@ -274,12 +266,15 @@ def test_array_delayed():
     assert val.sum().compute() == (arr + arr + 1).sum()
     assert val[0, 0].compute() == (arr + arr + 1)[0, 0]
 
-    task, dsk = to_task_dasks(darr)
+    task, dsk = to_task_dask(darr)
     orig = set(darr.dask)
     final = set(dsk)
     assert orig.issubset(final)
     diff = final.difference(orig)
     assert len(diff) == 1
+
+    delayed_arr = delayed(darr)
+    assert (delayed_arr.compute() == arr).all()
 
 
 def test_array_bag_delayed():