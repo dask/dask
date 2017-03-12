bfd3a5f349f798fec1cf07b3e59ecdefefd44725
Martin Durant <martin.durant@utoronto.ca>
2017-03-12 14:51:55 -0400

    Now with SQLalchemy
    The code looks a little less tidy, and I don't like the isinstance(, str), but
    seems to work well for a range of cases.

--- dask/dataframe/io/sql.py
+++ dask/dataframe/io/sql.py
@@ -1,5 +1,8 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+import sqlalchemy as sa
+from sqlalchemy import sql
+
 from dask import delayed
 from dask.dataframe import from_delayed
 
@@ -20,12 +23,14 @@ def read_sql_table(table, uri, partitions, index_col, limits=None, columns=None,
     index_col : string
         Column which becomes the index, and defines the partitioning. Should
         be a indexed column in the SQL server, and numerical.
-        Could be a function to return a value, e.g., ROW_NUMBER().
+        Could be a function to return a value, e.g., ``sqlalchemy.func.row_number``, where the function must
+        be something understood by thr DB backend.
     limits: 2-tuple or None
-        Manually give upper and lower range of values; if None, first fetched max/min from the DB. Upper limit is
-        non-inclusive.
+        Manually give upper and lower range of values; if None, first fetches max/min from the DB. Upper limit, if
+         given, isnon-inclusive.
     columns : list of strings or None
-        Which columns to select; if None, gets all
+        Which columns to select; if None, gets all; can include sqlalchemy functions, e.g.,
+        ``sql.func.abs(sql.column('value'))``
     kwargs : dict
         Additional parameters to pass to `pd.read_sql()`
 
@@ -35,26 +40,39 @@ def read_sql_table(table, uri, partitions, index_col, limits=None, columns=None,
     """
     if index_col is None:
         raise ValueError("Must specify index column to partition on")
+    engine = sa.create_engine(uri)
+    meta = sa.MetaData()
+    table = sa.Table(table, meta, autoload=True, autoload_with=engine)
+    index = table.columns[index_col] if isinstance(index_col, str) else index_col
     if isinstance(partitions, int):
         if limits is None:
-            maxi, mini = pd.read_sql('select max({col}), min({col}) from {table}'.format(
-                col=index_col, table=table), uri).iloc[0]
-            partitions = np.arange(mini, maxi, (maxi - mini) / partitions).tolist() + [maxi + 1]
+            # calculate max and min for given index
+            q = sql.select([sql.func.max(index), sql.func.min(index)]).select_from(table)
+            minmax = pd.read_sql(q, engine)
+            maxi, mini = minmax.iloc[0]
+            if minmax.dtypes['max_1'].kind == "M":
+                partitions = pd.date_range(start=mini, end=maxi,
+                                           freq='%iS' % ((maxi - mini) / partitions).total_seconds()).tolist()
+            else:
+                partitions = np.linspace(mini, maxi, partitions + 1).tolist()
+                partitions[-1] += 1
         else:
             mini, maxi = limits
-            partitions = np.arange(mini, maxi, (maxi - mini) / partitions).tolist() + [maxi]
-    if columns and index_col not in columns:
-        columns.append(index_col)
-    columns = ", ".join(['"{}"'.format(c) for c in columns]) if columns else "*"
+            partitions = np.linspace(mini, maxi, partitions + 1).tolist()
+    columns = [(table.columns[c] if isinstance(c, str) else c) for c in columns] if columns else list(table.columns)
+    if index_col not in columns:
+        columns.append(table.columns[index_col] if isinstance(index_col, str) else index_col)
+
+    if isinstance(index_col, str):
+        kwargs['index_col'] = index_col
+    else:
+        # function names get pandas auto-named
+        kwargs['index_col'] = index_col.name + '_1'
     parts = []
-    kwargs['index_col'] = index_col
     lowers, uppers = partitions[:-1], partitions[1:]
     for lower, upper in zip(lowers, uppers):
-        q = """
-            SELECT {columns} FROM {table}
-            WHERE {index_col} >= {lower} AND {index_col} < {upper};
-            """.format(columns=columns, table=table, index_col=index_col, lower=lower, upper=upper)
-        parts.append(delayed(pd.read_sql_query)(q, uri, **kwargs))
-    head = pd.read_sql_query("""SELECT {columns} FROM {table} LIMIT 5;""".format(columns=columns, table=table),
-                             uri, **kwargs)
+        q = sql.select(columns).where(sql.and_(index >= lower, index < upper)).select_from(table)
+        parts.append(delayed(pd.read_sql)(q, engine, **kwargs))
+    q = sql.select(columns).limit(5).select_from(table)
+    head = pd.read_sql(q, engine, **kwargs)
     return from_delayed(parts, head, divisions=partitions)
--- dask/dataframe/io/tests/test_sql.py
+++ dask/dataframe/io/tests/test_sql.py
@@ -11,14 +11,14 @@ pytest.importorskip('sqlite3')
 
 
 data = """
-name,number,age
-Alice,0,33
-Bob,1,40
-Chris,2,22
-Dora,3,16
-Edith,4,53
-Francis,5,30
-Garreth,6,20
+name,number,age,negish
+Alice,0,33,-5
+Bob,1,40,-3
+Chris,2,22,3
+Dora,3,16,5
+Edith,4,53,0
+Francis,5,30,0
+Garreth,6,20,0
 """
 
 df = pd.read_csv(io.StringIO(data), index_col='number')
@@ -59,3 +59,28 @@ def test_range(db):
     data = read_sql_table('test', db, partitions=2, index_col='number', limits=[1, 4])
     assert data.index.min().compute() == 1
     assert data.index.max().compute() == 3
+
+
+def test_datetimes():
+    import datetime
+    now = datetime.datetime.now()
+    d = datetime.timedelta(seconds=1)
+    df = pd.DataFrame({'a': list('ghjkl'), 'b': [now + i*d for i in range(2, -3, -1)]})
+    with tmpfile() as f:
+        uri = 'sqlite:///%s' % f
+        df.to_sql('test', uri, index=True, if_exists='replace')
+        data = read_sql_table('test', uri, partitions=2, index_col='b')
+        assert data.index.dtype.kind == "M"
+        assert data.divisions[0] == df.b.min()
+
+
+def test_with_func(db):
+    from sqlalchemy import sql
+    index = sql.func.abs(sql.column('negish'))
+    data = read_sql_table('test', db, partitions=2, index_col=index)
+    assert data.divisions[0] == 0
+    part = data.get_partition(0).compute()
+    assert (part.index == 0).all()
+
+    data = read_sql_table('test', db, partitions=2, index_col=index, columns=[index, -sql.column('age')])
+    assert (data.age.compute() < 0).all()
