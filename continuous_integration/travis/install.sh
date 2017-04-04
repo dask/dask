7a0a3f83490b4bfdf777dbc97f7d82cd4c7e20d2
Antoine Pitrou <antoine@python.org>
2017-04-04 21:21:59 +0200

    Fix miniconda URL again

--- continuous_integration/travis/install.sh
+++ continuous_integration/travis/install.sh
@@ -1,10 +1,10 @@
 # Install conda
 case "$(uname -s)" in
     'Darwin')
-        MINICONDA_FILENAME="Miniconda-latest-MacOSX-x86_64.sh"
+        MINICONDA_FILENAME="Miniconda3-latest-MacOSX-x86_64.sh"
         ;;
     'Linux')
-        MINICONDA_FILENAME="Miniconda-latest-Linux-x86_64.sh"
+        MINICONDA_FILENAME="Miniconda3-latest-Linux-x86_64.sh"
         ;;
     *)  ;;
 esac
@@ -16,7 +16,7 @@ export PATH="$HOME/miniconda/bin:$PATH"
 conda config --set always_yes yes --set changeps1 no
 
 # Create conda environment
-conda create -n test-environment python=$PYTHON
+conda create -q -n test-environment python=$PYTHON
 source activate test-environment
 
 # Install dependencies.
@@ -24,7 +24,7 @@ source activate test-environment
 # dependencies in two separate calls, otherwise we sometimes get version
 # incompatible with the installed version of numpy leading to crashes. This
 # seems to have to do with differences between conda-forge and defaults.
-conda install -c conda-forge \
+conda install -q -c conda-forge \
     numpy=$NUMPY \
     pandas=$PANDAS \
     bcolz \
@@ -43,35 +43,35 @@ conda install -c conda-forge \
     toolz
 
 # Specify numpy/pandas here to prevent upgrade/downgrade
-conda install -c conda-forge \
+conda install -q -c conda-forge \
     numpy=$NUMPY \
     pandas=$PANDAS \
     distributed \
     cloudpickle \
     bokeh \
 
-pip install git+https://github.com/dask/zict --upgrade --no-deps
-pip install git+https://github.com/dask/distributed --upgrade --no-deps
+pip install -q git+https://github.com/dask/zict --upgrade --no-deps
+pip install -q git+https://github.com/dask/distributed --upgrade --no-deps
 
 if [[ $PYTHONOPTIMIZE != '2' ]]; then
-    conda install -c conda-forge numba cython
-    pip install git+https://github.com/dask/fastparquet
+    conda install -q -c conda-forge numba cython
+    pip install -q git+https://github.com/dask/fastparquet
 fi
 
 if [[ $PYTHON == '2.7' ]]; then
-    pip install backports.lzma mock
+    pip install -q backports.lzma mock
 fi
 
-pip install \
+pip install -q \
     cachey \
     graphviz \
     moto \
     --upgrade --no-deps
 
-pip install \
+pip install -q \
     flake8 \
     pandas_datareader \
     pytest-xdist
 
 # Install dask
-pip install --no-deps -e .[complete]
+pip install -q --no-deps -e .[complete]
