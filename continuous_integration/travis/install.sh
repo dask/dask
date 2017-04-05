1cf2d9230055acd963cd887fb79d45568c271c85
Antoine Pitrou <pitrou@free.fr>
2017-04-05 15:55:44 +0200

    Activate OS X builder on Travis-CI (#2164)
    * Activate OS X builder on Travis-CI
    
    * Fix Miniconda download code to download for the right OS
    
    * Fix miniconda URL again
    
    * Re-add apt addon
    
    * Try installing graphviz with conda
    
    * Mark OS X build optional
    
    * Re-add other Linux builds

--- continuous_integration/travis/install.sh
+++ continuous_integration/travis/install.sh
@@ -1,11 +1,22 @@
 # Install conda
-wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
+case "$(uname -s)" in
+    'Darwin')
+        MINICONDA_FILENAME="Miniconda3-latest-MacOSX-x86_64.sh"
+        ;;
+    'Linux')
+        MINICONDA_FILENAME="Miniconda3-latest-Linux-x86_64.sh"
+        ;;
+    *)  ;;
+esac
+
+
+wget https://repo.continuum.io/miniconda/$MINICONDA_FILENAME -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 conda config --set always_yes yes --set changeps1 no
 
 # Create conda environment
-conda create -n test-environment python=$PYTHON
+conda create -q -n test-environment python=$PYTHON
 source activate test-environment
 
 # Install dependencies.
@@ -13,7 +24,7 @@ source activate test-environment
 # dependencies in two separate calls, otherwise we sometimes get version
 # incompatible with the installed version of numpy leading to crashes. This
 # seems to have to do with differences between conda-forge and defaults.
-conda install -c conda-forge \
+conda install -q -c conda-forge \
     numpy=$NUMPY \
     pandas=$PANDAS \
     bcolz \
@@ -21,6 +32,7 @@ conda install -c conda-forge \
     chest \
     coverage \
     cytoolz \
+    graphviz \
     h5py \
     ipython \
     partd \
@@ -32,35 +44,35 @@ conda install -c conda-forge \
     toolz
 
 # Specify numpy/pandas here to prevent upgrade/downgrade
-conda install -c conda-forge \
+conda install -q -c conda-forge \
     numpy=$NUMPY \
     pandas=$PANDAS \
     distributed \
     cloudpickle \
     bokeh \
 
-pip install git+https://github.com/dask/zict --upgrade --no-deps
-pip install git+https://github.com/dask/distributed --upgrade --no-deps
+pip install -q git+https://github.com/dask/zict --upgrade --no-deps
+pip install -q git+https://github.com/dask/distributed --upgrade --no-deps
 
 if [[ $PYTHONOPTIMIZE != '2' ]]; then
-    conda install -c conda-forge numba cython
-    pip install git+https://github.com/dask/fastparquet
+    conda install -q -c conda-forge numba cython
+    pip install -q git+https://github.com/dask/fastparquet
 fi
 
 if [[ $PYTHON == '2.7' ]]; then
-    pip install backports.lzma mock
+    pip install -q backports.lzma mock
 fi
 
-pip install \
+pip install -q \
     cachey \
     graphviz \
     moto \
     --upgrade --no-deps
 
-pip install \
+pip install -q \
     flake8 \
     pandas_datareader \
     pytest-xdist
 
 # Install dask
-pip install --no-deps -e .[complete]
+pip install -q --no-deps -e .[complete]
