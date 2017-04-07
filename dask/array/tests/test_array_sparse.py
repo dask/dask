91f1530f268b2480e14f120476c9af4c036936ea
Matthew Rocklin <mrocklin@gmail.com>
2017-04-07 06:05:46 -0400

    increase testing

--- dask/array/tests/test_array_sparse.py
+++ dask/array/tests/test_array_sparse.py
@@ -8,15 +8,18 @@ from dask.array.utils import assert_eq
 sparse = pytest.importorskip('scipy.sparse')
 
 
-def test_basic():
+@pytest.mark.parametrize('sparse_type', [sparse.csr_matrix, sparse.csc_matrix,
+    sparse.coo_matrix, sparse.dia_matrix])
+def test_basic(sparse_type):
     a = dask.delayed(sparse.eye)(5)
+    a = dask.delayed(sparse_type)(a)
     a, b, c = a * 1, a * 2, a * 3
 
     x = da.concatenate([da.from_delayed(x, shape=(5, 5), dtype=float)
                         for x in [a, b, c]])
 
     y = da.expm1(x * 10)
-    y = y + y
+    y = (y + y).astype('float32')
 
     z = y.T.dot(y).persist(get=dask.get)
     assert all(isinstance(v, sparse.spmatrix) for v in z.dask.values())
@@ -30,6 +33,20 @@ def test_basic():
                         for x in [a, b, c]])
 
     y = da.expm1(x * 10)
-    y = y + y
+    y = (y + y).astype('float32')
     z2 = y.dot(y.T).persist(get=dask.get)
     assert_eq(z, z2)
+
+
+@pytest.mark.parametrize('sparse_type', [sparse.csr_matrix, sparse.csc_matrix])
+def test_slicing(sparse_type):
+    a = dask.delayed(sparse.eye)(5)
+    a = dask.delayed(sparse_type)(a)
+    a, b, c = a * 1, a * 2, a * 3
+
+    x = da.concatenate([da.from_delayed(x, shape=(5, 5), dtype=float)
+                        for x in [a, b, c]])
+
+    y = x[1:-2:2].persist()
+    assert all(isinstance(v, sparse.spmatrix) for v in y.dask.values())
+    assert_eq(y, y)
