7e97bf246c77ac50bcd686cd8167a71aafaf0664
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-04-25 17:09:40 -0400

    Fix map_overlap for trivial padding
    In `add_dummy_padding` there was a special workaround for the `"none"`
    boundary condition that failed when the `depth` was trivial (i.e. `0`).
    To fix this, simply check to see that the boundary condition is `"none"`
    and the `depth` is positive-definite before proceeding. This appears to
    clear up the issue when working trivial depth and the `"none"` boundary
    condition.

--- dask/array/ghost.py
+++ dask/array/ghost.py
@@ -359,9 +359,8 @@ def add_dummy_padding(x, depth, boundary):
     array([..., 0, 1, 2, 3, 4, 5, ...])
     """
     for k, v in boundary.items():
-        if v == 'none':
-            d = depth[k]
-
+        d = depth[k]
+        if v == 'none' and d > 0:
             empty_shape = list(x.shape)
             empty_shape[k] = d
 
