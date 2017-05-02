f24257a57a64123b11407ff2fd4325dc970cd978
Matthew Rocklin <mrocklin@gmail.com>
2017-05-01 21:15:26 -0400

    xfail test

--- dask/tests/test_distributed.py
+++ dask/tests/test_distributed.py
@@ -85,6 +85,7 @@ def test_to_hdf_distributed(loop):
             test_to_hdf()
 
 
+@pytest.mark.xfail(reason='HDF not multi-process safe')
 @pytest.mark.parametrize('npartitions', [1, 4, 10])
 def test_to_hdf_scheduler_distributed(npartitions, loop):
     from ..dataframe.io.tests.test_hdf import test_to_hdf_schedulers
