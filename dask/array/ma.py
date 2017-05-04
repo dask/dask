08d0eeb93703ad506c126159a1964803b6dc2ec0
Jim Crist <jiminy.crist@gmail.com>
2017-05-04 16:27:46 -0500

    Add more masked methods

--- dask/array/ma.py
+++ dask/array/ma.py
@@ -1,13 +1,21 @@
 from __future__ import absolute_import, division, print_function
 
-from functools import wraps
+from functools import wraps, update_wrapper
 
 import numpy as np
 
-from .core import concatenate_lookup, tensordot_lookup
+from .core import concatenate_lookup, tensordot_lookup, elemwise, Array
 
 
-concatenate_lookup.register(np.ma.masked_array, np.ma.concatenate)
+@concatenate_lookup.register(np.ma.masked_array)
+def _concatenate(arrays, axis=0):
+    fill_values = [i.fill_value for i in arrays if hasattr(i, 'fill_value')]
+    out = np.ma.concatenate(arrays, axis=axis)
+    if fill_values:
+        fill_values = np.unique(fill_values)
+        if len(fill_values) == 1:
+            out.fill_value = fill_values[0]
+    return out
 
 
 @tensordot_lookup.register(np.ma.masked_array)
@@ -80,4 +88,52 @@ def _tensordot(a, b, axes=2):
 
 @wraps(np.ma.filled)
 def filled(a, fill_value=None):
-    return a.map_blocks(np.ma.filled, fill_value)
+    return elemwise(np.ma.filled, a, fill_value=fill_value)
+
+
+def _wrap_masked(f):
+    return update_wrapper(lambda a, value: elemwise(f, a, value), f)
+
+
+masked_greater = _wrap_masked(np.ma.masked_greater)
+masked_greater_equal = _wrap_masked(np.ma.masked_greater_equal)
+masked_less = _wrap_masked(np.ma.masked_less)
+masked_less_equal = _wrap_masked(np.ma.masked_less_equal)
+masked_equal = _wrap_masked(np.ma.masked_equal)
+masked_not_equal = _wrap_masked(np.ma.masked_not_equal)
+
+
+@wraps(np.ma.masked_invalid)
+def masked_invalid(a):
+    return elemwise(np.ma.masked_invalid, a)
+
+
+@wraps(np.ma.masked_inside)
+def masked_inside(x, v1, v2):
+    return elemwise(np.ma.masked_inside, x, v1, v2)
+
+
+@wraps(np.ma.masked_outside)
+def masked_outside(x, v1, v2):
+    return elemwise(np.ma.masked_outside, x, v1, v2)
+
+
+@wraps(np.ma.masked_where)
+def masked_where(condition, a):
+    cshape = getattr(condition, 'shape', ())
+    if not isinstance(a, Array):
+        raise TypeError("a must be a dask.Array")
+    if cshape != a.shape:
+        raise IndexError("Inconsistant shape between the condition and the "
+                         "input (got %s and %s)" % (cshape, a.shape))
+    return elemwise(np.ma.masked_where, condition, a)
+
+
+@wraps(np.ma.masked_values)
+def masked_values(x, value, rtol=1e-05, atol=1e-08, shrink=True):
+    return elemwise(value, x, rtol=rtol, atol=atol, shrink=shrink)
+
+
+@wraps(np.ma.fix_invalid)
+def fix_invalid(a, mask=False, fill_value=None):
+    return elemwise(np.ma.fix_invalid, a, mask, True, fill_value)
