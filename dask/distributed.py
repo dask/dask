a51d3d30384e1614e2bbf9e57621ff4b10f79414
Matthew Rocklin <mrocklin@gmail.com>
2017-07-03 16:43:45 -0400

    Import star from dask.distributed
    While generally a bad habit in this case it allows us to evolve the
    dask/distributed API without touching the dask/dask codebase.

--- dask/distributed.py
+++ dask/distributed.py
@@ -2,17 +2,10 @@
 from __future__ import absolute_import, division, print_function
 
 try:
-    from distributed import (Client, Executor, progress, as_completed, wait,
-            LocalCluster, Scheduler, Worker, Nanny, local_client, config,
-            CompatibleExecutor, worker_client)
+    from distributed import *
 except ImportError:
     msg = ("Dask's distributed scheduler is not installed.\n\n"
            "Please either conda or pip install dask distributed:\n\n"
            "  conda install dask distributed          # either conda install\n"
            "  pip install dask distributed --upgrade  # or pip install")
     raise ImportError(msg)
-else:
-    try:  # newer imports
-        from distributed import Queue, Variable, get_worker
-    except ImportError:
-        pass
