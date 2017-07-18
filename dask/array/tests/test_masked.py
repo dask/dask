32386414c0ab24541160edc2260a6dedadbf70d5
Jim Crist <jiminy.crist@gmail.com>
2017-07-18 17:01:42 -0500

    Test `from_array` on masked arrays

--- dask/array/tests/test_masked.py
+++ dask/array/tests/test_masked.py
@@ -22,6 +22,12 @@ def test_tokenize_masked_array():
     assert tokenize(m) != tokenize(m3)
 
 
+def test_from_array_masked_array():
+    m = np.ma.masked_array([1, 2, 3], mask=[True, True, False], fill_value=10)
+    dm = da.from_array(m, chunks=(2,), asarray=False)
+    assert_eq(dm, m)
+
+
 functions = [
     lambda x: x,
     lambda x: da.expm1(x),
