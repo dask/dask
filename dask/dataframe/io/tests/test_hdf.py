b66c6e955ed7e5fee2e7b137245bac503cf64d18
James Bourbeau <jrbourbeau@gmail.com>
2017-07-24 21:53:50 -0500

    Adds test for input file list

--- dask/dataframe/io/tests/test_hdf.py
+++ dask/dataframe/io/tests/test_hdf.py
@@ -495,6 +495,21 @@ def test_hdf_globbing():
             tm.assert_frame_equal(res.compute(), pd.concat([df] * 3))
 
 
+def test_hdf_file_list():
+    pytest.importorskip('tables')
+    df = pd.DataFrame({'x': ['a', 'b', 'c', 'd'],
+                       'y': [1, 2, 3, 4]}, index=[1., 2., 3., 4.])
+
+    with tmpdir() as tdir:
+        df.iloc[:2].to_hdf(os.path.join(tdir, 'one.h5'), 'dataframe', format='table')
+        df.iloc[2:].to_hdf(os.path.join(tdir, 'two.h5'), 'dataframe', format='table')
+
+        with dask.set_options(get=dask.get):
+            input_files = [os.path.join(tdir, 'one.h5'), os.path.join(tdir, 'two.h5')]
+            res = dd.read_hdf(input_files, 'dataframe')
+            tm.assert_frame_equal(res.compute(), df)
+
+
 def test_read_hdf_doesnt_segfault():
     pytest.importorskip('tables')
     with tmpfile('h5') as fn:
