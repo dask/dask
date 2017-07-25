e702ca092175c8df6ff8e09df289ca85fcb7d8e9
Jim Crist <jiminy.crist@gmail.com>
2017-07-25 12:49:49 -0500

    Support dask array conditional in compress
    Compress now works with dask arrays as conditionals without calling
    compute on them. If the conditional is concrete we take an optimized
    path and return an array of known shape. If it's lazy we return an
    array of unknown shape along the specified axis.

--- dask/array/tests/test_array_core.py
+++ dask/array/tests/test_array_core.py
@@ -432,16 +432,20 @@ def test_compress():
     x = np.arange(25).reshape((5, 5))
     a = from_array(x, chunks=(2, 2))
 
-    assert_eq(np.compress([True, False, True, False, True], x),
-              da.compress([True, False, True, False, True], a))
-    assert_eq(np.compress([True, False, True, False, True], x, axis=0),
-              da.compress([True, False, True, False, True], a, axis=0))
-    assert_eq(np.compress([True, False, True, False, True], x, axis=1),
-              da.compress([True, False, True, False, True], a, axis=1))
-    assert_eq(np.compress([True, False], x),
-              da.compress([True, False], a))
-    assert_eq(np.compress([True, False], x, axis=1),
-              da.compress([True, False], a, axis=1))
+    c1 = np.array([True, False, True, False, True])
+    c2 = np.array([True, False])
+    c3 = [True, False]
+    dc1 = da.from_array(c1, chunks=3)
+    dc2 = da.from_array(c2, chunks=2)
+
+    for c, dc in [(c1, c1), (c2, c2), (c3, c3),
+                  (c1, dc1), (c2, dc2), (c3, dc2)]:
+        for axis in [None, 0, 1]:
+            res = da.compress(dc, a, axis=axis)
+            assert_eq(np.compress(c, x, axis=axis), res)
+            if isinstance(dc, Array):
+                axis = axis or 0
+                assert np.isnan(res.chunks[axis]).all()
 
     with pytest.raises(ValueError):
         da.compress([True, False], a, axis=100)
