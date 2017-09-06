752e19444d8f36028d030a3f6f6ad570142603fc
Matthew Rocklin <mrocklin@gmail.com>
2017-09-06 14:54:57 -0400

    add failing test for boolean slicing

--- dask/array/tests/test_slicing.py
+++ dask/array/tests/test_slicing.py
@@ -517,6 +517,26 @@ def test_index_with_dask_array():
 
 
 @pytest.mark.xfail
+def test_index_with_dask_array_2():
+    x = np.random.random((10, 10, 10, 10))
+    ind = np.random.random(10) > 0.5
+
+    d = da.from_array(x, chunks=(2, 3, 4, 5))
+    dind = da.from_array(ind, chunks=4)
+
+    index = [slice(1, 9, 1), 4, slice(None)]
+
+    for i in range(x.ndim):
+        index2 = index[:]
+        index2.insert(i, dind)
+
+        index3 = index[:]
+        index3.insert(i, ind)
+
+        assert_eq(x[tuple(index3)], d[tuple(index2)])
+
+
+@pytest.mark.xfail
 def test_cull():
     x = da.ones(1000, chunks=(10,))
 
