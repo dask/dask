7f5ba4a5517320168fbadc6c804d8e338de91574
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-09-20 10:42:35 -0400

    Export ptp in Dask Array's public API

--- dask/array/__init__.py
+++ dask/array/__init__.py
@@ -5,7 +5,7 @@ from .core import (Array, concatenate, stack, from_array, store, map_blocks,
                    atop, to_hdf5, to_npy_stack, from_npy_stack, from_delayed,
                    asarray, asanyarray, broadcast_to)
 from .routines import (take, choose, argwhere, where, coarsen, insert,
-                       ravel, roll, unique, squeeze, topk, diff, ediff1d,
+                       ravel, roll, unique, squeeze, topk, ptp, diff, ediff1d,
                        bincount, digitize, histogram, cov, array, dstack,
                        vstack, hstack, compress, extract, round, count_nonzero,
                        flatnonzero, nonzero, around, isnull, notnull, isclose,
