bb2092bde5b62f6542bc4f9938dcf65cc13ffc93
Tom Augspurger <tom.w.augspurger@gmail.com>
2017-10-28 15:52:09 -0500

    Fix installing of pandas dev

--- continuous_integration/travis/install.sh
+++ continuous_integration/travis/install.sh
@@ -23,6 +23,7 @@ source activate test-environment
 # Please see PR ( https://github.com/dask/dask/pull/2185 ) for details.
 touch $CONDA_PREFIX/conda-meta/pinned
 if ! [[ ${UPSTREAM_DEV} ]]; then
+    echo "Pinning NumPy $NUMPY, pandas $PANDAS"
     echo "numpy $NUMPY" >> $CONDA_PREFIX/conda-meta/pinned
     echo "pandas $PANDAS" >> $CONDA_PREFIX/conda-meta/pinned
 fi;
@@ -52,44 +53,42 @@ conda install -q -c conda-forge \
     sqlalchemy \
     toolz
 
-# Install pandas dev
-conda uninstall -y --force numpy pandas
-
 if [[ ${UPSTREAM_DEV} ]]; then
+    echo "Installing NumPy and Pandas dev"
+    conda uninstall -y --force numpy pandas
     PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
-    pip install --pre --no-deps --upgrade --timeout=60 -f $PRE_WHEELS numpy pandas
+    pip install -q --pre --no-deps --upgrade --timeout=60 -f $PRE_WHEELS numpy pandas
 fi;
 
 # install pytables from defaults for now
 conda install -q pytables
 
-pip install -q git+https://github.com/dask/partd --upgrade --no-deps
-pip install -q git+https://github.com/dask/zict --upgrade --no-deps
-pip install -q git+https://github.com/dask/distributed --upgrade --no-deps
-pip install -q git+https://github.com/mrocklin/sparse --upgrade --no-deps
-pip install -q git+https://github.com/dask/s3fs --upgrade --no-deps
+pip install -q --upgrade --no-deps git+https://github.com/dask/partd
+pip install -q --upgrade --no-deps git+https://github.com/dask/zict
+pip install -q --upgrade --no-deps git+https://github.com/dask/distributed
+pip install -q --upgrade --no-deps git+https://github.com/mrocklin/sparse
+pip install -q --upgrade --no-deps git+https://github.com/dask/s3fs
 
 if [[ $PYTHONOPTIMIZE != '2' ]] && [[ $NUMPY > '1.11.0' ]] && [[ $NUMPY < '1.13.0' ]]; then
     conda install -q -c conda-forge numba cython
-    pip install -q git+https://github.com/dask/fastparquet
+    pip install -q --no-deps git+https://github.com/dask/fastparquet
 fi
 
 if [[ $PYTHON == '2.7' ]]; then
-    pip install -q backports.lzma mock
+    pip install -q --no-deps backports.lzma mock
 fi
 
-pip install -q \
+pip install -q --upgrade --no-deps \
     cachey \
     graphviz \
     moto \
     pyarrow \
-    --upgrade --no-deps
+    pandas_datareader
 
-pip install -U -q \
+pip install -q --upgrade \
     cityhash \
     flake8 \
     mmh3 \
-    pandas_datareader \
     pytest-xdist \
     xxhash
 
