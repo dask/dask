add3218a8be1bd47a32d5c485f372f5c934ad2cd
Matthew Rocklin <mrocklin@gmail.com>
2017-11-01 17:10:30 +0100

    Avoid None frames in profile.process (#1510)
    Fixes https://github.com/dask/distributed/commit/379bd9736ef309bb6c75f38a3f779a89b848a627#commitcomment-25172287

--- distributed/worker.py
+++ distributed/worker.py
@@ -2272,9 +2272,12 @@ class Worker(WorkerBase):
         frames = sys._current_frames()
         frames = {ident: frames[ident] for ident in active_threads}
         for ident, frame in frames.items():
-            key = key_split(active_threads[ident])
-            profile.process(frame, None, self.profile_recent, stop='_concurrent_futures_thread.py')
-            profile.process(frame, None, self.profile_keys[key], stop='_concurrent_futures_thread.py')
+            if frame is not None:
+                key = key_split(active_threads[ident])
+                profile.process(frame, None, self.profile_recent,
+                                stop='_concurrent_futures_thread.py')
+                profile.process(frame, None, self.profile_keys[key],
+                                stop='_concurrent_futures_thread.py')
         stop = time()
         if self.digests is not None:
             self.digests['profile-duration'].add(stop - start)
