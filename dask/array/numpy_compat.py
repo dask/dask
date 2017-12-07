092bca32aa84f39a22184f6bb0868e2a06fc7e5b
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-12-07 17:02:28 -0500

    Vendor NumPy 1.13's _Recurser in numpy_compat
    Unfortunately we need to vendor `numpy.core.shape_base._Recurser` as it
    appears to get dropped midway through NumPy 1.13 and is replaced with a
    new implementation, which will require some fiddling to get working with
    Dask Arrays. In comparison, the `numpy.core.shape_base._Recurser`
    basically just works with any so is pretty easy to get working with Dask
    Arrays as well.

--- dask/array/numpy_compat.py
+++ dask/array/numpy_compat.py
@@ -359,3 +359,84 @@ except ImportError:  # pragma: no cover
         """
         a, mask = _replace_nan(a, 1)
         return np.cumprod(a, axis=axis, dtype=dtype, out=out)
+
+
+class _Recurser(object):
+    """
+    Utility class for recursing over nested iterables
+    """
+
+    # This was copied almost verbatim from numpy.core.shape_base._Recurser
+    # See numpy license at https://github.com/numpy/numpy/blob/master/LICENSE.txt
+    # or NUMPY_LICENSE.txt within this directory
+
+    def __init__(self, recurse_if):
+        self.recurse_if = recurse_if
+
+    def map_reduce(self,
+                   x,
+                   f_map=lambda x, **kwargs: x,
+                   f_reduce=lambda x, **kwargs: x,
+                   f_kwargs=lambda **kwargs: kwargs,
+                   **kwargs):
+        """
+        Iterate over the nested list, applying:
+        * ``f_map`` (T -> U) to items
+        * ``f_reduce`` (Iterable[U] -> U) to mapped items
+
+        For instance, ``map_reduce([[1, 2], 3, 4])`` is::
+
+            f_reduce([
+              f_reduce([
+                f_map(1),
+                f_map(2)
+              ]),
+              f_map(3),
+              f_map(4)
+            ]])
+
+
+        State can be passed down through the calls with `f_kwargs`,
+        to iterables of mapped items. When kwargs are passed, as in
+        ``map_reduce([[1, 2], 3, 4], **kw)``, this becomes::
+
+            kw1 = f_kwargs(**kw)
+            kw2 = f_kwargs(**kw1)
+            f_reduce([
+              f_reduce([
+                f_map(1), **kw2)
+                f_map(2,  **kw2)
+              ],      **kw1),
+              f_map(3, **kw1),
+              f_map(4, **kw1)
+            ]],     **kw)
+        """
+        def f(x, **kwargs):
+            if not self.recurse_if(x):
+                return f_map(x, **kwargs)
+            else:
+                next_kwargs = f_kwargs(**kwargs)
+                return f_reduce((
+                    f(xi, **next_kwargs)
+                    for xi in x
+                ), **kwargs)
+        return f(x, **kwargs)
+
+    def walk(self, x, index=()):
+        """
+        Iterate over x, yielding (index, value, entering), where
+
+        * ``index``: a tuple of indices up to this point
+        * ``value``: equal to ``x[index[0]][...][index[-1]]``. On the first iteration, is
+                     ``x`` itself
+        * ``entering``: bool. The result of ``recurse_if(value)``
+        """
+        do_recurse = self.recurse_if(x)
+        yield index, x, do_recurse
+
+        if not do_recurse:
+            return
+        for i, xi in enumerate(x):
+            # yield from ...
+            for v in self.walk(xi, index + (i,)):
+                yield v
