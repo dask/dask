7270c17b9a536a0e388edee88dec0b549d3180cc
jakirkham <jakirkham@gmail.com>
2017-12-09 11:24:10 -0500

    Fix `store` region test (#2979)
    * Fix `store` region test
    
    Make sure that the `store` region test compares each store against the
    correct values.
    
    * Move target store creation into single region test

--- dask/array/tests/test_array_core.py
+++ dask/array/tests/test_array_core.py
@@ -1225,18 +1225,17 @@ def test_store_regions():
     d = da.ones((4, 4, 4), chunks=(2, 2, 2))
     a, b = d + 1, d + 2
 
-    at = np.zeros(shape=(8, 4, 6))
-    bt = np.zeros(shape=(8, 4, 6))
-
     region = (slice(None,None,2), slice(None), [1, 2, 4, 5])
 
     # Single region:
+    at = np.zeros(shape=(8, 4, 6))
+    bt = np.zeros(shape=(8, 4, 6))
     v = store([a, b], [at, bt], regions=region, compute=False)
     assert (at == 0).all() and (bt[region] == 0).all()
     v.compute()
     assert (at[region] == 2).all() and (bt[region] == 3).all()
     assert not (bt == 3).all() and not ( bt == 0 ).all()
-    assert not (at == 3).all() and not ( at == 0 ).all()
+    assert not (at == 2).all() and not ( at == 0 ).all()
 
     # Multiple regions:
     at = np.zeros(shape=(8, 4, 6))
@@ -1246,7 +1245,7 @@ def test_store_regions():
     v.compute()
     assert (at[region] == 2).all() and (bt[region] == 3).all()
     assert not (bt == 3).all() and not ( bt == 0 ).all()
-    assert not (at == 3).all() and not ( at == 0 ).all()
+    assert not (at == 2).all() and not ( at == 0 ).all()
 
 
 def test_store_compute_false():
