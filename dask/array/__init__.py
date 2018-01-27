efee8e7383d2da5689ebf386c9245e563b4f7d07
Markus Gonser <magonser@mksg.de>
2018-01-27 18:29:30 +0100

    Wrapup tasks

--- dask/array/__init__.py
+++ dask/array/__init__.py
@@ -50,3 +50,4 @@ from ..base import compute
 from .optimization import optimize
 from .creation import (arange, linspace, meshgrid, indices, diag, eye,
                        triu, tril, fromfunction, tile, repeat)
+from .gufunc import apply_gufunc, gufunc
