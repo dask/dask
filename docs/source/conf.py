743dbc90874916110d7ae17f905358bc658ed694
Joseph Lin <josephlin55555@gmail.com>
2018-02-01 17:58:32 -0500

    [update] license year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'dask'
-copyright = u'2017, Anaconda'
+copyright = u'2017-2018, Anaconda'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
