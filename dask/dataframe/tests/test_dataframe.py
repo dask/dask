a69e797dbc0967b0914ef686d94763186404c8fc
cr458 <christopher.x.ren@gmail.com>
2018-03-19 08:40:26 +0000

    skip test if version < 0.22.0

--- dask/dataframe/tests/test_dataframe.py
+++ dask/dataframe/tests/test_dataframe.py
@@ -2811,13 +2811,14 @@ def test_to_timedelta():
               dd.to_timedelta(ds, errors='coerce'))
 
 
-if PANDAS_VERSION >= '0.22.0':
-    @pytest.mark.parametrize('values', [[np.NaN, 0], [1, 1]])
-    def test_isna(values):
-        s = pd.Series(values)
-        ds = dd.from_pandas(s, npartitions=2)
+@pytest.mark.skipif(PANDAS_VERSION < '0.22.0',
+                    reason="No isna method")
+@pytest.mark.parametrize('values', [[np.NaN, 0], [1, 1]])
+def test_isna(values):
+    s = pd.Series(values)
+    ds = dd.from_pandas(s, npartitions=2)
 
-        assert_eq(pd.isna(s), dd.isna(ds))
+    assert_eq(pd.isna(s), dd.isna(ds))
 
 
 @pytest.mark.parametrize('drop', [0, 9])
