e832297146f07057de62fe723798fda7b046991a
jakirkham <jakirkham@gmail.com>
2018-03-30 08:04:40 -0400

    Fix broadcast_shapes handling of nan (#3356)
    * Fix `broadcast_shapes` for `nan` lengths
    
    Appears that Python's `max` will sometimes ignore `nan` values. To fix
    this, we switch to NumPy's `max`, which will always prefer `nan` values
    if they show up.
    
    * Test `broadcast_shapes` handles `nan` correctly
    
    Add some tests for `nan` lengths in shapes passed to `broadcast_shapes`.
    Tries to catch some cases that were mishandled before.
    
    * Add `broadcast_shapes` `nan` fix to changelog

--- dask/array/tests/test_array_core.py
+++ dask/array/tests/test_array_core.py
@@ -700,11 +700,18 @@ def test_broadcast_shapes():
     assert () == broadcast_shapes()
     assert (2, 5) == broadcast_shapes((2, 5))
     assert (0, 5) == broadcast_shapes((0, 1), (1, 5))
+    assert np.allclose(
+        (2, np.nan), broadcast_shapes((1, np.nan), (2, 1)), equal_nan=True
+    )
+    assert np.allclose(
+        (2, np.nan), broadcast_shapes((2, 1), (1, np.nan)), equal_nan=True
+    )
     assert (3, 4, 5) == broadcast_shapes((3, 4, 5), (4, 1), ())
     assert (3, 4) == broadcast_shapes((3, 1), (1, 4), (4,))
     assert (5, 6, 7, 3, 4) == broadcast_shapes((3, 1), (), (5, 6, 7, 1, 4))
     pytest.raises(ValueError, lambda: broadcast_shapes((3,), (3, 4)))
     pytest.raises(ValueError, lambda: broadcast_shapes((2, 3), (2, 3, 1)))
+    pytest.raises(ValueError, lambda: broadcast_shapes((2, 3), (1, np.nan)))
 
 
 def test_elemwise_on_scalars():
