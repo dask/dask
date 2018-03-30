Fix broadcast_shapes handling of nan (#3356)
* Fix `broadcast_shapes` for `nan` lengths

Appears that Python's `max` will sometimes ignore `nan` values. To fix
this, we switch to NumPy's `max`, which will always prefer `nan` values
if they show up.

* Test `broadcast_shapes` handles `nan` correctly

Add some tests for `nan` lengths in shapes passed to `broadcast_shapes`.
Tries to catch some cases that were mishandled before.

* Add `broadcast_shapes` `nan` fix to changelog

dask/array/core.py
@@ -2899,7 +2899,7 @@ def broadcast_shapes(*shapes):
         return shapes[0]
     out = []
     for sizes in zip_longest(*map(reversed, shapes), fillvalue=-1):
-        dim = 0 if 0 in sizes else max(sizes)
+        dim = 0 if 0 in sizes else np.max(sizes)
         if any(i not in [-1, 0, 1, dim] and not np.isnan(i) for i in sizes):
             raise ValueError("operands could not be broadcast together with "
                              "shapes {0}".format(' '.join(map(str, shapes))))