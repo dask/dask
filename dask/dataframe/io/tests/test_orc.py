ac3d3158cb08303b53f78637af36f48ee0a7fa7a
Martin Durant <martin.durant@utoronto.ca>
2018-04-01 15:46:20 -0400

    Move test data to GCS and download fewer times
    Note tha the ORC reader does not seem to guarantee column ordering

--- dask/dataframe/io/tests/test_orc.py
+++ dask/dataframe/io/tests/test_orc.py
@@ -1,28 +1,31 @@
 import os
 import pytest
 import shutil
+import tempfile
 
 from dask.dataframe import read_orc
 from dask.dataframe.utils import assert_eq
 import dask.dataframe as dd
 
 pytest.importorskip('pyarrow.orc')
-url = ('https://github.com/apache/orc/blob/master/examples/'
-       'TestOrcFile.testDate1900.orc?raw=true')
+url = ('https://www.googleapis.com/download/storage/v1/b/anaconda-public-data/o'
+       '/orc%2FTestOrcFile.testDate1900.orc?generation=1522611448751555&alt='
+       'media')
+columns = ['time', 'date']
 
 
 @pytest.mark.network
 def test_orc_with_backend():
     d = read_orc(url)
-    assert d.columns.tolist() == ['time', 'date']
+    assert set(d.columns) == {'time', 'date'}  # order is not guranteed
     assert len(d) == 70000
 
 
-@pytest.fixture()
-def orc_files(tmpdir):
+@pytest.fixture(scope='module')
+def orc_files():
     requests = pytest.importorskip('requests')
     data = requests.get(url).content
-    d = str(tmpdir)
+    d = tempfile.mkdtemp()
     files = [os.path.join(d, fn) for fn in ['test1.orc', 'test2.orc']]
     for fn in files:
         with open(fn, 'wb') as f:
@@ -39,7 +42,7 @@ def test_orc_single(orc_files):
     assert len(d) == 70000
     assert d.npartitions == 8
     d2 = read_orc(fn, columns=['time', 'date'])
-    assert_eq(d, d2)
+    assert_eq(d[columns], d2[columns])
     with pytest.raises(ValueError) as e:
         read_orc(fn, columns=['time', 'nonexist'])
     assert 'nonexist' in str(e)
@@ -48,6 +51,6 @@ def test_orc_single(orc_files):
 def test_orc_multiple(orc_files):
     d = read_orc(orc_files[0])
     d2 = read_orc(orc_files)
-    assert_eq(d2, dd.concat([d, d]), check_index=False)
+    assert_eq(d2[columns], dd.concat([d, d])[columns], check_index=False)
     d2 = read_orc(os.path.dirname(orc_files[0]) + '/*.orc')
-    assert_eq(d2, dd.concat([d, d]), check_index=False)
+    assert_eq(d2[columns], dd.concat([d, d])[columns], check_index=False)
