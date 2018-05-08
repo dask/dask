66502179d120ab69fb079d7ccd959e14d00502ab
Yu Feng <rainwoodman@gmail.com>
2018-05-08 11:02:50 -0700

    handle 0 in index.start properly.
    It is different from None.

--- dask/array/slicing.py
+++ dask/array/slicing.py
@@ -391,7 +391,7 @@ def _slice_1d(dim_shape, lengths, index):
         start = index.start or 0
         stop = index.stop if index.stop is not None else dim_shape
     else:
-        start = index.start or dim_shape - 1
+        start = index.start if index.start is not None else dim_shape - 1
         start = dim_shape - 1 if start >= dim_shape else start
         stop = -(dim_shape + 1) if index.stop is None else index.stop
 
