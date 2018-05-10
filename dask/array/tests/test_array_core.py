df1b66703e53c452af8931f25d37171a55026a4a
Markus Gonser <magonser@mksg.de>
2018-05-10 16:12:52 +0200

    Minor change of import statement to fix test suite
    Presence of another `broadcast_arrays` module crash original `dask.array.tests.test_array_core` tests that call `da.broadcast_arrays`

--- dask/array/tests/test_array_core.py
+++ dask/array/tests/test_array_core.py
@@ -31,7 +31,7 @@ from dask.array.core import (getem, getter, top, dotmany, concatenate3,
                              broadcast_to, blockdims_from_blockshape, store,
                              optimize, from_func, normalize_chunks,
                              broadcast_chunks, atop, from_delayed,
-                             concatenate_axes, common_blockdim)
+                             concatenate_axes, common_blockdim, broadcast_arrays)
 from dask.array.utils import assert_eq, same_keys
 
 # temporary until numpy functions migrated
@@ -928,7 +928,7 @@ def test_broadcast_to_chunks():
 def test_broadcast_arrays():
     # Calling `broadcast_arrays` with no arguments only works in NumPy 1.13.0+.
     if LooseVersion(np.__version__) >= LooseVersion("1.13.0"):
-        assert np.broadcast_arrays() == da.broadcast_arrays()
+        assert np.broadcast_arrays() == broadcast_arrays()
 
     a = np.arange(4)
     d_a = da.from_array(a, chunks=tuple(s // 2 for s in a.shape))
@@ -940,7 +940,7 @@ def test_broadcast_arrays():
     d_a_1 = d_a[:, None]
 
     a_r = np.broadcast_arrays(a_0, a_1)
-    d_r = da.broadcast_arrays(d_a_0, d_a_1)
+    d_r = broadcast_arrays(d_a_0, d_a_1)
 
     assert isinstance(d_r, list)
     assert len(a_r) == len(d_r)
