4509e72d8dabd339544d19cd8671a9894dca786c
Guido Imperiale <guido.imperiale@gmail.com>
2018-05-31 12:42:30 +0100

    More AST work

--- dask/optimization_ast.py
+++ dask/optimization_ast.py
@@ -1,15 +1,19 @@
-import functools
-import inspect
 import operator
 import pickle
 import re
+from functools import partial
+from inspect import ismodule
 
 import ast
-import numpy
-import toolz.functoolz
-from . import compatibility
-from . import optimization
-from . import order
+from numpy import dtype
+from toolz.functoolz import Compose
+from .compatibility import apply
+from .core import flatten
+from .optimization import cull
+from .utils import ensure_dict
+
+
+__all__ = ('compile_ast', )
 
 
 UNARY_OP_MAP = {
@@ -43,10 +47,30 @@ COMPARE_OP_MAP = {
 }
 
 
-class ASTFunction:
-    def __init__(self, tree, constant_kwargs):
-        self._constant_kwargs = constant_kwargs
+def compile_ast(dsk, keys):
+    dsk = ensure_dict(dsk)
+    keys = set(flatten(keys))
+    result = {}
+    for key in keys:
+        dsk_i, _ = cull(dsk, [key])
+        builder = ASTDaskBuilder(dsk_i, key)
+        result.update(builder.dsk)
+    return result
 
+
+class ASTFunction:
+    __slots__ = ('_code', '_source', '_func', 'constant_kwargs')
+
+
+    def __init__(self, tree, constant_kwargs):
+        self.constant_kwargs = constant_kwargs
+
+        # Do not store the AST tree as an attribute, because it takes ages to
+        # pickle. Besides, its only purpose is to generate the source - which
+        # is only used for debugging, but it's much faster to generate and
+        # instantaneous to pickle.
+        # FIXME: is there a way to generate the source code from the compiled
+        # code?
         try:
             import astor
         except ImportError:
@@ -66,49 +90,54 @@ class ASTFunction:
 
 
     def __getstate__(self):
-        return {
-            '_constant_kwargs': self._constant_kwargs,
-            '_code': self._code,
-            '_source': self._source
-        }
+        # Compiled bytecode is hashable; functions extracted from it aren't
+        return self._code, self._source, self.constant_kwargs
 
 
     def __setstate__(self, state):
-        self.__dict__.update(state)
+        self._code, self._source, self.constant_kwargs = state
         self._setup()
 
 
     def _setup(self):
         exec(self._code)
         for k, v in locals().items():
-            if inspect.ismodule(v):
+            if ismodule(v):
                 globals()[k] = v
         self._func = locals()['_ast_compiled']
 
 
     def __call__(self, *args):
-        return self._func(*args, **self._constant_kwargs)
+        return self._func(*args, **self.constant_kwargs)
 
 
     def __hash__(self):
-        return hash((self._code, self._constant_kwargs))
+        kwargs = []
+        for k, v in self.constant_kwargs.items():
+            try:
+                kwargs += [k, hash(v)]
+            except TypeError:
+                # Unhashable type
+                kwargs += [k, id(v)]
+        return hash((self._code, ) + tuple(kwargs))
 
 
 class ASTDaskBuilder:
     def __init__(self, dsk, key):
+        self.dsk = ensure_dict(dsk)
         self.imports = set()
-        self.args = []
-        self.constant_kwargs = {}
         self.assigns = []
+        self.args = []
+        self.arg_keys = []
+        self.constant_kwargs = {}
         self.obj_names = {}
         self.name_counters = {}
         self.dsk_key_map = {}
+        self.delete_keys = set()
 
-        self.dsk = dict(dsk)
-        self.dsk, _ = optimization.cull(self.dsk, [key])
-        keyorder = order.order(self.dsk)
-        for key, _ in sorted(keyorder.items(), key=lambda i: i[1]):
-            root_name = self._add_dsk_pair(key, dsk[key])
+        # Start recursion
+        root = self._traverse(key)
+        self.assigns.append(ast.Return(root))
 
         imports = [ast.Import([ast.alias(modname, None)])
                    for modname in sorted(self.imports)]
@@ -116,25 +145,47 @@ class ASTDaskBuilder:
         func = ast.FunctionDef(
             name='_ast_compiled',
             args=ast.arguments(
-                args=[],
-                defaults=[], kwarg=None,
-                kwonlyargs=[ast.arg(a, None) for a in
-                            self.constant_kwargs.keys()],
-                kw_defaults=[None] * len(self.constant_kwargs),
+                args=[ast.arg(a, None) for a in self.args + list(
+                    self.constant_kwargs.keys())],
+                defaults=[],
+                kwarg=None,
+                kwonlyargs=[],
+                kw_defaults=[],
                 vararg=None),
-            body=self.assigns + [ast.Return(root_name)],
+            body=self.assigns,
             decorator_list=[],
             returns=None)
 
         self.tree = ast.fix_missing_locations(
             ast.Module(body=imports + [func]))
 
+        func = ASTFunction(self.tree, self.constant_kwargs)
+        self.dsk = {
+            k: v for k, v in self.dsk.items()
+            if k not in self.delete_keys
+        }
+        self.dsk[key] = (func, ) + tuple(self.arg_keys)
 
-    def _add_dsk_pair(self, key, val):
-        name = self._unique_name(key)
-        self.dsk_key_map[key] = name
-        ast_code = ast.Assign([ast.Name(name, ast.Store())], self._to_ast(val))
-        self.assigns.append(ast_code)
+
+    def _traverse(self, key):
+        # Avoid try... except Keyerror to keep stack traces clean
+        if key in self.dsk_key_map:
+            name = self.dsk_key_map[key]
+        else:
+            name = self._unique_name(key)
+            self.dsk_key_map[key] = name
+            val = self.dsk[key]
+            # Stop recursion when a ASTFunction is found
+            if (isinstance(val, tuple) and val and
+                    isinstance(val[0], ASTFunction)):
+                self.args.append(name)
+                self.arg_keys.append(key)
+            else:
+                ast_code = ast.Assign([ast.Name(name, ast.Store())],
+                                      self._to_ast(val))
+                self.assigns.append(ast_code)
+                self.delete_keys.add(key)
+
         return ast.Name(name, ast.Load())
 
 
@@ -166,6 +217,9 @@ class ASTDaskBuilder:
         if vtype is bytes:
             return ast.Bytes(v)
         if vtype is str:
+            # Is it a reference to another dask graph node?
+            if v in self.dsk:
+                return self._traverse(v)
             return ast.Str(v)
         if vtype is set:
             return ast.Set([self._to_ast(x) for x in v])
@@ -175,11 +229,15 @@ class ASTDaskBuilder:
             return ast.Dict([self._to_ast(k) for k in v.keys()],
                             [self._to_ast(k) for k in v.values()])
         if vtype is tuple:
+            # Is it a reference to another dask graph node?
             try:
-                return ast.Name(self.dsk_key_map[v], ast.Load())
-            except (TypeError, KeyError):
-                # Not a dask key
+                is_key = v in self.dsk
+            except TypeError:
+                # Unhashable
                 pass
+            else:
+                if is_key:
+                    return self._traverse(v)
 
             if v and callable(v[0]):
                 return self._dsk_function_to_ast(v[0], v[1:], {})
@@ -190,7 +248,7 @@ class ASTDaskBuilder:
         # Additional objects explicitly handled for convenience
         # This is unnecessary - if you remove this section, they
         # will be processed as constant kwargs
-        if vtype is numpy.dtype:
+        if vtype is dtype:
             return ast.Str(v.name)
 
         # Generic object - processed as import or constant kwarg
@@ -202,17 +260,17 @@ class ASTDaskBuilder:
         kwargs = kwargs.copy()
 
         # Unpack partials
-        if func is compatibility.apply:
+        if func is apply:
             if len(args) == 3:
                 kwargs.update(args[2])
             return self._dsk_function_to_ast(args[0], args[1], kwargs)
 
-        if isinstance(func, functools.partial):
+        if isinstance(func, partial):
             kwargs.update(func.keywords)
             return self._dsk_function_to_ast(func.func, func.args + args,
                                              kwargs)
 
-        if isinstance(func, toolz.functoolz.Compose):
+        if isinstance(func, Compose):
             assert not kwargs
             funcs = ((func.first,) + func.funcs)
             tup = (funcs[0],) + args
@@ -261,7 +319,12 @@ class ASTDaskBuilder:
 
     def _add_local(self, obj):
         try:
-            return self.obj_names[obj]
+            lookup_key = hash(obj)
+        except TypeError:
+            # Unhashable type
+            lookup_key = id(obj)
+        try:
+            return self.obj_names[lookup_key]
         except KeyError:
             pass
 
@@ -284,5 +347,5 @@ class ASTDaskBuilder:
                 path = [ast.Attribute(path[0], path[1], ast.Load())] + path[2:]
             res = path[0]
 
-        self.obj_names[obj] = res
+        self.obj_names[lookup_key] = res
         return res
