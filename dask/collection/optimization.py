1312a3a2266119510d66ce0a3574f2f7582ed7d8
Unknown <crusaderky@gmail.com>
2018-06-12 22:08:28 +0100

    Overhaul dask.collection.optimization

--- dask/collection/optimization.py
+++ dask/collection/optimization.py
@@ -1,28 +1,54 @@
 from .. import optimization as oz
 from .. import optimization_compile as ozc
+from ..base import tokenize
+from ..core import flatten
+from ..sharedict import ShareDict
 from ..utils import ensure_dict
 
 
+def get_name_from_keys(keys):
+    names = set()
+    for name in flatten(keys):
+        if type(name) is tuple:
+            name = name[0]
+        names.add(name)
+    if len(names) == 1:
+        return names.pop()
+    else:
+        # e.g. xarray.Dataset
+        return 'multiname-' + tokenize(names)
+
+
+def postpersist(x, dsk):
+    if isinstance(dsk, ShareDict):
+        sharedsk = dsk
+    else:
+        sharedsk = ShareDict()
+        name = get_name_from_keys(x.__dask_keys__())
+        sharedsk.update_with_key(dsk, name)
+    func, args = x.__dask_postpersist__()
+    return func(sharedsk, *args)
+
+
 def cull(x):
-    dsk = ensure_dict(x.__dask_graph__())
+    dsk = x.__dask_graph__()
+    if not dsk:
+        return x, {}
+    dsk = ensure_dict(dsk)
     dsk, dependencies = oz.cull(dsk, x.__dask_keys__())
-    func, args = x.__dask_postpersist__()
-    return func(dsk, *args), dependencies
+    return postpersist(x, dsk), dependencies
 
 
-def compiled(x):
-    dsk = ozc.compiled(x.__dask_graph__(), x.__dask_keys__())
-    func, args = x.__dask_postpersist__()
-    return func(dsk, *args)
+def apply_oz(oz_func):
+    def wrapper(x, **kwargs):
+        dsk = x.__dask_graph__()
+        if not dsk:
+            return x
+        dsk = oz_func(dsk, x.__dask_keys__(), **kwargs)
+        return postpersist(x, dsk)
+    return wrapper
 
 
-def preserve_keys(x):
-    dsk = ozc.preserve_keys(x.__dask_graph__(), x.__dask_keys__())
-    func, args = x.__dask_postpersist__()
-    return func(dsk, *args)
-
-
-def preserve_deps(x):
-    dsk = ozc.preserve_deps(x.__dask_graph__(), x.__dask_keys__())
-    func, args = x.__dask_postpersist__()
-    return func(dsk, *args)
+compiled = apply_oz(ozc.compiled)
+preserve_keys = apply_oz(ozc.preserve_keys)
+preserve_deps = apply_oz(ozc.preserve_deps)
