51a49981f9b41eec29db0c35a35f783b121c9ac7
Matthew Rocklin <mrocklin@gmail.com>
2018-06-14 13:56:14 -0400

    bump version to 0.18.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ extras_require = {
   'bag': ['cloudpickle >= 0.2.1', 'toolz >= 0.7.3', 'partd >= 0.3.8'],
   'dataframe': ['numpy >= 1.11.0', 'pandas >= 0.19.0', 'toolz >= 0.7.3',
                 'partd >= 0.3.8', 'cloudpickle >= 0.2.1'],
-  'distributed': ['distributed >= 1.21'],
+  'distributed': ['distributed >= 1.22'],
   'delayed': ['toolz >= 0.7.3'],
 }
 extras_require['complete'] = sorted(set(sum(extras_require.values(), [])))
