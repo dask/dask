f7eb20aafdf1f590203475f9cc4260425e7ff8d6
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-06-24 20:19:22 -0400

    Expose `rollaxis` to API
    Make `rollaxis` part of the Dask Array public API.

--- dask/array/__init__.py
+++ dask/array/__init__.py
@@ -11,8 +11,8 @@ from .routines import (take, choose, argwhere, where, coarsen, insert,
                        dstack, vstack, hstack, compress, extract, round,
                        count_nonzero, flatnonzero, nonzero, around, isin,
                        isnull, notnull, isclose, allclose, corrcoef, swapaxes,
-                       tensordot, transpose, dot, vdot, matmul, outer,
-                       apply_along_axis, apply_over_axes, result_type,
+                       rollaxis, tensordot, transpose, dot, vdot, matmul,
+                       outer, apply_along_axis, apply_over_axes, result_type,
                        atleast_1d, atleast_2d, atleast_3d, piecewise, flip,
                        flipud, fliplr, einsum, average)
 from .reshape import reshape
