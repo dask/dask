94f076c92bd3e2ab9340a0876e10eab5b278bbd6
Matthew Rocklin <mrocklin@gmail.com>
2018-07-01 20:28:19 -0400

    Use default pygments styling

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -104,7 +104,7 @@ exclude_patterns = []
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
