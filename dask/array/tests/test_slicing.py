ace97204f3f696a89ff8c13965b2bcfbff8085ac
Matthew Rocklin <mrocklin@gmail.com>
2018-07-05 12:30:23 -0400

    Add test using dask array API

--- dask/array/tests/test_slicing.py
+++ dask/array/tests/test_slicing.py
@@ -815,3 +815,8 @@ def test_setitem_with_different_chunks_preserves_shape(params):
     x[mask] = 1
     result = x.compute()
     assert x.shape == result.shape
+
+
+def test_gh3579():
+    assert_eq(np.arange(10)[0::-1], da.arange(10, chunks=3)[0::-1])
+    assert_eq(np.arange(10)[::-1], da.arange(10, chunks=3)[::-1])
