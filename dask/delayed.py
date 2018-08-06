a7b6bbab0483cf8b30b50735ab525423953975a4
Scott Sievert <stsievert@users.noreply.github.com>
2018-08-06 20:46:27 +0000

    DOC: clarify delayed docstring (#3709)
    * DOC: clarify delayed docstring
    
    * tweak docstring [skip ci]

--- dask/delayed.py
+++ dask/delayed.py
@@ -270,9 +270,12 @@ def delayed(obj, name=None, pure=None, nout=None, traverse=True):
     AttributeError("'list' object has no attribute 'not_a_real_method'")
 
     "Magic" methods (e.g. operators and attribute access) are assumed to be
-    pure, meaning that subsequent calls must return the same results.  This is
-    not overrideable. To invoke an impure attribute or operator, you'd need to
-    use it in a delayed function with ``pure=False``.
+    pure, meaning that subsequent calls must return the same results. This
+    behavior is not overrideable through the ``delayed`` call, but can be
+    modified using other ways as described below.
+
+    To invoke an impure attribute or operator, you'd need to use it in a
+    delayed function with ``pure=False``:
 
     >>> class Incrementer(object):
     ...     def __init__(self):
