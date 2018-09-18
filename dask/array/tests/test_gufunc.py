881ec129101906162a13ad9bee559595c3172a6c
Markus Gonser <magonser@mksg.de>
2018-09-18 19:17:51 +0200

    Minor flake8 fixes

--- dask/array/tests/test_gufunc.py
+++ dask/array/tests/test_gufunc.py
@@ -354,14 +354,14 @@ def test_apply_gufunc_axes_01(axes):
 @pytest.mark.parametrize('axes', [[0, 1], [(0,), (1,)]])
 def test_apply_gufunc_axes_01b(axes):
     def mystats(x, y):
-        return np. std(x, axis=-1)*np.mean(y, axis=-1)
+        return np. std(x, axis=-1) * np.mean(y, axis=-1)
 
     a = np.random.randn(10, 5)
     b = np.random.randn(5, 6)
     da_ = da.from_array(a, chunks=2)
     db_ = da.from_array(b, chunks=2)
 
-    m = np.std(a, axis=0)*np.mean(b, axis=1)
+    m = np.std(a, axis=0) * np.mean(b, axis=1)
     dm = apply_gufunc(mystats, "(i),(j)->()", da_, db_, axes=axes, allow_rechunk=True)
     assert_eq(m, dm)
 
