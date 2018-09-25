b0d1df4b9e8edc9e2062f7c3ac09b0e5643ffbaa
Jim Crist <jiminy.crist@gmail.com>
2018-09-25 13:17:31 -0500

    Fix dask.dataframe.optimize test

--- dask/dataframe/io/tests/test_parquet.py
+++ dask/dataframe/io/tests/test_parquet.py
@@ -427,7 +427,8 @@ def test_optimize(tmpdir, c):
     assert_eq(df[c], ddf2[c])
     x = ddf2[c]
 
-    dsk = x.__dask_optimize__(x.dask, x.__dask_keys__())
+    with dask.config.set(fuse_rename_keys=False):
+        dsk = x.__dask_optimize__(x.dask, x.__dask_keys__())
     assert len(dsk) == x.npartitions
     assert all(v[4] == c for v in dsk.values())
 
