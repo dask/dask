9c842a09ebfca3b42c4bccbf59cf9aaeff8e2161
Matthew Rocklin <mrocklin@gmail.com>
2018-09-29 19:14:56 -0400

    add docstrings and import_required message

--- dask/tests/test_datasets.py
+++ dask/tests/test_datasets.py
@@ -9,3 +9,13 @@ def test_mimesis():
     assert b.take(5)
 
     assert b.take(3) == b.take(3)
+
+
+def test_no_mimesis():
+    try:
+        import mimesis  # noqa: F401
+    except ImportError:
+        with pytest.raises(Exception) as info:
+            dask.datasets.make_people()
+
+        assert "pip install mimesis" in str(info.value)
