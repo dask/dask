ecc1e806898410784efc3d4cecf1f02ea5ae2de1
Matthew Rocklin <mrocklin@gmail.com>
2018-10-01 14:34:34 -0400

    Sort result in Bag.frequencies (#4033)

--- dask/bag/tests/test_bag.py
+++ dask/bag/tests/test_bag.py
@@ -1,13 +1,13 @@
 # coding=utf-8
 from __future__ import absolute_import, division, print_function
 
-import pytest
+from itertools import repeat
 import math
 import os
 import random
-from itertools import repeat
 
 import partd
+import pytest
 from toolz import merge, join, filter, identity, valmap, groupby, pluck
 
 import dask
@@ -243,6 +243,11 @@ def test_frequencies():
     bag = db.from_sequence([0, 0, 0, 0], npartitions=4)
     bag2 = bag.filter(None).frequencies(split_every=2)
     assert_eq(bag2, [])
+
+
+def test_frequencies_sorted():
+    b = db.from_sequence(['a', 'b', 'b', 'b', 'c', 'c'])
+    assert list(b.frequencies(sort=True).compute()) == [('b', 3), ('c', 2), ('a', 1)]
 
 
 def test_topk():
