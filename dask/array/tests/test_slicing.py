3c5c00c8958ba9a6a94bf2f4f1d9e733e917a674
Matthew Rocklin <mrocklin@gmail.com>
2018-10-03 12:32:28 -0400

    Avoid asarray and lock arguments for from_array(getitem) (#4044)
    * Avoid asarray and lock arguments for from_array(getitem)
    
    Fixes #4043
    
    * != -> is not

--- dask/array/tests/test_slicing.py
+++ dask/array/tests/test_slicing.py
@@ -820,3 +820,13 @@ def test_setitem_with_different_chunks_preserves_shape(params):
 def test_gh3579():
     assert_eq(np.arange(10)[0::-1], da.arange(10, chunks=3)[0::-1])
     assert_eq(np.arange(10)[::-1], da.arange(10, chunks=3)[::-1])
+
+
+@pytest.mark.parametrize('lock', [True, False])
+@pytest.mark.parametrize('asarray', [True, False])
+@pytest.mark.parametrize('fancy', [True, False])
+def test_gh4043(lock, asarray, fancy):
+    a1 = da.from_array(np.zeros(3,), chunks=1, asarray=asarray, lock=lock, fancy=fancy)
+    a2 = da.from_array(np.ones(3,), chunks=1, asarray=asarray, lock=lock, fancy=fancy)
+    al = da.stack([a1, a2])
+    assert_eq(al, al)
