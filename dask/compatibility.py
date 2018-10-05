5d720ea16c81b43f49c1c6c7dd0925567d922ae4
Matthew Rocklin <mrocklin@gmail.com>
2018-10-05 15:27:54 -0400

    remove exec

--- dask/compatibility.py
+++ dask/compatibility.py
@@ -111,9 +111,6 @@ if PY3:
             raise exc.with_traceback(tb)
         raise exc
 
-    def exec_(codestr, glbls):
-        exec(codestr, glbls)
-
 else:
     import __builtin__ as builtins
     import copy_reg as copyreg
@@ -153,10 +150,6 @@ else:
 
     reraise = _make_reraise()
     del _make_reraise
-
-    eval(compile(("def exec_(codestr, glbls):\n"
-                  "    exec codestr in glbls\n"),
-                  "<_exec>", "exec"))
 
     def _getargspec(func):
         return inspect.getargspec(func)
