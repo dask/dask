1c3b5abba413e21f873868ee9599bfc82dd96707
Matthew Rocklin <mrocklin@gmail.com>
2018-10-26 07:37:59 -0400

    Avoid collections.Mapping/Sequence (#4138)
    These are deprecated

--- dask/array/top.py
+++ dask/array/top.py
@@ -1,4 +1,3 @@
-import collections
 import itertools
 import numbers
 
@@ -6,7 +5,7 @@ import numpy as np
 import toolz
 
 from .. import base, core, sharedict, utils
-from ..compatibility import apply
+from ..compatibility import apply, Mapping
 from ..delayed import to_task_dask
 from ..optimization import SubgraphCallable
 
@@ -117,7 +116,7 @@ def _top(func, output, output_indices, *arrind_pairs, **kwargs):
     return graph
 
 
-class TOP(collections.Mapping):
+class TOP(Mapping):
     """ Tensor Operation
 
     This is a lazily constructed mapping for tensor operation graphs.
