b6f5aa2a95dc5b511b3ca0ca3f1cc80029730b48
David Hoese <david.hoese@ssec.wisc.edu>
2018-12-24 09:50:55 -0600

    Switch to using config object directly
    Instead of using internal dictionary. Requires donfig 0.2.1+

--- dask/context.py
+++ dask/context.py
@@ -7,9 +7,6 @@ import threading
 from functools import partial
 from . import config
 
-_globals = config.config
-
-
 thread_state = threading.local()
 
 
@@ -65,13 +62,13 @@ def globalmethod(default=None, key=None, falsey=None):
 class GlobalMethod(object):
     def __init__(self, default, key, falsey=None):
         self._default = default
-        self._key = config.normalize_key(key)
+        self._key = key
         self._falsey = falsey
 
     def __get__(self, instance, owner=None):
-        if self._key in _globals:
-            if _globals[self._key]:
-                return _globals[self._key]
+        if self._key in config:
+            if config[self._key]:
+                return config[self._key]
             elif self._falsey is not None:
                 return self._falsey
         return self._default
