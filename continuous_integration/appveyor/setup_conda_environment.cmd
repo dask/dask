6f8071de163f399f42e4af834dbeeb6d66f17d7a
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-01-14 12:57:52 -0600

    pyarrow, fastparquet from cf

--- continuous_integration/appveyor/setup_conda_environment.cmd
+++ continuous_integration/appveyor/setup_conda_environment.cmd
@@ -25,12 +25,13 @@ copy NUL %CONDA_PREFIX%\conda-meta\pinned
 
 @rem Install optional dependencies for tests
 %CONDA_INSTALL% numpy pandas cloudpickle distributed
-%CONDA_INSTALL% bcolz bokeh h5py ipython lz4 psutil pytables s3fs scipy pyarrow fastparquet
+%CONDA_INSTALL% bcolz bokeh h5py ipython lz4 psutil pytables s3fs scipy
+%CONDA_INSTALL% -c conda-forge pyarrow fastparquet
 
 %PIP_INSTALL% --no-deps --upgrade locket git+https://github.com/dask/partd
 %PIP_INSTALL% --no-deps --upgrade heapdict git+https://github.com/dask/cachey
 %PIP_INSTALL%           --upgrade git+https://github.com/dask/distributed
-%PIP_INSTALL% --no-deps --upgrade git+https://github.com/pydata/sparse
+%PIP_INSTALL% --no-deps           git+https://github.com/pydata/sparse
 %PIP_INSTALL% --no-deps --upgrade blosc --upgrade
 %PIP_INSTALL% --no-deps moto Jinja2 boto boto3 botocore cryptography requests xmltodict six werkzeug PyYAML pytz python-dateutil python-jose mock docker jsondiff==1.1.2 aws-xray-sdk responses idna cfn-lint
 
