7c2b4ef609b00b5b25774ec588ce4b20660bd6c0
Matthew Rocklin <mrocklin@gmail.com>
2019-01-29 20:04:43 -0800

    replace WINDOWS with explicit check

--- dask/diagnostics/tests/test_profiler.py
+++ dask/diagnostics/tests/test_profiler.py
@@ -1,9 +1,9 @@
 from operator import add, mul
 import os
+import sys
 from time import sleep
 from distutils.version import LooseVersion
 
-from dask.compatibility import WINDOWS
 from dask.diagnostics import Profiler, ResourceProfiler, CacheProfiler
 from dask.threaded import get
 from dask.utils import ignoring, tmpfile
@@ -76,7 +76,7 @@ def test_two_gets():
     assert len(prof.results) == n + m + n
 
 
-@pytest.mark.skipif(WINDOWS, reason="https://github.com/dask/dask/issues/4432")
+@pytest.mark.skipif(sys.platform == 'win32', reason="https://github.com/dask/dask/issues/4432")
 @pytest.mark.skipif("not psutil")
 def test_resource_profiler():
     with ResourceProfiler(dt=0.01) as rprof:
@@ -101,7 +101,7 @@ def test_resource_profiler():
     assert len(rprof.results) > 0
 
 
-@pytest.mark.skipif(WINDOWS, reason="https://github.com/dask/dask/issues/4432")
+@pytest.mark.skipif(sys.platform == 'win32', reason="https://github.com/dask/dask/issues/4432")
 @pytest.mark.skipif("not psutil")
 def test_resource_profiler_multiple_gets():
     with ResourceProfiler(dt=0.01) as rprof:
@@ -150,7 +150,7 @@ def test_cache_profiler():
     assert CacheProfiler(metric=nbytes, metric_name='foo')._metric_name == 'foo'
 
 
-@pytest.mark.skipif(WINDOWS, reason="https://github.com/dask/dask/issues/4432")
+@pytest.mark.skipif(sys.platform == 'win32', reason="https://github.com/dask/dask/issues/4432")
 @pytest.mark.parametrize(
     'profiler',
     [Profiler,
@@ -239,7 +239,7 @@ def test_profiler_plot():
     assert len(record) == 0
 
 
-@pytest.mark.skipif(WINDOWS, reason="https://github.com/dask/dask/issues/4432")
+@pytest.mark.skipif(sys.platform == 'win32', reason="https://github.com/dask/dask/issues/4432")
 @pytest.mark.skipif("not bokeh")
 @pytest.mark.skipif("not psutil")
 def test_resource_profiler_plot():
