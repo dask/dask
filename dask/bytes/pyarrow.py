dask/dask

commit 57ed629c9c453d971f9f516e096c63d4307550d7

Pass username as 'user' when using pyarrow (#4438)
fixes #4437

dask/bytes/pyarrow.py
@@ -21,11 +21,18 @@ class HDFS3Wrapper(pa.filesystem.DaskFileSystem):
 _MIN_PYARROW_VERSION_SUPPORTED = '0.8.1.dev81'
 
 
+def update_hdfs_options(options):
+    username = options.pop('username', None)
+    if username is not None:
+        options['user'] = username
+    return options
+
+
 class PyArrowHadoopFileSystem(object):
     sep = "/"
 
     def __init__(self, **kwargs):
-        self.fs = pa.hdfs.HadoopFileSystem(**kwargs)
+        self.fs = pa.hdfs.HadoopFileSystem(update_hdfs_options(**kwargs))
 
     @classmethod
     def from_pyarrow(cls, fs):

commit 6c8c4143ff71ffd3fc9362751aad2c3079734b66

Fix bug introduced by #4438
Pass kwargs as dict without splicing.

dask/bytes/pyarrow.py
@@ -32,7 +32,7 @@ class PyArrowHadoopFileSystem(object):
     sep = "/"
 
     def __init__(self, **kwargs):
-        self.fs = pa.hdfs.HadoopFileSystem(update_hdfs_options(**kwargs))
+        self.fs = pa.hdfs.HadoopFileSystem(update_hdfs_options(kwargs))
 
     @classmethod
     def from_pyarrow(cls, fs):