61e1b5c134bf8524934f6cc08033d5067d0c83e0
Matthew Rocklin <mrocklin@gmail.com>
2019-02-08 12:06:10 -0800

    flake8

--- dask/dataframe/backends.py
+++ dask/dataframe/backends.py
@@ -12,4 +12,4 @@ from .core import get_parallel_type, meta_nonempty, make_meta
 @meta_nonempty.register_lazy('cudf')
 @make_meta.register_lazy('cudf')
 def _register_cudf():
-    import dask_cudf
+    import dask_cudf  # noqa: F401
