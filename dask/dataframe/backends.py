4a60e275235928a4a59f9ea64f9734138826e9e6
Matthew Rocklin <mrocklin@gmail.com>
2019-02-08 11:03:54 -0800

    Replace cudf backend code with dask_cudf import
    This allows us to delegate control of the cudf backend system to dask-cudf.
    This allows that project to move quickly without needing changes upstream.
    It also removes cuda specific code from Dask, even if optional.

--- dask/dataframe/backends.py
+++ dask/dataframe/backends.py
@@ -12,28 +12,4 @@ from .core import get_parallel_type, meta_nonempty, make_meta
 @meta_nonempty.register_lazy('cudf')
 @make_meta.register_lazy('cudf')
 def _register_cudf():
-    import cudf
     import dask_cudf
-    get_parallel_type.register(cudf.DataFrame, lambda _: dask_cudf.DataFrame)
-    get_parallel_type.register(cudf.Series, lambda _: dask_cudf.Series)
-    get_parallel_type.register(cudf.Index, lambda _: dask_cudf.Index)
-
-    @meta_nonempty.register((cudf.DataFrame, cudf.Series, cudf.Index))
-    def _(x):
-        y = meta_nonempty(x.to_pandas())  # TODO: add iloc[:5]
-        return cudf.from_pandas(y)
-
-    @make_meta.register((cudf.Series, cudf.DataFrame))
-    def _(x):
-        return x.head(0)
-
-    @make_meta.register(cudf.Index)
-    def _(x):
-        return x[:0]
-
-    @concat_dispatch.register((cudf.DataFrame, cudf.Series, cudf.Index))
-    def _(dfs, axis=0, join='outer', uniform=False, filter_warning=True):
-        assert axis == 0
-        assert join == 'outer'
-        assert filter_warning is True
-        return cudf.concat(dfs)
