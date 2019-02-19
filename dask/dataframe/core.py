f20d33b99f9dd244ae29e89b985c93d81efd146e
Matthew Rocklin <mrocklin@gmail.com>
2019-02-19 08:13:37 -0800

    Add error message around set_index(inplace=True) (#4501)
    * Add error message around set_index(inplace=True)
    
    Fixes https://github.com/dask/dask/issues/4484
    
    * Add inplace to set_index docstring

--- dask/dataframe/core.py
+++ dask/dataframe/core.py
@@ -2610,7 +2610,7 @@ class DataFrame(_Frame):
         return self[list(cs)]
 
     def set_index(self, other, drop=True, sorted=False, npartitions=None,
-                  divisions=None, **kwargs):
+                  divisions=None, inplace=False, **kwargs):
         """Set the DataFrame index (row labels) using an existing column
 
         This realigns the dataset to be sorted by a new column.  This can have a
@@ -2655,6 +2655,9 @@ class DataFrame(_Frame):
             that if ``sorted=True``, specified divisions are assumed to match
             the existing partitions in the data. If this is untrue, you should
             leave divisions empty and call ``repartition`` after ``set_index``.
+        inplace : bool, optional
+            Modifying the DataFrame in place is not supported by Dask.
+            Defaults to False.
         compute: bool
             Whether or not to trigger an immediate computation. Defaults to False.
 
@@ -2673,6 +2676,8 @@ class DataFrame(_Frame):
         >>> divisions = pd.date_range('2000', '2010', freq='1D')
         >>> df2 = df.set_index('timestamp', sorted=True, divisions=divisions)  # doctest: +SKIP
         """
+        if inplace:
+            raise NotImplementedError("The inplace= keyword is not supported")
         pre_sorted = sorted
         del sorted
 
