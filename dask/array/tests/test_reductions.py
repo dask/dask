ebf8bfa0f64b5be89654c583e3777371abf1c2b3
Peter Andreas Entschev <peter@entschev.com>
2019-03-10 21:03:48 +0100

    Fix numel() test, previously failing in Python 2.7.

--- dask/array/tests/test_reductions.py
+++ dask/array/tests/test_reductions.py
@@ -23,12 +23,12 @@ def test_numel(dtype, keepdims):
     x = np.ones((2, 3, 4))
 
     for length in range(x.ndim):
-        for sub in itertools.combinations([x for x in range(x.ndim)], length):
+        for sub in itertools.combinations([d for d in range(x.ndim)], length):
             assert_eq(da.reductions.numel(x, axis=sub, keepdims=keepdims, dtype=dtype),
                       np.sum(x, axis=sub, keepdims=keepdims, dtype=dtype))
 
     for length in range(x.ndim):
-        for sub in itertools.combinations([x for x in range(x.ndim)], length):
+        for sub in itertools.combinations([d for d in range(x.ndim)], length):
             ssub = np.random.shuffle(list(sub))
             assert_eq(da.reductions.numel(x, axis=ssub, keepdims=keepdims, dtype=dtype),
                       np.sum(x, axis=ssub, keepdims=keepdims, dtype=dtype))
