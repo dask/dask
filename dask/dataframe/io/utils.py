74dbade9ade2c60d8e2864875a8f2cff17525f9f
Martin Durant <martin.durant@utoronto.ca>
2019-03-29 23:25:48 -0400

    lint

--- dask/dataframe/io/utils.py
+++ dask/dataframe/io/utils.py
@@ -10,12 +10,15 @@ def _get_pyarrow_dtypes(schema, categories):
     has_pandas_metadata = schema.metadata is not None and b'pandas' in schema.metadata
     if has_pandas_metadata:
         pandas_metadata = json.loads(schema.metadata[b'pandas'].decode('utf8'))
-        pandas_metadata_dtypes = {c.get('field_name', c.get('name', None)):
-                                      c['numpy_type']
-                                  for c in pandas_metadata.get('columns', [])}
-        tz = {c.get('field_name', c.get('name', None)): c[
-            'metadata'].get('timezone', None)
-              for c in pandas_metadata.get('columns', []) if c['metadata']}
+        pandas_metadata_dtypes = {
+            c.get('field_name', c.get('name', None)):
+                c['numpy_type']
+            for c in pandas_metadata.get('columns', [])}
+        tz = {
+            c.get('field_name', c.get('name', None)):
+                c['metadata'].get('timezone', None)
+            for c in pandas_metadata.get('columns', []) if c['metadata']
+        }
     else:
         pandas_metadata_dtypes = {}
 
