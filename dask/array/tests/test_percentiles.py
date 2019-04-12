3e32ccb07390441fe7682c149212375af0df1e29
Janne Vuorela <jannevv@gmail.com>
2019-04-12 10:37:35 +0300

    Add tests for array.percentile() when using 'tdigest' method

--- dask/array/tests/test_percentiles.py
+++ dask/array/tests/test_percentiles.py
@@ -9,18 +9,34 @@ from dask.array.utils import assert_eq, same_keys
 
 def test_percentile():
     d = da.ones((16,), chunks=(4,))
-    assert_eq(da.percentile(d, [0, 50, 100]),
+    qs = [0, 50, 100]
+
+    assert_eq(da.percentile(d, qs),
+              np.array([1, 1, 1], dtype=d.dtype))
+
+    assert_eq(da.percentile(d, qs, method='tdigest'),
               np.array([1, 1, 1], dtype=d.dtype))
 
     x = np.array([0, 0, 5, 5, 5, 5, 20, 20])
     d = da.from_array(x, chunks=(3,))
-    result = da.percentile(d, [0, 50, 100])
-    assert_eq(da.percentile(d, [0, 50, 100]),
+
+    result = da.percentile(d, qs)
+    assert_eq(result,
               np.array([0, 5, 20], dtype=result.dtype))
-    assert same_keys(da.percentile(d, [0, 50, 100]),
-                     da.percentile(d, [0, 50, 100]))
-    assert not same_keys(da.percentile(d, [0, 50, 100]),
+
+    result = da.percentile(d, qs, method='tdigest')
+    assert_eq(result,
+              np.array([0, 5, 20], dtype=result.dtype))
+
+    assert same_keys(da.percentile(d, qs),
+                     da.percentile(d, qs))
+    assert not same_keys(da.percentile(d, qs),
                          da.percentile(d, [0, 50]))
+
+    assert same_keys(da.percentile(d, qs, method='tdigest'),
+                     da.percentile(d, qs, method='tdigest'))
+    assert not same_keys(da.percentile(d, qs, method='tdigest'),
+                         da.percentile(d, [0, 50], method='tdigest'))
 
     x = np.array(['a', 'a', 'd', 'd', 'd', 'e'])
     d = da.from_array(x, chunks=(3,))
@@ -51,6 +67,7 @@ def test_percentile_with_categoricals():
 def test_percentiles_with_empty_arrays():
     x = da.ones(10, chunks=((5, 0, 5),))
     assert_eq(da.percentile(x, [10, 50, 90]), np.array([1, 1, 1], dtype=x.dtype))
+    assert_eq(da.percentile(x, [10, 50, 90], method='tdigest'), np.array([1, 1, 1], dtype=x.dtype))
 
 
 @pytest.mark.parametrize('q', [5, 5.0, np.int64(5), np.float64(5)])
@@ -59,6 +76,7 @@ def test_percentiles_with_scaler_percentile(q):
     # See #3020
     d = da.ones((16,), chunks=(4,))
     assert_eq(da.percentile(d, q), np.array([1], dtype=d.dtype))
+    assert_eq(da.percentile(d, q, method='tdigest'), np.array([1], dtype=d.dtype))
 
 
 def test_unknown_chunk_sizes():
@@ -68,7 +86,17 @@ def test_unknown_chunk_sizes():
     result = da.percentile(x, 50).compute()
     assert 0.1 < result < 0.9
 
+    result = da.percentile(x, 50, method='tdigest').compute()
+    assert 0.1 < result < 0.9
+
     a, b = da.percentile(x, [40, 60]).compute()
     assert 0.1 < a < 0.9
     assert 0.1 < b < 0.9
     assert a < b
+
+    a, b = da.percentile(x, [40, 60], method='tdigest').compute()
+    assert 0.1 < a < 0.9
+    assert 0.1 < b < 0.9
+    assert a < b
+
+
