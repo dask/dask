b5655166259a7b02a0fc57df48dda33e5a1816e3
Janne Vuorela <jannevv@gmail.com>
2019-04-23 21:45:51 +0300

    Change to use pytest.mark.parametrize when needed

--- dask/array/tests/test_percentiles.py
+++ dask/array/tests/test_percentiles.py
@@ -7,41 +7,31 @@ import dask.array as da
 from dask.array.utils import assert_eq, same_keys
 
 
-def test_percentile():
+@pytest.mark.parametrize('method', ['tdigest', 'dask'])
+def test_percentile(method):
     d = da.ones((16,), chunks=(4,))
     qs = [0, 50, 100]
 
-    assert_eq(da.percentile(d, qs),
-              np.array([1, 1, 1], dtype=d.dtype))
-
-    assert_eq(da.percentile(d, qs, method='tdigest'),
+    assert_eq(da.percentile(d, qs, method=method),
               np.array([1, 1, 1], dtype=d.dtype))
 
     x = np.array([0, 0, 5, 5, 5, 5, 20, 20])
     d = da.from_array(x, chunks=(3,))
 
-    result = da.percentile(d, qs)
+    result = da.percentile(d, qs, method=method)
     assert_eq(result,
               np.array([0, 5, 20], dtype=result.dtype))
 
-    result = da.percentile(d, qs, method='tdigest')
-    assert_eq(result,
-              np.array([0, 5, 20], dtype=result.dtype))
+    assert same_keys(da.percentile(d, qs, method=method),
+                     da.percentile(d, qs, method=method))
+    assert not same_keys(da.percentile(d, qs, method=method),
+                         da.percentile(d, [0, 50], method=method))
 
-    assert same_keys(da.percentile(d, qs),
-                     da.percentile(d, qs))
-    assert not same_keys(da.percentile(d, qs),
-                         da.percentile(d, [0, 50]))
-
-    assert same_keys(da.percentile(d, qs, method='tdigest'),
-                     da.percentile(d, qs, method='tdigest'))
-    assert not same_keys(da.percentile(d, qs, method='tdigest'),
-                         da.percentile(d, [0, 50], method='tdigest'))
-
-    x = np.array(['a', 'a', 'd', 'd', 'd', 'e'])
-    d = da.from_array(x, chunks=(3,))
-    assert_eq(da.percentile(d, [0, 50, 100]),
-              np.array(['a', 'd', 'e'], dtype=x.dtype))
+    if method != 'tdigest':
+        x = np.array(['a', 'a', 'd', 'd', 'd', 'e'])
+        d = da.from_array(x, chunks=(3,))
+        assert_eq(da.percentile(d, [0, 50, 100]),
+                  np.array(['a', 'd', 'e'], dtype=x.dtype))
 
 
 @pytest.mark.skip
@@ -64,10 +54,10 @@ def test_percentile_with_categoricals():
                      da.percentile(x, [50]))
 
 
-def test_percentiles_with_empty_arrays():
+@pytest.mark.parametrize('method', ['tdigest', 'dask'])
+def test_percentiles_with_empty_arrays(method):
     x = da.ones(10, chunks=((5, 0, 5),))
-    assert_eq(da.percentile(x, [10, 50, 90]), np.array([1, 1, 1], dtype=x.dtype))
-    assert_eq(da.percentile(x, [10, 50, 90], method='tdigest'), np.array([1, 1, 1], dtype=x.dtype))
+    assert_eq(da.percentile(x, [10, 50, 90], method=method), np.array([1, 1, 1], dtype=x.dtype))
 
 
 @pytest.mark.parametrize('q', [5, 5.0, np.int64(5), np.float64(5)])
