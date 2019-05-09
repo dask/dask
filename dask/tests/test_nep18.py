ef178b26b952958c88004149747d2ccc3eccaa24
Peter Andreas Entschev <peter@entschev.com>
2019-05-09 16:46:23 +0200

    NEP-18 tests: move (most) imports to the top

--- dask/tests/test_nep18.py
+++ dask/tests/test_nep18.py
@@ -6,15 +6,15 @@
 #
 # Passing tests are added at the bottom after the "PASSING" comment line
 
-import pytest
+import numpy as np
+import cupy
+import dask.array as da
+from cupy import asnumpy
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+from dask.array.utils import assert_eq
+
 
 def test_dask_mean_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
-
     x = cupy.random.random((1000, 1000))
 
     d = da.from_array(x, chunks=(100, 100), asarray=False)
@@ -22,13 +22,8 @@ def test_dask_mean_nep18():
     assert_array_equal(asnumpy(x.mean()), asnumpy(d.mean().compute()))
     assert_array_equal(asnumpy(np.cov(x)), asnumpy(da.cov(d).compute()))
 
-def test_cupy_conj_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
+def test_cupy_conj_nep18():
     x = cupy.random.random(5000000)
 
     d = da.from_array(x, chunks=(1000000), asarray=False)
@@ -36,12 +31,7 @@ def test_cupy_conj_nep18():
 
 
 def test_dask_glm_algorithms_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
     import dask_glm.algorithms
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
     X = np.random.random((1000, 10))
     dX = da.from_array(X, chunks=(100, 10), asarray=False)
@@ -75,44 +65,25 @@ def test_dask_glm_algorithms_nep18():
 
 
 def test_cupy_isclose_nep18():
-    import numpy as np
-    import cupy
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
-
     x = cupy.random.random((5000, 1000))
     assert_array_equal(np.isclose(x, x), asnumpy(cupy.isclose(x, x)))
 
-def test_cupy_types_nep18():
-    import numpy as np
-    import cupy
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
+def test_cupy_types_nep18():
     x = cupy.random.random((5000, 1000))
-    assert cupy.can_cast(x, numpy.float16, 'safe') == True
+    assert cupy.can_cast(x, np.float16, 'safe')
     assert cupy.common_type(x, x) == cupy.float64
 
-def test_dask_diag_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_almost_equal
 
+def test_dask_diag_nep18():
     x = cupy.random.random(5000)
 
     d = da.from_array(x, chunks=(1000), asarray=False)
 
     assert_array_almost_equal(asnumpy(np.diag(x)), asnumpy(da.diag(d).compute()))
 
-def test_dask_bincount_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
+def test_dask_bincount_nep18():
     x = cupy.random.randint(0, 100, size=(5000))
 
     d = da.from_array(x, chunks=(1000), asarray=False)
@@ -120,13 +91,8 @@ def test_dask_bincount_nep18():
     assert_array_equal(asnumpy(np.bincount(x, minlength=10)), asnumpy(da.bincount(d, minlength=10).compute()))
     assert_array_equal(asnumpy(np.histogram(x, bins=10)), asnumpy(da.histogram(d, bins=10, range=[0, 99])))
 
-def test_dask_roundings_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
+def test_dask_roundings_nep18():
     x = (cupy.random.random(5000) + cupy.random.randint(-1, 1, 5000)) * 10.
 
     d = da.from_array(x, chunks=(1000), asarray=False)
@@ -137,13 +103,8 @@ def test_dask_roundings_nep18():
     assert_array_equal(asnumpy(np.floor(x)), asnumpy(da.floor(d).compute()))
     assert_array_equal(asnumpy(np.round(x)), asnumpy(da.round(d).compute()))
 
-def test_dask_flatnonzero_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
+def test_dask_flatnonzero_nep18():
     x = cupy.random.random(5000) * cupy.random.randint(-1, 1, 5000)
 
     d = da.from_array(x, chunks=(1000), asarray=False)
@@ -153,25 +114,16 @@ def test_dask_flatnonzero_nep18():
 
 ########################### PASSING ###########################
 
-def test_dask_sum_nep18():
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_almost_equal
 
+def test_dask_sum_nep18():
     x = cupy.random.random((1000, 1000))
 
     d = da.from_array(x, chunks=(100, 100), asarray=False)
 
     assert_array_almost_equal(asnumpy(x.sum()), asnumpy(d.sum().compute()))
 
-def test_dask_bitwise_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
+def test_dask_bitwise_nep18():
     x = cupy.random.randint(0, 100, size=(5000, 1000))
 
     d = da.from_array(x, chunks=(1000, 1000), asarray=False)
@@ -181,13 +133,8 @@ def test_dask_bitwise_nep18():
     assert_array_equal(asnumpy(np.bitwise_or(x, x)), asnumpy(da.bitwise_or(d, d).compute()))
     assert_array_equal(asnumpy(np.bitwise_xor(x, x)), asnumpy(da.bitwise_xor(d, d).compute()))
 
-def test_dask_concatenate_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
+def test_dask_concatenate_nep18():
     x = cupy.random.random(5000)
     y = cupy.random.random(5000)
     z = cupy.asnumpy(y)
@@ -196,13 +143,8 @@ def test_dask_concatenate_nep18():
 
     assert_array_equal(asnumpy(np.concatenate((x,y))), asnumpy(da.concatenate(d).compute()))
 
-def test_dask_copysign_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
+def test_dask_copysign_nep18():
     N = 5000000
     C = 1000000
 
@@ -214,26 +156,16 @@ def test_dask_copysign_nep18():
 
     assert_array_equal(asnumpy(np.copysign(x, y)), asnumpy(da.copysign(dx, dy).compute()))
 
-def test_dask_count_nonzero_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
+def test_dask_count_nonzero_nep18():
     x = cupy.random.randint(0, 100, size=(5000))
 
     d = da.from_array(x, chunks=(1000), asarray=False)
 
     assert_array_equal(asnumpy(np.count_nonzero(x)), asnumpy(da.count_nonzero(d).compute()))
 
-def test_dask_deg_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
+def test_dask_deg_nep18():
     x = cupy.random.random(5000)
 
     d = da.from_array(x, chunks=(1000), asarray=False)
@@ -241,13 +173,8 @@ def test_dask_deg_nep18():
     assert_array_equal(asnumpy(np.deg2rad(x)), asnumpy(da.deg2rad(d).compute()))
     assert_array_equal(asnumpy(np.degrees(x)), asnumpy(da.degrees(d).compute()))
 
-def test_dask_trig_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
+def test_dask_trig_nep18():
     N = 5000
     C = 1000
 
@@ -265,39 +192,24 @@ def test_dask_trig_nep18():
 
     assert_array_equal(asnumpy(getattr(np, 'arctan2')(x, x)), asnumpy(getattr(da, 'arctan2')(d, d).compute()))
 
-def test_dask_dot_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_almost_equal
 
+def test_dask_dot_nep18():
     x = cupy.random.random(5000000)
 
     d = da.from_array(x, chunks=(1000000), asarray=False)
 
     assert_array_almost_equal(asnumpy(np.dot(x, x)), asnumpy(da.dot(d, d).compute()))
 
-def test_dask_div_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
+def test_dask_div_nep18():
     x = cupy.random.random(5000)
 
     d = da.from_array(x, chunks=(1000), asarray=False)
 
     assert_array_equal(asnumpy(np.divide(x, x)), asnumpy(da.divide(d, d).compute()))
 
-def test_dask_exp_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
+def test_dask_exp_nep18():
     x = cupy.random.random(5000)
 
     d = da.from_array(x, chunks=(1000), asarray=False)
@@ -305,13 +217,8 @@ def test_dask_exp_nep18():
     assert_array_equal(asnumpy(np.exp(x)), asnumpy(da.exp(d).compute()))
     assert_array_equal(asnumpy(np.expm1(x)), asnumpy(da.expm1(d).compute()))
 
-def test_dask_comparison_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
+def test_dask_comparison_nep18():
     x = cupy.random.random(5000)
     y = cupy.random.random(5000)
 
@@ -322,13 +229,8 @@ def test_dask_comparison_nep18():
     assert_array_equal(asnumpy(np.fmin(x, y)), asnumpy(da.fmin(dx, dy).compute()))
     assert_array_equal(asnumpy(np.fmod(x, y)), asnumpy(da.fmod(dx, dy).compute()))
 
-def test_dask_flip_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
+def test_dask_flip_nep18():
     x = cupy.random.random((50, 50))
 
     d = da.from_array(x, chunks=(10, 10), asarray=False)
@@ -338,13 +240,8 @@ def test_dask_flip_nep18():
     assert_array_equal(asnumpy(np.fliplr(x)), asnumpy(da.fliplr(d).compute()))
     assert_array_equal(asnumpy(np.flipud(x)), asnumpy(da.flipud(d).compute()))
 
-def test_dask_fp_construct_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from cupy import asnumpy
-    from numpy.testing import assert_array_equal
 
+def test_dask_fp_construct_nep18():
     x = cupy.random.random(5000)
     x1, x2 = np.frexp(x)
     xr = np.ldexp(x1, x2)
@@ -361,11 +258,6 @@ def test_dask_fp_construct_nep18():
 
 
 def test_dask_svd_nep18():
-    import numpy as np
-    import cupy
-    import dask.array as da
-    from dask.array.utils import assert_eq
-
     x = cupy.random.random((5000, 1000))
     # Dask doesn't support full_matrices
     # https://github.com/dask/dask/issues/3576
