Fix _meta computation for multidimensional frexp/modf

dask/array/ufunc.py
@@ -300,10 +300,10 @@ def frexp(x):
 
     a = numpy_like_safe(np.empty, np.empty_like,
                         x._meta if hasattr(x, '_meta') else x,
-                        (1, ), dtype=x.dtype)
+                        (1, ) * x.ndim, dtype=x.dtype)
     l, r = np.frexp(a)
     lmeta = l[tuple(slice(0, 0, None) for _ in range(l.ndim))]
-    rmeta = r[tuple(slice(0, 0, None) for _ in range(l.ndim))]
+    rmeta = r[tuple(slice(0, 0, None) for _ in range(r.ndim))]
 
     graph = HighLevelGraph.from_collections(left, ldsk, dependencies=[tmp])
     L = Array(graph, left, chunks=tmp.chunks, meta=lmeta)
@@ -325,10 +325,10 @@ def modf(x):
 
     a = numpy_like_safe(np.empty, np.empty_like,
                         x._meta if hasattr(x, '_meta') else x,
-                        (1, ), dtype=x.dtype)
+                        (1, ) * x.ndim, dtype=x.dtype)
     l, r = np.modf(a)
     lmeta = l[tuple(slice(0, 0, None) for _ in range(l.ndim))]
-    rmeta = r[tuple(slice(0, 0, None) for _ in range(l.ndim))]
+    rmeta = r[tuple(slice(0, 0, None) for _ in range(r.ndim))]
 
     graph = HighLevelGraph.from_collections(left, ldsk, dependencies=[tmp])
     L = Array(graph, left, chunks=tmp.chunks, meta=lmeta)