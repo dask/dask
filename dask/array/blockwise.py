Split _meta computation from blockwise, fix some reduction errors

dask/array/blockwise.py
@@ -11,6 +11,34 @@ from ..highlevelgraph import HighLevelGraph
 from ..blockwise import blockwise as core_blockwise
 
 
+def blockwise_meta(func, dtype, *args, **kwargs):
+    arrays = args[::2]
+    ndims = [a.ndim if hasattr(a, 'ndim') else 0 for a in arrays]
+    args_meta = [arg._meta if hasattr(arg, '_meta') else
+                 arg[tuple(slice(0, 0, None) for _ in range(nd))] if nd > 0 else arg
+                 for arg, nd in zip(arrays, ndims)]
+    kwargs_meta = {k: v._meta if hasattr(v, '_meta') else v for k, v in kwargs.items()}
+
+    # TODO: look for alternative to this, causes issues when using map_blocks()
+    # with np.vectorize, such as dask.array.routines._isnonzero_vec().
+    if isinstance(func, np.vectorize):
+        meta = func(*args_meta)
+        return meta.astype(dtype)
+
+    try:
+        meta = func(*args_meta, **kwargs_meta)
+    except TypeError:
+        # The concatenate argument is an argument introduced by this
+        # function and may not be support by some external functions,
+        # such as in NumPy
+        kwargs_meta.pop('concatenate', None)
+        meta = func(*args_meta, **kwargs_meta)
+    except ValueError:
+        return None
+
+    return meta.astype(dtype)
+
+
 def blockwise(func, out_ind, *args, **kwargs):
     """ Tensor operation: Generalized inner and outer products
 
@@ -199,36 +227,9 @@ def blockwise(func, out_ind, *args, **kwargs):
                         "adjust_chunks values must be callable, int, or tuple")
     chunks = tuple(chunks)
 
-    # Attempt to construct meta, fallback to Array's dtype constructor if it's
-    # a basic type
     try:
-        arrays = args[::2]
-        ndims = [a.ndim if hasattr(a, 'ndim') else 0 for a in arrays]
-        args_meta = [arg._meta if hasattr(arg, '_meta') else
-                     arg[tuple(slice(0, 0, None) for _ in range(nd))] if nd > 0 else arg
-                     for arg, nd in zip(arrays, ndims)]
-        kwargs_meta = {k: v._meta if hasattr(v, '_meta') else v for k, v in kwargs.items()}
-
-        # TODO: look for alternative to this, causes issues when using map_blocks()
-        # with np.vectorize, such as dask.array.routines._isnonzero_vec().
-        if isinstance(func, np.vectorize):
-            meta = func(*args_meta)
-            return Array(graph, out, chunks, meta=meta.astype(dtype))
-
-        try:
-            meta = func(*args_meta, **kwargs_meta)
-        except TypeError:
-            # The concatenate argument is an argument introduced by this
-            # function and may not be support by some external functions,
-            # such as in NumPy
-            kwargs_meta.pop('concatenate')
-            meta = func(*args_meta, **kwargs_meta)
-
-        if np.isscalar(meta):
-            ndim = max([a.ndim for a in arrays])
-            meta = arrays[0]._meta[tuple(slice(0, 0, None)
-                                   for _ in range(ndim))]
-        return Array(graph, out, chunks, meta=meta.astype(dtype))
+        meta = blockwise_meta(func, dtype, *args, **kwargs)
+        return Array(graph, out, chunks, meta=meta)
     except Exception:
         return Array(graph, out, chunks, dtype=dtype)
 

dask/array/reductions.py
@@ -12,7 +12,7 @@ from toolz import compose, partition_all, get, accumulate, pluck
 
 from . import chunk
 from .core import _concatenate2, Array, handle_out
-from .blockwise import blockwise
+from .blockwise import blockwise, blockwise_meta
 from ..blockwise import lol_tuples
 from .creation import arange
 from .ufunc import sqrt
@@ -147,9 +147,14 @@ def reduction(x, chunk, aggregate, axis=None, keepdims=False, dtype=None,
                         for i, c in enumerate(tmp.chunks))
 
     if hasattr(x, '_meta'):
-        meta_inds = tuple(range(x._meta.ndim))
-        reduced_meta = blockwise(chunk, meta_inds, x._meta, meta_inds, axis=axis,
-                                 keepdims=True, dtype=x.dtype)
+        try:
+            reduced_meta = blockwise_meta(chunk, x.dtype, x._meta, axis=axis,
+                                          keepdims=True, meta=True)
+        except TypeError:
+            reduced_meta = blockwise_meta(chunk, x.dtype, x._meta, axis=axis,
+                                          keepdims=True)
+        except ValueError:
+            pass
     else:
         reduced_meta = None
 
@@ -239,10 +244,10 @@ def partial_reduce(func, x, split_every, keepdims=False, dtype=None, name=None,
     meta = x._meta
     if reduced_meta is not None:
         try:
-            meta = func(reduced_meta.compute(scheduler=core.get), meta=True)
+            meta = func(reduced_meta, meta=True)
         # no meta keyword argument exists for func, and it isn't required
         except TypeError:
-            meta = func(reduced_meta.compute(scheduler=core.get))
+            meta = func(reduced_meta)
         # when no work can be computed on the empty array (e.g., func is a ufunc)
         except ValueError:
             pass
@@ -388,9 +393,9 @@ def nannumel(x, **kwargs):
 
 
 def mean_chunk(x, sum=chunk.sum, numel=numel, dtype='f8', meta=False, **kwargs):
-    n = numel(x, dtype=dtype, **kwargs)
     if meta:
-        return n
+        return x
+    n = numel(x, dtype=dtype, **kwargs)
 
     total = sum(x, dtype=dtype, **kwargs)
 
@@ -400,8 +405,10 @@ def mean_chunk(x, sum=chunk.sum, numel=numel, dtype='f8', meta=False, **kwargs):
 def mean_combine(pairs, sum=chunk.sum, numel=numel, dtype='f8', axis=None, meta=False, **kwargs):
     if not isinstance(pairs, list):
         pairs = [pairs]
-    ns = deepmap(lambda pair: pair['n'], pairs)
+
+    ns = deepmap(lambda pair: pair['n'], pairs) if not meta else pairs
     n = _concatenate2(ns, axes=axis).sum(axis=axis, **kwargs)
+
     if meta:
         return n
 
@@ -412,8 +419,9 @@ def mean_combine(pairs, sum=chunk.sum, numel=numel, dtype='f8', axis=None, meta=
 
 
 def mean_agg(pairs, dtype='f8', axis=None, meta=False, **kwargs):
-    ns = deepmap(lambda pair: pair['n'], pairs)
+    ns = deepmap(lambda pair: pair['n'], pairs) if not meta else pairs
     n = _concatenate2(ns, axes=axis).sum(axis=axis, dtype=dtype, **kwargs)
+
     if meta:
         return n
 
@@ -452,9 +460,9 @@ with ignoring(AttributeError):
 
 
 def moment_chunk(A, order=2, sum=chunk.sum, numel=numel, dtype='f8', meta=False, **kwargs):
-    n = numel(A, **kwargs)
     if meta:
-        return n
+        return A
+    n = numel(A, **kwargs)
 
     n = n.astype(np.int64)
     total = sum(A, dtype=dtype, **kwargs)
@@ -476,12 +484,13 @@ def moment_combine(pairs, order=2, ddof=0, dtype='f8', sum=np.sum, axis=None, me
     if not isinstance(pairs, list):
         pairs = [pairs]
 
-    ns = _concatenate2(deepmap(lambda pair: pair['n'], pairs), axes=axis)
-
     kwargs['dtype'] = dtype
     kwargs['keepdims'] = True
 
+    ns = deepmap(lambda pair: pair['n'], pairs) if not meta else pairs
+    ns = _concatenate2(ns, axes=axis)
     n = ns.sum(axis=axis, **kwargs)
+
     if meta:
         return n
 
@@ -507,12 +516,14 @@ def moment_agg(pairs, order=2, ddof=0, dtype='f8', sum=np.sum, axis=None, meta=F
     keepdim_kw = kwargs.copy()
     keepdim_kw['keepdims'] = True
 
-    ns = _concatenate2(deepmap(lambda pair: pair['n'], pairs), axes=axis)
+    ns = deepmap(lambda pair: pair['n'], pairs) if not meta else pairs
+    ns = _concatenate2(ns, axes=axis)
+    n = ns.sum(axis=axis, **keepdim_kw)
+
     if meta:
-        return ns.sum(axis=axis, **kwargs)
+        return n
 
     totals = _concatenate2(deepmap(lambda pair: pair['total'], pairs), axes=axis)
-    n = ns.sum(axis=axis, **keepdim_kw)
     Ms = _concatenate2(deepmap(lambda pair: pair['M'], pairs), axes=axis)
 
     mu = divide(totals.sum(axis=axis, **keepdim_kw), n, dtype=dtype)