84e0489351e98e11eeee632736e7abbe11e94845
jakirkham <jakirkham@gmail.com>
2019-06-12 14:20:59 -0400

    Raise ValueError if concatenate is given no arrays (#4927)
    * Raise `ValueError` if `concatenate` gets no arrays
    
    NumPy will raise if no arrays are provided to concatenate as it is
    unclear what to do. This adds a similar exception for Dask Arrays. Also
    this short circuits handling unusual cases later. Plus raises a clearer
    exception than one might see if this weren't raised.
    
    * Test `concatenate` raises when no arrays are given

--- dask/array/core.py
+++ dask/array/core.py
@@ -2982,6 +2982,10 @@ def concatenate(seq, axis=0, allow_unknown_chunksizes=False):
     seq = [asarray(a) for a in seq]
 
     n = len(seq)
+
+    if n == 0:
+        raise ValueError("Need array(s) to concatenate")
+
     ndim = len(seq[0].shape)
 
     if axis < 0:
