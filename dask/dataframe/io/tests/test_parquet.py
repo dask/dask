07e23a25360cb85d1a87479d19eda04d198c592d
rjzamora <rzamora217@gmail.com>
2019-06-28 15:23:42 -0700

    copying dataframe for safety

--- dask/dataframe/io/tests/test_parquet.py
+++ dask/dataframe/io/tests/test_parquet.py
@@ -615,10 +615,11 @@ def test_optimize(tmpdir, c):
 @write_read_engines()
 def test_roundtrip_from_pandas(tmpdir, write_engine, read_engine):
     fn = str(tmpdir.join("test.parquet"))
-    df.index.name = "index"
-    df.to_parquet(fn, index=True, engine=write_engine)
+    dfp = df.copy()
+    dfp.index.name = "index"
+    dfp.to_parquet(fn, index=True, engine=write_engine)
     ddf = dd.read_parquet(fn, index="index", engine=read_engine)
-    assert_eq(df, ddf)
+    assert_eq(dfp, ddf)
 
 
 @write_read_engines_xfail
