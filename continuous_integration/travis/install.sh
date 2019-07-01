01d00716dea7ff7025adbd8e44f9845fb1f20e3b
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-07-01 14:42:39 -0500

    fixup

--- continuous_integration/travis/install.sh
+++ continuous_integration/travis/install.sh
@@ -24,10 +24,15 @@ conda config --set always_yes yes --set changeps1 no --set remote_max_retries 10
 conda env create -q -n test-environment -f $ENV_FILE python=$PYTHON
 source activate test-environment
 
+# We don't have a conda-forge package for cityhash
+# We don't include it in the conda environment.yaml, since that may
+# make things harder for contributors that don't have a C++ compiler
+pip install --no-deps cityhash
+
 if [[ ${UPSTREAM_DEV} ]]; then
     conda uninstall --force numpy pandas
     pip install -f https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com \
-        -- no-deps \
+        --no-deps \
         --pre \
         numpy \
         pandas
