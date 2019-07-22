b185ba8b2763485a6b1e01f583756b6632e326af
Martin Durant <martindurant@users.noreply.github.com>
2019-07-22 17:18:46 -0400

    fix HDFS in dask (#5130)
    * fix hdfs stuff
    
    * skip new compressions
    
    * apply black [test-hdfs]
    
    * flake

--- dask/bytes/tests/test_compression.py
+++ dask/bytes/tests/test_compression.py
@@ -10,6 +10,8 @@ from ..utils import compress
 def test_files(fmt, File):
     if fmt == "zip" and sys.version_info.minor == 5:
         pytest.skip("zipfile is read-only on py35")
+    if fmt not in compress:
+        pytest.skip("compression function not provided")
     if fmt is None:
         return
     data = b"1234" * 1000
