5b50e6a0a45feae6b026e9639188a2ffc6c95719
Richard (Rick) Zamora <rzamora217@gmail.com>
2019-08-12 14:17:46 -0500

    Add rearrange_by_divisions and set_index support for cudf (#5205)

--- dask/dataframe/shuffle.py
+++ dask/dataframe/shuffle.py
@@ -19,6 +19,7 @@ from ..delayed import delayed
 from ..highlevelgraph import HighLevelGraph
 from ..sizeof import sizeof
 from ..utils import digit, insert, M
+from .utils import series_type_like_df, is_series_like
 
 
 def set_index(
@@ -173,14 +174,16 @@ def set_partition(
     shuffle
     partd
     """
+    divisions = series_type_like_df(df)(divisions)
+    meta = series_type_like_df(df)([0])
     if np.isscalar(index):
         partitions = df[index].map_partitions(
-            set_partitions_pre, divisions=divisions, meta=pd.Series([0])
+            set_partitions_pre, divisions=divisions, meta=meta
         )
         df2 = df.assign(_partitions=partitions)
     else:
         partitions = index.map_partitions(
-            set_partitions_pre, divisions=divisions, meta=pd.Series([0])
+            set_partitions_pre, divisions=divisions, meta=meta
         )
         df2 = df.assign(_partitions=partitions, _index=index)
 
@@ -208,7 +211,7 @@ def set_partition(
             column_dtype=df.columns.dtype,
         )
 
-    df4.divisions = divisions
+    df4.divisions = [v for v in divisions]
 
     return df4.map_partitions(M.sort_index)
 
@@ -255,9 +258,11 @@ def shuffle(df, index, shuffle=None, npartitions=None, max_branch=32, compute=No
 
 def rearrange_by_divisions(df, column, divisions, max_branch=None, shuffle=None):
     """ Shuffle dataframe so that column separates along divisions """
+    divisions = series_type_like_df(df)(divisions)
+    meta = series_type_like_df(df)([0])
     # Assign target output partitions to every row
     partitions = df[column].map_partitions(
-        set_partitions_pre, divisions=divisions, meta=pd.Series([0])
+        set_partitions_pre, divisions=divisions, meta=meta
     )
     df2 = df.assign(_partitions=partitions)
 
@@ -556,15 +561,17 @@ def collect(p, part, meta, barrier_token):
 
 
 def set_partitions_pre(s, divisions):
-    partitions = pd.Series(divisions).searchsorted(s, side="right") - 1
-    partitions[(s >= divisions[-1]).values] = len(divisions) - 2
+    partitions = divisions.searchsorted(s, side="right") - 1
+    if is_series_like(partitions):
+        partitions = partitions.values
+    partitions[(s >= divisions.iloc[-1]).values] = len(divisions) - 2
     return partitions
 
 
 def shuffle_group_2(df, col):
     if not len(df):
         return {}, df
-    ind = df[col]._values.astype(np.int64)
+    ind = df[col].values.astype(np.int64)
     n = ind.max() + 1
     indexer, locations = groupsort_indexer(ind.view(np.int64), n)
     df2 = df.take(indexer)
@@ -613,7 +620,7 @@ def shuffle_group(df, col, stage, k, npartitions):
     else:
         ind = hash_pandas_object(df[col], index=False)
 
-    c = ind._values
+    c = ind.values
     typ = np.min_scalar_type(npartitions * 2)
 
     c = np.mod(c, npartitions).astype(typ, copy=False)
