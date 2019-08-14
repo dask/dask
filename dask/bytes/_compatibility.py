b207fc33644a0b303d0503939241dac35a76fd59
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-08-14 11:13:13 -0500

    test-upstream

--- dask/bytes/_compatibility.py
+++ dask/bytes/_compatibility.py
@@ -2,4 +2,4 @@ from distutils.version import LooseVersion
 import fsspec
 
 FSSPEC_VERSION = LooseVersion(fsspec.__version__)
-FSSPEC_042 = FSSPEC_VERSION >= "0.4.1"
+FSSPEC_042 = FSSPEC_VERSION > "0.4.1"
