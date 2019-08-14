dbea0ea3c95ec4ee8f42e5c8c6e40e5132c3a045
Peter Andreas Entschev <peter@entschev.com>
2019-08-14 17:50:40 +0200

    Avoid adjusting gufunc's meta dtype twice (#5274)
    * Remove xfail from test_Array_numpy_gufunc_call__array_ufunc__01

--- dask/array/tests/test_array_core.py
+++ dask/array/tests/test_array_core.py
@@ -254,7 +254,6 @@ def test_Array_numpy_gufunc_call__array_ufunc__01():
     LooseVersion(np.__version__) < "1.14.0",
     reason="NumPy doesn't have `np.linalg._umath_linalg` yet",
 )
-@pytest.mark.xfail(reason="Protect from `np.linalg._umath_linalg.eig` breaking")
 def test_Array_numpy_gufunc_call__array_ufunc__02():
     x = da.random.normal(size=(3, 10, 10), chunks=(2, 10, 10))
     nx = x.compute()
