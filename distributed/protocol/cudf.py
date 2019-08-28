bc6f4a6395cbb28b493ff7984b941aa1e0dc4fd5
Benjamin Zaitlen <quasiben@users.noreply.github.com>
2019-08-28 15:57:03 -0400

    better name for cudf deserialization function name (#3008)

--- distributed/protocol/cudf.py
+++ distributed/protocol/cudf.py
@@ -16,7 +16,7 @@ def serialize_cudf_dataframe(x):
 
 
 @cuda_deserialize.register((cudf.DataFrame, cudf.Series, cudf.groupby.groupby._Groupby))
-def serialize_cudf_dataframe(header, frames):
+def deserialize_cudf_dataframe(header, frames):
     with log_errors():
         cudf_typ = pickle.loads(header["type"])
         cudf_obj = cudf_typ.deserialize(header, frames)
