d025df938e897a7e43083175e3f776e29b49c1b5
Oliver Hofkens <oliver@novemberfive.co>
2019-08-28 03:27:26 +0200

    Add support for DF and Series groupby().idxmin/max() (#5273)

--- dask/dataframe/tests/test_groupby.py
+++ dask/dataframe/tests/test_groupby.py
@@ -1978,3 +1978,143 @@ def test_groupby_cov(columns):
         assert_eq(expected, result, check_index=False)
     else:
         assert_eq(expected, result)
+
+
+def test_df_groupby_idxmin():
+    pdf = pd.DataFrame(
+        {"idx": list(range(4)), "group": [1, 1, 2, 2], "value": [10, 20, 20, 10]}
+    ).set_index("idx")
+
+    ddf = dd.from_pandas(pdf, npartitions=3)
+
+    expected = pd.DataFrame({"group": [1, 2], "value": [0, 3]}).set_index("group")
+
+    result_pd = pdf.groupby("group").idxmin()
+    result_dd = ddf.groupby("group").idxmin()
+
+    assert_eq(result_pd, result_dd)
+    assert_eq(expected, result_dd)
+
+
+@pytest.mark.parametrize("skipna", [True, False])
+def test_df_groupby_idxmin_skipna(skipna):
+    pdf = pd.DataFrame(
+        {
+            "idx": list(range(4)),
+            "group": [1, 1, 2, 2],
+            "value": [np.nan, 20.1, np.nan, 10.1],
+        }
+    ).set_index("idx")
+
+    ddf = dd.from_pandas(pdf, npartitions=3)
+
+    result_pd = pdf.groupby("group").idxmin(skipna=skipna)
+    result_dd = ddf.groupby("group").idxmin(skipna=skipna)
+
+    assert_eq(result_pd, result_dd)
+
+
+def test_df_groupby_idxmax():
+    pdf = pd.DataFrame(
+        {"idx": list(range(4)), "group": [1, 1, 2, 2], "value": [10, 20, 20, 10]}
+    ).set_index("idx")
+
+    ddf = dd.from_pandas(pdf, npartitions=3)
+
+    expected = pd.DataFrame({"group": [1, 2], "value": [1, 2]}).set_index("group")
+
+    result_pd = pdf.groupby("group").idxmax()
+    result_dd = ddf.groupby("group").idxmax()
+
+    assert_eq(result_pd, result_dd)
+    assert_eq(expected, result_dd)
+
+
+@pytest.mark.parametrize("skipna", [True, False])
+def test_df_groupby_idxmax_skipna(skipna):
+    pdf = pd.DataFrame(
+        {
+            "idx": list(range(4)),
+            "group": [1, 1, 2, 2],
+            "value": [np.nan, 20.1, np.nan, 10.1],
+        }
+    ).set_index("idx")
+
+    ddf = dd.from_pandas(pdf, npartitions=3)
+
+    result_pd = pdf.groupby("group").idxmax(skipna=skipna)
+    result_dd = ddf.groupby("group").idxmax(skipna=skipna)
+
+    assert_eq(result_pd, result_dd)
+
+
+def test_series_groupby_idxmin():
+    pdf = pd.DataFrame(
+        {"idx": list(range(4)), "group": [1, 1, 2, 2], "value": [10, 20, 20, 10]}
+    ).set_index("idx")
+
+    ddf = dd.from_pandas(pdf, npartitions=3)
+
+    expected = (
+        pd.DataFrame({"group": [1, 2], "value": [0, 3]}).set_index("group").squeeze()
+    )
+
+    result_pd = pdf.groupby("group")["value"].idxmin()
+    result_dd = ddf.groupby("group")["value"].idxmin()
+
+    assert_eq(result_pd, result_dd)
+    assert_eq(expected, result_dd)
+
+
+@pytest.mark.parametrize("skipna", [True, False])
+def test_series_groupby_idxmin_skipna(skipna):
+    pdf = pd.DataFrame(
+        {
+            "idx": list(range(4)),
+            "group": [1, 1, 2, 2],
+            "value": [np.nan, 20.1, np.nan, 10.1],
+        }
+    ).set_index("idx")
+
+    ddf = dd.from_pandas(pdf, npartitions=3)
+
+    result_pd = pdf.groupby("group")["value"].idxmin(skipna=skipna)
+    result_dd = ddf.groupby("group")["value"].idxmin(skipna=skipna)
+
+    assert_eq(result_pd, result_dd)
+
+
+def test_series_groupby_idxmax():
+    pdf = pd.DataFrame(
+        {"idx": list(range(4)), "group": [1, 1, 2, 2], "value": [10, 20, 20, 10]}
+    ).set_index("idx")
+
+    ddf = dd.from_pandas(pdf, npartitions=3)
+
+    expected = (
+        pd.DataFrame({"group": [1, 2], "value": [1, 2]}).set_index("group").squeeze()
+    )
+
+    result_pd = pdf.groupby("group")["value"].idxmax()
+    result_dd = ddf.groupby("group")["value"].idxmax()
+
+    assert_eq(result_pd, result_dd)
+    assert_eq(expected, result_dd)
+
+
+@pytest.mark.parametrize("skipna", [True, False])
+def test_series_groupby_idxmax_skipna(skipna):
+    pdf = pd.DataFrame(
+        {
+            "idx": list(range(4)),
+            "group": [1, 1, 2, 2],
+            "value": [np.nan, 20.1, np.nan, 10.1],
+        }
+    ).set_index("idx")
+
+    ddf = dd.from_pandas(pdf, npartitions=3)
+
+    result_pd = pdf.groupby("group")["value"].idxmax(skipna=skipna)
+    result_dd = ddf.groupby("group")["value"].idxmax(skipna=skipna)
+
+    assert_eq(result_pd, result_dd)
