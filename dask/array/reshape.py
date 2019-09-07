0ba222379e2dab8225a7743c3da32de6a8d44809
Scott Sievert <stsievert@users.noreply.github.com>
2019-09-07 15:35:12 +0000

    API: provides method to compute unknown array chunks sizes (#5312)

--- dask/array/reshape.py
+++ dask/array/reshape.py
@@ -171,7 +171,10 @@ def reshape(x, shape):
         shape = tuple(missing_size if s == -1 else s for s in shape)
 
     if np.isnan(sum(x.shape)):
-        raise ValueError("Array chunk size or shape is unknown. shape: %s", x.shape)
+        raise ValueError(
+            "Array chunk size or shape is unknown. shape: %s\n\n"
+            "Possible solution with x.compute_chunk_sizes()" % x.shape
+        )
 
     if reduce(mul, shape, 1) != x.size:
         raise ValueError("total size of new array must be unchanged")
