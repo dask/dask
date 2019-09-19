8e0a1bff8b4a97e32497d457abcbc5fbb72a8c86
James Bourbeau <jrbourbeau@gmail.com>
2019-09-19 11:21:22 -0500

    Bump fsspec in dataframe extra

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ extras_require = {
   'bag': ['fsspec >= 0.5.1', 'cloudpickle >= 0.2.1', 'toolz >= 0.7.3',
           'partd >= 0.3.10'],
   'dataframe': ['numpy >= 1.13.0', 'pandas >= 0.21.0', 'toolz >= 0.7.3',
-                'partd >= 0.3.10', 'cloudpickle >= 0.2.1', 'fsspec >= 0.3.3'],
+                'partd >= 0.3.10', 'cloudpickle >= 0.2.1', 'fsspec >= 0.5.1'],
   'distributed': ['distributed >= 2.0'],
   'diagnostics': ['bokeh >= 1.0.0'],
   'delayed': ['toolz >= 0.7.3'],
