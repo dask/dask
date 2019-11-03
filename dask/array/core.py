111ad146e1ced7eb177b6889ca9ffdfc85afba5b
James Bourbeau <jrbourbeau@users.noreply.github.com>
2019-11-03 13:10:03 -0600

    Removes bytes from sparse array HTML repr (#5556)

--- dask/array/core.py
+++ dask/array/core.py
@@ -54,6 +54,7 @@ from ..utils import (
     M,
     ndimlist,
     format_bytes,
+    typename,
 )
 from ..core import quote
 from ..delayed import delayed, Delayed
@@ -1244,7 +1245,10 @@ class Array(DaskMethodsMixin):
         return "\n".join(both)
 
     def _repr_html_table(self):
-        if not math.isnan(self.nbytes):
+        if "sparse" in typename(type(self._meta)):
+            nbytes = None
+            cbytes = None
+        elif not math.isnan(self.nbytes):
             nbytes = format_bytes(self.nbytes)
             cbytes = format_bytes(np.prod(self.chunksize) * self.dtype.itemsize)
         else:
@@ -1258,7 +1262,9 @@ class Array(DaskMethodsMixin):
             "  </thead>",
             "  <tbody>",
             "    <tr><th> Bytes </th><td> %s </td> <td> %s </td></tr>"
-            % (nbytes, cbytes),
+            % (nbytes, cbytes)
+            if nbytes is not None
+            else "",
             "    <tr><th> Shape </th><td> %s </td> <td> %s </td></tr>"
             % (str(self.shape), str(self.chunksize)),
             "    <tr><th> Count </th><td> %d Tasks </td><td> %d Chunks </td></tr>"
