2ec2413315ed5e35f41c804d7e80caeb861cf3a3
Samuel Kampa <samuelkampa@gmail.com>
2019-11-04 15:24:41 -0500

    Update accessor.py

--- dask/dataframe/accessor.py
+++ dask/dataframe/accessor.py
@@ -137,16 +137,23 @@ class StringAccessor(Accessor):
     def cat(self, others=None, sep=None, na_rep=None):
         from .core import Series, Index
 
-        if others is None:
-            raise NotImplementedError("x.str.cat() with `others == None`")
-
         valid_types = (Series, Index, pd.Series, pd.Index)
-        if isinstance(others, valid_types):
-            others = [others]
-        elif not all(isinstance(a, valid_types) for a in others):
-            raise TypeError("others must be Series/Index")
 
-        return self._series.map_partitions(
+        if others is None:
+            if not isinstance(self, valid_types):
+                raise TypeError("self must be Series/Index")
+            return self._series.map_partitions(
+            str_cat, meta=self._series._meta
+        ).compute().str.cat(sep=sep, na_rep=na_rep)
+
+        else:
+
+            if isinstance(others, valid_types):
+                others = [others]
+            elif not all(isinstance(a, valid_types) for a in others):
+                raise TypeError("others must be Series/Index")
+
+            return self._series.map_partitions(
             str_cat, *others, sep=sep, na_rep=na_rep, meta=self._series._meta
         )
 
