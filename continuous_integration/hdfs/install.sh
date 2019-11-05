8c8a8444de03027a01226ed136854406d6095043
James Bourbeau <jrbourbeau@gmail.com>
2019-11-05 12:00:28 -0600

    Update travis conda env yaml files [test-hdfs]

--- continuous_integration/hdfs/install.sh
+++ continuous_integration/hdfs/install.sh
@@ -1,6 +1,6 @@
 set -xe
 
-docker exec -it $CONTAINER_ID conda install -y -q dask pyarrow">0.13.0" fsspec -c conda-forge
+docker exec -it $CONTAINER_ID conda install -y -q dask pyarrow">=0.14.0" fsspec -c conda-forge
 docker exec -it $CONTAINER_ID pip install -e .
 
 set +xe
