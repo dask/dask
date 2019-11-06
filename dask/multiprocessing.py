7800baed4d1065c47ef37b2e0760bb0f9e1a430f
Guido Imperiale <crusaderky@gmail.com>
2019-11-06 17:37:06 +0000

    Do not confuse novice users

--- dask/multiprocessing.py
+++ dask/multiprocessing.py
@@ -33,7 +33,7 @@ except ImportError:
         except (pickle.PicklingError, AttributeError):
             warn(
                 "pickle failed; install cloudpickle to allow more objects "
-                "(such as lambdas and locally defined functions) to be pickled",
+                "to be pickled",
                 UserWarning,
             )
             raise
