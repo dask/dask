2f46ab18372e747dcf0179341ffcdaa5900aa522
Richard (Rick) Zamora <rzamora217@gmail.com>
2019-11-08 09:28:05 -0600

    Adding PatchedDeviceArray to drop stride attribute for cupy<7.0 (#3198)

--- distributed/protocol/cupy.py
+++ distributed/protocol/cupy.py
@@ -5,6 +5,17 @@ import cupy
 from .cuda import cuda_serialize, cuda_deserialize
 
 
+class PatchedCudaArrayInterface(object):
+    # TODO: This class wont be necessary
+    #       once Cupy<7.0 is no longer supported
+    def __init__(self, ary):
+        cai = ary.__cuda_array_interface__
+        cai_cupy_vsn = cupy.ndarray(0).__cuda_array_interface__["version"]
+        if cai.get("strides") is None and cai_cupy_vsn < 2:
+            cai.pop("strides", None)
+        self.__cuda_array_interface__ = cai
+
+
 @cuda_serialize.register(cupy.ndarray)
 def serialize_cupy_ndarray(x):
     # Making sure `x` is behaving
@@ -18,6 +29,8 @@ def serialize_cupy_ndarray(x):
 @cuda_deserialize.register(cupy.ndarray)
 def deserialize_cupy_array(header, frames):
     (frame,) = frames
+    if not isinstance(frame, cupy.ndarray):
+        frame = PatchedCudaArrayInterface(frame)
     arr = cupy.ndarray(
         header["shape"], dtype=header["typestr"], memptr=cupy.asarray(frame).data
     )
--- distributed/protocol/tests/test_cupy.py
+++ distributed/protocol/tests/test_cupy.py
@@ -1,4 +1,5 @@
 from distributed.protocol import serialize, deserialize
+import pickle
 import pytest
 
 cupy = pytest.importorskip("cupy")
@@ -12,3 +13,19 @@ def test_serialize_cupy(size, dtype):
     y = deserialize(header, frames, deserializers=("cuda", "dask", "pickle", "error"))
 
     assert (x == y).all()
+
+
+@pytest.mark.parametrize("dtype", ["u1", "u4", "u8", "f4"])
+def test_serialize_cupy_from_numba(dtype):
+    numba = pytest.importorskip("numba")
+    np = pytest.importorskip("numpy")
+
+    size = 10
+    x_np = np.arange(size, dtype=dtype)
+    x = numba.cuda.to_device(x_np)
+    header, frames = serialize(x, serializers=("cuda", "dask", "pickle"))
+    header["type-serialized"] = pickle.dumps(cupy.ndarray)
+
+    y = deserialize(header, frames, deserializers=("cuda", "dask", "pickle", "error"))
+
+    assert (x_np == cupy.asnumpy(y)).all()
