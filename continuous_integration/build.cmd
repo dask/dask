6a76ca7ac6db9f6243869057b6805f14c23e3811
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2020-01-16 08:39:36 +0000

    Move Windows CI to GitHub Actions (#3373)
    * Move Windows CI to GitHub Actions
    
    * Switch to CMD and conda
    
    * Remove indirection and add conda environment file
    
    * Fix bad indentation
    
    * Remove Python version
    
    * Name steps, remove unnecessary conda activations, remove JUnit
    
    * Align pytest options with Travis config
    
    * Run single test for faster iterating
    
    * Hmm pytest is fine. Trying two runners to improve performance
    
    * Remove bad -n flag
    
    * Add multicore support as GitHub Actions has dual-core runners
    
    * Removing multicore again as that had some unexpected results
    
    * Try bash instead of powershell

--- continuous_integration/build.cmd
+++
@@ -1,6 +0,0 @@
-call activate %CONDA_ENV%
-
-@echo on
-
-@rem Install Distributed
-%PIP_INSTALL% --no-deps -e .
--- continuous_integration/run_tests.cmd
+++
@@ -1,9 +0,0 @@
-call activate %CONDA_ENV%
-
-@echo on
-
-set PYTHONFAULTHANDLER=1
-
-set PYTEST=py.test --tb=native --timeout=120 -r s
-
-%PYTEST% -v -m "not avoid_travis" --junit-xml="%JUNIT_OUT%" distributed
