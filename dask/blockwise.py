5f8816ce631f76128627efd5e5a9276980131615
Bruce Merry <bmerry@ska.ac.za>
2020-02-24 10:11:33 +0200

    Fix doctest strings for lol_product

--- dask/blockwise.py
+++ dask/blockwise.py
@@ -446,8 +446,8 @@ def lol_product(head, values):
     >>> lol_product(('x',), (1, 2, 3))
     ('x', 1, 2, 3)
     >>> lol_product(('x',), (1, [2, 3], 4, [5, 6]))  # doctest: +NORMALIZE_WHITESPACE
-    [[('x', 1, 2, 5), ('x', 1, 2, 6)],
-     [('x', 1, 3, 5), ('x', 1, 3, 6)]]
+    [[('x', 1, 2, 4, 5), ('x', 1, 2, 4, 6)],
+     [('x', 1, 3, 4, 5), ('x', 1, 3, 4, 6)]]
     """
     if not values:
         return head
