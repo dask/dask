0d7a31adaabd801a189fa529c6b7670fe98395b1
Davis Bennett <davis.v.bennett@gmail.com>
2020-02-28 11:15:44 -0500

    fix typo in docstring (#3528)

--- distributed/deploy/adaptive_core.py
+++ distributed/deploy/adaptive_core.py
@@ -13,7 +13,7 @@ class AdaptiveCore:
     The core logic for adaptive deployments, with none of the cluster details
 
     This class controls our adaptive scaling behavior.  It is intended to be
-    sued as a super-class or mixin.  It expects the following state and methods:
+    used as a super-class or mixin.  It expects the following state and methods:
 
     **State**
 
