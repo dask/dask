061f90feeae6f6a112f11893788470db19365df0
John Kirkham <jakirkham@gmail.com>
2020-03-09 23:02:04 -0700

    Run `black` on `dask/array/backends.py`

--- dask/array/backends.py
+++ dask/array/backends.py
@@ -26,8 +26,9 @@ def register_cupyx():
         from cupy.sparse import hstack
         from cupy.sparse import vstack
     except ImportError:
-        raise ImportError("Stacking of sparse arrays requires "
-                          "at least CuPy version 8.0.0")
+        raise ImportError(
+            "Stacking of sparse arrays requires " "at least CuPy version 8.0.0"
+        )
 
     def _concat_cupy_sparse(L, axis=0):
         if axis == 0:
