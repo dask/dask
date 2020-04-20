35df913972e53df0451dc6331055dd46a5f18316
psimaj <przemyslaw.simajchel@gmail.com>
2020-04-20 04:45:45 +0200

    Add the ewm application function

--- dask/dataframe/ewm.py
+++ dask/dataframe/ewm.py
@@ -1,3 +1,70 @@
+from ..base import tokenize
+from ..utils import funcname
+from ..highlevelgraph import HighLevelGraph
+from .core import _emulate
+from .utils import make_meta
+
+
+def map_ewm_merge(func, merge_func, df, *args, **kwargs):
+    """Apply an ewm-type function to the dataframe, merging the results of applying
+       the function to an increasing prefix of partitions.
+
+    Parameters
+    ----------
+    func : function
+        Function applied to the dataframe.
+    df : dd.DataFrame, dd.Series
+    merge_func: function
+        Function that merges the partial result of applying func to the current partition
+        with results from previous partitions.
+    args, kwargs :
+        Arguments and keywords to pass to the function. The partition will
+        be the first argument, and these will be passed *after*.
+    """
+
+    merge_func_name = funcname(merge_func)
+    if "token" in kwargs:
+        func_name = kwargs.pop("token")
+        token = tokenize(df, *args, **kwargs)
+    else:
+        func_name = "ewm-" + funcname(func)
+        token = tokenize(func, merge_func, df, *args, **kwargs)
+
+    if "meta" in kwargs:
+        meta = kwargs.pop("meta")
+    else:
+        meta = _emulate(func, df, *args, **kwargs)
+    meta = make_meta(meta, index=df._meta.index)
+
+    name = "{0}-{1}".format(func_name, token)
+    name_partial = "partial-" + func_name
+    name_merge = "merge-" + merge_func_name
+
+    dsk = {}
+
+    for i, input_key in enumerate(df.__dask_keys__()):
+        dsk.update({(name_partial, i): (func, input_key, args, kwargs)})
+        kwargs["min_periods"] = max(
+            0, kwargs["min_periods"] - df.partitions[i].shape[kwargs["axis"]]
+        )
+
+    partial_keys = list(dsk)
+    dsk.update(
+        {
+            (name_merge, i): (
+                merge_func,
+                input_key,
+                partial_keys[:i],
+                df.__dask_keys__()[:i],
+            )
+            for i, input_key in enumerate(partial_keys[1:], 1)
+        }
+    )
+
+    graph = HighLevelGraph.from_collections(name, dsk, dependencies=[df])
+    return df._constructor(graph, name, meta, df.divisions)
+
+
 def pandas_ewm_method(df, ewm_kwargs, name, *args, **kwargs):
     ewm = df.ewm(**ewm_kwargs)
     return getattr(ewm, name)(*args, **kwargs)
@@ -13,7 +80,7 @@ class EWM(object):
         span=None,
         halflife=None,
         alpha=None,
-        min_periods=None,
+        min_periods=0,
         adjust=True,
         ignore_na=False,
         axis=0,
