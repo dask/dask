6c7695ec024964298e27d65ad1060c082a701689
psimaj <przemyslaw.simajchel@gmail.com>
2020-04-20 02:02:29 +0200

    Add the EWM class

---
+++ dask/dataframe/ewm.py
@@ -0,0 +1,40 @@
+class EWM(object):
+    """Provides exponentially weighted calculations."""
+
+    def __init__(
+        self,
+        obj,
+        com=None,
+        span=None,
+        halflife=None,
+        alpha=None,
+        min_periods=None,
+        adjust=True,
+        ignore_na=False,
+        axis=0,
+    ):
+        if adjust:
+            raise NotImplementedError("Only unadjusted implementation available")
+        self.obj = obj  # dataframe or series
+        self.com = com
+        self.span = span
+        self.halflife = halflife
+        self.alpha = alpha
+        self.min_periods = min_periods
+        self.adjust = adjust
+        self.ignore_na = ignore_na
+        self.axis = axis
+        # Allow pandas to raise if appropriate
+        obj._meta.ewm(**self._ewm_kwargs())
+
+    def _ewm_kwargs(self):
+        return {
+            "com": self.com,
+            "span": self.span,
+            "halflife": self.halflife,
+            "alpha": self.alpha,
+            "min_periods": self.min_periods,
+            "adjust": self.adjust,
+            "ignore_na": self.ignore_na,
+            "axis": self.axis,
+        }
