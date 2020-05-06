6d7ae70e2fc19cfe383da53dce6cab012b8ef499
Richard (Rick) Zamora <rzamora217@gmail.com>
2020-05-06 08:09:02 -0500

    [REVIEW] Harden inconsistent-schema handling in pyarrow-based read_parquet (#6160)
    * handle inconsistent schemas in read_parquet
    
    * small comment tweak
    
    * use ParquetDataset
    
    * Update dask/dataframe/io/tests/test_parquet.py
    
    Co-authored-by: Tom Augspurger <TomAugspurger@users.noreply.github.com>
    
    * Update dask/dataframe/io/tests/test_parquet.py
    
    Co-authored-by: Tom Augspurger <TomAugspurger@users.noreply.github.com>

--- dask/dataframe/io/tests/test_parquet.py
+++ dask/dataframe/io/tests/test_parquet.py
@@ -2065,6 +2065,40 @@ def test_timeseries_nulls_in_schema_pyarrow(tmpdir, timestamp, numerical):
     )
 
 
+def test_read_inconsistent_schema_pyarrow(tmpdir):
+    check_pyarrow()
+
+    # Note: This is a proxy test for a cudf-related issue fix
+    # (see cudf#5062 github issue).  The cause of that issue is
+    # schema inconsistencies that do not actually correspond to
+    # different types, but whether or not the file/column contains
+    # null values.
+
+    df1 = pd.DataFrame({"id": [0, 1], "val": [10, 20]})
+    df2 = pd.DataFrame({"id": [2, 3], "val": [30, 40]})
+
+    desired_type = "int64"
+    other_type = "int32"
+    df1.val = df1.val.astype(desired_type)
+    df2.val = df2.val.astype(other_type)
+
+    df_expect = pd.concat([df1, df2], ignore_index=True)
+    df_expect["val"] = df_expect.val.astype(desired_type)
+
+    df1.to_parquet(os.path.join(tmpdir, "0.parquet"))
+    df2.to_parquet(os.path.join(tmpdir, "1.parquet"))
+
+    # Read Directory
+    check = dd.read_parquet(str(tmpdir), dataset={"validate_schema": False})
+    assert_eq(check.compute(), df_expect, check_index=False)
+
+    # Read List
+    check = dd.read_parquet(
+        os.path.join(tmpdir, "*.parquet"), dataset={"validate_schema": False}
+    )
+    assert_eq(check.compute(), df_expect, check_index=False)
+
+
 def test_graph_size_pyarrow(tmpdir, engine):
     import pickle
 
