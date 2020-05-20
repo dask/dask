c93e8b7abfb813480bb81aab04b838b19bafbdd7
Lucas Rademaker <44430780+lr4d@users.noreply.github.com>
2020-05-20 11:07:20 +0200

    Make re-creating conda environments from the CI output easier (#3816)
    Co-authored-by: lr4d <lr4d@users.noreply.github.com>

--- continuous_integration/travis/install.sh
+++ continuous_integration/travis/install.sh
@@ -96,8 +96,8 @@ fi
 python -m pip install --no-deps -e .
 
 # For debugging
-echo -e "--\n--Conda Environment\n--"
-conda list
+echo -e "--\n--Conda Environment (re-create this with \`conda env create --name <name> -f <output_file>\`)\n--"
+conda env export | grep -E -v '^prefix:.*$'
 
 echo -e "--\n--Pip Environment\n--"
 python -m pip list --format=columns
