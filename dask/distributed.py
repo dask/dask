bc481d93bb5d408f8042766717c3d33bb7378590
Gaurav Sheni <gvsheni@gmail.com>
2020-05-25 17:34:19 -0400

    Update distributed.py

--- dask/distributed.py
+++ dask/distributed.py
@@ -6,6 +6,6 @@ except ImportError:
         "Dask's distributed scheduler is not installed.\n\n"
         "Please either conda or pip install dask distributed:\n\n"
         "  conda install dask distributed          # either conda install\n"
-        "  python -m pip install dask distributed --upgrade  # or python -m pip install"
+        "  python -m pip install "dask[distributed]" --upgrade  # or python -m pip install"
     )
     raise ImportError(msg)
