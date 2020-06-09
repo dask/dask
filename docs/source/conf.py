ddafc3d7649d1ca727f0280cdb7353a80821d878
Antonio Ercole De Luca <eracle@posteo.eu>
2020-06-09 16:00:11 +0200

    now readthedocs builds unrelased features' docstrings; fixing #6266 (#6295)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,11 +13,6 @@
 
 import os
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-# sys.path.insert(0, os.path.abspath('../../'))
-
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -26,6 +21,12 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 import sys
+
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath('../../'))
 
 source_dir = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(source_dir, "ext"))
