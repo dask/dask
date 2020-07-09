ad7f60256c50d0fe7d5262f395a81e6925b2e25f
Zhengnan Zhao <zzhao212@gmail.com>
2020-07-08 22:07:41 -0400

    Fix docstrings to reflect filename can contain extension

--- dask/base.py
+++ dask/base.py
@@ -54,8 +54,9 @@ class DaskMethodsMixin(object):
         Parameters
         ----------
         filename : str or None, optional
-            The name (without an extension) of the file to write to disk.  If
-            `filename` is None, no file will be written, and we communicate
+            The name of the file to write to disk. If the provided `filename`
+            doesn't include an extension, '.png' will be used by default.
+            If `filename` is None, no file will be written, and we communicate
             with dot using only pipes.
         format : {'png', 'pdf', 'dot', 'svg', 'jpeg', 'jpg'}, optional
             Format in which to write output file.  Default is 'png'.
@@ -70,7 +71,7 @@ class DaskMethodsMixin(object):
 
         Examples
         --------
-        >>> x.visualize(filename='dask.pdf')  # doctest: +SKIP
+        >>> x.visualize(filename='dask')  # doctest: +SKIP
         >>> x.visualize(filename='dask.pdf', color='order')  # doctest: +SKIP
 
         Returns
@@ -143,8 +144,8 @@ class DaskMethodsMixin(object):
         """Compute this dask collection
 
         This turns a lazy Dask collection into its in-memory equivalent.
-        For example a Dask.array turns into a  :func:`numpy.array` and a Dask.dataframe
-        turns into a Pandas dataframe.  The entire dataset must fit into memory
+        For example a Dask.array turns into a :func:`numpy.array` and a Dask.dataframe
+        turns into a :func:`pandas.dataframe`.  The entire dataset must fit into memory
         before calling this operation.
 
         Parameters
@@ -457,8 +458,9 @@ def visualize(*args, **kwargs):
     dsk : dict(s) or collection(s)
         The dask graph(s) to visualize.
     filename : str or None, optional
-        The name (without an extension) of the file to write to disk.  If
-        `filename` is None, no file will be written, and we communicate
+        The name of the file to write to disk. If the provided `filename`
+        doesn't include an extension, '.png' will be used by default.
+        If `filename` is None, no file will be written, and we communicate
         with dot using only pipes.
     format : {'png', 'pdf', 'dot', 'svg', 'jpeg', 'jpg'}, optional
         Format in which to write output file.  Default is 'png'.
@@ -479,7 +481,7 @@ def visualize(*args, **kwargs):
 
     Examples
     --------
-    >>> x.visualize(filename='dask.pdf')  # doctest: +SKIP
+    >>> x.visualize(filename='dask')  # doctest: +SKIP
     >>> x.visualize(filename='dask.pdf', color='order')  # doctest: +SKIP
 
     Returns
