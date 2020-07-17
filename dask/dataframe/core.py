a4b9b0217a05c2794ba823db3a6e0a2fbee2ebaf
Tom McTiernan <tmct@users.noreply.github.com>
2020-07-17 19:03:11 +0100

    Correct typo in error message (#6422)

--- dask/dataframe/core.py
+++ dask/dataframe/core.py
@@ -1166,7 +1166,7 @@ Dask Name: {name}, {task} tasks"""
         ):
             raise ValueError(
                 "Please provide exactly one of ``npartitions=``, ``freq=``, "
-                "``divisisions=``, ``partitions_size=`` keyword arguments"
+                "``divisions=``, ``partitions_size=`` keyword arguments"
             )
 
         if partition_size is not None:
