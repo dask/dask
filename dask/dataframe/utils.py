7304b8b22311afee4abf5f38d5ceadf51e64d399
Gil Forsyth <Gilbert.Forsyth@capitalone.com>
2020-07-30 09:51:07 -0400

    Add single quotes around column names if strings

--- dask/dataframe/utils.py
+++ dask/dataframe/utils.py
@@ -647,7 +647,9 @@ def check_meta(x, meta, funcname=None, numeric_equal=True):
     elif is_dataframe_like(meta):
         dtypes = pd.concat([x.dtypes, meta.dtypes], axis=1, sort=True)
         bad_dtypes = [
-            (col, a, b)
+            # add single quotes around string variables
+            # to more clearly demarcate column type
+            (f"'{col}'", a, b) if isinstance(col, str) else (col, a, b)
             for col, a, b in dtypes.fillna("-").itertuples()
             if not equal_dtypes(a, b)
         ]
