07b1f97abd14aebc04913ac1088c3d101e975b8c
Julia Signell <jsignell@gmail.com>
2020-08-11 11:11:21 -0400

    Bump up tolerance for rolling tests

--- dask/dataframe/tests/test_rolling.py
+++ dask/dataframe/tests/test_rolling.py
@@ -270,7 +270,7 @@ def test_time_rolling_constructor():
 @pytest.mark.parametrize("window", ["1S", "2S", "3S", pd.offsets.Second(5)])
 def test_time_rolling_methods(method, args, window, check_less_precise):
     if dd._compat.PANDAS_GT_110:
-        check_less_precise = {}
+        check_less_precise = {"atol": 1e-4, "rtol": 1e-2}
     else:
         check_less_precise = {"check_less_precise": check_less_precise}
 
