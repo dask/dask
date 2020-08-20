f5a8b239259e0222a6e3613be4d3362e83f19d14
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-08-20 15:39:55 +0200

    add keyword names

--- dask/dataframe/io/parquet/arrow.py
+++ dask/dataframe/io/parquet/arrow.py
@@ -938,7 +938,9 @@ class ArrowEngine(Engine):
         else:
             index_cols = []
 
-        t = cls._pandas_to_arrow_table(df, preserve_index, schema)
+        t = cls._pandas_to_arrow_table(
+            df, preserve_index=preserve_index, schema=schema
+        )
 
         if partition_on:
             md_list = _write_partitioned(
