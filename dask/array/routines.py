006cd303a0c2c49ab785d4561b8c2c323632ec7a
Matthew Rocklin <mrocklin@gmail.com>
2020-08-30 15:22:20 -0700

    fixup typo in doctest

--- dask/array/routines.py
+++ dask/array/routines.py
@@ -1411,12 +1411,11 @@ def aligned_coarsen_chunks(chunks: List[int], multiple: int) -> List[int]:
 
     Examples
     --------
-
-    >>> align_coarsen_chunks(chunks=(1, 2, 3), multiple=4)
-    (4, 4, 2)
-    >>> align_coarsen_chunks(chunks=(1, 20, 3, 4), multiple=4)
+    >>> aligned_coarsen_chunks(chunks=(1, 2, 3), multiple=4)
+    (4, 2)
+    >>> aligned_coarsen_chunks(chunks=(1, 20, 3, 4), multiple=4)
     (20, 4, 4)
-    >>> align_coarsen_chunks(chunks=(20, 10, 15, 23, 24), multiple=10)
+    >>> aligned_coarsen_chunks(chunks=(20, 10, 15, 23, 24), multiple=10)
     (20, 10, 20, 20, 20, 2)
     """
 
