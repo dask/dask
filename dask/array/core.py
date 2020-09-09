99ea30d4e5f999cdb689771ab6e68139f2c2e326
Tom Augspurger <TomAugspurger@users.noreply.github.com>
2020-09-09 10:49:46 -0500

    Avoid large chunks from getitem with lists (#6514)
    Closes #6270

--- dask/array/core.py
+++ dask/array/core.py
@@ -1567,7 +1567,6 @@ class Array(DaskMethodsMixin):
         )
 
         index2 = normalize_index(index, self.shape)
-
         dependencies = {self.name}
         for i in index2:
             if isinstance(i, Array):
@@ -1582,7 +1581,7 @@ class Array(DaskMethodsMixin):
             return self
 
         out = "getitem-" + tokenize(self, index2)
-        dsk, chunks = slice_array(out, self.name, self.chunks, index2)
+        dsk, chunks = slice_array(out, self.name, self.chunks, index2, self.itemsize)
 
         graph = HighLevelGraph.from_collections(out, dsk, dependencies=[self])
 
