f4aeda40f3df38fd2f243c056f9347044121842f
Daniel Saxton <2658661+dsaxton@users.noreply.github.com>
2020-09-11 10:30:46 -0500

    BUG: Handle min_count in Series.sum / prod (#6618)
    * BUG: Handle min_count in Series.sum

--- dask/dataframe/tests/test_arithmetics_reduction.py
+++ dask/dataframe/tests/test_arithmetics_reduction.py
@@ -1367,3 +1367,16 @@ def test_empty_df_reductions(func):
     ddf = dd.from_pandas(pdf, npartitions=1)
 
     assert_eq(dsk_func(ddf), pd_func(pdf))
+
+
+@pytest.mark.parametrize("method", ["sum", "prod"])
+@pytest.mark.parametrize("min_count", [0, 9])
+def test_series_agg_with_min_count(method, min_count):
+    df = pd.DataFrame([[1]], columns=["a"])
+    ddf = dd.from_pandas(df, npartitions=1)
+    func = getattr(ddf["a"], method)
+    result = func(min_count=min_count).compute()
+    if min_count == 0:
+        assert result == 1
+    else:
+        assert result is np.nan
