a4be23003858506705a1e36e687044704e634ddc
Julia Signell <jsignell@gmail.com>
2020-09-15 10:44:42 -0400

    Update pre-commit-config to latest black. (#6641)
    * Use recommended repo for black
    
    * Re-run black on everything with 20.8b1

--- docs/source/scripts/scheduling.py
+++ docs/source/scripts/scheduling.py
@@ -8,33 +8,45 @@ import matplotlib.pyplot as plt
 def noop(x):
     pass
 
+
 nrepetitions = 1
+
 
 def trivial(width, height):
     """ Embarrassingly parallel dask """
-    d = {('x', 0, i): i for i in range(width)}
+    d = {("x", 0, i): i for i in range(width)}
     for j in range(1, height):
-        d.update({('x', j, i): (noop, ('x', j - 1, i))
-                                  for i in range(width)})
-    return d, [('x', height - 1, i) for i in range(width)]
+        d.update({("x", j, i): (noop, ("x", j - 1, i)) for i in range(width)})
+    return d, [("x", height - 1, i) for i in range(width)]
+
 
 def crosstalk(width, height, connections):
     """ Natural looking dask with some inter-connections """
-    d = {('x', 0, i): i for i in range(width)}
+    d = {("x", 0, i): i for i in range(width)}
     for j in range(1, height):
-        d.update({('x', j, i): (noop, [('x', j - 1, randint(0, width))
-                                            for _ in range(connections)])
-                    for i in range(width)})
-    return d, [('x', height - 1, i) for i in range(width)]
+        d.update(
+            {
+                ("x", j, i): (
+                    noop,
+                    [("x", j - 1, randint(0, width)) for _ in range(connections)],
+                )
+                for i in range(width)
+            }
+        )
+    return d, [("x", height - 1, i) for i in range(width)]
+
 
 def dense(width, height):
     """ Full barriers between each step """
-    d = {('x', 0, i): i for i in range(width)}
+    d = {("x", 0, i): i for i in range(width)}
     for j in range(1, height):
-        d.update({('x', j, i): (noop, [('x', j - 1, k)
-                                            for k in range(width)])
-                    for i in range(width)})
-    return d, [('x', height - 1, i) for i in range(width)]
+        d.update(
+            {
+                ("x", j, i): (noop, [("x", j - 1, k) for k in range(width)])
+                for i in range(width)
+            }
+        )
+    return d, [("x", height - 1, i) for i in range(width)]
 
 
 import numpy as np
@@ -56,21 +68,23 @@ for get in [dask.get, threaded.get, local.get_sync, multiprocessing.get]:
 # Plot #
 ########
 
-f, (left, right) = plt.subplots(nrows=1, ncols=2, sharex=True, figsize=(12, 5), squeeze=True)
+f, (left, right) = plt.subplots(
+    nrows=1, ncols=2, sharex=True, figsize=(12, 5), squeeze=True
+)
 
 for get in trivial_results:
     left.loglog(x * 5, trivial_results[get], label=get.__module__)
     right.loglog(x * 5, trivial_results[get] / x, label=get.__module__)
 
-left.set_title('Cost for Entire graph')
-right.set_title('Cost per task')
-left.set_ylabel('Duration (s)')
-right.set_ylabel('Duration (s)')
-left.set_xlabel('Number of tasks')
-right.set_xlabel('Number of tasks')
+left.set_title("Cost for Entire graph")
+right.set_title("Cost per task")
+left.set_ylabel("Duration (s)")
+right.set_ylabel("Duration (s)")
+left.set_xlabel("Number of tasks")
+right.set_xlabel("Number of tasks")
 
 plt.legend()
-plt.savefig('images/scaling-nodes.png')
+plt.savefig("images/scaling-nodes.png")
 
 #####################
 # Crosstalk example #
@@ -92,17 +106,19 @@ for get in [threaded.get, local.get_sync]:
 # Plot #
 ########
 
-f, (left, right) = plt.subplots(nrows=1, ncols=2, sharex=True, figsize=(12, 5), squeeze=True)
+f, (left, right) = plt.subplots(
+    nrows=1, ncols=2, sharex=True, figsize=(12, 5), squeeze=True
+)
 
 for get in crosstalk_results:
     left.plot(x, crosstalk_results[get], label=get.__module__)
-    right.semilogy(x, crosstalk_results[get] / 5000. / x, label=get.__module__)
+    right.semilogy(x, crosstalk_results[get] / 5000.0 / x, label=get.__module__)
 
-left.set_title('Cost for Entire graph')
-right.set_title('Cost per edge')
-left.set_ylabel('Duration (s)')
-right.set_ylabel('Duration (s)')
-left.set_xlabel('Number of edges per task')
-right.set_xlabel('Number of edges per task')
+left.set_title("Cost for Entire graph")
+right.set_title("Cost per edge")
+left.set_ylabel("Duration (s)")
+right.set_ylabel("Duration (s)")
+left.set_xlabel("Number of edges per task")
+right.set_xlabel("Number of edges per task")
 plt.legend()
-plt.savefig('images/scaling-edges.png')
+plt.savefig("images/scaling-edges.png")
