4dd0e1ba2ddcbc8178cd1113e1120f311d4080e0
Matthew Rocklin <mrocklin@gmail.com>
2020-09-28 08:59:08 -0700

    Stop writing config files by default (#4123)
    Follows on #6647 upstream

--- distributed/config.py
+++ distributed/config.py
@@ -12,7 +12,6 @@ config = dask.config.config
 
 
 fn = os.path.join(os.path.dirname(__file__), "distributed.yaml")
-dask.config.ensure_file(source=fn)
 
 with open(fn) as f:
     defaults = yaml.safe_load(f)
