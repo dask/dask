705b6345456c0f697495bcbccea255b1e6718b79
Gabor Szabo <gabor@szabgab.com>
2020-10-02 05:59:02 +0300

    Update author to Dask developers

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     cmdclass=versioneer.get_cmdclass(),
     description="Parallel PyData with Task Scheduling",
     url="https://github.com/dask/dask/",
-    author="Matthew Rocklin",
+    author="Dask developers",
     maintainer="Matthew Rocklin",
     maintainer_email="mrocklin@gmail.com",
     license="BSD",
