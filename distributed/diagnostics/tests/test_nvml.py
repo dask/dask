d35628ea33092a2a0add89ae7ce9dbaa7c82c842
jakirkham <jakirkham@gmail.com>
2020-10-07 05:54:59 -0700

    Fix `test_gpu_metrics` failure (#4154)
    * Import `_pynvml_handles`
    
    There is no `handles` in `distributed.diagnostics.nvml`. Though there is
    `_pynvml_handles`. So try `import`ing that.
    
    * Test attributes of single handle
    
    * Run `black`
    
    * Consolidate NVML tests
    
    * Access `_pynvml_handles` via `nvml`
    
    * Fix NVML memory total test as well
    
    * Skip test needing 2 GPUs on single GPU
    
    * Workaround pyNVML initialization
    
    Skip inside the test to make sure pyNVML is already initialized first.

--- distributed/diagnostics/tests/test_nvml.py
+++ distributed/diagnostics/tests/test_nvml.py
@@ -4,6 +4,7 @@ import os
 pynvml = pytest.importorskip("pynvml")
 
 from distributed.diagnostics import nvml
+from distributed.utils_test import gen_cluster
 
 
 def test_one_time():
@@ -17,11 +18,15 @@ def test_one_time():
 def test_1_visible_devices():
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     output = nvml.one_time()
-    assert len(output["memory-total"]) == 1
+    h = nvml._pynvml_handles()
+    assert output["memory-total"] == pynvml.nvmlDeviceGetMemoryInfo(h).total
 
 
 @pytest.mark.parametrize("CVD", ["1,0", "0,1"])
 def test_2_visible_devices(CVD):
+    if pynvml.nvmlDeviceGetCount() <= 1:
+        pytest.skip("Machine only has a single GPU")
+
     os.environ["CUDA_VISIBLE_DEVICES"] = CVD
     idx = int(CVD.split(",")[0])
 
@@ -32,3 +37,19 @@ def test_2_visible_devices(CVD):
     s2 = pynvml.nvmlDeviceGetSerial(h2)
 
     assert s == s2
+
+
+@gen_cluster()
+async def test_gpu_metrics(s, a, b):
+    h = nvml._pynvml_handles()
+
+    assert "gpu" in a.metrics
+    assert (
+        s.workers[a.address].metrics["gpu"]["memory-used"]
+        == pynvml.nvmlDeviceGetMemoryInfo(h).used
+    )
+    assert "gpu" in a.startup_information
+    assert (
+        s.workers[a.address].extra["gpu"]["name"]
+        == pynvml.nvmlDeviceGetName(h).decode()
+    )
