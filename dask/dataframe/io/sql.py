45fd9092049a61c1d43067a0132574b3fe715153
Jim Circadian <JimCircadian@users.noreply.github.com>
2020-10-12 19:49:14 +0100

    Added doctest extension for documentation make and testing… (#6397)
    * Sphinx configuration missing doctest extension for Makefile
    * Fixed the majority of doctest failures for warnings originating in the dask library documentation itself.
    
    There are quite a few originating from the dask/distributed integration and the NumPY FFT integration.
    I'm going to review and debug these with respect to the version of distributed coming from the requirements.
    
    * Fixing up, or ignoring, remaining doctest errors from imported methods or newer changes

--- dask/dataframe/io/sql.py
+++ dask/dataframe/io/sql.py
@@ -342,13 +342,13 @@ def to_sql(
     Dask Name: from_pandas, 2 tasks
 
     >>> from dask.utils import tmpfile
-    >>> from sqlalchemy import create_engine
-    >>> with tmpfile() as f:
-    ...     db = 'sqlite:///%s' % f
-    ...     ddf.to_sql('test', db)
-    ...     engine = create_engine(db, echo=False)
-    ...     result = engine.execute("SELECT * FROM test").fetchall()
-    >>> result
+    >>> from sqlalchemy import create_engine    # doctest: +SKIP
+    >>> with tmpfile() as f:                    # doctest: +SKIP
+    ...     db = 'sqlite:///%s' %f              # doctest: +SKIP
+    ...     ddf.to_sql('test', db)              # doctest: +SKIP
+    ...     engine = create_engine(db, echo=False) # doctest: +SKIP
+    ...     result = engine.execute("SELECT * FROM test").fetchall() # doctest: +SKIP
+    >>> result                                  # doctest: +SKIP
     [(0, 0, '00'), (1, 1, '11'), (2, 2, '22'), (3, 3, '33')]
     """
     if not isinstance(uri, str):
