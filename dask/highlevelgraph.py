323227486130e5a8725e98cc71c45b67ee0aa5a4
James Bourbeau <jrbourbeau@users.noreply.github.com>
2020-10-13 21:25:58 -0500

    Ensure HighLevelGraph layers always contain Layer instances (#6716)

--- dask/highlevelgraph.py
+++ dask/highlevelgraph.py
@@ -230,7 +230,7 @@ class HighLevelGraph(Mapping):
 
     def __init__(
         self,
-        layers: Mapping[str, Mapping],
+        layers: Mapping[str, Layer],
         dependencies: Mapping[str, Set],
         key_dependencies: Mapping[Hashable, Set] = {},
     ):
