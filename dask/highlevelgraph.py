1cc37a37f131aa7ce71736449432b4cf3155b94b
Mads R. B. Kristensen <madsbk@gmail.com>
2020-10-20 00:04:24 +0200

    Serialization of layers (#6693)

--- dask/highlevelgraph.py
+++ dask/highlevelgraph.py
@@ -118,6 +118,24 @@ class Layer(collections.abc.Mapping):
 
         return BasicLayer({k: func(v) for k, v in self.items()})
 
+    def __reduce__(self):
+        """Default serialization implementation, which materializes the Layer
+
+        This should follow the standard pickle protocol[1] but must always return
+        a tuple and the arguments for the callable object must be compatible with
+        msgpack. This is because Distributed uses msgpack to send Layers to the
+        scheduler.
+
+        [1] <https://docs.python.org/3/library/pickle.html#object.__reduce__>
+        """
+        return (BasicLayer, (dict(self),))
+
+    def __copy__(self):
+        """Default shallow copy implementation"""
+        obj = type(self).__new__(self.__class__)
+        obj.__dict__.update(self.__dict__)
+        return obj
+
 
 class BasicLayer(Layer):
     """Basic implementation of `Layer`
