80b15644e4a3cbaf1fc9222c70ffb0448e16a680
Erik Welch <erik.n.welch@gmail.com>
2020-10-26 14:39:19 -0500

    Begin experimenting with parallel prefix scan for cumsum and cumprod (#6675)
    * Begin experimenting with parallel prefix scan for cumsum and cumprod in dask.array
    
    This is a WIP and needs benchmarked.  I think it's interesting, though, and want to share.
    It's been a while since I've worked on dask.array, so feedback is most welcome.
    
    This is a work-efficient parallel prefix scan.  It uses a Brent-Kung construction and
    is known as the Blelloch algorithm.  We adapt it to work on chunks.
    
    Previously, to do a cumsum across N chunks would require N levels of dependencies.
    This PR takes approximately 2 * lg(N) levels of dependencies.  It exposes parallelism.
    It is work-efficient and only requires a third more tasks than the previous method.
    Scans on floating point values should also be more accurate.
    
    A parallel cumsum works by first taking the sum of each block, then do a binary tree
    merge followed by a fan-out (i.e., the Brent-Kung pattern).  We then take the cumsum
    of each block and add the sum of the previous blocks.
    
    NumPy calculates cumsum and cumprod very fast, but it calculates sum and prod
    significantly faster.  This is why I think this approach will be faster.
    Exposing parallelism and an efficient communication pattern is another reason I think
    this should be faster (especially when communication costs are significant).
    
    I also think this will be an interesting test for `dask.order` and the scheduler.
    
    Q: Should we allow users to choose which method to use (i.e., prev or new in this PR)?
    Does the answer to this depend on benchmarks?
    
    Benchmarks and graph diagrams are forthcoming :)
    
    * Choose cumsum/cumprod with `method=` keyword argument.
    
    Current choices are "sequential", "blelloch", and "blelloch-split".
    Default is "sequential".  I need to document these.
    
    * black
    
    * Add docstrings for "blelloch" method for cumsum/cumprod

--- dask/array/core.py
+++ dask/array/core.py
@@ -2270,16 +2270,38 @@ class Array(DaskMethodsMixin):
         )
 
     @derived_from(np.ndarray)
-    def cumsum(self, axis, dtype=None, out=None):
+    def cumsum(self, axis, dtype=None, out=None, *, method="sequential"):
+        """Dask added an additional keyword-only argument ``method``.
+
+        method : {'sequential', 'blelloch'}, optional
+            Choose which method to use to perform the cumsum.  Default is 'sequential'.
+
+            * 'sequential' performs the cumsum of each prior block before the current block.
+            * 'blelloch' is a work-efficient parallel cumsum.  It exposes parallelism by
+              first taking the sum of each block and combines the sums via a binary tree.
+              This method may be faster or more memory efficient depending on workload,
+              scheduler, and hardware.  More benchmarking is necessary.
+        """
         from .reductions import cumsum
 
-        return cumsum(self, axis, dtype, out=out)
+        return cumsum(self, axis, dtype, out=out, method=method)
 
     @derived_from(np.ndarray)
-    def cumprod(self, axis, dtype=None, out=None):
+    def cumprod(self, axis, dtype=None, out=None, *, method="sequential"):
+        """Dask added an additional keyword-only argument ``method``.
+
+        method : {'sequential', 'blelloch'}, optional
+            Choose which method to use to perform the cumprod.  Default is 'sequential'.
+
+            * 'sequential' performs the cumprod of each prior block before the current block.
+            * 'blelloch' is a work-efficient parallel cumprod.  It exposes parallelism by first
+              taking the product of each block and combines the products via a binary tree.
+              This method may be faster or more memory efficient depending on workload,
+              scheduler, and hardware.  More benchmarking is necessary.
+        """
         from .reductions import cumprod
 
-        return cumprod(self, axis, dtype, out=out)
+        return cumprod(self, axis, dtype, out=out, method=method)
 
     @derived_from(np.ndarray)
     def squeeze(self, axis=None):
