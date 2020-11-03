931289570caba7128fbc2228153564aa6647f998
Jacob Tomlinson <jtomlinson@nvidia.com>
2020-11-03 17:06:48 +0000

    Disable docker tty flag

--- continuous_integration/hdfs/install.sh
+++ continuous_integration/hdfs/install.sh
@@ -5,7 +5,7 @@ export CONTAINER_ID=$(docker run -d -v $(pwd):/working daskdev/dask-hdfs-testing
 
 # Remove <2 version constraint once we've added pyarrow 2.0 compatibility
 # xref https://github.com/dask/dask/issues/6754
-docker exec -it $CONTAINER_ID conda install -y -q dask pyarrow">=0.14.0,<2" fsspec -c conda-forge
-docker exec -it $CONTAINER_ID python -m pip install -e .
+docker exec $CONTAINER_ID conda install -y -q dask pyarrow">=0.14.0,<2" fsspec -c conda-forge
+docker exec $CONTAINER_ID python -m pip install -e .
 
 set +xe
