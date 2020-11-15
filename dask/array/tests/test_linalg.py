a68c26a7fea995736a09ab1a067bb10b08019efa
RogerMoens <45488927+RogerMoens@users.noreply.github.com>
2020-11-15 18:54:38 +0100

    Fix transpose of u in SVD (#6799)
    Fixes  #6757

--- dask/array/tests/test_linalg.py
+++ dask/array/tests/test_linalg.py
@@ -449,35 +449,30 @@ def test_dask_svd_self_consistent(m, n):
         assert d_e.dtype == e.dtype
 
 
-@pytest.mark.slow
 def test_svd_compressed():
-    m, n = 2000, 250
-    r = 10
-    np.random.seed(4321)
-    mat1 = np.random.randn(m, r)
-    mat2 = np.random.randn(r, n)
-    mat = mat1.dot(mat2)
-    data = da.from_array(mat, chunks=(500, 50))
+    m, n = 100, 50
+    r = 5
+    a = da.random.random((m, n), chunks=(m, n))
 
-    u, s, vt = svd_compressed(data, r, seed=4321, n_power_iter=2)
+    # calculate approximation and true singular values
+    u, s, vt = svd_compressed(a, 2 * r, seed=4321, n_power_iter=2)  # worst case
+    s_true = scipy.linalg.svd(a.compute(), compute_uv=False)
 
-    usvt = da.dot(u, da.dot(da.diag(s), vt))
+    # compute the difference with original matrix
+    norm = scipy.linalg.norm((a - (u[:, :r] * s[:r]) @ vt[:r, :]).compute(), 2)
 
-    tol = 0.2
-    assert_eq(
-        da.linalg.norm(usvt), np.linalg.norm(mat), rtol=tol, atol=tol
-    )  # average accuracy check
+    # ||a-a_hat||_2 <= (1+tol)s_{k+1}: based on eq. 1.10/1.11:
+    # Halko, Nathan, Per-Gunnar Martinsson, and Joel A. Tropp.
+    # "Finding structure with randomness: Probabilistic algorithms for constructing
+    # approximate matrix decompositions." SIAM review 53.2 (2011): 217-288.
+    frac = norm / s_true[r + 1] - 1
+    # Tolerance determined via simulation to be slightly above max norm of difference matrix in 10k samples.
+    # See https://github.com/dask/dask/pull/6799#issuecomment-726631175 for more details.
+    tol = 0.4
+    assert frac < tol
 
-    u = u[:, :r]
-    s = s[:r]
-    vt = vt[:r, :]
-
-    s_exact = np.linalg.svd(mat)[1]
-    s_exact = s_exact[:r]
-
-    assert_eq(np.eye(r, r), da.dot(u.T, u))  # u must be orthonormal
-    assert_eq(np.eye(r, r), da.dot(vt, vt.T))  # v must be orthonormal
-    assert_eq(s, s_exact)  # s must contain the singular values
+    assert_eq(np.eye(r, r), da.dot(u[:, :r].T, u[:, :r]))  # u must be orthonormal
+    assert_eq(np.eye(r, r), da.dot(vt[:r, :], vt[:r, :].T))  # v must be orthonormal
 
 
 @pytest.mark.parametrize(
