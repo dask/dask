020df15b199565ae3b63c691c351e733ac2a3c95
Jacob Tomlinson <jtomlinson@nvidia.com>
2020-11-24 15:20:58 +0000

    Improve docstring readability and add type hints

--- dask/utils.py
+++ dask/utils.py
@@ -1272,42 +1272,53 @@ def format_time(n):
     return "%.2f us" % (n * 1e6)
 
 
-def format_time_ago(n):
+def format_time_ago(n: datetime) -> str:
     """Calculate a '3 hours ago' type string from a Python datetime.
 
     Examples
     --------
     >>> from datetime import datetime, timedelta
+
     >>> now = datetime.now()
     >>> format_time_ago(now)
     'Just now'
+
     >>> past = datetime.now() - timedelta(minutes=1)
     >>> format_time_ago(past)
     '1 minute ago'
+
     >>> past = datetime.now() - timedelta(minutes=2)
     >>> format_time_ago(past)
     '2 minutes ago'
+
     >>> past = datetime.now() - timedelta(hours=1)
     >>> format_time_ago(past)
     '1 hour ago'
+
     >>> past = datetime.now() - timedelta(hours=6)
     >>> format_time_ago(past)
     '6 hours ago'
+
     >>> past = datetime.now() - timedelta(days=1)
     >>> format_time_ago(past)
     '1 day ago'
+
     >>> past = datetime.now() - timedelta(days=5)
     >>> format_time_ago(past)
     '5 days ago'
+
     >>> past = datetime.now() - timedelta(days=8)
     >>> format_time_ago(past)
     '1 week ago'
+
     >>> past = datetime.now() - timedelta(days=16)
     >>> format_time_ago(past)
     '2 weeks ago'
+
     >>> past = datetime.now() - timedelta(days=190)
     >>> format_time_ago(past)
     '6 months ago'
+
     >>> past = datetime.now() - timedelta(days=800)
     >>> format_time_ago(past)
     '2 years ago'
