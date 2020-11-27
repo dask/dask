6334cc944e40d7e5d579a874ae86a77e7628de2a
Martin Durant <martin.durant@utoronto.ca>
2020-11-27 14:25:33 -0500

    Fixes #6898

--- dask/dataframe/io/parquet/core.py
+++ dask/dataframe/io/parquet/core.py
@@ -839,7 +839,7 @@ def sorted_columns(statistics):
             continue
         divisions = [c["min"]]
         max = c["max"]
-        success = True
+        success = c["min"] is not None
         for stats in statistics[1:]:
             c = stats["columns"][i]
             if c["min"] is None:
