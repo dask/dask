9c173395908c8424f9e78383d280c411d36feb67
rjzamora <rzamora217@gmail.com>
2020-12-02 10:07:46 -0800

    update hdfs constraints

--- continuous_integration/hdfs/install.sh
+++ continuous_integration/hdfs/install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -xe
 
-# Remove <2 version constraint once we've added pyarrow 2.0 compatibility
-# xref https://github.com/dask/dask/issues/6754
-docker exec hdfs conda install -y -q dask pyarrow">=0.14.0,<2" fsspec pyyaml -c conda-forge
+docker exec hdfs conda install -y -q dask pyarrow">=0.14.0,<2|>2" fsspec pyyaml -c conda-forge
 docker exec hdfs python -m pip install -e .
 
 set +xe
