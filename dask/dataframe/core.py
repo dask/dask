d6943a666264d44c6199bd46dca708fb4b94c70c
Mads R. B. Kristensen <madsbk@gmail.com>
2020-12-18 13:06:47 +0100

    Use __class__ when comparing meta data (#6981)
    * Use __class__ when comparing meta data
    
    * Added some doc

--- dask/dataframe/core.py
+++ dask/dataframe/core.py
@@ -4946,7 +4946,9 @@ def handle_out(out, result):
         else:
             out = None
 
-    if out is not None and type(out) != type(result):
+    # Notice, we use .__class__ as opposed to type() in order to support
+    # object proxies see <https://github.com/dask/dask/pull/6981>
+    if out is not None and out.__class__ != result.__class__:
         raise TypeError(
             "Mismatched types between result and out parameter. "
             "out=%s, result=%s" % (str(type(out)), str(type(result)))
