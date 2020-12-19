0be17d9c0aa36eed1c77581aed8bbb37f260b748
Ryan Williams <ryan@runsascoded.com>
2020-12-19 10:12:22 -0500

    preserve_partition_sizes: pd.isna

--- dask/dataframe/core.py
+++ dask/dataframe/core.py
@@ -6966,7 +6966,7 @@ if hasattr(pd, "isna"):
 
     @wraps(pd.isna)
     def isna(arg):
-        return map_partitions(pd.isna, arg)
+        return map_partitions(pd.isna, arg, preserve_partition_sizes=True)
 
 
 def _repr_data_series(s, index):
