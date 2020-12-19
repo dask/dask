1f08f5fa2b87abe68a14fd65bcc9275eb4703631
Ryan Williams <ryan@runsascoded.com>
2020-12-18 22:04:35 -0500

    preserve_partition_sizes: binary methods

--- dask/dataframe/core.py
+++ dask/dataframe/core.py
@@ -4799,6 +4799,7 @@ class DataFrame(_Frame):
                         axis=axis,
                         fill_value=fill_value,
                         enforce_metadata=False,
+                        preserve_partition_sizes=True,
                     )
 
             meta = _emulate(op, self, other, axis=axis, fill_value=fill_value)
@@ -4810,6 +4811,7 @@ class DataFrame(_Frame):
                 axis=axis,
                 fill_value=fill_value,
                 enforce_metadata=False,
+                preserve_partition_sizes=True,
             )
 
         meth.__name__ = name
