94fb33f976b1505f40ec45b11db447febf691788
Illviljan <14371165+Illviljan@users.noreply.github.com>
2021-01-01 15:59:47 +0100

    Add cached_property
    Add cached_propert for backwards compatibillity. Not needed in python +3.8 though

--- dask/utils.py
+++ dask/utils.py
@@ -15,6 +15,7 @@ from threading import Lock
 import uuid
 from weakref import WeakValueDictionary
 from functools import lru_cache
+from _thread import RLock
 
 from .core import get_deps
 
@@ -1467,3 +1468,55 @@ def key_split(s):
             return result
     except Exception:
         return "Other"
+
+
+ _NOT_FOUND = object()
+
+# TODO: Copied from functools.cached_property in python 3.8:
+class cached_property:
+    def __init__(self, func):
+        self.func = func
+        self.attrname = None
+        self.__doc__ = func.__doc__
+        self.lock = RLock()
+
+    def __set_name__(self, owner, name):
+        if self.attrname is None:
+            self.attrname = name
+        elif name != self.attrname:
+            raise TypeError(
+                "Cannot assign the same cached_property to two different names "
+                f"({self.attrname!r} and {name!r})."
+            )
+
+    def __get__(self, instance, owner=None):
+        if instance is None:
+            return self
+        if self.attrname is None:
+            raise TypeError(
+                "Cannot use cached_property instance without calling __set_name__ on it."
+            )
+        try:
+            cache = instance.__dict__
+        except AttributeError:  # not all objects have __dict__ (e.g. class defines slots)
+            msg = (
+                f"No '__dict__' attribute on {type(instance).__name__!r} "
+                f"instance to cache {self.attrname!r} property."
+            )
+            raise TypeError(msg) from None
+        val = cache.get(self.attrname, _NOT_FOUND)
+        if val is _NOT_FOUND:
+            with self.lock:
+                # check if another thread filled cache while we awaited lock
+                val = cache.get(self.attrname, _NOT_FOUND)
+                if val is _NOT_FOUND:
+                    val = self.func(instance)
+                    try:
+                        cache[self.attrname] = val
+                    except TypeError:
+                        msg = (
+                            f"The '__dict__' attribute on {type(instance).__name__!r} instance "
+                            f"does not support item assignment for caching {self.attrname!r} property."
+                        )
+                        raise TypeError(msg) from None
+        return val
