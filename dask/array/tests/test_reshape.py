09003c272f565864515e15676a007d8f413c7c38
Tom Augspurger <1312546+TomAugspurger@users.noreply.github.com>
2021-01-14 09:24:34 -0600

    Add option to control rechunking in reshape (#6753)
    * Avoid rechunking in reshape with chunksize=1
    
    When the slow-moving (early) axes in `.reshape` are all size 1, then we
    can avoid an intermediate rechunk which could cause memory issues.
    
    ```
    00 01 | 02 03   # a[0, :, :]
    ----- | -----
    04 05 | 06 07
    08 09 | 10 11
    
    =============
    
    12 13 | 14 15   # a[1, :, :]
    ----- | -----
    16 17 | 18 19
    20 21 | 22 23
    
    -> (3, 4)
    
    00 01 | 02 03
    ----- | -----
    04 05 | 06 07
    08 09 | 10 11
    ----- | -----
    12 13 | 14 15
    ----- | -----
    16 17 | 18 19
    20 21 | 22 23
    ```
    
    xref https://github.com/dask/dask/issues/5544, specifically the examples
    given in https://github.com/dask/dask/issues/5544#issuecomment-593723196.
    
    * fix conditioni
    
    * remove breakpoint comment
    
    * API: Added merge_chunks to reshape
    
    Adds a keyword to reshape to control merge / rechunking. See the
    documentation for an explanation.
    
    * update images

--- dask/array/tests/test_reshape.py
+++ dask/array/tests/test_reshape.py
@@ -149,3 +149,56 @@ def test_reshape_all_not_chunked_merge(
     result = a.reshape(outshape)
     assert result.chunks == outchunks
     assert_eq(result, base.reshape(outshape))
+
+
+@pytest.mark.parametrize(
+    "inshape, inchunks, outshape, outchunks",
+    [
+        # (2, 3, 4) -> (6, 4)
+        ((2, 3, 4), ((2,), (1, 2), (2, 2)), (6, 4), ((1, 2, 1, 2), (2, 2))),
+        # (1, 2, 3, 4) -> (12, 4)
+        ((1, 2, 3, 4), ((1,), (2,), (1, 2), (2, 2)), (6, 4), ((1, 2, 1, 2), (2, 2))),
+        # (2, 2, 3, 4) -> (12, 4) (3 cases)
+        (
+            (2, 2, 3, 4),
+            ((1, 1), (2,), (1, 2), (2, 2)),
+            (12, 4),
+            ((1, 2, 1, 2, 1, 2, 1, 2), (2, 2)),
+        ),
+        (
+            (2, 2, 3, 4),
+            ((2,), (1, 1), (1, 2), (2, 2)),
+            (12, 4),
+            ((1, 2, 1, 2, 1, 2, 1, 2), (2, 2)),
+        ),
+        (
+            (2, 2, 3, 4),
+            ((2,), (2,), (1, 2), (2, 2)),
+            (12, 4),
+            ((1, 2, 1, 2, 1, 2, 1, 2), (2, 2)),
+        ),
+        # (2, 2, 3, 4) -> (4, 3, 4)
+        # TODO: I'm confused about the behavior in this case.
+        # (
+        #     (2, 2, 3, 4),
+        #     ((2,), (2,), (1, 2), (2, 2)),
+        #     (4, 3, 4),
+        #     ((1, 1, 1, 1), (1, 2), (2, 2)),
+        # ),
+        # (2, 2, 3, 4) -> (4, 3, 4)
+        ((2, 2, 3, 4), ((2,), (2,), (1, 2), (4,)), (4, 3, 4), ((2, 2), (1, 2), (4,))),
+    ],
+)
+def test_reshape_merge_chunks(inshape, inchunks, outshape, outchunks):
+    # https://github.com/dask/dask/issues/5544#issuecomment-712280433
+    # When the early axes are completely chunked then we are just moving blocks
+    # and can avoid any rechunking. The outchunks will always be ...
+    base = np.arange(np.prod(inshape)).reshape(inshape)
+    a = da.from_array(base, chunks=inchunks)
+
+    # and via reshape
+    result = a.reshape(outshape, merge_chunks=False)
+    assert result.chunks == outchunks
+    assert_eq(result, base.reshape(outshape))
+
+    assert result.chunks != a.reshape(outshape).chunks
