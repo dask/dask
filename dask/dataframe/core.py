366c7c2b70988de376d094dc2bc483d2fe88c32c
Ryan Williams <ryan@runsascoded.com>
2021-01-15 19:45:18 -0500

    new_dd_object: mode

--- dask/dataframe/core.py
+++ dask/dataframe/core.py
@@ -4850,7 +4850,9 @@ class DataFrame(_Frame):
         graph = HighLevelGraph.from_collections(
             name, dsk, dependencies=mode_series_list
         )
-        ddf = new_dd_object(graph, name, meta, divisions=(None, None))
+        ddf = new_dd_object(
+            graph, name, meta, divisions=(None, None), partition_sizes=None
+        )
 
         return ddf
 
