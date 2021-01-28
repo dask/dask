64c52e6627229569580af0d06aca6f30f5892ae2
Davis Vann Bennett <davis.v.bennett@gmail.com>
2021-01-28 16:04:58 -0500

    add failing tests

--- dask/array/tests/test_routines.py
+++ dask/array/tests/test_routines.py
@@ -1548,12 +1548,11 @@ def test_coarsen_with_excess():
         np.array([0 + 1 + 2, 3 + 4 + 5, 6 + 7 + 8]),
     )
 
-
-def test_coarsen_bad_chunks():
-
-    x1 = da.arange(10, chunks=5)
-    x2 = x1.rechunk((1, 2, 3, 4))
-    assert_eq(da.coarsen(np.sum, x1, {0: 5}), da.coarsen(np.sum, x2, {0: 5}))
+@pytest.mark.parametrize('chunks', [(x,) * 3 for x in range(16,32)])
+def test_coarsen_bad_chunks(chunks):
+    x1 = da.arange(np.sum(chunks), chunks=5)
+    x2 = x1.rechunk(tuple(chunks))
+    assert_eq(da.coarsen(np.sum, x1, {0: 10}, trim_excess=True), da.coarsen(np.sum, x2, {0: 10}, trim_excess=True))
 
 
 def test_aligned_coarsen_chunks():
