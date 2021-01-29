bf3179157a92852b2016c0d0cc063f93a23bf1ce
D-Stacks <78099568+D-Stacks@users.noreply.github.com>
2021-01-29 13:21:35 +0100

    Update test_routines.py

--- dask/array/tests/test_routines.py
+++ dask/array/tests/test_routines.py
@@ -977,10 +977,13 @@ def test_ravel():
     assert_eq(np.ravel(x), da.ravel(a))
 
 def test_ravel_array_like():
+    
     #int
     assert_eq(np.ravel(0), da.ravel(0))
+    
     #list
     assert_eq(np.ravel([0,0]), da.ravel([0,0]))
+    
     #tuple
     assert_eq(np.ravel((0,0)), da.ravel((0,0)))
     
