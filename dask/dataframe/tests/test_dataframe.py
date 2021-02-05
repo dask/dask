e32f6a4d799e6a0c9007e7394c1b8b8db9a840eb
Nils Braun <nils-braun@users.noreply.github.com>
2021-02-05 19:27:10 +0100

    Added support for Float64 in column assignment (#7173)
    * Added support for Float64, solving #7156
    
    * Early exit on older pandas versions

--- dask/dataframe/tests/test_dataframe.py
+++ dask/dataframe/tests/test_dataframe.py
@@ -4467,3 +4467,17 @@ def test_join_series():
     expected_df = dd.from_pandas(df.join(df["x"], lsuffix="_"), npartitions=1)
     actual_df = ddf.join(ddf["x"], lsuffix="_")
     assert_eq(actual_df, expected_df)
+
+
+@pytest.mark.skipif(
+    not dd._compat.PANDAS_GT_120, reason="Float64 was introduced in pandas>=1.2"
+)
+def test_assign_na_float_columns():
+    # See https://github.com/dask/dask/issues/7156
+    df_pandas = pd.DataFrame({"a": [1.1]}, dtype="Float64")
+    df = dd.from_pandas(df_pandas, npartitions=1)
+
+    df = df.assign(new_col=df["a"])
+
+    assert df.compute()["a"].dtypes == "Float64"
+    assert df.compute()["new_col"].dtypes == "Float64"
