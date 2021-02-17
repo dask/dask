0c260483a687213a28465540853a34f7465249f7
crusaderky <crusaderky@gmail.com>
2021-02-17 13:27:41 +0000

    Hack around mamba#412

--- continuous_integration/scripts/install.sh
+++ continuous_integration/scripts/install.sh
@@ -9,7 +9,10 @@ set -xe
 if [[ ${UPSTREAM_DEV} ]]; then
     mamba update -y -c arrow-nightlies pyarrow
 
-    mamba uninstall --force numpy pandas
+    # FIXME https://github.com/mamba-org/mamba/issues/412
+    # mamba uninstall --force numpy pandas
+    conda uninstall --force numpy pandas
+
     python -m pip install --no-deps --pre \
         -i https://pypi.anaconda.org/scipy-wheels-nightly/simple \
         numpy \
