eaaec80ffe768d13be6776edf42c4af55d3de8db
crusaderky <crusaderky@gmail.com>
2021-02-19 01:41:26 +0000

    coverage fixes

--- continuous_integration/scripts/run_tests.sh
+++ continuous_integration/scripts/run_tests.sh
@@ -5,7 +5,7 @@ set -e
 # Need to make test order deterministic when parallelizing tests, hence PYTHONHASHSEED
 # (see https://github.com/pytest-dev/pytest-xdist/issues/63)
 if [[ $PARALLEL == 'true' ]]; then
-    export XTRATESTARGS="-n4 $XTRATESTARGS"
+    export XTRATESTARGS="-n3 $XTRATESTARGS"
     export PYTHONHASHSEED=42
 fi
 
