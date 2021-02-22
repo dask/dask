73178ab4ea8c9ea815854b022be04fed1dea416d
crusaderky <crusaderky@gmail.com>
2021-02-22 22:49:15 +0000

    Replace coveralls with codecov.io (#7246)

--- continuous_integration/scripts/coverage.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-coverage report --show-missing
-python -m pip install coveralls
-coveralls
--- continuous_integration/scripts/run_tests.sh
+++ continuous_integration/scripts/run_tests.sh
@@ -5,16 +5,15 @@ set -e
 # Need to make test order deterministic when parallelizing tests, hence PYTHONHASHSEED
 # (see https://github.com/pytest-dev/pytest-xdist/issues/63)
 if [[ $PARALLEL == 'true' ]]; then
-    export XTRATESTARGS="-n3 $XTRATESTARGS"
+    export XTRATESTARGS="-n4 $XTRATESTARGS"
     export PYTHONHASHSEED=42
 fi
 
 if [[ $COVERAGE == 'true' ]]; then
-    echo "coverage run `which py.test` dask --runslow --doctest-modules $XTRATESTARGS"
-    coverage run `which py.test` dask --runslow --doctest-modules $XTRATESTARGS
-else
-    echo "py.test dask --runslow $XTRATESTARGS"
-    py.test dask --runslow $XTRATESTARGS
+    export XTRATESTARGS="--cov=dask --cov-report=xml $XTRATESTARGS"
 fi
+
+echo "py.test dask --runslow $XTRATESTARGS"
+py.test dask --runslow $XTRATESTARGS
 
 set +e
