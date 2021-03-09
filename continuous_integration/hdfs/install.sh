39d98b438fc06876af09cf255a4c12b8e299100c
James Bourbeau <jrbourbeau@gmail.com>
2021-03-09 11:09:12 -0600

    Pin pyarrow<3.0.0

--- continuous_integration/hdfs/install.sh
+++ continuous_integration/hdfs/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-docker exec hdfs conda install -y -q dask pyarrow">=0.14.0,!=2.0.0" fsspec pyyaml -c conda-forge
+docker exec hdfs conda install -y -q dask pyarrow">=0.14.0,!=2.0.0,<3.0.0" fsspec pyyaml -c conda-forge
 docker exec hdfs python -m pip install -e .
 
 set +xe
