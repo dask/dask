f6023545070b02c07287be06554feacfedd3e815
Tom White <tom.e.white@gmail.com>
2021-03-09 11:12:58 +0000

    Add test for chunktype of xarray.DataArray.

--- dask/array/tests/test_xarray.py
+++ dask/array/tests/test_xarray.py
@@ -22,3 +22,15 @@ def test_asanyarray():
     y = da.asanyarray(xr.DataArray([1, 2, 3.0]))
     assert isinstance(y, da.Array)
     assert_eq(y, y)
+
+
+def test_asarray_recognises_xarray_data():
+    module = xr.DataArray.__module__
+    try:
+        xr.DataArray.__module__ = "xarray"
+        y = da.asarray(xr.DataArray([1, 2, 3.0]))
+        assert isinstance(y, da.Array)
+        assert type(y._meta).__name__ == "ndarray"
+        assert_eq(y, y)
+    finally:
+        xr.DataArray.__module__ = module
