659b330bce5775be5a25f533f40ae76f8ab61226
Julia Signell <jsignell@gmail.com>
2021-03-10 11:03:12 -0500

    Run doctests with mindeps as well as latest

--- conftest.py
+++ conftest.py
@@ -26,6 +26,11 @@ try:
 except ImportError:
     collect_ignore_glob.append("dask/dataframe/*")
 
+try:
+    import scipy  # noqa: F401
+except ImportError:
+    collect_ignore.append("dask/array/stats.py")
+
 
 def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true", help="run slow tests")
