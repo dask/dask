42b10e2d603c3a2fd2bc43cfe5f87d97adfbce13
David Hassell <david.hassell@ncas.ac.uk>
2021-03-18 09:17:15 +0000

    add missing return

--- dask/array/slicing.py
+++ dask/array/slicing.py
@@ -32,6 +32,7 @@ def _sanitize_index_element(ind):
     elif is_dask_collection(ind):
         if ind.dtype.kind != "i" or ind.size != 1:
             raise IndexError(f"Bad index. Must be integer-like: {ind}")
+        return ind
     else:
         raise TypeError("Invalid index type", type(ind), ind)
 
