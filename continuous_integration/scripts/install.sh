cb34b298913d19681d4ec61f77085f3578eb2131
crusaderky <crusaderky@gmail.com>
2021-03-29 22:52:16 +0100

    Replace conda with mamba (#7227)

--- continuous_integration/scripts/install.sh
+++ continuous_integration/scripts/install.sh
@@ -7,9 +7,12 @@ set -xe
 # python -m pip install --no-deps cityhash
 
 if [[ ${UPSTREAM_DEV} ]]; then
-    conda update -y -c arrow-nightlies pyarrow
+    mamba update -y -c arrow-nightlies pyarrow
 
+    # FIXME https://github.com/mamba-org/mamba/issues/412
+    # mamba uninstall --force numpy pandas
     conda uninstall --force numpy pandas
+
     python -m pip install --no-deps --pre \
         -i https://pypi.anaconda.org/scipy-wheels-nightly/simple \
         numpy \
@@ -29,11 +32,11 @@ fi
 
 # Install dask
 python -m pip install --quiet --no-deps -e .[complete]
-echo conda list
-conda list
+echo mamba list
+mamba list
 
 # For debugging
 echo -e "--\n--Conda Environment (re-create this with \`conda env create --name <name> -f <output_file>\`)\n--"
-conda env export | grep -E -v '^prefix:.*$'
+mamba env export | grep -E -v '^prefix:.*$'
 
 set +xe
