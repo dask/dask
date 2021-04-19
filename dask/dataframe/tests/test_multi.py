e68235a9d33443e3a8a6cb807f4e3f62b709af14
rjzamora <rzamora217@gmail.com>
2021-04-19 09:30:22 -0700

    ensure ordering in test_merge_outer_empty

--- dask/dataframe/tests/test_multi.py
+++ dask/dataframe/tests/test_multi.py
@@ -2077,8 +2077,10 @@ def test_merge_outer_empty():
 
     for x in range(0, k_clusters + 1):
         assert_eq(
-            dd.merge(empty_df, df[df.cluster == x], how="outer").compute(),
-            df[df.cluster == x].compute(),
+            dd.merge(empty_df, df[df.cluster == x], how="outer")
+            .compute()
+            .sort_values("user"),
+            df[df.cluster == x].compute().sort_values("user"),
             check_index=False,
         )
 
