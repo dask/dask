f91d6c1ac51684476540bf92c8b7750c4dd5124a
Richard (Rick) Zamora <rzamora217@gmail.com>
2021-04-26 09:34:54 -0500

    support all or no overlap between partition columns and real columns (#7541)

--- dask/dataframe/io/parquet/fastparquet.py
+++ dask/dataframe/io/parquet/fastparquet.py
@@ -180,6 +180,19 @@ def _determine_pf_parts(fs, paths, gather_statistics, **kwargs):
             paths[0], open_with=fs.open, sep=fs.sep, **kwargs.get("file", {})
         )
 
+    # Ensure that there is no overlap between partition columns
+    # and explicit columns in `pf`
+    if pf.cats:
+        _partitions = [p for p in pf.cats if p not in pf.columns]
+        if not _partitions:
+            pf.cats = {}
+        elif len(_partitions) != len(pf.cats):
+            raise ValueError(
+                "No partition-columns should be written in the \n"
+                "file unless they are ALL written in the file.\n"
+                "columns: {} | partitions: {}".format(pf.columns, pf.cats.keys())
+            )
+
     return parts, pf, gather_statistics, base
 
 
