83fafc40b45f4379246ca3b5a4c96966521576da
rjzamora <rzamora217@gmail.com>
2021-04-27 11:31:57 -0700

    improve documentation

--- dask/blockwise.py
+++ dask/blockwise.py
@@ -32,17 +32,38 @@ from .utils import (
 
 
 class BlockwiseDep:
-    """Base class for (indexable) Blockwise-IO arguments"""
+    """Blockwise-IO argument
+
+    This is the base class for indexable Blockwise-IO arguments.
+    When constructing a ``Blockwise`` Layer, one or more of the
+    collection tuples passed in with ``indices`` may contain a
+    ``BlockwiseDep`` instance (in place of a "real" collection name).
+    This allows a new collection to be created (via IO) within a
+    ``Blockwise`` layer.
+
+    All ``BlockwiseDep`` instances must define a ``numblocks``
+    attribute to speficy the number of blocks/partitions the
+    object can support along each dimension. The object should
+    also define a ``produces_tasks`` attribute to specify if
+    any nested tasks will be passed to the Blockwise function.
+
+    See Also
+    --------
+    dask.blockwise.Blockwise
+    dask.blockwise.BlockwiseDepDict
+    """
 
     numblocks: Tuple[int, ...]
     produces_tasks: bool
 
     def __getitem__(self, idx: Tuple[int, ...]) -> Any:
+        """Return Blockwise-function arguments for a specific index"""
         raise NotImplementedError(
             "Must define `__getitem__` for `BlockwiseDep` subclass."
         )
 
     def get(self, idx: Tuple[int, ...], default) -> Any:
+        """BlockwiseDep ``__getitem__`` Wrapper"""
         try:
             return self.__getitem__(idx)
         except KeyError:
@@ -51,12 +72,23 @@ class BlockwiseDep:
     def __dask_distributed_pack__(
         self, required_indices: Optional[List[Tuple[int, ...]]] = None
     ):
+        """Client-side serialization for ``BlockwiseDep`` objects.
+
+        Should return a ``state`` dictionary, with msgpack-serializable
+        values, that can be used to initialize a new ``BlockwiseDep`` object
+        on a scheduler process.
+        """
         raise NotImplementedError(
             "Must define `__dask_distributed_pack__` for `BlockwiseDep` subclass."
         )
 
     @classmethod
     def __dask_distributed_unpack__(cls, state):
+        """Scheduler-side deserialization for ``BlockwiseDep`` objects.
+
+        Should use an input ``state`` dictionary to initialize a new
+        ``BlockwiseDep`` object.
+        """
         raise NotImplementedError(
             "Must define `__dask_distributed_unpack__` for `BlockwiseDep` subclass."
         )
@@ -66,7 +98,60 @@ class BlockwiseDep:
 
 
 class BlockwiseDepDict(BlockwiseDep):
-    """Base class for dictionary-based Blockwise-IO arguments"""
+    """Dictionary-based Blockwise-IO argument
+
+    This is a dictionary-backed instance of ``BlockwiseDep``.
+    The purpose of this class is to simplify the construction
+    of IO-based Blockwise Layers with block/partition-dependent
+    function arguments that are difficult to calculate at
+    graph-materialization time.
+
+    Examples
+    --------
+
+    Specify an IO-based function for the Blockwise Layer. Note
+    that the function will be passed a single input object when
+    the task is executed (e.g. a single ``tuple`` or ``dict``):
+
+    >>> import pandas as pd  # doctest: +SKIP
+    >>> func = lambda x: pd.read_csv(**x)  # doctest: +SKIP
+
+    Use ``BlockwiseDepDict`` to define the input argument to
+    ``func`` for each block/partition:
+
+    >>> dep = BlockwiseDepDict(
+    ...     mapping={
+    ...         (0,) : {
+    ...             "filepath_or_buffer": "data.csv",
+    ...             "skiprows": 1,
+    ...             "nrows": 2,
+    ...             "names": ["a", "b"],
+    ...         },
+    ...         (1,) : {
+    ...             "filepath_or_buffer": "data.csv",
+    ...             "skiprows": 3,
+    ...             "nrows": 2,
+    ...             "names": ["a", "b"],
+    ...         },
+    ...     }
+    ... )  # doctest: +SKIP
+
+    Construct a Blockwise Layer with ``dep`` speficied
+    in the ``indices`` list:
+
+    >>> layer = Blockwise(
+    ...     output="collection-name",
+    ...     output_indices="i",
+    ...     dsk={"collection-name": (func, '_0')},
+    ...     indices=[(dep, "i")],
+    ...     numblocks={},
+    ... )  # doctest: +SKIP
+
+    See Also
+    --------
+    dask.blockwise.Blockwise
+    dask.blockwise.BlockwiseDep
+    """
 
     def __init__(
         self,
