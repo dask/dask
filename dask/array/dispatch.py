65f4ff2d1b1f0033413af139f7f9629962ef52fe
James Bourbeau <jrbourbeau@users.noreply.github.com>
2021-05-24 17:05:23 -0500

    Consolidate array Dispatch objects (#7505)

---
+++ dask/array/dispatch.py
@@ -0,0 +1,13 @@
+"""
+Dispatch in dask.array.
+
+Also see backends.py
+"""
+
+from ..utils import Dispatch
+
+concatenate_lookup = Dispatch("concatenate")
+tensordot_lookup = Dispatch("tensordot")
+einsum_lookup = Dispatch("einsum")
+empty_lookup = Dispatch("empty")
+divide_lookup = Dispatch("divide")
--- dask/array/reductions.py
+++ dask/array/reductions.py
@@ -14,7 +14,6 @@ from ..base import tokenize
 from ..blockwise import lol_tuples
 from ..highlevelgraph import HighLevelGraph
 from ..utils import (
-    Dispatch,
     deepmap,
     derived_from,
     funcname,
@@ -26,18 +25,11 @@ from . import chunk
 from .blockwise import blockwise
 from .core import Array, _concatenate2, handle_out, implements
 from .creation import arange, diagonal
-from .numpy_compat import divide as np_divide
-from .numpy_compat import ma_divide
+
+# Keep empty_lookup here for backwards compatibility
+from .dispatch import divide_lookup, empty_lookup  # noqa: F401
 from .utils import compute_meta, full_like_safe, is_arraylike, validate_axis
 from .wrap import ones, zeros
-
-# Generic functions to support chunks of different types
-empty_lookup = Dispatch("empty")
-empty_lookup.register((object, np.ndarray), np.empty)
-empty_lookup.register(np.ma.masked_array, np.ma.empty)
-divide_lookup = Dispatch("divide")
-divide_lookup.register((object, np.ndarray), np_divide)
-divide_lookup.register(np.ma.masked_array, ma_divide)
 
 
 def divide(a, b, dtype=None):
