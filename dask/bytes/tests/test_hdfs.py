b2db6158c2ea23edee0ddf9455062735c413f6bd
Julia Signell <jsignell@gmail.com>
2021-06-02 16:24:47 -0400

    Import properly

--- dask/bytes/tests/test_hdfs.py
+++ dask/bytes/tests/test_hdfs.py
@@ -18,6 +18,7 @@ except (ImportError, SyntaxError):
 
 try:
     import pyarrow
+    from pyarrow.hdfs import HadoopFileSystem
 except ImportError:
     pyarrow = None
 
@@ -31,7 +32,7 @@ basedir = "/tmp/test-dask"
 
 @pytest.fixture
 def hdfs(request):
-    hdfs = pyarrow.fs.HadoopFileSystem(host="localhost", port=8020)
+    hdfs = HadoopFileSystem(host="localhost", port=8020)
 
     if hdfs.exists(basedir):
         hdfs.rm(basedir, recursive=True)
