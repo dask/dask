f93bc86c016d983d792fae65f6f2edeb007e20bd
Gabe Joseph <gjoseph92@gmail.com>
2021-06-03 11:56:15 -0600

    Array: correct number of outputs in `apply_gufunc` (#7669)
    * Array: correct number of outputs in `apply_gufunc`
    
    * Stricter #6736
    
    `args_meta[0]` could be wrong when there are multiple arguments. Even this I don't really like, since it's making a lot of assumptions about what `func` might be that may not be true in generality for any user-defined function.
    
    * keep std

--- dask/array/tests/test_gufunc.py
+++ dask/array/tests/test_gufunc.py
@@ -156,14 +156,15 @@ def test_apply_gufunc_output_dtypes_string(vectorize):
 @pytest.mark.parametrize("vectorize", [False, True])
 def test_apply_gufunc_output_dtypes_string_many_outputs(vectorize):
     def stats(x):
-        return np.mean(x, axis=-1), np.std(x, axis=-1)
+        return np.mean(x, axis=-1), np.std(x, axis=-1), np.min(x, axis=-1)
 
     a = da.random.normal(size=(10, 20, 30), chunks=(5, 5, 30))
-    mean, std = apply_gufunc(
-        stats, "(i)->(),()", a, output_dtypes=("f", "f"), vectorize=vectorize
+    mean, std, min = apply_gufunc(
+        stats, "(i)->(),(),()", a, output_dtypes=("f", "f", "f"), vectorize=vectorize
     )
     assert mean.compute().shape == (10, 20)
     assert std.compute().shape == (10, 20)
+    assert min.compute().shape == (10, 20)
 
 
 def test_apply_gufunc_pass_additional_kwargs():
@@ -171,8 +172,8 @@ def test_apply_gufunc_pass_additional_kwargs():
         assert bar == 2
         return x
 
-    ret = apply_gufunc(foo, "()->()", 1.0, output_dtypes="f", bar=2)
-    assert_eq(ret, np.array(1.0, dtype="f"))
+    ret = apply_gufunc(foo, "()->()", 1.0, output_dtypes=float, bar=2)
+    assert_eq(ret, np.array(1.0, dtype=float))
 
 
 def test_apply_gufunc_02():
