24bf5b993a53635da79ebfd5a8a18cddc4b9184e
Freyam Mehta <62539811+freyam@users.noreply.github.com>
2021-06-08 18:11:00 +0400

    Fixing calling `.visualize()` with `filename=None` (#7740)
    * Fixed #7685 (adding an extra if statement)
    * Modularised the no_filename test and edited the If statement
    * Added Comments and Condensed the If statement
    * Added a simple test for visualize when filename=None

--- dask/tests/test_base.py
+++ dask/tests/test_base.py
@@ -954,6 +954,10 @@ def test_visualize():
         visualize(x, filename=os.path.join(d, "mydask.png"))
         assert os.path.exists(os.path.join(d, "mydask.png"))
 
+        # To see if visualize() works when the filename parameter is set to None
+        # If the function raises an error, the test will fail
+        x.visualize(filename=None)
+
 
 @pytest.mark.skipif("not da")
 @pytest.mark.skipif(
