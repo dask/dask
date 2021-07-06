7b85da0afe83b6e04b6e3d6280f9252e34d73684
James Bourbeau <jrbourbeau@gmail.com>
2021-07-06 17:16:56 -0500

    Consolidate error messages

--- dask/distributed.py
+++ dask/distributed.py
@@ -1,15 +1,17 @@
 # flake8: noqa
+
+_import_error_message = (
+    "dask.distributed is not installed.\n\n"
+    "Please either conda or pip install distributed:\n\n"
+    "  conda install dask distributed             # either conda install\n"
+    '  python -m pip install "dask[distributed]" --upgrade    # or pip install'
+)
+
 try:
     from distributed import *
 except ImportError as e:
     if e.msg == "No module named 'distributed'":
-        msg = (
-            "Dask's distributed scheduler is not installed.\n\n"
-            "Please either conda or pip install dask distributed:\n\n"
-            "  conda install dask distributed          # either conda install\n"
-            '  python -m pip install "dask[distributed]" --upgrade  # or python -m pip install'
-        )
-        raise ImportError(msg) from e
+        raise ImportError(_import_error_message) from e
     else:
         raise
 
@@ -18,11 +20,5 @@ def __getattr__(value):
     try:
         import distributed
     except ImportError as e:
-        msg = (
-            "dask.distributed is not installed.\n\n"
-            "Please either conda or pip install distributed:\n\n"
-            "  conda install dask distributed             # either conda install\n"
-            "  python -m pip install dask[distributed] --upgrade    # or pip install"
-        )
-        raise ImportError(msg) from e
+        raise ImportError(_import_error_message) from e
     return getattr(distributed, value)
