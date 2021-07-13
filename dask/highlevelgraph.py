805437d1a97ec2bca7950f4d278f438868a55f70
Freyam Mehta <frey77mehta@gmail.com>
2021-07-14 02:08:35 +0400

    used get method and added padding

--- dask/highlevelgraph.py
+++ dask/highlevelgraph.py
@@ -502,10 +502,9 @@ class Layer(collections.abc.Mapping):
         from dask.array.svg import svg
 
         svg_repr = ""
-        if "type" in self.collection_annotations:
-            if self.collection_annotations["type"] == "dask.array.core.Array":
-                chunks = self.collection_annotations["chunks"]
-                svg_repr = svg(chunks, size=200)
+        if self.collection_annotations.get("type") == "dask.array.core.Array":
+            chunks = self.collection_annotations["chunks"]
+            svg_repr = svg(chunks)
 
         info = self.layer_info_dict()
         layer_info_table = html_from_dict(info)
@@ -519,6 +518,7 @@ class Layer(collections.abc.Mapping):
                     <p style="color: var(--jp-ui-font-color2, #5D5851); margin: -0.25em 0px 0px 0px;">
                         {highlevelgraph_key}
                     </p>
+                    <br>
                     {svg_repr}
                     {layer_info_table}
                 </details>
