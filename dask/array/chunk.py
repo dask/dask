a48193e1815d06ec4d807bf1e98e5b5262cfc810
Peter Andreas Entschev <peter@entschev.com>
2021-07-23 06:27:17 -0700

    Fix more black errors

--- dask/array/chunk.py
+++ dask/array/chunk.py
@@ -319,6 +319,7 @@ def slice_with_int_dask_array(x, idx, offset, x_size, axis):
     current chunk.
     """
     from .utils import asarray_safe, meta_from_array
+
     idx = asarray_safe(idx, like=meta_from_array(x))
 
     # Needed when idx is unsigned
