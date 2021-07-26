ae5a5283c3e2e45157a7243dcff63850c422e1c8
Zhengnan Zhao <zzhengnan@protonmail.com>
2021-07-26 10:27:31 -0400

    Remove skips from doctests (3 of 6) (#7872)

--- dask/array/core.py
+++ dask/array/core.py
@@ -622,8 +622,8 @@ def map_blocks(
     You may specify the key name prefix of the resulting task in the graph with
     the optional ``token`` keyword argument.
 
-    >>> x.map_blocks(lambda x: x + 1, name='increment')  # doctest: +SKIP
-    dask.array<increment, shape=(100,), dtype=int64, chunksize=(10,), chunktype=numpy.ndarray>
+    >>> x.map_blocks(lambda x: x + 1, name='increment')
+    dask.array<increment, shape=(1000,), dtype=int64, chunksize=(100,), chunktype=numpy.ndarray>
 
     For functions that may not handle 0-d arrays, it's also possible to specify
     ``meta`` with an empty array matching the type of the expected result. In
@@ -933,7 +933,6 @@ def store(
 
     Examples
     --------
-    >>> x = ...  # doctest: +SKIP
 
     >>> import h5py  # doctest: +SKIP
     >>> f = h5py.File('myfile.hdf5', mode='a')  # doctest: +SKIP
