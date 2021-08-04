611e767a8e47c825f64adb909f60b3732a7a5f56
rjzamora <rzamora@nvidia.com>
2021-08-04 14:32:38 -0500

    remove usage of pyorc

--- dask/dataframe/io/tests/test_orc.py
+++ dask/dataframe/io/tests/test_orc.py
@@ -1,6 +1,5 @@
 import os
 import shutil
-import sys
 import tempfile
 
 import numpy as np
@@ -134,45 +133,11 @@ def test_orc_roundtrip_aggregate_files(tmpdir, split_stripes):
     assert_eq(data, df2, check_index=False)
 
 
-@pytest.mark.skipif(
-    sys.version_info[:2] > (3, 8),
-    reason=("PyORC sometimes crashes for python-3.9"),
-)
-def test_orc_roundtrip_aggregate_files_offset(tmp_path):
-
-    # PyORC required to write orc files with specific
-    # stripe sizes
-    pyorc = pytest.importorskip("pyorc")
-
-    size = 10_000
-    df = pd.DataFrame(
-        {
-            "num": np.random.randint(100, size=size),
-            "text": np.random.choice(["dog", "cat", "bird"], size),
-        }
-    )
-
-    # TODO: Use pyarrow to write the files for this test
-    # once the python API exposes the stripe size. Note
-    # that pyorc sometimes crashes for python-3.9
-    # See: https://github.com/noirello/pyorc/issues/10
-    for i in range(0, size, size // 2):
-        _df = df[i : i + size // 2]
-        with open(os.path.join(tmp_path, f"data_{i}.orc"), "wb") as f:
-            with pyorc.Writer(
-                f,
-                "struct<num:int,text:string>",
-                struct_repr=pyorc.StructRepr.DICT,
-                stripe_size=1_000,
-                compression=pyorc.CompressionKind.NONE,
-                compression_block_size=1_000,
-            ) as writer:
-                writer.writerows(_df.to_dict(orient="records"))
-
-    # Default read should give back 10 partitions. Therefore,
-    # specifying split_stripes=6 & aggregate_files=True should
+def test_orc_aggregate_files_offset(orc_files):
+    # Default read should give back 16 partitions. Therefore,
+    # specifying split_stripes=11 & aggregate_files=True should
     # produce 2 partitions (with the first being larger than
     # the second)
-    df2 = dd.read_orc(tmp_path, split_stripes=6, aggregate_files=True)
+    df2 = dd.read_orc(orc_files[:2], split_stripes=11, aggregate_files=True)
     assert df2.npartitions == 2
-    assert len(df2.partitions[0].index) > size // 2
+    assert len(df2.partitions[0].index) > len(df2.index) // 2
