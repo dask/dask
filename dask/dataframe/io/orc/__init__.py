0cb31dbff833dc7de69aa2ae2af9eb9fe708b166
Richard (Rick) Zamora <rzamora@nvidia.com>
2021-08-05 17:23:06 -0500

    Refactor and Expand Dask-Dataframe ORC API (#7756)

---
+++ dask/dataframe/io/orc/__init__.py
@@ -0,0 +1,1 @@
+from .core import read_orc, to_orc
---
+++ dask/dataframe/io/orc/utils.py
@@ -0,0 +1,16 @@
+class ORCEngine:
+    """The API necessary to provide a new ORC reader/writer"""
+
+    @classmethod
+    def read_metadata(
+        cls, fs, paths, columns, index, split_stripes, aggregate_files, **kwargs
+    ):
+        raise NotImplementedError()
+
+    @classmethod
+    def read_partition(cls, fs, part, columns, **kwargs):
+        raise NotImplementedError()
+
+    @classmethod
+    def write_partition(cls, df, path, fs, filename, **kwargs):
+        raise NotImplementedError
