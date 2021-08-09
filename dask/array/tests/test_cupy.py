e550e0abd0055dca65a6739765c37326c872e126
Peter Andreas Entschev <peter@entschev.com>
2021-08-09 11:42:05 -0700

    Add CuPy tests for {nan,}arg{min,max}

--- dask/array/tests/test_cupy.py
+++ dask/array/tests/test_cupy.py
@@ -1882,3 +1882,60 @@ def test_diff_append(n):
             # When order is 0 the result is the icupyut array, it doesn't raise
             # an error
             da.diff(a, n, append=cupy.zeros((3, 3)))
+
+
+@pytest.mark.parametrize(
+    ["dfunc", "func"],
+    [
+        (da.argmin, np.argmin),
+        (da.argmax, np.argmax),
+        (da.nanargmin, np.nanargmin),
+        (da.nanargmax, np.nanargmax),
+    ],
+)
+def test_arg_reductions(dfunc, func):
+    x = cupy.random.random((10, 10, 10))
+    a = da.from_array(x, chunks=(3, 4, 5))
+
+    assert_eq(dfunc(a), func(x))
+    assert_eq(dfunc(a, 0), func(x, 0))
+    assert_eq(dfunc(a, 1), func(x, 1))
+    assert_eq(dfunc(a, 2), func(x, 2))
+    with dask.config.set(split_every=2):
+        assert_eq(dfunc(a), func(x))
+        assert_eq(dfunc(a, 0), func(x, 0))
+        assert_eq(dfunc(a, 1), func(x, 1))
+        assert_eq(dfunc(a, 2), func(x, 2))
+
+    pytest.raises(ValueError, lambda: dfunc(a, 3))
+    pytest.raises(TypeError, lambda: dfunc(a, (0, 1)))
+
+    x2 = cupy.arange(10)
+    a2 = da.from_array(x2, chunks=3)
+    assert_eq(dfunc(a2), func(x2))
+    assert_eq(dfunc(a2, 0), func(x2, 0))
+    assert_eq(dfunc(a2, 0, split_every=2), func(x2, 0))
+
+
+@pytest.mark.parametrize(
+    ["dfunc", "func"], [(da.nanargmin, np.nanargmin), (da.nanargmax, np.nanargmax)]
+)
+def test_nanarg_reductions(dfunc, func):
+    x = cupy.random.random((10, 10, 10))
+    x[5] = cupy.nan
+    a = da.from_array(x, chunks=(3, 4, 5))
+    assert_eq(dfunc(a), func(x))
+    assert_eq(dfunc(a, 0), func(x, 0))
+    with pytest.raises(ValueError):
+        with pytest.warns(None):  # All NaN axis
+            dfunc(a, 1).compute()
+
+    with pytest.raises(ValueError):
+        with pytest.warns(None):  # All NaN axis
+            dfunc(a, 2).compute()
+
+    x[:] = cupy.nan
+    a = da.from_array(x, chunks=(3, 4, 5))
+    with pytest.raises(ValueError):
+        with pytest.warns(None):  # All NaN axis
+            dfunc(a).compute()
