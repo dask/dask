da67bbe7ac0d1a2e5e32e2d031af8f9ebf606374
Jacob Tomlinson <jtomlinson@nvidia.com>
2021-08-11 15:48:31 +0100

    Add widget machinery

---
+++ dask/widgets/__init__.py
@@ -0,0 +1,1 @@
+from .widgets import FILTERS, TEMPLATE_PATHS, get_environment, get_template
---
+++ dask/widgets/tests/test_widgets.py
@@ -0,0 +1,45 @@
+import os.path
+
+import pytest
+from jinja2 import Environment, Template, TemplateNotFound
+
+from dask.utils import format_bytes
+from dask.widgets import FILTERS, TEMPLATE_PATHS, get_environment, get_template
+
+
+@pytest.fixture(autouse=True)
+def setup_testing():
+    TEMPLATE_PATHS.append(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
+    )
+    FILTERS["custom_filter"] = lambda x: "baz"
+
+
+def test_widgets():
+    template = get_template("example.html.j2")
+    assert isinstance(template, Template)
+    rendered = template.render(foo="bar")
+    assert "Hello bar" in rendered
+
+
+def test_environment():
+    environment = get_environment()
+    assert isinstance(environment, Environment)
+
+
+def test_unknown_template():
+    with pytest.raises(TemplateNotFound) as e:
+        get_template("does_not_exist.html.j2")
+
+        # The error should contain all the registered template directories to help the user
+        # understand where jinja2 is looking. Including the one we registered in the fixture.
+        assert os.path.dirname(os.path.abspath(__file__)) in str(e)
+
+
+def test_filters():
+    template = get_template("bytes.html.j2")
+    assert format_bytes in FILTERS.values()
+    assert format_bytes(2e9) in template.render(foo=2e9)
+
+    template = get_template("custom_filter.html.j2")
+    assert "baz" in template.render(foo=None)
