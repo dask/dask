fdb857636a02908b7c90030dce111e865c474d84
Aaron Meurer <asmeurer@gmail.com>
2021-08-26 17:24:16 -0600

    Store both args and kwargs on Expr

--- dask/expr.py
+++ dask/expr.py
@@ -1,10 +1,17 @@
 class Expr:
-    def __init__(self, *args):
+    def __init__(self, *args, **kwargs):
         self.args = args
+        self.kwargs = kwargs
 
     def __repr__(self):
-        return self.__class__.__name__ + "(" + ', '.join(map(repr, self.args)) + ")"
+        func = self.__class__.__name__
+        args = ', '.join(map(repr, self.args))
+        kwargs = ', '.join(f"{key}={val}" for key, val in self.kwargs.items())
+        sep = ', ' if args and kwargs else ''
+        return f"{func}({args}{sep}{kwargs})"
 
-    __str__ = __repr__
+    def __str__(self):
+        return self.__repr__()
+
     # def __eq__(self, other):
     #     return type(self) == type(other) and self.args == other.args
