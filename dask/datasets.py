4f14dcb45a488968478bd792dde3935b1ddc47fe
dahn <daniel.jahn@targomo.com>
2021-08-31 01:14:21 +0200

    MAINT: Remove commended-out code

--- dask/datasets.py
+++ dask/datasets.py
@@ -76,8 +76,6 @@ def _generate_mimesis(field, schema_description, records_per_partition, seed):
     field = Field(seed=seed, **field)
     schema = Schema(schema=lambda: schema_description(field))
     return [schema.create(iterations=1)[0] for i in range(records_per_partition)]
-    # for i in range(records_per_partition):
-    #     yield schema.create(iterations=1)[0]
 
 
 def _make_mimesis(field, schema, npartitions, records_per_partition, seed=None):
