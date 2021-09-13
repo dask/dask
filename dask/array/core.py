6dd4a98244bd166cc104dcc1bcc84f22eafb6f5f
neel iyer <33139997+spiyer99@users.noreply.github.com>
2021-09-14 00:51:50 +1000

    add error if drop_index is negative (#8064)

--- dask/array/core.py
+++ dask/array/core.py
@@ -694,6 +694,8 @@ def map_blocks(
         dtype = apply_infer_dtype(func, args, original_kwargs, "map_blocks")
 
     if drop_axis:
+        if any(i < 0 for i in drop_axis):
+            raise ValueError(f"Expected drop_axis to be non-negative; got {drop_axis}.")
         out_ind = tuple(x for i, x in enumerate(out_ind) if i not in drop_axis)
     if new_axis is None and chunks is not None and len(out_ind) < len(chunks):
         new_axis = range(len(chunks) - len(out_ind))
