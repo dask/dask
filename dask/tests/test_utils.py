f6f251d786b7620557e0fee362a475051af63290
Jim Crist-Harif <jcrist@users.noreply.github.com>
2021-09-27 16:11:05 -0500

    Dispatch walks mro for lazily registered handlers (#8185)
    Previously when `Dispatch` walked the `mro` of a class, it would only
    look for eagerly registered handlers (instead of lazily registered
    handlers). This meant that subclasses of lazily registered handlers
    would only be used when dispatching on a subclass if the parent class
    had been dispatched on earlier (this triggering the registration).
    
    This patch fixes this to check for lazily registered handlers when
    walking the mro. This results in negligible slowdown during the fast
    path.
    
    Fixes #8111.

--- dask/tests/test_utils.py
+++ dask/tests/test_utils.py
@@ -161,6 +161,38 @@ def test_dispatch_lazy():
     assert foo(1) == 1
 
 
+def test_dispatch_lazy_walks_mro():
+    """Check that subclasses of classes with lazily registered handlers still
+    use their parent class's handler by default"""
+    import decimal
+
+    class Lazy(decimal.Decimal):
+        pass
+
+    class Eager(Lazy):
+        pass
+
+    foo = Dispatch()
+
+    @foo.register(Eager)
+    def eager_handler(x):
+        return "eager"
+
+    def lazy_handler(a):
+        return "lazy"
+
+    @foo.register_lazy("decimal")
+    def register_decimal():
+        foo.register(decimal.Decimal, lazy_handler)
+
+    assert foo.dispatch(Lazy) == lazy_handler
+    assert foo(Lazy(1)) == "lazy"
+    assert foo.dispatch(decimal.Decimal) == lazy_handler
+    assert foo(decimal.Decimal(1)) == "lazy"
+    assert foo.dispatch(Eager) == eager_handler
+    assert foo(Eager(1)) == "eager"
+
+
 def test_random_state_data():
     np = pytest.importorskip("numpy")
     seed = 37
