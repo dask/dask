c7b5acc4878e1c69385588ac5104a208e11538c7
James Bourbeau <jrbourbeau@gmail.com>
2021-10-08 11:43:46 -0500

    Include Bag and DataFramae APIs too

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -107,7 +107,10 @@ html_theme = "dask_sphinx_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {"logo_only": True, "remove_toctrees_from": ["array-api.rst"]}
+html_theme_options = {
+    "logo_only": True,
+    "remove_toctrees_from": ["array-api.rst", "bag-api.rst", "dataframe-api.rst"],
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
