608a54b9080f91eb763b0bf64d632d005795c92f
Jim Crist-Harif <jcristharif@gmail.com>
2021-10-14 10:17:12 -0500

    Fixups
    - Add test
    - Simplify logic

--- dask/dataframe/core.py
+++ dask/dataframe/core.py
@@ -1068,9 +1068,11 @@ Dask Name: {name}, {task} tasks"""
         compute : bool, optional
             Whether to compute the result, default is True.
         """
-        if npartitions == self.npartitions or npartitions == -1:
-            return self._head(n=n, npartitions=npartitions, compute=compute, safe=False)
-        return self._head(n=n, npartitions=npartitions, compute=compute, safe=True)
+        if npartitions <= -1:
+            npartitions = self.npartitions
+        # No need to warn if we're already looking at all partitions
+        safe = npartitions != self.npartitions
+        return self._head(n=n, npartitions=npartitions, compute=compute, safe=safe)
 
     def _head(self, n, npartitions, compute, safe):
         if npartitions <= -1:
