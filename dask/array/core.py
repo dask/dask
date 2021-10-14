451dc6bc98d0ba23a5310d454ed63fd6a1099916
ek234 <71319388+ek234@users.noreply.github.com>
2021-10-14 12:07:07 +0400

    changed set() to {}

--- dask/array/core.py
+++ dask/array/core.py
@@ -4351,11 +4351,10 @@ def elemwise(op, *args, **kwargs):
     blockwise
     """
     out = kwargs.pop("out", None)
-    if not set(["name", "dtype", "where"]).issuperset(kwargs):
+    if not {"name", "dtype", "where"}.issuperset(kwargs):
         msg = "%s does not take the following keyword arguments %s"
         raise TypeError(
-            msg
-            % (op.__name__, str(sorted(set(kwargs) - set(["name", "dtype", "where"]))))
+            msg % (op.__name__, str(sorted(set(kwargs) - {"name", "dtype", "where"})))
         )
 
     args = [np.asarray(a) if isinstance(a, (list, tuple)) else a for a in args]
