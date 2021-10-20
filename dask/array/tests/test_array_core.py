2d04ee7c40a2205bef24b877b141cc22b4a4c889
ek234 <71319388+ek234@users.noreply.github.com>
2021-10-21 02:26:46 +0400

    Used da instead of np for da variables

--- dask/array/tests/test_array_core.py
+++ dask/array/tests/test_array_core.py
@@ -920,17 +920,20 @@ def test_elemwise_where():
     x = np.arange(10, dtype=np.uint64)
     mask = [True, False, True, False, True, False, True, False, False, False]
 
-    o_da = o_np = np.arange(20, 30, dtype=np.uint64)
+    o_da = da.arange(20, 30, dtype=np.uint64)
+    o_np = np.arange(20, 30, dtype=np.uint64)
     assert np.array_equal(
         da.invert(x, out=o_da, where=mask), np.invert(x, out=o_np, where=mask)
     )
 
-    o_da = o_np = np.arange(20, 30, dtype=np.uint64)
+    o_da = da.arange(20, 30, dtype=np.uint64)
+    o_np = np.arange(20, 30, dtype=np.uint64)
     assert np.array_equal(
         da.invert(x, out=o_da, where=False), np.invert(x, out=o_np, where=False)
     )
 
-    o_da = o_np = np.arange(20, 30, dtype=np.uint64)
+    o_da = da.arange(20, 30, dtype=np.uint64)
+    o_np = np.arange(20, 30, dtype=np.uint64)
     assert np.array_equal(da.invert(x, out=o_da), np.invert(x, out=o_np))
 
 
