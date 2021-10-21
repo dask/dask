6d5eead10422ead6a54096d88ab4eb239c929072
ek234 <71319388+ek234@users.noreply.github.com>
2021-10-21 14:02:23 +0400

    Now, `out` option will only be added when `where` option exists.

--- dask/array/core.py
+++ dask/array/core.py
@@ -4410,10 +4410,11 @@ def elemwise(op, *args, **kwargs):
 
     name = kwargs.get("name", None) or f"{funcname(op)}-{tokenize(op, dt, *args)}"
 
-    blockwise_kwargs = dict(dtype=dt, name=name, out=out, token=funcname(op).strip("_"))
+    blockwise_kwargs = dict(dtype=dt, name=name, token=funcname(op).strip("_"))
 
     if "where" in kwargs:
         blockwise_kwargs["where"] = kwargs.get("where")
+        blockwise_kwargs["out"] = out
 
     if need_enforce_dtype:
         blockwise_kwargs["enforce_dtype"] = dt
