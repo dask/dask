e98d0a5cba21651806256abdfcbe39162e23eb4f
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2021-11-18 20:57:24 +1100

    Fix indentation level

--- dask/array/overlap.py
+++ dask/array/overlap.py
@@ -637,12 +637,12 @@ def map_overlap(
         trim = get(sig.index("trim"), args, trim)
         func, args = args[0], [func]
 
-        if boundary is None:
-            warnings.warn(
-                "Default 'boundary' argument value will change from 'reflect' "
-                "to 'none' in future versions.",
-                FutureWarning,
-            )
+    if boundary is None:
+        warnings.warn(
+            "Default 'boundary' argument value will change from 'reflect' "
+            "to 'none' in future versions.",
+            FutureWarning,
+        )
 
     if not callable(func):
         raise TypeError(
