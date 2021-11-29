c9ae13ed2a845036d2483a7b7bac4fb67ca1bf42
Martin Durant <martin.durant@alumni.utoronto.ca>
2021-11-29 14:33:50 -0500

    fix following fsspec release

--- dask/bytes/tests/test_http.py
+++ dask/bytes/tests/test_http.py
@@ -1,4 +1,5 @@
 import os
+from packaging.version import parse
 import subprocess
 import sys
 import time
@@ -115,8 +116,12 @@ def test_ops_blocksize(dir_server):
         fn = files[1]
         f = open_files(root + fn, block_size=2)[0]
         with f as f:
-            # fails because we want only 12 bytes
-            with pytest.raises(ValueError):
+            if parse(fsspec.__version__) < parse("2021.11.1"):
+                # fails because we want only 12 bytes
+                with pytest.raises(ValueError):
+                    assert f.read(10) == expected[:10]
+            else:
+                # fixed in https://github.com/fsspec/filesystem_spec/pull/830
                 assert f.read(10) == expected[:10]
 
 
