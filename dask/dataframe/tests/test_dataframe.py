18df87e97bac8164f9292410f7152b9435ae1e8d
Gabe Joseph <gjoseph92@gmail.com>
2021-12-02 13:40:58 -0700

    Use `hlg_layer` helper

--- dask/dataframe/tests/test_dataframe.py
+++ dask/dataframe/tests/test_dataframe.py
@@ -30,6 +30,7 @@ from dask.dataframe.core import (
 from dask.dataframe.utils import assert_eq, assert_max_deps, make_meta
 from dask.datasets import timeseries
 from dask.utils import M, is_dataframe_like, is_series_like, put_lines
+from dask.utils_test import hlg_layer
 
 dsk = {
     ("x", 0): pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}, index=[0, 1, 3]),
@@ -907,9 +908,7 @@ def test_map_partitions():
     assert_eq(d.map_partitions(lambda df: df, meta=d), full)
     assert_eq(d.map_partitions(lambda df: df), full)
     result = d.map_partitions(lambda df: df.sum(axis=1))
-    layer = [
-        layer for k, layer in result.dask.layers.items() if k.startswith("lambda-")
-    ][0]
+    layer = hlg_layer(result.dask, "lambda-")
     assert not layer.is_materialized(), layer
     result.dask.validate()
     assert_eq(result, full.sum(axis=1))
