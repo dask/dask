9d5637431d7d1e7f8cf1135ca3cac6507434c718
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2021-12-07 12:26:28 -0500

    Revert "Always use divisions setter in constructors"
    This reverts commit b7d3368b98d7512ac8448337841c1aa394660abd.

--- dask/dataframe/core.py
+++ dask/dataframe/core.py
@@ -322,7 +322,7 @@ class _Frame(DaskMethodsMixin, OperatorMethodMixin):
                 f"{typename(type(meta))}"
             )
         self._meta = meta
-        self.divisions = divisions
+        self._divisions = tuple(divisions)
 
     def __dask_graph__(self):
         return self.dask
@@ -438,7 +438,7 @@ class _Frame(DaskMethodsMixin, OperatorMethodMixin):
         return self._args
 
     def __setstate__(self, state):
-        self.dask, self._name, self._meta, self.divisions = state
+        self.dask, self._name, self._meta, self._divisions = state
 
     def copy(self, deep=False):
         """Make a copy of the dataframe
