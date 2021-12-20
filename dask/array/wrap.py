aff761e0cd0e0ff7fa82e3b178db0e593815e608
aeisenbarth <54448967+aeisenbarth@users.noreply.github.com>
2021-12-20 18:39:07 +0100

    Check for dtype attribute instead of np.dtype
    Co-authored-by: Julia Signell <jsignell@gmail.com>

--- dask/array/wrap.py
+++ dask/array/wrap.py
@@ -188,7 +188,10 @@ def full(shape, fill_value, *args, **kwargs):
             f"fill_value must be scalar. Received {type(fill_value).__name__} instead."
         )
     if "dtype" not in kwargs:
-        kwargs["dtype"] = np.dtype(fill_value)
+        if hasattr(fill_value, "dtype"):
+            kwargs["dtype"] = fill_value.dtype
+        else:
+            kwargs["dtype"] = type(fill_value)
     return _full(shape=shape, fill_value=fill_value, *args, **kwargs)
 
 
