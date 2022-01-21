9151daca315d523d06bb1d4dd028bf5f93454f36
pedro <pedro@macboom>
2022-01-21 18:15:29 +0100

    ProgressBar out parameter (#8524)
    Added documentation for the out parameter from diagnostics.ProgressBar

--- dask/diagnostics/progress.py
+++ dask/diagnostics/progress.py
@@ -37,7 +37,9 @@ class ProgressBar(Callback):
         Width of the bar
     dt : float, optional
         Update resolution in seconds, default is 0.1 seconds
-
+    out : file object, optional
+        File object to which the progress bar will be written. Default is sys.stdout.
+        
     Examples
     --------
 
