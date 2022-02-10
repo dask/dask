dccc41a9681d98d7f5bc58bf862197e915c8a044
ParticularMiner <78448465+ParticularMiner@users.noreply.github.com>
2022-02-10 15:20:27 +0100

    Result of reducing an array should not depend on its chunk-structure (#8637)
    In most cases implemented in `dask/array/reductions.py` (for example, `sum`, `mean`, etc.) the reduction operation is *axis-independent*.  The bug only showed up in "axis-dependent reductions".

--- dask/array/tests/test_reductions.py
+++ dask/array/tests/test_reductions.py
@@ -1,6 +1,6 @@
 import os
 import warnings
-from itertools import zip_longest
+from itertools import permutations, zip_longest
 
 import pytest
 
@@ -809,3 +809,27 @@ def test_nan_func_does_not_warn(func):
     with pytest.warns(None) as rec:
         getattr(da, func)(d).compute()
     assert not rec  # did not warn
+
+
+@pytest.mark.parametrize("chunks", list(permutations(((2, 1) * 8, (3,) * 8, (6,) * 4))))
+@pytest.mark.parametrize("split_every", [2, 4])
+@pytest.mark.parametrize(
+    "axes", list(permutations((0, 1, 2), 2)) + list(permutations((0, 1, 2)))
+)
+def test_chunk_structure_independence(axes, split_every, chunks):
+    # Reducing an array should not depend on its chunk-structure!!!
+    # See Issue #8541: https://github.com/dask/dask/issues/8541
+    shape = tuple(np.sum(s) for s in chunks)
+    np_array = np.arange(np.prod(shape)).reshape(*shape)
+    x = da.from_array(np_array, chunks=chunks)
+    reduced_x = da.reduction(
+        x,
+        lambda x, axis, keepdims: x,
+        lambda x, axis, keepdims: x,
+        keepdims=True,
+        axis=axes,
+        split_every=split_every,
+        dtype=x.dtype,
+        meta=x._meta,
+    )
+    _assert_eq(reduced_x, np_array, check_chunks=False, check_shape=False)
