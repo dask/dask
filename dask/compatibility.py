bd8e8dc3f1329e7f9ef24072479729d9effeb6ad
James Bourbeau <jrbourbeau@users.noreply.github.com>
2022-02-16 17:54:58 -0600

    Drop Python 3.7 (#8572)

--- dask/compatibility.py
+++ dask/compatibility.py
@@ -2,15 +2,4 @@ import sys
 
 from packaging.version import parse as parse_version
 
-try:
-    from math import prod
-except ImportError:
-    # Python < 3.8
-    def prod(iterable, *, start=1):  # type: ignore
-        acc = start
-        for el in iterable:
-            acc *= el
-        return acc
-
-
 _PY_VERSION = parse_version(".".join(map(str, sys.version_info[:3])))
