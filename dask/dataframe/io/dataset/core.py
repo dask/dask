a5b3168b2f71fbdf923afc720311a1303713aa75
rjzamora <rzamora217@gmail.com>
2022-04-14 12:51:27 -0700

    initial progress on from_dataset (a lot more work to do to establish base functionality)

--- dask/dataframe/io/dataset/core.py
+++ dask/dataframe/io/dataset/core.py
@@ -1,196 +1,211 @@
-# def read_dataset(
-#     path,
-#     columns=None,
-#     filters=None,
-#     index=None,
-#     calculate_divisions=False,
-#     storage_options=None,
-#     file_format="parquet",
-#     engine="pyarrow",
-# ):
+import pyarrow.dataset as ds
+import pyarrow.parquet as pq
+from fsspec.core import get_fs_token_paths
 
-#     engine = get_engine(engine)
-
-#     # Store initial function arguments
-#     input_kwargs = {
-#         "columns": columns,
-#         "filters": filters,
-#         "categories": categories,
-#         "index": index,
-#         "storage_options": storage_options,
-#         "engine": engine,
-#         "gather_statistics": gather_statistics,
-#         "ignore_metadata_file": ignore_metadata_file,
-#         "metadata_task_size": metadata_task_size,
-#         "split_row_groups": split_row_groups,
-#         "chunksize=": chunksize,
-#         "aggregate_files": aggregate_files,
-#         **kwargs,
-#     }
-
-#     if isinstance(columns, str):
-#         input_kwargs["columns"] = [columns]
-#         df = read_parquet(path, **input_kwargs)
-#         return df[columns]
-
-#     if columns is not None:
-#         columns = list(columns)
-
-#     if isinstance(engine, str):
-#         engine = get_engine(engine, bool(kwargs))
-
-#     if hasattr(path, "name"):
-#         path = stringify_path(path)
-
-#     # Update input_kwargs and tokenize inputs
-#     label = "read-parquet-"
-#     input_kwargs.update({"columns": columns, "engine": engine})
-#     output_name = label + tokenize(path, **input_kwargs)
-
-#     fs, _, paths = get_fs_token_paths(path, mode="rb", storage_options=storage_options)
-#     paths = sorted(paths, key=natural_sort_key)  # numeric rather than glob ordering
-
-#     auto_index_allowed = False
-#     if index is None:
-#         # User is allowing auto-detected index
-#         auto_index_allowed = True
-#     if index and isinstance(index, str):
-#         index = [index]
-
-#     if chunksize or (
-#         split_row_groups and int(split_row_groups) > 1 and aggregate_files
-#     ):
-#         # Require `gather_statistics=True` if `chunksize` is used,
-#         # or if `split_row_groups>1` and we are aggregating files.
-#         if gather_statistics is False:
-#             raise ValueError("read_parquet options require gather_statistics=True")
-#         gather_statistics = True
-
-#     read_metadata_result = engine.read_metadata(
-#         fs,
-#         paths,
-#         categories=categories,
-#         index=index,
-#         gather_statistics=gather_statistics,
-#         filters=filters,
-#         split_row_groups=split_row_groups,
-#         chunksize=chunksize,
-#         aggregate_files=aggregate_files,
-#         ignore_metadata_file=ignore_metadata_file,
-#         metadata_task_size=metadata_task_size,
-#         **kwargs,
-#     )
-
-#     # In the future, we may want to give the engine the
-#     # option to return a dedicated element for `common_kwargs`.
-#     # However, to avoid breaking the API, we just embed this
-#     # data in the first element of `parts` for now.
-#     # The logic below is inteded to handle backward and forward
-#     # compatibility with a user-defined engine.
-#     meta, statistics, parts, index = read_metadata_result[:4]
-#     common_kwargs = {}
-#     aggregation_depth = False
-#     if len(parts):
-#         # For now, `common_kwargs` and `aggregation_depth`
-#         # may be stored in the first element of `parts`
-#         common_kwargs = parts[0].pop("common_kwargs", {})
-#         aggregation_depth = parts[0].pop("aggregation_depth", aggregation_depth)
-
-#     # Parse dataset statistics from metadata (if available)
-#     parts, divisions, index, index_in_columns = process_statistics(
-#         parts,
-#         statistics,
-#         filters,
-#         index,
-#         chunksize,
-#         split_row_groups,
-#         fs,
-#         aggregation_depth,
-#     )
-
-#     # Account for index and columns arguments.
-#     # Modify `meta` dataframe accordingly
-#     meta, index, columns = set_index_columns(
-#         meta, index, columns, index_in_columns, auto_index_allowed
-#     )
-#     if meta.index.name == NONE_LABEL:
-#         meta.index.name = None
-
-#     # Set the index that was previously treated as a column
-#     if index_in_columns:
-#         meta = meta.set_index(index)
-#         if meta.index.name == NONE_LABEL:
-#             meta.index.name = None
-
-#     if len(divisions) < 2:
-#         # empty dataframe - just use meta
-#         graph = {(output_name, 0): meta}
-#         divisions = (None, None)
-#     else:
-#         # Create Blockwise layer
-#         layer = DataFrameIOLayer(
-#             output_name,
-#             columns,
-#             parts,
-#             ParquetFunctionWrapper(
-#                 engine,
-#                 fs,
-#                 meta,
-#                 columns,
-#                 index,
-#                 {},  # All kwargs should now be in `common_kwargs`
-#                 common_kwargs,
-#             ),
-#             label=label,
-#             creation_info={
-#                 "func": read_parquet,
-#                 "args": (path,),
-#                 "kwargs": input_kwargs,
-#             },
-#         )
-#         graph = HighLevelGraph({output_name: layer}, {output_name: set()})
-
-#     return new_dd_object(graph, output_name, meta, divisions)
+from dask.base import tokenize
+from dask.dataframe.core import new_dd_object
+from dask.highlevelgraph import HighLevelGraph
+from dask.layers import DataFrameIOLayer
 
 
-# def check_multi_support(engine):
-#     # Helper function to check that the engine
-#     # supports a multi-partition read
-#     return hasattr(engine, "multi_support") and engine.multi_support()
+class DatasetEngine:
+    def get_collection_mapping(
+        self, columns, filters, index, calculate_divisions, storage_options
+    ):
+        """Returns required inputs to from_map"""
+        raise NotImplementedError
 
 
-# def read_parquet_part(fs, engine, meta, part, columns, index, kwargs):
-#     """Read a part of a parquet dataset
+class ReadArrowFileFragment:
+    """Generic class for converting pyarrow file fragments to partitions"""
 
-#     This function is used by `read_parquet`."""
-#     if isinstance(part, list):
-#         if len(part) == 1 or part[0][1] or not check_multi_support(engine):
-#             # Part kwargs expected
-#             func = engine.read_partition
-#             dfs = [
-#                 func(fs, rg, columns.copy(), index, **toolz.merge(kwargs, kw))
-#                 for (rg, kw) in part
-#             ]
-#             df = concat(dfs, axis=0) if len(dfs) > 1 else dfs[0]
-#         else:
-#             # No part specific kwargs, let engine read
-#             # list of parts at once
-#             df = engine.read_partition(
-#                 fs, [p[0] for p in part], columns.copy(), index, **kwargs
-#             )
-#     else:
-#         # NOTE: `kwargs` are the same for all parts, while `part_kwargs` may
-#         #       be different for each part.
-#         rg, part_kwargs = part
-#         df = engine.read_partition(
-#             fs, rg, columns, index, **toolz.merge(kwargs, part_kwargs)
-#         )
+    def __init__(self, columns, filters, index, dataset_options):
+        self.columns = columns
+        ds_filters = None
+        if filters is not None:
+            ds_filters = pq._filters_to_expression(filters)
+        self.filters = ds_filters
+        self.index = index
+        self.dataset_options = dataset_options
 
-#     if meta.columns.name:
-#         df.columns.name = meta.columns.name
-#     columns = columns or []
-#     index = index or []
-#     df = df[[c for c in columns if c not in index]]
-#     if index == [NONE_LABEL]:
-#         df.index.name = None
-#     return df
+    def project_columns(self, columns):
+        if columns == self.columns:
+            return self
+        return type(self)(columns, self.filters)
+
+    def __call__(self, fragment):
+        if isinstance(fragment, str):
+            df = (
+                ds.dataset(fragment, **self.dataset_options)
+                .to_table(filter=self.filters, columns=self.columns)
+                .to_pandas()
+            )
+        else:
+            df = fragment.to_table(
+                filter=self.filters, columns=self.columns
+            ).to_pandas()
+        if self.index:
+            return df.set_index(self.index)
+        return df
+
+
+class ArrowEngine:
+    def __init__(
+        self,
+        partitioning="hive",
+        filesystem=None,
+        **dataset_options,
+    ):
+        self.dataset_options = dataset_options
+        self.partitioning = partitioning
+        self.filesystem = filesystem
+
+    def get_dataset(self, path, columns, filters, storage_options, mode="rb"):
+        """Returns an engine-specific dataset object"""
+
+        # Check if we already have a file-system object
+        if self.filesystem is None:
+            self.filesystem = get_fs_token_paths(
+                path, mode=mode, storage_options=storage_options
+            )[0]
+
+        return ds.dataset(
+            path,
+            partitioning=self.partitioning,
+            filesystem=self.filesystem,
+            **self.dataset_options,
+        )
+
+    def create_meta(self, dataset, index):
+        """Returns an empty-DataFrame meta object"""
+
+        # Start with simple schema -> pandas mapping
+        meta = dataset.schema.empty_table().to_pandas()
+        # Set index if necessary
+        if index:
+            return meta.set_index(index)
+        return meta
+
+    def get_read_function(self, columns, filters, index):
+        """Returns a function to convert a fragment to a DataFrame partition"""
+
+        return ReadArrowFileFragment(
+            columns,
+            filters,
+            index,
+            dataset_options={
+                "partitioning": self.partitioning,
+                "filesystem": self.filesystem,
+                **self.dataset_options,
+            },
+        )
+
+    def get_fragments(self, dataset, filters):
+        ds_filters = None
+        if filters is not None:
+            ds_filters = pq._filters_to_expression(filters)
+        return dataset.get_fragments(ds_filters)
+
+    def get_collection_mapping(
+        self, path, columns, filters, index, calculate_divisions, storage_options
+    ):
+
+        # Get dataset
+        dataset = self.get_dataset(path, columns, filters, storage_options)
+
+        # Create meta
+        meta = self.create_meta(dataset, index)
+
+        # Get fragments
+        fragments = self.get_fragments(dataset, filters)
+
+        # Get IO function
+        io_func = self.get_read_function(columns, filters, index)
+
+        # Extract divisions
+        if calculate_divisions:
+            raise ValueError("calculate_divisions=True is not supported.")
+        divisions = (None,) * (len(dataset.files) + 1)
+
+        return io_func, fragments, meta, divisions
+
+
+class ArrowParquetEngine(ArrowEngine):
+    def __init__(self, **kwargs):
+        super().__init__(format="parquet", **kwargs)
+
+    def get_fragments(self, dataset, filters):
+        # For Parquet, we can convert fragments back to paths
+        # to avoid large graph sizes
+        return [f.path for f in super().get_fragments(dataset, filters)]
+
+
+def get_engine(engine, **engine_options):
+    if engine in ("parquet", "pyarrow-parquet"):
+        return ArrowParquetEngine(**engine_options)
+    elif engine in ("orc", "pyarrow-orc"):
+        return ArrowEngine(format="orc", **engine_options)
+    elif not isinstance(engine, DatasetEngine):
+        raise ValueError
+    return engine
+
+
+def from_dataset(
+    path,
+    columns=None,
+    filters=None,
+    index=None,
+    calculate_divisions=False,
+    engine="pyarrow-parquet",
+    engine_options=None,
+    storage_options=None,
+):
+
+    # Create output-collection name
+    label = "from-dataset-"
+    input_kwargs = {
+        "columns": columns,
+        "filters": filters,
+        "index": index,
+        "calculate_divisions": calculate_divisions,
+        "engine": engine,
+        "engine_options": engine_options,
+        "storage_options": storage_options,
+    }
+    output_name = label + tokenize(path, **input_kwargs)
+
+    # Get dataset engine
+    engine = get_engine(engine, **(engine_options or {}))
+
+    # Get collection-mapping information
+    io_func, fragments, meta, divisions = engine.get_collection_mapping(
+        path,
+        columns,
+        filters,
+        index,
+        calculate_divisions,
+        storage_options,
+    )
+
+    # Special Case (Empty DataFrame)
+    if len(divisions) < 2:
+        io_func = lambda x: x
+        fragments = [meta]
+        divisions = (None, None)
+
+    # Create Blockwise layer
+    # TODO: use `from_map`
+    layer = DataFrameIOLayer(
+        output_name,
+        columns,
+        fragments,
+        io_func,
+        label=label,
+        creation_info={
+            "func": from_dataset,
+            "args": (path,),
+            "kwargs": input_kwargs,
+        },
+    )
+    graph = HighLevelGraph({output_name: layer}, {output_name: set()})
+    return new_dd_object(graph, output_name, meta, divisions)
