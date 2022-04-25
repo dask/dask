d3c33f427a41d89ea9607bf5c1d967562ded2bb8
Paul Hobson <pmhobson@gmail.com>
2022-04-25 08:22:29 -0700

    correct fixture name

--- dask/tests/test_distributed.py
+++ dask/tests/test_distributed.py
@@ -746,7 +746,7 @@ async def test_non_recursive_df_reduce(c, s, a, b):
     assert (await c.compute(result)).val == 170
 
 
-def test_set_index_no_resursion_error(client):
+def test_set_index_no_resursion_error(c):
     # see: https://github.com/dask/dask/issues/8955
     try:
         ddf = (
