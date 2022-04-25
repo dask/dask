8ceeb146b87082239f8719cd94182d9628bb52c9
Paul Hobson <pmhobson@gmail.com>
2022-04-25 08:47:47 -0700

    add importskips

--- dask/tests/test_distributed.py
+++ dask/tests/test_distributed.py
@@ -748,6 +748,9 @@ async def test_non_recursive_df_reduce(c, s, a, b):
 
 def test_set_index_no_resursion_error(c):
     # see: https://github.com/dask/dask/issues/8955
+    pytest.importorskip("numpy")
+    pytest.importorskip("pandas")
+    pytest.importorskip("dask.dataframe")
     try:
         ddf = (
             dask.datasets.timeseries(start="2000-01-01", end="2000-07-01", freq="12h")
