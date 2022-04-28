ef614f6baf13fe306e3d8cdc6fb5d91932c3c304
Jim Crist-Harif <jcristharif@gmail.com>
2022-04-28 10:25:46 -0500

    Call `fs.invalidate_cache` in `to_parquet`
    Invalidate the client-side filesystem listings cache before returning
    from `to_parquet`. This helps ensure that doing a write followed by an
    immediate read works - without it the filesystem might use outdated
    cache information when reading recently written files resulting in
    `FileNotFound` errors.
    
    There's not really a good way to test this, so using a mock for now.

--- dask/dataframe/io/tests/test_parquet.py
+++ dask/dataframe/io/tests/test_parquet.py
@@ -4,6 +4,7 @@ import os
 import sys
 import warnings
 from decimal import Decimal
+from unittest.mock import MagicMock
 
 import numpy as np
 import pandas as pd
@@ -1609,6 +1610,18 @@ def test_to_parquet_lazy(tmpdir, scheduler, engine):
     ddf2 = dd.read_parquet(tmpdir, engine=engine)
 
     assert_eq(ddf, ddf2, check_divisions=False)
+
+
+@PYARROW_MARK
+@pytest.mark.parametrize("compute", [False, True])
+def test_to_parquet_calls_invalidate_cache(tmpdir, monkeypatch, compute):
+    from fsspec.implementations.local import LocalFileSystem
+
+    invalidate_cache = MagicMock()
+    monkeypatch.setattr(LocalFileSystem, "invalidate_cache", invalidate_cache)
+    ddf.to_parquet(tmpdir, compute=compute, engine="pyarrow")
+    assert invalidate_cache.called
+    assert invalidate_cache.call_args.args[0] == str(tmpdir)
 
 
 @FASTPARQUET_MARK
