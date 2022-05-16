6aa5be4ec43a6fadd672dda94fd0f8d69e3dfa15
rjzamora <rzamora217@gmail.com>
2022-05-16 12:27:57 -0700

    fix bug in compute_divisions

--- dask/dataframe/shuffle.py
+++ dask/dataframe/shuffle.py
@@ -1069,7 +1069,7 @@ def compute_divisions(df, col=None, **kwargs) -> tuple:
 def compute_and_set_divisions(df, **kwargs):
     mins, maxes, lens = _compute_partition_stats(df.index, allow_overlap=True, **kwargs)
     if len(mins) == len(df.divisions) - 1:
-        df._divisions = tuple(mins) + (maxes[-1],)
+        df.divisions = tuple(mins) + (maxes[-1],)
         if not any(mins[i] >= maxes[i - 1] for i in range(1, len(mins))):
             return df
 
