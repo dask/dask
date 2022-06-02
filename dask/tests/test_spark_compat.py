b9466c42bd7ae2a1e25a0c3ea77b7633645b7d56
Ian Rose <ian@coiled.io>
2022-06-01 18:48:04 -0700

    Add test for round-tripping hive-partitioned datasets

--- dask/tests/test_spark_compat.py
+++ dask/tests/test_spark_compat.py
@@ -41,6 +41,30 @@ def test_roundtrip_parquet_spark_to_dask(spark_session, npartitions, tmpdir, eng
     assert_eq(ddf, pdf, check_index=False)
 
 
+@pytest.mark.parametrize("engine", ("pyarrow", "fastparquet"))
+def test_roundtrip_hive_parquet_spark_to_dask(spark_session, tmpdir, engine):
+    tmpdir = str(tmpdir)
+    # TODO: pyspark auto-converts timezones -- round-tripping timestamps
+    # is tricky, so drop timeseries for now.
+    pdf = timeseries(freq="1H").compute().reset_index(drop=True)
+    sdf = spark_session.createDataFrame(pdf)
+    # not overwriting any data, but spark complains if the directory
+    # already exists and we don't set overwrite
+    sdf.write.parquet(tmpdir, mode="overwrite", partitionBy="name")
+
+    # TODO: fastparquet requires the glob, pyarrow does not.
+    ddf = dd.read_parquet(tmpdir + "/**.parquet", engine=engine)
+
+    # Partitioning can change the column order. This is mostly okay,
+    # but we sort them here to ease comparison
+    ddf = ddf.compute().sort_index(axis=1)
+    # Dask automatically converts hive-partitioned columns to categories.
+    # This is fine, but convert back to strings for comparison.
+    ddf = ddf.assign(name=ddf.name.astype("str"))
+
+    assert_eq(ddf, pdf, check_index=False)
+
+
 @pytest.mark.parametrize("npartitions", (1, 5, 10))
 @pytest.mark.parametrize("engine", ("pyarrow", "fastparquet"))
 def test_roundtrip_parquet_dask_to_spark(spark_session, npartitions, tmpdir, engine):
