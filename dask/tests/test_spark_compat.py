8bc784cbd7c54a423711c263917ec0e0b985c2e6
Ian Rose <ian@coiled.io>
2022-06-02 15:56:24 -0700

    Round-trip timestamps

--- dask/tests/test_spark_compat.py
+++ dask/tests/test_spark_compat.py
@@ -18,15 +18,25 @@ pytest.mark.skipif(
 )
 
 
+# pyspark auto-converts timezones -- round-tripping timestamps is easier if
+# we set everything to UTC.
+pdf = timeseries(freq="1H").compute()
+pdf.index = pdf.index.tz_localize("UTC")
+pdf = pdf.reset_index()
+
+
 @pytest.fixture(scope="module")
 def spark_session():
     # Spark registers a global signal handler that can cause problems elsewhere
     # in the test suite. In particular, the handler fails if the spark session
     # is stopped (a bug in pyspark).
     prev = signal.getsignal(signal.SIGINT)
+    # Create a spark session. Note that we set the timezone to UTC to avoid
+    # conversion to local time when reading parquet files.
     spark = (
         pyspark.sql.SparkSession.builder.master("local")
         .appName("Dask Testing")
+        .config("spark.sql.session.timeZone", "UTC")
         .getOrCreate()
     )
     yield spark
@@ -40,16 +50,16 @@ def spark_session():
 @pytest.mark.parametrize("engine", ("pyarrow", "fastparquet"))
 def test_roundtrip_parquet_spark_to_dask(spark_session, npartitions, tmpdir, engine):
     tmpdir = str(tmpdir)
-    # TODO: pyspark auto-converts timezones -- round-tripping timestamps
-    # is tricky, so drop timeseries for now.
-    pdf = timeseries(freq="1H").compute().reset_index(drop=True)
+
     sdf = spark_session.createDataFrame(pdf)
-    # not overwriting any data, but spark complains if the directory
-    # already exists and we don't set overwrite
+    # We are not overwriting any data, but spark complains if the directory
+    # already exists (as tmpdir does) and we don't set overwrite
     sdf.repartition(npartitions).write.parquet(tmpdir, mode="overwrite")
 
     # TODO: fastparquet requires the glob, pyarrow does not.
     ddf = dd.read_parquet(tmpdir + "/**.parquet", engine=engine)
+    # Papercut: pandas TZ localization doesn't survive roundtrip
+    ddf = ddf.assign(timestamp=ddf.timestamp.dt.tz_localize("UTC"))
     assert ddf.npartitions == npartitions
 
     assert_eq(ddf, pdf, check_index=False)
@@ -58,9 +68,7 @@ def test_roundtrip_parquet_spark_to_dask(spark_session, npartitions, tmpdir, eng
 @pytest.mark.parametrize("engine", ("pyarrow", "fastparquet"))
 def test_roundtrip_hive_parquet_spark_to_dask(spark_session, tmpdir, engine):
     tmpdir = str(tmpdir)
-    # TODO: pyspark auto-converts timezones -- round-tripping timestamps
-    # is tricky, so drop timeseries for now.
-    pdf = timeseries(freq="1H").compute().reset_index(drop=True)
+
     sdf = spark_session.createDataFrame(pdf)
     # not overwriting any data, but spark complains if the directory
     # already exists and we don't set overwrite
@@ -68,6 +76,8 @@ def test_roundtrip_hive_parquet_spark_to_dask(spark_session, tmpdir, engine):
 
     # TODO: fastparquet requires the glob, pyarrow does not.
     ddf = dd.read_parquet(tmpdir + "/**.parquet", engine=engine)
+    # Papercut: pandas TZ localization doesn't survive roundtrip
+    ddf = ddf.assign(timestamp=ddf.timestamp.dt.tz_localize("UTC"))
 
     # Partitioning can change the column order. This is mostly okay,
     # but we sort them here to ease comparison
@@ -76,22 +86,24 @@ def test_roundtrip_hive_parquet_spark_to_dask(spark_session, tmpdir, engine):
     # This is fine, but convert back to strings for comparison.
     ddf = ddf.assign(name=ddf.name.astype("str"))
 
-    assert_eq(ddf, pdf, check_index=False)
+    assert_eq(ddf, pdf.sort_index(axis=1), check_index=False)
 
 
 @pytest.mark.parametrize("npartitions", (1, 5, 10))
 @pytest.mark.parametrize("engine", ("pyarrow", "fastparquet"))
 def test_roundtrip_parquet_dask_to_spark(spark_session, npartitions, tmpdir, engine):
     tmpdir = str(tmpdir)
-    # TODO: pyspark auto-converts timezones -- round-tripping timestamps
-    # is tricky, so drop timeseries for now.
-    ddf = (
-        timeseries(freq="1H")
-        .repartition(npartitions=npartitions)
-        .reset_index(drop=True)
-    )
-    ddf.to_parquet(tmpdir, engine=engine, write_index=False)
+    ddf = dd.from_pandas(pdf, npartitions=npartitions)
+
+    # Papercut: https://github.com/dask/fastparquet/issues/646#issuecomment-885614324
+    kwargs = {"times": "int96"} if engine == "fastparquet" else {}
+
+    ddf.to_parquet(tmpdir, engine=engine, write_index=False, **kwargs)
 
     sdf = spark_session.read.parquet(tmpdir)
+    sdf = sdf.toPandas()
 
-    assert_eq(sdf.toPandas(), ddf, check_index=False)
+    # Papercut: pandas TZ localization doesn't survive roundtrip
+    sdf = sdf.assign(timestamp=sdf.timestamp.dt.tz_localize("UTC"))
+
+    assert_eq(sdf, ddf, check_index=False)
