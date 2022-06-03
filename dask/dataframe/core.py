bf44e5a5588cfe90d4dc4fec0885b7ebe6fcbeed
Fabien Aulaire <fabien.aulaire@gmail.com>
2022-06-03 09:18:16 +0200

    [map_overlap] blake / pep8

--- dask/dataframe/core.py
+++ dask/dataframe/core.py
@@ -6484,7 +6484,9 @@ def map_partitions(
     return new_dd_object(graph, name, meta, divisions)
 
 
-def _get_divisions_map_partitions(align_dataframes, transform_divisions, dfs, func, args, kwargs):
+def _get_divisions_map_partitions(
+    align_dataframes, transform_divisions, dfs, func, args, kwargs
+):
     """
     Helper to get divisions for map_partitions and map_overlap output.
     """
