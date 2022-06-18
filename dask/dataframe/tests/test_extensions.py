b08e2eff893ee8a0557cb889f434376867a5a683
Hao Chun Chang <changhaochun84@gmail.com>
2022-06-18 15:25:27 +0800

    Use sort_results=False in test_extension.py

--- dask/dataframe/tests/test_extensions.py
+++ dask/dataframe/tests/test_extensions.py
@@ -26,17 +26,17 @@ def test_register_extension_type():
     arr = DecimalArray._from_sequence([Decimal("1.0")] * 10)
     ser = pd.Series(arr)
     dser = dd.from_pandas(ser, 2)
-    assert_eq(ser, dser)
+    assert_eq(ser, dser, sort_results=False)
 
     df = pd.DataFrame({"A": ser})
     ddf = dd.from_pandas(df, 2)
-    assert_eq(df, ddf)
+    assert_eq(df, ddf, sort_results=False)
 
 
 def test_reduction():
     ser = pd.Series(DecimalArray._from_sequence([Decimal("0"), Decimal("1")]))
     dser = dd.from_pandas(ser, 2)
-    assert_eq(ser.mean(skipna=False), dser.mean(skipna=False))
+    assert_eq(ser.mean(skipna=False), dser.mean(skipna=False), sort_results=False)
 
     # It's unclear whether this can be reliably provided, at least with the current
     # implementation, which uses pandas.DataFrame.sum(), returning a (homogenous)
