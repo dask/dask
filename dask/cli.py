4ce35acc38f5129526aedcd8f3897e5e2c2f3379
Doug Davis <ddavis@ddavis.io>
2022-07-18 11:15:06 -0500

    use importlib.metadata; types

--- dask/cli.py
+++ dask/cli.py
@@ -14,13 +14,13 @@ except ImportError as e:
 
 @click.group
 @click.version_option(__version__)
-def cli():
+def cli() -> None:
     """Dask command line interface."""
     pass
 
 
 @cli.command
-def docs():
+def docs() -> None:
     """Open Dask documentation in a web browser."""
     import webbrowser
 
@@ -28,26 +28,43 @@ def docs():
 
 
 @cli.group("info")
-def info():
+def info() -> None:
     """Information about your dask installation."""
     pass
 
 
 @info.command()
-def versions():
+def versions() -> None:
     """Print versions of Dask related projects."""
     from dask.utils import show_versions
 
     show_versions()
 
 
-def register_third_party(cli):
-    import pkg_resources
+def register_third_party(cli: click.Group) -> None:
+    """Discover third party dask_cli entry points.
 
-    for ep in pkg_resources.iter_entry_points(group="dask_cli"):
-        cli.add_command(ep.load())
+    If a package includes the "dask_cli" entry point category, this
+    function discovers and loads the associated entry points with
+    ``importlib.metadata``. We only consider ``click.Command`` and
+    ``click.Group`` instances to be valid entry points for the
+    `dask_cli` category.
+
+    """
+    import importlib.metadata
+
+    eps = importlib.metadata.entry_points()
+
+    for ep in eps.select(group="dask_cli"):
+        command = ep.load()
+        if not isinstance(command, (click.Command, click.Group)):
+            raise TypeError(
+                "entry points in 'dask_cli' must be instances of "
+                "click.Command or click.Group"
+            )
+        cli.add_command(command)
 
 
-def run_cli():
+def run_cli() -> None:
     register_third_party(cli)
     cli()
