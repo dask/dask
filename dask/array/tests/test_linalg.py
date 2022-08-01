15c97cf00815a9d113e6372f86df15a8791db0c5
James Bourbeau <jrbourbeau@gmail.com>
2022-08-01 18:45:15 -0500

    Parse version strings

--- dask/array/tests/test_linalg.py
+++ dask/array/tests/test_linalg.py
@@ -761,9 +761,8 @@ def _get_symmat(size):
 
 
 @pytest.mark.xfail(
-    scipy.__version__ >= "1.9.0",
+    parse_version(scipy.__version__) >= parse_version("1.9.0"),
     reason="https://github.com/dask/dask/issues/9335",
-    strict=False,
 )
 @pytest.mark.parametrize(("shape", "chunk"), [(20, 10), (30, 6)])
 def test_solve_sym_pos(shape, chunk):
