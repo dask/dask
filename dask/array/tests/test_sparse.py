686d9c221e48a335156b1475434dc375efdf7e78
Ian Rose <ian@coiled.io>
2022-08-02 12:52:28 -0700

    Remove tests which have not been supported by sparse since 2018

--- dask/array/tests/test_sparse.py
+++ dask/array/tests/test_sparse.py
@@ -1,5 +1,3 @@
-import random
-
 import numpy as np
 import pytest
 from packaging.version import parse as parse_version
@@ -127,56 +125,6 @@ def test_tensordot():
         da.tensordot(x, y, axes=((1, 2), (1, 0))),
         da.tensordot(xx, yy, axes=((1, 2), (1, 0))),
     )
-
-
-@pytest.mark.xfail(reason="upstream change", strict=False)
-@pytest.mark.parametrize("func", functions)
-def test_mixed_concatenate(func):
-    x = da.random.random((2, 3, 4), chunks=(1, 2, 2))
-
-    y = da.random.random((2, 3, 4), chunks=(1, 2, 2))
-    y[y < 0.8] = 0
-    yy = y.map_blocks(sparse.COO.from_numpy)
-
-    d = da.concatenate([x, y], axis=0)
-    s = da.concatenate([x, yy], axis=0)
-
-    dd = func(d)
-    ss = func(s)
-
-    assert_eq(dd, ss)
-
-
-@pytest.mark.xfail(reason="upstream change", strict=False)
-@pytest.mark.parametrize("func", functions)
-def test_mixed_random(func):
-    d = da.random.random((4, 3, 4), chunks=(1, 2, 2))
-    d[d < 0.7] = 0
-
-    fn = lambda x: sparse.COO.from_numpy(x) if random.random() < 0.5 else x
-    s = d.map_blocks(fn)
-
-    dd = func(d)
-    ss = func(s)
-
-    assert_eq(dd, ss)
-
-
-@pytest.mark.xfail(reason="upstream change", strict=False)
-def test_mixed_output_type():
-    y = da.random.random((10, 10), chunks=(5, 5))
-    y[y < 0.8] = 0
-    y = y.map_blocks(sparse.COO.from_numpy)
-
-    x = da.zeros((10, 1), chunks=(5, 1))
-
-    z = da.concatenate([x, y], axis=1)
-
-    assert z.shape == (10, 11)
-
-    zz = z.compute()
-    assert isinstance(zz, sparse.COO)
-    assert zz.nnz == y.compute().nnz
 
 
 def test_metadata():
