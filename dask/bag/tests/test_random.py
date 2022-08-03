b9d406602aaa651207a68ccfaf3875513cd3f255
Ian Rose <ian@coiled.io>
2022-08-03 11:52:25 -0700

    Allow intermediate sampled results to be smaller than the target number of samples. This allows you to have some small partitions.

--- dask/bag/tests/test_random.py
+++ dask/bag/tests/test_random.py
@@ -86,9 +86,7 @@ def test_sample_k_bigger_than_bag_size():
     seq = range(3)
     sut = db.from_sequence(seq, npartitions=3)
     # should raise: Sample larger than population or is negative
-    with pytest.raises(
-        ValueError, match="Sample larger than population or is negative"
-    ):
+    with pytest.raises(ValueError, match="Sample larger than population"):
         random.sample(sut, k=4).compute()
 
 
