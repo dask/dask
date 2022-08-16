d6c550efbbfce916ea283422e7df83731c5765dd
James Bourbeau <jrbourbeau@users.noreply.github.com>
2022-08-16 18:01:47 -0500

    Upload environment file artifact for each CI build (#9372)

--- continuous_integration/scripts/install.sh
+++ continuous_integration/scripts/install.sh
@@ -44,6 +44,7 @@ mamba list
 
 # For debugging
 echo -e "--\n--Conda Environment (re-create this with \`conda env create --name <name> -f <output_file>\`)\n--"
-mamba env export | grep -E -v '^prefix:.*$'
+mamba env export | grep -E -v '^prefix:.*$' > env.yaml
+cat env.yaml
 
 set +xe
