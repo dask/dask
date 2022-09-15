3a79096f5a5fbb6273d67d44b6e2c44918c7208b
James Bourbeau <jrbourbeau@gmail.com>
2022-09-15 16:24:02 -0500

    Be more explicit about method name in _getattr_numeric_only [test-upstream]

--- dask/dataframe/core.py
+++ dask/dataframe/core.py
@@ -1917,7 +1917,7 @@ Dask Name: {name}, {layers}"""
                 token=token,
                 skipna=skipna,
                 axis=axis,
-                _name=name,
+                method_name=name,
                 **numeric_only_kwargs,
             )
             return handle_out(out, result)
@@ -1929,7 +1929,7 @@ Dask Name: {name}, {layers}"""
                 skipna=skipna,
                 axis=axis,
                 split_every=split_every,
-                _name=name,
+                method_name=name,
                 **numeric_only_kwargs,
             )
             if isinstance(self, DataFrame):
@@ -8111,7 +8111,6 @@ def _raise_if_not_series_or_dataframe(x, funcname):
         )
 
 
-def _getattr_numeric_only(*args, **kwargs):
-    name = kwargs.pop("_name")
+def _getattr_numeric_only(*args, method_name, **kwargs):
     with check_numeric_only_deprecation():
-        return getattr(M, name)(*args, **kwargs)
+        return getattr(M, method_name)(*args, **kwargs)
