2098c11a01a41670e8736786aea77b1db76dc99f
James Bourbeau <jrbourbeau@gmail.com>
2022-09-15 12:31:41 -0500

    More [test-upstream]

--- continuous_integration/scripts/install.sh
+++ continuous_integration/scripts/install.sh
@@ -7,35 +7,8 @@ set -xe
 # python -m pip install --no-deps cityhash
 
 if [[ ${UPSTREAM_DEV} ]]; then
-    # # FIXME workaround for https://github.com/mamba-org/mamba/issues/1682
-    # arr=($(mamba search --override-channels -c arrow-nightlies pyarrow | tail -n 1))
-    # export PYARROW_VERSION=${arr[1]}
-    # mamba install -y -c arrow-nightlies "pyarrow=$PYARROW_VERSION"
-
-    # FIXME https://github.com/mamba-org/mamba/issues/412
-    # mamba uninstall --force ...
-    # conda uninstall --force numpy pandas fastparquet bokeh scipy
     conda uninstall --force pandas
-
-    # mamba install -y -c bokeh/label/dev bokeh
-
-    python -m pip install --no-deps --pre --retries 10 \
-        -i https://pypi.anaconda.org/scipy-wheels-nightly/simple \
-        # numpy \
-        pandas
-        # scipy
-
-    # python -m pip install \
-    #     --upgrade \
-    #     locket \
-    #     git+https://github.com/pydata/sparse \
-    #     git+https://github.com/dask/s3fs \
-    #     git+https://github.com/intake/filesystem_spec \
-    #     git+https://github.com/dask/partd \
-    #     git+https://github.com/dask/zict \
-    #     git+https://github.com/dask/distributed \
-    #     git+https://github.com/dask/fastparquet \
-    #     git+https://github.com/zarr-developers/zarr-python
+    python -m pip install --no-deps --pre --retries 10 -i https://pypi.anaconda.org/scipy-wheels-nightly/simple pandas
 fi
 
 # Install dask
