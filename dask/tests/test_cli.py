243cf8522e799d85fe580e2bf7cbb15cdc3535b3
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2022-10-27 12:52:36 -0700

    Resolve upstream importlib failures

--- dask/tests/test_cli.py
+++ dask/tests/test_cli.py
@@ -2,10 +2,10 @@ import pytest
 
 click = pytest.importorskip("click")
 
-import importlib.metadata
 import json
 import platform
 import sys
+from importlib import metadata as importlib_metadata
 
 from click.testing import CliRunner
 
@@ -64,13 +64,13 @@ def good_command_2():
 def test_register_command_ep():
     from dask.cli import _register_command_ep
 
-    bad_ep = importlib.metadata.EntryPoint(
+    bad_ep = importlib_metadata.EntryPoint(
         name="bad",
         value="dask.tests.test_cli:bad_command",
         group="dask_cli",
     )
 
-    good_ep = importlib.metadata.EntryPoint(
+    good_ep = importlib_metadata.EntryPoint(
         name="good",
         value="dask.tests.test_cli:good_command",
         group="dask_cli",
@@ -92,13 +92,13 @@ def dummy_cli_2():
 def test_repeated_name_registration_warn():
     from dask.cli import _register_command_ep
 
-    one = importlib.metadata.EntryPoint(
+    one = importlib_metadata.EntryPoint(
         name="one",
         value="dask.tests.test_cli:good_command",
         group="dask_cli",
     )
 
-    two = importlib.metadata.EntryPoint(
+    two = importlib_metadata.EntryPoint(
         name="two",
         value="dask.tests.test_cli:good_command_2",
         group="dask_cli",
