330397e823dd49f1858f10816b7e4d53ed5ca10a
jakirkham <jakirkham@gmail.com>
2022-10-28 00:52:01 -0700

    Upgrade versioneer to 0.28

--- dask/__init__.py
+++ dask/__init__.py
@@ -16,3 +16,6 @@ versions = get_versions()
 __version__ = versions["version"]
 __git_revision__ = versions["full-revisionid"]
 del get_versions, versions
+
+from . import _version
+__version__ = _version.get_versions()['version']
