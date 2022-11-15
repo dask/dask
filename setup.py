9fdd11d6d5314b91b7c9c54656ec367e72bec7c4
James Bourbeau <jrbourbeau@users.noreply.github.com>
2022-11-14 20:12:38 -0600

    Allow `bokeh=3` (#9659)

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ extras_require: dict[str, list[str]] = {
     "dataframe": ["numpy >= 1.18", "pandas >= 1.0"],
     "distributed": ["distributed == 2022.10.2"],
     "diagnostics": [
-        "bokeh >= 2.4.2, <3",
+        "bokeh >= 2.4.2",
         "jinja2",
     ],
     "delayed": [],  # keeping for backwards compatibility
