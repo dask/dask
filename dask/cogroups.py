a14ce784ac267b1f543c5d082beace548894e0ee
Gabe Joseph <gjoseph92@gmail.com>
2022-12-01 15:36:16 -0700

    consolidate priorities vs indices - much cleaner

--- dask/cogroups.py
+++ dask/cogroups.py
@@ -12,27 +12,31 @@ def cogroup(
 ) -> Iterator[tuple[list[Hashable], bool]]:
     dependents: dict[Hashable, set[Hashable]] = reverse_dict(dependencies)
     kps = sorted(priorities.items(), key=operator.itemgetter(1))
+    # ^ can't `zip(*sorted...)` because of mypy: https://github.com/python/mypy/issues/5247
+    keys = [kp[0] for kp in kps]
+    prios = [kp[1] for kp in kps]
+    del kps
 
-    # Assume priorities are consecutive, starting from 0
-    assert all(kp[1] == i for kp, i in zip(kps, range(len(kps)))), kps
+    # Assume priorities are consecutive, starting from 0.
+    # This makes priorities and indices interchangeable: `keys[i]` has priority `i`.
+    assert all(p == i for p, i in zip(prios, range(len(keys)))), prios
+    del prios
 
-    cogroup_start_i = 0
-    while cogroup_start_i < len(kps):
-        key, current_prio = kps[cogroup_start_i]
-        prev_prio = start_prio = current_prio
-        assert start_prio == cogroup_start_i, (start_prio, cogroup_start_i)
-        # TODO `start_prio` and `cogroup_start_i` are redundant if prios are consecutive from 0
+    i = 0
+    while i < len(keys):
+        start_i = prev_i = i
+        key = keys[i]
         isolated_cogroup: bool = False
 
         # Walk linear chains of consecutive priority, either until we hit a priority jump,
         # or a task with dependencies that are outside of our group.
         while downstream := dependents[key]:
             key = min(downstream, key=priorities.__getitem__)
-            current_prio = priorities[key]
+            i = priorities[key]
 
             if (
                 # linear chain
-                (was_chain := (current_prio == prev_prio + 1))
+                (was_chain := (i == prev_i + 1))
                 # TODO update this comment:
                 # where inputs don't already belong to a different cogroup.
                 # If a task has multiple families as inputs, we don't know how to group
@@ -43,12 +47,14 @@ def cogroup(
                 # IDEA: if it depends on exactly 1 other cogroup, then roll this cogroup
                 # into that previous cogroup?
                 and not any(
-                    priorities[dk] < start_prio and len(dependents[dk]) == 1
+                    priorities[dk] < start_i and len(dependents[dk]) == 1
                     for dk in dependencies[key]
                 )
             ):
-                # TODO if this chain is all linear, try again from the start with the next-smallest dependent
-                prev_prio = current_prio
+                # walk up the linear chain
+                # TODO if we reach the top without a jump, try again from the start with
+                # the next-smallest dependent
+                prev_i = i
             else:
                 # non-consecutive priority jump. this is our max node.
 
@@ -57,15 +63,14 @@ def cogroup(
                     # inputs belonged to a different cogroup. so this is an isolated cogroup
                     # because it doesn't need to consider the location of any inputs.
                     isolated_cogroup = True
-                    assert current_prio > start_prio + 1, (
-                        current_prio,
-                        start_prio,
+                    assert i > start_i + 1, (
+                        i,
+                        start_i,
                         key,
                     )
 
                 break
 
-        # all tasks from the start to the max (inclusive) belong to the cogroup.
-        next_start_i = cogroup_start_i + (current_prio - start_prio) + 1
-        yield [kp[0] for kp in kps[cogroup_start_i:next_start_i]], isolated_cogroup
-        cogroup_start_i = next_start_i
+        # all tasks from the start to the current (inclusive) belong to the cogroup.
+        i = i + 1
+        yield keys[start_i:i], isolated_cogroup
