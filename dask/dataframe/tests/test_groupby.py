3f3730ae9ccf8f975fd14f5bde432fadb1fd1ba0
Lawrence Mitchell <lmitchell@nvidia.com>
2022-12-05 09:54:19 +0000

    Test groupby with multi-character series name

--- dask/dataframe/tests/test_groupby.py
+++ dask/dataframe/tests/test_groupby.py
@@ -1688,6 +1688,13 @@ def test_cumulative(func, key, sel):
             dg.cumcount(axis=0)
 
 
+@pytest.mark.xfail(reason="https://github.com/dask/dask/issues/9715")
+def test_series_groupby_multi_character_column_name():
+    df = pd.DataFrame({"aa": [1, 2, 1, 3, 4, 1, 2]})
+    ddf = dd.from_pandas(df, npartitions=3)
+    assert_eq(df.groupby("aa").aa.cumsum(), ddf.groupby("aa").aa.cumsum())
+
+
 @pytest.mark.parametrize("func", ["cumsum", "cumprod"])
 def test_cumulative_axis1(func):
     df = pd.DataFrame(
