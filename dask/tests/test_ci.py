51282141385050f8b35142fe33a5a704f0a7aee2
James Bourbeau <jrbourbeau@gmail.com>
2022-12-08 16:56:34 -0600

    Punt on test [test-upstream]

--- dask/tests/test_ci.py
+++ dask/tests/test_ci.py
@@ -12,21 +12,26 @@ from packaging.version import Version
 def test_upstream_packages_installed():
     # List of packages should match those specified in
     # `continuous_integration/scripts/install.sh`
+
+    # FIXME: This test isn't sensative to projects that use git tags
+    # to determine versions (e.g. versionseer) when installed
+    # directly from GitHub as the latest `main` branch can sometimes
+    # be pointing to a released version of the project.
     packages = [
         "bokeh",
-        "dask",
-        "distributed",
-        "fastparquet",
-        "fsspec",
+        # "dask",
+        # "distributed",
+        # "fastparquet",
+        # "fsspec",
         "numpy",
         "pandas",
-        "partd",
+        # "partd",
         "pyarrow",
-        "s3fs",
+        # "s3fs",
         "scipy",
-        "sparse",
-        "zarr",
-        "zict",
+        # "sparse",
+        # "zarr",
+        # "zict",
     ]
     for package in packages:
         v = Version(importlib.metadata.version(package))
