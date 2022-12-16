919bf76ba73a07c28c979ac52d3b3ea69d706d55
Thomas Grainger <tagrain@gmail.com>
2022-12-16 19:22:33 +0000

    Add support for Python 3.11 (#9708)

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Scientific/Engineering",
         "Topic :: System :: Distributed Computing",
     ],
