a29f841b7606696d11355db16e2361eaa053a146
Irina Truong <i.chernyavska@gmail.com>
2023-01-19 14:14:16 -0800

    Fix test_full_groupby, default behavior change in pandas 2.0.

--- dask/dataframe/core.py
+++ dask/dataframe/core.py
@@ -98,7 +98,13 @@ DEFAULT_GET = named_schedulers.get("threads", named_schedulers["sync"])
 
 no_default = "__no_default__"
 
-GROUP_KEYS_DEFAULT = None if PANDAS_GT_150 else True
+GROUP_KEYS_DEFAULT = None
+if PANDAS_GT_200:
+    GROUP_KEYS_DEFAULT = True
+elif PANDAS_GT_150:
+    GROUP_KEYS_DEFAULT = None
+else:
+    GROUP_KEYS_DEFAULT = True
 
 pd.set_option("compute.use_numexpr", False)
 
