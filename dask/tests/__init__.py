0049f6d1ca4cd43af62275935bada5fb01888f9f
Irina Truong <i.chernyavska@gmail.com>
2023-03-06 10:15:38 -0800

    More cleanup.

--- dask/tests/__init__.py
+++ dask/tests/__init__.py
@@ -6,8 +6,3 @@ xfail_with_pyarrow_strings = pytest.mark.xfail(
     bool(dask.config.get("dataframe.convert_string")),
     reason="Known failure with pyarrow strings",
 )
-
-
-def skip_with_pyarrow_strings(engine):
-    if engine == "fastparquet":
-        pytest.skip("fastparquet doesn't support `dataframe.convert_string`")
