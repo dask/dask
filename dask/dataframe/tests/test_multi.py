29919b6757e0b0c2b87bfd1bcf04dced0478e828
Irina Truong <i.chernyavska@gmail.com>
2023-03-13 18:00:06 -0700

    Fix `test_melt` with `pyarrow` strings active (#10052)

--- dask/dataframe/tests/test_multi.py
+++ dask/dataframe/tests/test_multi.py
@@ -28,6 +28,11 @@ from dask.dataframe.utils import (
 )
 from dask.utils_test import hlg_layer, hlg_layer_topological
 
+try:
+    import pyarrow as pa
+except ImportError:
+    pa = None
+
 
 def test_align_partitions():
     A = pd.DataFrame(
@@ -1627,38 +1632,37 @@ def test_merge_by_multiple_columns(how, shuffle_method):
             )
 
 
-@pytest.mark.xfail_with_pyarrow_strings  # https://github.com/dask/dask/issues/10029
-def test_melt():
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        {},
+        dict(id_vars="int"),
+        dict(value_vars="int"),
+        dict(value_vars=["obj", "int"], var_name="myvar"),
+        dict(id_vars="s1", value_vars=["obj", "int"], value_name="myval"),
+        dict(value_vars=["obj", "s1"]),
+        dict(value_vars=["s1", "s2"]),
+    ],
+)
+def test_melt(kwargs):
     pdf = pd.DataFrame(
-        {"A": list("abcd") * 5, "B": list("XY") * 10, "C": np.random.randn(20)}
+        {
+            "obj": list("abcd") * 5,
+            "s1": list("XY") * 10,
+            "s2": list("abcde") * 4,
+            "int": np.random.randn(20),
+        }
     )
+    if pa:
+        # If pyarrow is installed, test that `string[pyarrow]` dtypes
+        # give the same result with `pandas` and `dask`
+        pdf = pdf.astype({"s1": "string[pyarrow]", "s2": "string[pyarrow]"})
+
     ddf = dd.from_pandas(pdf, 4)
 
-    list_eq(dd.melt(ddf), pd.melt(pdf))
-
-    list_eq(dd.melt(ddf, id_vars="C"), pd.melt(pdf, id_vars="C"))
-    list_eq(dd.melt(ddf, value_vars="C"), pd.melt(pdf, value_vars="C"))
-    list_eq(
-        dd.melt(ddf, value_vars=["A", "C"], var_name="myvar"),
-        pd.melt(pdf, value_vars=["A", "C"], var_name="myvar"),
-    )
-    list_eq(
-        dd.melt(ddf, id_vars="B", value_vars=["A", "C"], value_name="myval"),
-        pd.melt(pdf, id_vars="B", value_vars=["A", "C"], value_name="myval"),
-    )
-
+    list_eq(dd.melt(ddf, **kwargs), pd.melt(pdf, **kwargs))
     # test again as DataFrame method
-    list_eq(ddf.melt(), pdf.melt())
-    list_eq(ddf.melt(id_vars="C"), pdf.melt(id_vars="C"))
-    list_eq(ddf.melt(value_vars="C"), pdf.melt(value_vars="C"))
-    list_eq(
-        ddf.melt(value_vars=["A", "C"], var_name="myvar"),
-        pdf.melt(value_vars=["A", "C"], var_name="myvar"),
-    )
-    list_eq(
-        ddf.melt(id_vars="B", value_vars=["A", "C"], value_name="myval"),
-        pdf.melt(id_vars="B", value_vars=["A", "C"], value_name="myval"),
-    )
+    list_eq(ddf.melt(**kwargs), pdf.melt(**kwargs))
 
 
 def test_cheap_inner_merge_with_pandas_object():
--- dask/dataframe/utils.py
+++ dask/dataframe/utils.py
@@ -531,19 +531,23 @@ def _maybe_sort(a, check_index: bool):
     return a.sort_index() if check_index else a
 
 
-def assert_eq_dtypes(a, b):
-    # Temporary changes to look for pyarrow dtype failures
+def _maybe_convert_string(a, b):
     import dask
 
     if dask.config.get("dataframe.convert_string"):
         from dask.dataframe._pyarrow import to_pyarrow_string
 
-        if isinstance(a, (pd.DataFrame, pd.Series)):
+        if isinstance(a, (pd.DataFrame, pd.Series, pd.Index)):
             a = to_pyarrow_string(a)
 
-        if isinstance(b, (pd.DataFrame, pd.Series)):
+        if isinstance(b, (pd.DataFrame, pd.Series, pd.Index)):
             b = to_pyarrow_string(b)
 
+    return a, b
+
+
+def assert_eq_dtypes(a, b):
+    a, b = _maybe_convert_string(a, b)
     tm.assert_series_equal(a.dtypes.value_counts(), b.dtypes.value_counts())
 
 
@@ -578,17 +582,7 @@ def assert_eq(
     if hasattr(b, "to_pandas"):
         b = b.to_pandas()
 
-    # Temporary changes to look for pyarrow dtype failures
-    import dask
-
-    if dask.config.get("dataframe.convert_string"):
-        from dask.dataframe._pyarrow import to_pyarrow_string
-
-        if isinstance(a, (pd.DataFrame, pd.Series, pd.Index)):
-            a = to_pyarrow_string(a)
-
-        if isinstance(b, (pd.DataFrame, pd.Series, pd.Index)):
-            b = to_pyarrow_string(b)
+    a, b = _maybe_convert_string(a, b)
 
     if isinstance(a, (pd.DataFrame, pd.Series)) and sort_results:
         a = _maybe_sort(a, check_index)
