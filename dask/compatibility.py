de35d52b93f158e86ede8d60ec187f4139693c08
Thomas Grainger <tagrain@gmail.com>
2023-03-15 15:40:21 +0000

    remove entry_points from dask.compatibility

--- dask/compatibility.py
+++ dask/compatibility.py
@@ -1,9 +1,6 @@
 import sys
 
-from importlib_metadata import entry_points
 from packaging.version import parse as parse_version
-
-__all__ = ["entry_points"]
 
 _PY_VERSION = parse_version(".".join(map(str, sys.version_info[:3])))
 
