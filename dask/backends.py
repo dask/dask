099bc6ed25f65bf61e8c9258993b5fb5e34bfedf
Thomas Grainger <tagrain@gmail.com>
2023-03-15 13:45:44 +0000

    use group kwarg of compatability.entry_points

--- dask/backends.py
+++ dask/backends.py
@@ -38,7 +38,7 @@ class DaskBackendEntrypoint:
 
 @lru_cache(maxsize=1)
 def detect_entrypoints(entry_point_name):
-    entrypoints = entry_points(entry_point_name)
+    entrypoints = entry_points(group=entry_point_name)
     return {ep.name: ep for ep in entrypoints}
 
 
