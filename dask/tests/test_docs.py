81015c2af0ed6256b3b1a6e1be8ca73e2875f3fb
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2023-03-22 09:37:05 -0700

    Only run test_development_guidelines_matches_ci on editable install

--- dask/tests/test_docs.py
+++ dask/tests/test_docs.py
@@ -1,6 +1,24 @@
+import os
+import sys
 from pathlib import Path
 
+import pytest
 
+
+# borrowing from https://github.com/pypa/pip/blob/0964c9797c1e8de901b045f66fe4d91502cc9877/pip/utils/__init__.py
+def is_editable_install():
+    """Is distribution an editable install?"""
+    for path_item in sys.path:
+        egg_link = os.path.join(path_item, "dask.egg-link")
+        if os.path.isfile(egg_link):
+            return True
+    return False
+
+
+@pytest.mark.skipif(
+    not is_editable_install(),
+    reason="Requires tests to be ran from an editable install",
+)
 def test_development_guidelines_matches_ci():
     """When the environment.yaml changes in CI, make sure to change it in the docs as well"""
     root_dir = Path(__file__).parent.parent.parent
