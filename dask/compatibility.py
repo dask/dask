212fe233671b44648189e0b3ff633dd2dccd5699
Jacob Tomlinson <jtomlinson@nvidia.com>
2023-03-23 09:57:17 +0000

    Add deprecation warning

--- dask/compatibility.py
+++ dask/compatibility.py
@@ -1,8 +1,18 @@
 import sys
+import warnings
 
-from importlib_metadata import entry_points  # noqa
+from importlib_metadata import entry_points as _entry_points
 from packaging.version import parse as parse_version
 
 _PY_VERSION = parse_version(".".join(map(str, sys.version_info[:3])))
 
 _EMSCRIPTEN = sys.platform == "emscripten"
+
+
+def entry_points(*args, **kwargs):
+    warnings.warn(
+        "`dask.compatibility.entry_points` has been replaced by `importlib_metadata.entry_points` and will be removed "
+        "in a future version. Please use `importlib_metadata.entry_points` instead.",
+        DeprecationWarning,
+    )
+    return _entry_points(*args, **kwargs)
