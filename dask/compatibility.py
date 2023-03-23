f7b53f9bdcba3cb5473645ece4f226b9c3214d9c
Jacob Tomlinson <jtomlinson@nvidia.com>
2023-03-23 09:53:33 +0000

    Restore dask.compatibility.entry_points as an alias to importlib_metadata.entry_points

--- dask/compatibility.py
+++ dask/compatibility.py
@@ -1,5 +1,6 @@
 import sys
 
+from importlib_metadata import entry_points  # noqa
 from packaging.version import parse as parse_version
 
 _PY_VERSION = parse_version(".".join(map(str, sys.version_info[:3])))
