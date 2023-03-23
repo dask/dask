535376de0c47bd4a86ed6f454707253381546c78
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2023-03-23 15:10:57 +0000

    Apply suggestions from code review
    Co-authored-by: Thomas Grainger <tagrain@gmail.com>

--- dask/compatibility.py
+++ dask/compatibility.py
@@ -9,10 +9,10 @@ _PY_VERSION = parse_version(".".join(map(str, sys.version_info[:3])))
 _EMSCRIPTEN = sys.platform == "emscripten"
 
 
-def entry_points(*args, **kwargs):
+def entry_points(group=None):
     warnings.warn(
         "`dask.compatibility.entry_points` has been replaced by `importlib_metadata.entry_points` and will be removed "
         "in a future version. Please use `importlib_metadata.entry_points` instead.",
         DeprecationWarning,
     )
-    return _entry_points(*args, **kwargs)
+    return _entry_points(group=group)
--- dask/tests/test_compatibility.py
+++ dask/tests/test_compatibility.py
@@ -4,5 +4,5 @@ from dask.compatibility import entry_points
 
 
 def test_entry_points():
-    with pytest.raises(DeprecationWarning):
+    with pytest.warns(DeprecationWarning):
         assert "pytest" in [ep.name for ep in entry_points(group="console_scripts")]
