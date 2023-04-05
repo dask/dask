cd629c40b52ecac74de3af5eb9ebec78ccf66317
Irina Truong <i.chernyavska@gmail.com>
2023-04-05 13:23:43 -0700

    Revert changes to ci.

--- continuous_integration/scripts/run_tests.sh
+++ continuous_integration/scripts/run_tests.sh
@@ -11,7 +11,6 @@ if [[ $COVERAGE == 'true' ]]; then
 fi
 
 echo "py.test dask --runslow $XTRATESTARGS"
-# py.test dask --runslow $XTRATESTARGS TODO: revert
-py.test -svv dask/dataframe/tests/test_groupby.py::test_groupby_unique $XTRATESTARGS
+py.test dask --runslow $XTRATESTARGS
 
 set +e
