a13370efa9c6ca5f77dd02daf42426b9ea0637a7
galipremsagar <sagarprem75@gmail.com>
2023-04-12 13:06:28 -0700

    Revert grouper related changes

--- dask/dataframe/groupby.py
+++ dask/dataframe/groupby.py
@@ -3056,8 +3056,8 @@ def _unique_aggregate(series_gb, name=None):
     ret = type(series_gb.obj)(
         {k: v.explode().unique() for k, v in series_gb},
         name=name,
-        index=series_gb.grouper.result_index,
     )
+    ret.index.names = series_gb.obj.index.names
     return ret
 
 
@@ -3072,7 +3072,8 @@ def _value_counts(x, **kwargs):
 
 def _value_counts_aggregate(series_gb):
     return pd.concat(
-        [v.groupby(by=series_gb.grouper.axis.names).sum() for _, v in series_gb],
+        {k: v.groupby(level=-1).sum() for k, v in series_gb},
+        names=series_gb.obj.index.names,
     )
 
 
