f0ea4241cef53a0210039b6b494b798801bc6f18
Eray Aslan <eras@gentoo.org>
2023-05-10 11:37:23 +0200

    Accommodate python-3.8

--- dask/array/random.py
+++ dask/array/random.py
@@ -5,6 +5,7 @@ from itertools import chain, product
 from numbers import Integral
 from operator import getitem
 from threading import Lock
+from typing import Dict
 
 import numpy as np
 
@@ -1041,7 +1042,7 @@ Use da.random.default_rng() to get a Generator based rng and use its
 methods instead.
 """
 
-_cached_states: dict[str, RandomState] = {}
+_cached_states: Dict[str, RandomState] = {}
 _cached_states_lock = Lock()
 
 
