7104ac2eb3dbbc81cb7b0cf3bc48340dd5df9079
Irina Truong <i.chernyavska@gmail.com>
2023-05-14 13:29:42 -0700

    Comment.

--- dask/array/tests/test_chunk.py
+++ dask/array/tests/test_chunk.py
@@ -99,6 +99,7 @@ def test_coarsen():
 
 
 def test_coarsen_unaligned_shape():
+    """https://github.com/dask/dask/issues/10274"""
     x = da.random.random(100)
     res = da.coarsen(np.mean, x, {0: 3}, trim_excess=True)
     assert res.chunks == ((33,),)
