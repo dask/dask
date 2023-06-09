051a86bd2761c930995962227c76b65e752c7a5b
Rick Zamora <rzamora217@gmail.com>
2023-06-09 07:52:21 -0500

    ci

--- dask/dataframe/backends.py
+++ dask/dataframe/backends.py
@@ -223,7 +223,7 @@ def get_pyarrow_table_pandas(obj, preserve_index=True):
 
 
 @from_pyarrow_table_dispatch.register((pd.DataFrame,))
-def get_pandas_dataframe_pyarrow(obj, table, **kwargs):
+def get_pandas_dataframe_pyarrow(_, table, **kwargs):
     return table.to_pandas(**kwargs)
 
 
