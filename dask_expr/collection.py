27577967a427fda9d8c6f34f8d8dcf312e23e3ab
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-06-23 22:53:57 +0200

    Implement ``select_dtypes`` (#177)

--- dask_expr/collection.py
+++ dask_expr/collection.py
@@ -682,6 +682,10 @@ class DataFrame(FrameBase):
 
         return to_parquet(self, path, **kwargs)
 
+    def select_dtypes(self, include=None, exclude=None):
+        columns = self._meta.select_dtypes(include=include, exclude=exclude).columns
+        return new_collection(self.expr[columns])
+
 
 class Series(FrameBase):
     """Series-like Expr Collection"""
