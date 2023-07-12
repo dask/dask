968b3018d781b2850b2d0b74386993020dc8921d
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-07-12 13:08:36 -0500

    Add release stuff (#226)

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
+from __future__ import annotations
+
+import versioneer
 from setuptools import setup
 
 setup(
-    name="dask_expr",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
 )
