1c2b5cb3e6e339478c479eac3185eed6fda31a72
Sebastien Ehrhardt <sebastien.ehrhardt@onfido.com>
2023-08-22 17:55:44 +0100

    Avoiding tying things together when write_meta_file=False

--- dask/dataframe/io/parquet/core.py
+++ dask/dataframe/io/parquet/core.py
@@ -1045,18 +1045,14 @@ def to_parquet(
                 {"append": append, "compression": compression},
             )
         }
-    else:
-        # NOTE: We still define a single task to tie everything together
-        # when we are not writing a _metadata file. We do not want to
-        # return `data_write` (or a `data_write.to_bag()`), because calling
-        # `compute()` on a multi-partition collection requires the overhead
-        # of trying to concatenate results on the client.
-        final_name = "store-" + data_write._name
-        dsk = {(final_name, 0): (lambda x: None, data_write.__dask_keys__())}
 
-    # Convert data_write + dsk to computable collection
-    graph = HighLevelGraph.from_collections(final_name, dsk, dependencies=(data_write,))
-    out = Scalar(graph, final_name, "")
+        # Convert data_write + dsk to computable collection
+        graph = HighLevelGraph.from_collections(
+            final_name, dsk, dependencies=(data_write,)
+        )
+        out = Scalar(graph, final_name, "")
+    else:
+        out = data_write
 
     if compute:
         out = out.compute(**compute_kwargs)
