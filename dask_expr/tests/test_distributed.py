58f26acca0520a6d0bcffc1490d962576797d6bb
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-08-30 10:23:08 +0200

    Avoid pushing Projections through HashP2P (#279)

--- dask_expr/tests/test_distributed.py
+++ dask_expr/tests/test_distributed.py
@@ -70,3 +70,19 @@ async def test_index_merge_p2p_shuffle(c, s, a, b, npartitions_left):
         x.sort_index(),
         df_left.merge(df_right, left_index=True, right_on="a").sort_index(),
     )
+
+
+@gen_cluster(client=True)
+async def test_merge_p2p_shuffle(c, s, a, b):
+    df_left = lib.DataFrame({"a": [1, 2, 3] * 100, "b": 2, "e": 2})
+    df_right = lib.DataFrame({"a": [4, 2, 3] * 100, "c": 2})
+    left = from_pandas(df_left, npartitions=6)
+    right = from_pandas(df_right, npartitions=5)
+
+    out = left.merge(right, shuffle_backend="p2p")[["b", "c"]]
+    assert out.npartitions == 6
+    x = c.compute(out)
+    x = await x
+    lib.testing.assert_frame_equal(
+        x.reset_index(drop=True), df_left.merge(df_right)[["b", "c"]]
+    )
