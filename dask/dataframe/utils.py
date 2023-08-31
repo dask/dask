4178feb58a7e708345ce4e41e018a746b0d1fd06
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-09-01 00:01:13 +0200

    Update tests for ``pandas=2.1.0`` (#10439)

--- dask/dataframe/utils.py
+++ dask/dataframe/utils.py
@@ -318,7 +318,7 @@ _simple_fake_mapping = {
     "m": np.timedelta64(1),
     "S": np.str_("foo"),
     "a": np.str_("foo"),
-    "U": np.unicode_("foo"),
+    "U": np.str_("foo"),
     "O": "foo",
 }
 
