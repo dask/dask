6e430259d53c19b434839464b1ed2c633854758b
Martin Durant <martin.durant@alumni.utoronto.ca>
2023-10-24 13:28:56 -0400

    rename variable and add comments

--- dask/array/core.py
+++ dask/array/core.py
@@ -3097,9 +3097,10 @@ def normalize_chunks(chunks, shape=None, limit=None, dtype=None, previous_chunks
     if shape is not None:
         chunks = tuple(c if c not in {None, -1} else s for c, s in zip(chunks, shape))
 
-    nonans = None
+    allints = None
     if chunks and shape is not None:
-        nonans = all(isinstance(c, int) for c in chunks)
+        # allints: did we start with chunks as a simple tuple of ints?
+        allints = all(isinstance(c, int) for c in chunks)
         chunks = sum(
             (
                 blockdims_from_blockshape((s,), (c,))
@@ -3130,8 +3131,9 @@ def normalize_chunks(chunks, shape=None, limit=None, dtype=None, previous_chunks
                 "Chunks do not add up to shape. "
                 "Got chunks=%s, shape=%s" % (chunks, shape)
             )
-    if nonans or isinstance(sum(sum(_) for _ in chunks), int):
-        return tuple(tuple(_) for _ in chunks)
+    if allints or isinstance(sum(sum(_) for _ in chunks), int):
+        # Fastpath for when we already know chunks contains only integers
+        return tuple(tuple(ch) for ch in chunks)
     return tuple(
         tuple(int(x) if not math.isnan(x) else np.nan for x in c) for c in chunks
     )
