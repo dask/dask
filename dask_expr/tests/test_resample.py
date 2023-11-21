413f7223e51b3b059bf3f1fe46d6c11fb881905a
Miles <miles59923@gmail.com>
2023-11-21 09:40:21 +0000

    Add remaining _resample.Resampler API (#399)

--- dask_expr/tests/test_resample.py
+++ dask_expr/tests/test_resample.py
@@ -20,15 +20,57 @@ def df(pdf):
     yield from_pandas(pdf, npartitions=4)
 
 
-def test_resample_count(df, pdf):
-    result = df.resample("2T").count()
-    expected = pdf.resample("2T").count()
+@pytest.mark.parametrize(
+    "api",
+    [
+        "count",
+        "prod",
+        "mean",
+        "sum",
+        "min",
+        "max",
+        "first",
+        "last",
+        "var",
+        "std",
+        "size",
+        "nunique",
+        "median",
+        "quantile",
+        "ohlc",
+        "sem",
+    ],
+)
+def test_resample_apis(df, pdf, api):
+    result = getattr(df.resample("2T"), api)()
+    expected = getattr(pdf.resample("2T"), api)()
     assert_eq(result, expected)
 
-    result = df.resample("2T").count()["foo"]
-    expected = pdf.resample("2T").count()["foo"]
+    # No column output
+    if api not in ("size",):
+        result = getattr(df.resample("2T"), api)()["foo"]
+        expected = getattr(pdf.resample("2T"), api)()["foo"]
+        assert_eq(result, expected)
+
+        q = result.simplify()
+        eq = getattr(df["foo"].resample("2T"), api)().simplify()
+        assert q._name == eq._name
+
+
+def test_resample_agg(df, pdf):
+    def my_sum(vals, foo=None, *, bar=None):
+        return vals.sum()
+
+    # TODO: w/o .compute() assert_eq fails w/ dtype assertion error
+    result = df.resample("2T").agg(my_sum, "foo", bar="bar")
+    expected = pdf.resample("2T").agg(my_sum, "foo", bar="bar")
     assert_eq(result, expected)
 
-    q = result.simplify()
-    eq = df["foo"].resample("2T").count().simplify()
-    assert q._name == eq._name
+    result = df.resample("2T").agg(my_sum)["foo"]
+    expected = pdf.resample("2T").agg(my_sum)["foo"]
+    assert_eq(result, expected)
+
+    # simplify up disabled for `agg`, function may access other columns
+    q = df.resample("2T").agg(my_sum)["foo"].simplify()
+    eq = df["foo"].resample("2T").agg(my_sum).simplify()
+    assert q._name != eq._name
