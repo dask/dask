84c4bd475442a9edf7f62851db6d1829b183427f
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-11-22 10:56:09 +0100

    Raise for unsupported engine and pop from kwargs (#411)

--- dask_expr/io/tests/test_io.py
+++ dask_expr/io/tests/test_io.py
@@ -334,6 +334,13 @@ def test_to_parquet(tmpdir, write_metadata_file):
         df2.to_parquet(tmpdir, overwrite=True)
 
 
+def test_to_parquet_engine(tmpdir):
+    pdf = lib.DataFrame({"x": [1, 4, 3, 2, 0, 5]})
+    df = from_pandas(pdf, npartitions=2)
+    with pytest.raises(NotImplementedError, match="not supported"):
+        df.to_parquet(tmpdir + "engine.parquet", engine="fastparquet")
+
+
 @pytest.mark.parametrize(
     "fmt,read_func,read_cls",
     [("parquet", read_parquet, ReadParquet), ("csv", read_csv, ReadCSV)],
