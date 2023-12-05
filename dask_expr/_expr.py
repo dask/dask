d78a77f4bdde926944fa222d368f8829ad53d957
Hendrik Makait <hendrik@makait.com>
2023-12-05 18:14:09 +0100

    to_datetime (#467)

--- dask_expr/_expr.py
+++ dask_expr/_expr.py
@@ -1721,16 +1721,18 @@ class ToNumeric(Elemwise):
     _defaults = {"errors": "raise", "downcast": None}
     operation = staticmethod(pd.to_numeric)
 
-    def _divisions(self):
-        return tuple(
-            pd.Index(
-                pd.to_numeric(
-                    self.frame.divisions,
-                    errors=self.errors,
-                    downcast=self.downcast,
-                )
-            )
-        )
+
+class ToDatetime(Elemwise):
+    _parameters = ["frame", "kwargs"]
+    _defaults = {"kwargs": None}
+    _keyword_only = ["kwargs"]
+    operation = staticmethod(pd.to_datetime)
+
+    @functools.cached_property
+    def _kwargs(self):
+        if (kwargs := self.operand("kwargs")) is None:
+            return {}
+        return kwargs
 
 
 class AsType(Elemwise):
