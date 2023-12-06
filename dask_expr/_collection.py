17d971e6ecb81b35a67af37ce796f8b127b264fd
Miles <miles59923@gmail.com>
2023-12-06 11:33:30 +0100

    Add method conditionals (#476)

--- dask_expr/_collection.py
+++ dask_expr/_collection.py
@@ -263,6 +263,24 @@ class FrameBase(DaskMethodsMixin):
         """Return a copy of this object"""
         return new_collection(self.expr)
 
+    def eq(self, other):
+        return self.__eq__(other)
+
+    def ne(self, other):
+        return self.__ne__(other)
+
+    def gt(self, other):
+        return self.__gt__(other)
+
+    def ge(self, other):
+        return self.__ge__(other)
+
+    def lt(self, other):
+        return self.__lt__(other)
+
+    def le(self, other):
+        return self.__le__(other)
+
     def isin(self, values):
         if isinstance(self, DataFrame):
             # DataFrame.isin does weird alignment stuff
