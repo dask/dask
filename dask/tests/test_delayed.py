36f9197c173e3b149391e1db67317a2e296f321e
Andrew Rosen <asrosen93@gmail.com>
2023-12-12 08:43:48 -0800

    fix test

--- dask/tests/test_delayed.py
+++ dask/tests/test_delayed.py
@@ -855,5 +855,4 @@ def test_delayed_function_attributes_forwarded():
 
     assert add.__name__ == "add"
     assert add.__doc__ == "This is a docstring"
-    assert not isinstance(add.__wrapped__, Delayed)
     assert add.__wrapped__(1, 2) == 3
