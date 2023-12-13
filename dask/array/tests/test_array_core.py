d1e03c412c61559085bbbe32bef58c2aecbe5ebb
Rick Zamora <rzamora217@gmail.com>
2023-12-13 12:41:25 -0600

    remove old xfail

--- dask/array/tests/test_array_core.py
+++ dask/array/tests/test_array_core.py
@@ -14,7 +14,6 @@ np = pytest.importorskip("numpy")
 import math
 import operator
 import os
-import sys
 import time
 import warnings
 from functools import reduce
@@ -22,13 +21,11 @@ from io import StringIO
 from operator import add, sub
 from threading import Lock
 
-from packaging.version import Version
 from tlz import concat, merge
 from tlz.curried import identity
 
 import dask
 import dask.array as da
-from dask._compatibility import PY_VERSION
 from dask.array.chunk import getitem
 from dask.array.core import (
     Array,
@@ -3914,10 +3911,6 @@ def test_setitem_1d():
         dx[index] = 1
 
 
-@pytest.mark.xfail(
-    sys.platform == "win32" and PY_VERSION >= Version("3.12.0"),
-    reason="https://github.com/dask/dask/issues/10604",
-)
 def test_setitem_hardmask():
     x = np.ma.array([1, 2, 3, 4], dtype=int)
     x.harden_mask()
