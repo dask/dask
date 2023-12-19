201f82c0756b9994a8e79260d9c7a67abc69aea2
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-12-19 08:27:43 +0100

    Fixup some groupby warning related stuff (#598)

--- dask_expr/_groupby.py
+++ dask_expr/_groupby.py
@@ -588,9 +588,9 @@ def nunique_df_aggregate(dfs, levels, name, sort=False):
     df = concat(dfs)
     if df.ndim == 1:
         # split out reduces to a Series
-        return df.groupby(level=levels, sort=sort).nunique()
+        return df.groupby(level=levels, sort=sort, observed=True).nunique()
     else:
-        return df.groupby(level=levels, sort=sort)[name].nunique()
+        return df.groupby(level=levels, sort=sort, observed=True)[name].nunique()
 
 
 class NUnique(SingleAggregation):
@@ -1034,7 +1034,7 @@ class GroupBy:
 
         self.obj = obj[projection] if projection is not None else obj
         self.sort = sort
-        self.observed = observed
+        self.observed = observed if observed is not None else False
         self.dropna = dropna
         self.group_keys = group_keys
         self.by = [by] if np.isscalar(by) or isinstance(by, Expr) else list(by)
@@ -1421,12 +1421,13 @@ class SeriesGroupBy(GroupBy):
     ):
         # Raise pandas errors if applicable
         if isinstance(obj, Series):
-            if (
-                isinstance(by, FrameBase)
-                or isinstance(by, (list, tuple))
-                and any(isinstance(x, FrameBase) for x in by)
+            if isinstance(by, FrameBase):
+                obj._meta.groupby(by._meta, **_as_dict("observed", observed))
+            elif isinstance(by, (list, tuple)) and any(
+                isinstance(x, FrameBase) for x in by
             ):
-                pass
+                metas = [x._meta if isinstance(x, FrameBase) else x for x in by]
+                obj._meta.groupby(metas, **_as_dict("observed", observed))
             elif isinstance(by, list):
                 if len(by) == 0:
                     raise ValueError("No group keys passed!")
