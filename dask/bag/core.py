98b0b853355ae26b4a91a4164ee64f6fbf333730
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2023-12-20 22:24:29 -0800

    int(tokenize(x), 16) fixes bag groupby none keys

--- dask/bag/core.py
+++ dask/bag/core.py
@@ -1735,7 +1735,7 @@ def partition(grouper, sequence, npartitions, p, nelements=2**20):
         d = groupby(grouper, block)
         d2 = defaultdict(list)
         for k, v in d.items():
-            d2[abs(hash(k)) % npartitions].extend(v)
+            d2[abs(int(tokenize(k), 16)) % npartitions].extend(v)
         p.append(d2, fsync=True)
     return p
 
@@ -2355,7 +2355,7 @@ def make_group(k, stage):
     return h
 
 
-def groupby_tasks(b, grouper, hash=hash, max_branch=32):
+def groupby_tasks(b, grouper, hash=lambda x: int(tokenize(x), 16), max_branch=32):
     max_branch = max_branch or 32
     n = b.npartitions
 
