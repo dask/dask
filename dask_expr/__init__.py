814a4bfd5e1f1550feca4862cad0cde9ba5c7760
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-12-22 12:00:06 +0100

    Add from_delayed (#617)
    Co-authored-by: Hendrik Makait <hendrik@makait.com>

--- dask_expr/__init__.py
+++ dask_expr/__init__.py
@@ -1,5 +1,6 @@
 from dask_expr import _version, datasets
 from dask_expr._collection import *
+from dask_expr.io._delayed import from_delayed
 from dask_expr.io.parquet import to_parquet
 
 __version__ = _version.get_versions()["version"]
