294cb4c4c207f98151b151686d0ce4c9d7bafb12
Miles <miles59923@gmail.com>
2024-01-03 14:03:03 +0100

    Allow other types than DataFrame in operator methods (#629)

--- dask_expr/tests/test_collection.py
+++ dask_expr/tests/test_collection.py
@@ -14,6 +14,7 @@ from dask.dataframe.utils import UNKNOWN_CATEGORIES
 from dask.utils import M
 
 from dask_expr import (
+    Series,
     expr,
     from_pandas,
     is_scalar,
@@ -482,25 +483,41 @@ def test_boolean_operators(func):
     ),
 )
 @pytest.mark.parametrize("series", (True, False))
-def test_method_operators(pdf, df, axis, level, fill_value, op, series):
+@pytest.mark.parametrize("other", ("series", "dataframe", 1))
+def test_method_operators(pdf, df, axis, level, fill_value, op, series, other):
     kwargs = {
         k: v
         for k, v in (("axis", axis), ("level", level), ("fill_value", fill_value))
         if v is not None
     }
-    if series:
-        kwargs.pop("axis")
-        pdf = pdf.x
-        df = df.x
 
     if level is not None:
         with pytest.raises(NotImplementedError, match="level must be None"):
             getattr(df, op)(other=df, **kwargs)
         return
 
-    expected = getattr(pdf, op)(other=pdf, **kwargs)
-    actual = getattr(df, op)(other=df, **kwargs)
-    assert_eq(expected, actual)
+    if other == "series":
+        pother = pdf.x
+        other = df.x
+    elif other == "dataframe":
+        pother = pdf
+        other = df
+    else:
+        pother = other
+
+    if isinstance(other, Series) and axis in (1, "columns"):
+        with pytest.raises(ValueError, match=f"Unable to {op} dd.Series with axis=1"):
+            getattr(df, op)(other=other, **kwargs)
+
+    elif isinstance(other, Series) and axis in (0, "index") and fill_value:
+        msg = f"fill_value {fill_value} not supported"
+        with pytest.raises(NotImplementedError, match=msg):
+            getattr(df, op)(other=other, **kwargs)
+
+    else:
+        expected = getattr(pdf, op)(other=pother, **kwargs)
+        actual = getattr(df, op)(other=other, **kwargs)
+        assert_eq(expected, actual)
 
 
 @pytest.mark.parametrize(
