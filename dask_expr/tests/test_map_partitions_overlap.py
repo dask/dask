d8d65c7c1bcca0dc86ffd15dd49be96ab4cc7258
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-01-11 10:36:22 +0100

    Implement alignment for map_overelap (#701)

--- dask_expr/tests/test_map_partitions_overlap.py
+++ dask_expr/tests/test_map_partitions_overlap.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pytest
 from dask.dataframe._compat import PANDAS_GE_200
 
@@ -239,3 +241,82 @@ def test_align_dataframes(clear_divisions):
     )
     expected = pd.merge(df2, df1, left_on="A", right_on="A", how="right")
     assert_eq(actual, expected, check_index=False, check_divisions=False)
+
+
+def shifted_sum(df, before, after, c=0):
+    a = df.shift(before)
+    b = df.shift(-after)
+    return df + a + b + c
+
+
+@pytest.mark.parametrize("use_dask_input", [True, False])
+@pytest.mark.parametrize("npartitions", [1, 4])
+@pytest.mark.parametrize("enforce_metadata", [True, False])
+@pytest.mark.parametrize("transform_divisions", [True, False])
+@pytest.mark.parametrize("align_dataframes", [True, False])
+@pytest.mark.parametrize(
+    "overlap_setup",
+    [
+        (0, 3),
+        (3, 0),
+        (3, 3),
+        (0, 0),
+    ],
+)
+def test_map_overlap_multiple_dataframes(
+    use_dask_input,
+    npartitions,
+    enforce_metadata,
+    transform_divisions,
+    align_dataframes,
+    overlap_setup,
+    pdf,
+):
+    before, after = overlap_setup
+
+    ddf = pdf
+    ddf2 = pdf * 2
+    if use_dask_input:
+        ddf = from_pandas(ddf, npartitions)
+        ddf2 = from_pandas(ddf2, 2 if align_dataframes else npartitions)
+
+    def get_shifted_sum_arg(overlap):
+        return (
+            overlap.seconds - 1 if isinstance(overlap, datetime.timedelta) else overlap
+        )
+
+    before_shifted_sum, after_shifted_sum = get_shifted_sum_arg(
+        before
+    ), get_shifted_sum_arg(after)
+
+    # DataFrame
+    res = map_overlap(
+        shifted_sum,
+        ddf,
+        before,
+        after,
+        before_shifted_sum,
+        after_shifted_sum,
+        ddf2,
+        align_dataframes=align_dataframes,
+        transform_divisions=transform_divisions,
+        enforce_metadata=enforce_metadata,
+    )
+    sol = shifted_sum(pdf, before_shifted_sum, after_shifted_sum, pdf * 2)
+    assert_eq(res, sol)
+
+    # Series
+    res = map_overlap(
+        shifted_sum,
+        ddf.x,
+        before,
+        after,
+        before_shifted_sum,
+        after_shifted_sum,
+        ddf2.x,
+        align_dataframes=align_dataframes,
+        transform_divisions=transform_divisions,
+        enforce_metadata=enforce_metadata,
+    )
+    sol = shifted_sum(pdf.x, before_shifted_sum, after_shifted_sum, pdf.x * 2)
+    assert_eq(res, sol)
