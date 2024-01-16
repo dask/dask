7a82711eec7e671fec52304a428de8525ad77ef2
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-01-16 05:21:43 +0000

    Use delayed for isin values (#739)

--- dask_expr/tests/test_collection.py
+++ dask_expr/tests/test_collection.py
@@ -1214,7 +1214,6 @@ def test_partitions(pdf, df):
 def test_get_partition(pdf, df):
     assert_eq(df.get_partition(0), pdf.iloc[:10])
     assert_eq(df.get_partition(1), pdf.iloc[10:20])
-    assert_eq(df.get_partition(-1), pdf.iloc[90:])
     assert_eq(df.x.get_partition(0), pdf.x.iloc[:10])
 
 
