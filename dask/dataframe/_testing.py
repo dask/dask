626e14c38ede28c8ecf959f38de1da2f3c3f7fe4
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-01-18 17:42:46 +0000

    Update

--- dask/dataframe/_testing.py
+++ dask/dataframe/_testing.py
@@ -6,28 +6,7 @@ import sys
 PKG = os.path.dirname(os.path.dirname(__file__))
 
 
-def test_dataframe(
-    extra_args: list[str] | None = None, run_doctests: bool = False
-) -> None:
-    """
-    Run the pandas test suite using pytest.
-
-    By default, runs with the marks -m "not slow and not network and not db"
-
-    Parameters
-    ----------
-    extra_args : list[str], default None
-        Extra marks to run the tests.
-    run_doctests : bool, default False
-        Whether to only run the Python and Cython doctests. If you would like to run
-        both doctests/regular tests, just append "--doctest-modules"/"--doctest-cython"
-        to extra_args.
-
-    Examples
-    --------
-    >>> pd.test()  # doctest: +SKIP
-    running: pytest...
-    """
+def test_dataframe() -> None:
     import pytest
 
     cmd = PKG + "/dataframe"
