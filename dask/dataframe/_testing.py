9cb90b118f414c401060dddaa75092c7ba5a316d
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-01-19 11:33:37 +0000

    Activate query_planning when exporting tests (#10833)

--- dask/dataframe/_testing.py
+++ dask/dataframe/_testing.py
@@ -6,11 +6,14 @@ import sys
 PKG = os.path.dirname(os.path.dirname(__file__))
 
 
-def test_dataframe() -> None:
+def test_dataframe(query_planning="True") -> None:
+    import os
+
     import pytest
 
     cmd = PKG + "/dataframe"
     print(f"running: pytest {cmd}")
+    os.environ["DASK_DATAFRAME__QUERY_PLANNING"] = query_planning
     sys.exit(pytest.main(["-n 4"] + [cmd]))
 
 
