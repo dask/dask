b656a3dd0f33120bc3b21ab7eae269206bfefd9d
Miles Granger <miles59923@gmail.com>
2024-01-22 12:17:55 +0100

    Deprecate 'method' parameter in DataFrame.fillna

--- dask/dataframe/tests/test_dataframe.py
+++ dask/dataframe/tests/test_dataframe.py
@@ -2645,6 +2645,9 @@ def test_fillna():
         pytest.raises(NotImplementedError, lambda: ddf.fillna(0, limit=10))
         pytest.raises(NotImplementedError, lambda: ddf.fillna(0, limit=10, axis=1))
 
+    with pytest.warns(FutureWarning, match="'method' keyword is deprecated"):
+        ddf.fillna(method="ffill")
+
 
 def test_ffill():
     df = _compat.makeMissingDataframe()
