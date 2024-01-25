891247591ec10207b2bcfa5b148ef29ae79ffcc3
crusaderky <crusaderky@gmail.com>
2024-01-25 10:57:59 +0000

    Trivial cosmetic tweaks to _testing.py (#10857)

--- dask/dataframe/_testing.py
+++ dask/dataframe/_testing.py
@@ -6,15 +6,13 @@ import sys
 PKG = os.path.dirname(os.path.dirname(__file__))
 
 
-def test_dataframe(query_planning="True") -> None:
-    import os
-
+def test_dataframe(query_planning: bool = True) -> None:
     import pytest
 
     cmd = PKG + "/dataframe"
     print(f"running: pytest {cmd}")
-    os.environ["DASK_DATAFRAME__QUERY_PLANNING"] = query_planning
-    sys.exit(pytest.main(["-n 4"] + [cmd]))
+    os.environ["DASK_DATAFRAME__QUERY_PLANNING"] = str(query_planning)
+    sys.exit(pytest.main(["-n 4", cmd]))
 
 
 __all__ = ["test_dataframe"]
