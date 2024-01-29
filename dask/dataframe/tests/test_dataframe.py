26c772418e8b25768c750280b14f50065cfd264b
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-01-29 09:52:38 +0000

    Update test_dataframe.py

--- dask/dataframe/tests/test_dataframe.py
+++ dask/dataframe/tests/test_dataframe.py
@@ -2645,8 +2645,9 @@ def test_fillna():
         pytest.raises(NotImplementedError, lambda: ddf.fillna(0, limit=10))
         pytest.raises(NotImplementedError, lambda: ddf.fillna(0, limit=10, axis=1))
 
-    with pytest.warns(FutureWarning, match="'method' keyword is deprecated"):
-        ddf.fillna(method="ffill")
+    if not DASK_EXPR_ENABLED:
+        with pytest.warns(FutureWarning, match="'method' keyword is deprecated"):
+            ddf.fillna(method="ffill")
 
 
 def test_ffill():
