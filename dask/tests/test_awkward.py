f1e196f446cf812928be02caa361cb58c93c71a0
Lindsey Gray <lindsey.gray@gmail.com>
2024-02-11 17:29:31 -0600

    appease linter

--- dask/tests/test_awkward.py
+++ dask/tests/test_awkward.py
@@ -1,13 +1,13 @@
-import json
-import awkward as ak
+from __future__ import annotations
 
 import pytest
-from dask.base import tokenize, normalize_token
+
+from dask.base import normalize_token, tokenize
 
 
 def test_tokenize_touching_data():
     ak = pytest.importorskip("awkward")
-    
+
     a = ak.Array([1, 2, 3, 4, 5])
     layout, report = ak.typetracer.typetracer_with_report(a.layout.form_with_key())
     b = ak.Array(layout)
