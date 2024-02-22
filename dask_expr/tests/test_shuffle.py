447f2f98e4c0e56fe0dfe4898364688e9f5ff8c1
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-02-22 19:19:39 +0100

    Add shuffle if objects are not aligned and partitions are unknown in assign (#887)
    Co-authored-by: Hendrik Makait <hendrik@makait.com>

--- dask_expr/tests/test_shuffle.py
+++ dask_expr/tests/test_shuffle.py
@@ -741,3 +741,10 @@ def test_set_index_filter_pushdown():
     expected = df[["x", "y"]]
     expected = expected[expected.y == 1].set_index("x")
     assert result.simplify()._name == expected.simplify()._name
+
+
+def test_shuffle_index_shuffle(df):
+    with pytest.raises(TypeError, match="Must shuffle on either "):
+        df.shuffle()
+    with pytest.raises(TypeError, match="Cannot shuffle on both"):
+        df.shuffle("x", on_index=True)
