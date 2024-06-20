b9535f1d8af597e1944fc48b09ef8e6a9bdb62fb
Fabien Aulaire <306648+faulaire@users.noreply.github.com>
2024-06-20 15:08:40 +0200

    Update test_methods.py

--- dask/dataframe/tests/test_methods.py
+++ dask/dataframe/tests/test_methods.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import numpy as np
 import pandas as pd
+import pytest
 
 import dask.dataframe as dd
 import dask.dataframe.methods as methods
@@ -17,16 +18,22 @@ def test_assign_not_modifying_array_inplace():
         assert np.shares_memory(df["b"].values, result["b"].values)
 
 
-def test_cumulative_empty_partitions():
+@pytest.mark.parametrize(
+    "drop_range", [("00:00", "00:10"), ("00:10", "00:20"), ("00:25", "00:35")]
+)
+def test_cumulative_empty_partitions(drop_range):
     pdf = pd.DataFrame(
-        {"x": [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]},
+        {
+            "x": [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0],
+            "y": [8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0],
+        },
         index=pd.date_range("1995-02-26", periods=8, freq="5min"),
         dtype=float,
     )
-    pdf2 = pdf.drop(pdf.between_time("00:10", "00:20").index)
+    pdf2 = pdf.drop(pdf.between_time(*drop_range).index)
 
     df = dd.from_pandas(pdf, npartitions=8)
-    df2 = dd.from_pandas(pdf2, npartitions=1).repartition(df.divisions)
+    df2 = dd.from_pandas(pdf2, npartitions=1).repartition(df.divisions, force=True)
 
     assert_eq(df2.cumprod(), pdf2.cumprod())
     assert_eq(df2.cumsum(), pdf2.cumsum())
