6ccc7f3cf7dfe67d7c2322bec830245d71f5637e
Fabien Aulaire <306648+faulaire@users.noreply.github.com>
2024-06-20 10:42:59 +0200

    Add tests on cumulative aggregations for empty partitions

--- dask/dataframe/tests/test_methods.py
+++ dask/dataframe/tests/test_methods.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import numpy as np
 import pandas as pd
 
+import dask.dataframe as dd
+from dask.dataframe.utils import assert_eq
 import dask.dataframe.methods as methods
 from dask.dataframe._compat import PANDAS_GE_140
 
@@ -13,3 +15,20 @@ def test_assign_not_modifying_array_inplace():
     assert not np.shares_memory(df["a"].values, result["a"].values)
     if PANDAS_GE_140:
         assert np.shares_memory(df["b"].values, result["b"].values)
+
+
+def test_cumulative_empty_partitions():
+    pdf = pd.DataFrame(
+        {"x": [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]},
+        index=pd.date_range("1995-02-26", periods=8, freq="5min"),
+        dtype=float,
+    )
+    pdf2 = pdf.drop(pdf.between_time("00:10", "00:20").index)
+
+    df = dd.from_pandas(pdf, npartitions=8)
+    df2 = dd.from_pandas(pdf2, npartitions=1).repartition(df.divisions)
+
+    assert_eq(df2.cumprod(), pdf2.cumprod())
+    assert_eq(df2.cumsum(), pdf2.cumsum())
+    assert_eq(df2.max(), pdf2.max())
+    assert_eq(df2.min(), pdf2.min())
