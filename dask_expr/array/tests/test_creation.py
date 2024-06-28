cd27aa02d1e2a319daf65582edea7ab1a9b82627
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-06-28 22:10:57 +0200

    Implement linspace (#1098)

--- dask_expr/array/tests/test_creation.py
+++ dask_expr/array/tests/test_creation.py
@@ -8,3 +8,7 @@ def test_arange():
     assert_eq(da.arange(1, 100, 7), np.arange(1, 100, 7))
     assert_eq(da.arange(100), np.arange(100))
     assert_eq(da.arange(100, like=np.arange(100)), np.arange(100))
+
+
+def test_linspace():
+    assert_eq(da.linspace(1, 100, 30), np.linspace(1, 100, 30))
