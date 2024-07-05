b40744113987e058f08aaae34cea81bacda8b753
James Bourbeau <jrbourbeau@gmail.com>
2024-07-05 12:56:54 -0500

    Test fixup

--- dask/tests/test_sizeof.py
+++ dask/tests/test_sizeof.py
@@ -8,7 +8,7 @@ import pytest
 
 from dask.multiprocessing import get_context
 from dask.sizeof import sizeof
-from dask.utils import funcname
+from dask.utils import funcname, tmpdir
 
 try:
     import pandas as pd
@@ -274,29 +274,30 @@ def test_xarray():
     assert sizeof(dataset.indexes) >= sizeof(ind)
 
 
-def test_xarray_not_in_memory(tmpdir):
+def test_xarray_not_in_memory():
     xr = pytest.importorskip("xarray")
     np = pytest.importorskip("numpy")
+    pytest.importorskip("zarr")
 
     ind = np.arange(-66, 67, 1).astype(float)
     arr = np.random.random((len(ind),))
 
-    path = tmpdir / "test.zarr"
-    xr.DataArray(
-        arr,
-        dims=["coord"],
-        coords={"coord": ind},
-    ).rename(
-        "foo"
-    ).to_dataset().to_zarr(path)
-    dataset = xr.open_dataset(path, chunks={"foo": 10})
-    assert not dataset.foo._in_memory
-    assert sizeof(ind) < sizeof(dataset) < sizeof(arr) + sizeof(ind)
-    assert sizeof(dataset.foo) < sizeof(arr)
-    assert sizeof(dataset["coord"]) >= sizeof(ind)
-    assert sizeof(dataset.indexes) >= sizeof(ind)
-    assert not dataset.foo._in_memory
+    with tmpdir() as path:
+        xr.DataArray(
+            arr,
+            dims=["coord"],
+            coords={"coord": ind},
+        ).rename(
+            "foo"
+        ).to_dataset().to_zarr(path)
+        dataset = xr.open_zarr(path, chunks={"foo": 10})
+        assert not dataset.foo._in_memory
+        assert sizeof(ind) < sizeof(dataset) < sizeof(arr) + sizeof(ind)
+        assert sizeof(dataset.foo) < sizeof(arr)
+        assert sizeof(dataset["coord"]) >= sizeof(ind)
+        assert sizeof(dataset.indexes) >= sizeof(ind)
+        assert not dataset.foo._in_memory
 
-    dataset.load()
-    assert dataset.foo._in_memory
-    assert sizeof(dataset) >= sizeof(arr) + sizeof(ind)
+        dataset.load()
+        assert dataset.foo._in_memory
+        assert sizeof(dataset) >= sizeof(arr) + sizeof(ind)
