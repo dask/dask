ac90e5608e1aebfd17c411ff01f57f41cb82eb84
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-08-12 19:04:04 +0200

    Update dask/array/reshape.py
    Co-authored-by: Hendrik Makait <hendrik@makait.com>

--- dask/array/reshape.py
+++ dask/array/reshape.py
@@ -142,6 +142,7 @@ def _smooth_chunks(ileft, ii, max_in_chunk, result_inchunks):
     # The first dimension of the reshaped dimension are the chunks with length 2
     # Assume we split the second dimension into (2, 1), i.e. setting the chunks to
     # ((2, 2), (2, 1)) and the output chunks to ((4, 2, 4, 2), )
+    # Then, the result will be different because we first reshape the individual, non-contiguous chunks before concatenating them:
     # [ 0  1  3  4  2  5  6  7  9 10  8 11]
     # This is equivalent to
     # arr = np.arange(0, 12).reshape(4, 3)
