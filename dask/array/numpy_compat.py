a969dee09d0c63d991cdf34df46a94ebc66c95fb
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-08-13 13:52:36 +0200

    Fix upstream numpy build (#11304)

--- dask/array/numpy_compat.py
+++ dask/array/numpy_compat.py
@@ -16,6 +16,7 @@ NUMPY_GE_123 = _np_version.release >= (1, 23)
 NUMPY_GE_124 = _np_version.release >= (1, 24)
 NUMPY_GE_125 = _np_version.release >= (1, 25)
 NUMPY_GE_200 = _np_version.release >= (2, 0)
+NUMPY_GE_210 = _np_version.release >= (2, 1)
 
 
 if NUMPY_GE_200:
