7c04f33565deef5fb1f21354d7dcc6b212d67b35
rjzamora <rzamora217@gmail.com>
2024-08-15 09:55:12 -0700

    use existing utils

--- dask/array/slicing.py
+++ dask/array/slicing.py
@@ -13,6 +13,7 @@ from tlz import concat, memoize, merge, pluck
 
 from dask import core
 from dask.array.chunk import getitem
+from dask.array.utils import arange_safe, asarray_safe
 from dask.base import is_dask_collection, tokenize
 from dask.highlevelgraph import HighLevelGraph
 from dask.utils import _deprecated, cached_cumsum, is_arraylike
@@ -566,8 +567,7 @@ def take(outname, inname, chunks, index, axis=0):
     if not np.isnan(chunks[axis]).any():
         from dask.array._shuffle import _shuffle
 
-        like = None if isinstance(index, (list, tuple)) else index
-        arange = np.arange(np.sum(chunks[axis]), like=like)
+        arange = arange_safe(np.sum(chunks[axis]), like=index)
         if len(index) == len(arange) and np.abs(index - arange).sum() == 0:
             # TODO: This should be a real no-op, but the call stack is
             # too deep to do this efficiently for now
@@ -578,7 +578,7 @@ def take(outname, inname, chunks, index, axis=0):
         average_chunk_size = int(sum(chunks[axis]) / len(chunks[axis]))
 
         indexer = []
-        index = np.asarray(index, like=like)
+        index = asarray_safe(index, like=index)
         for i in range(0, len(index), average_chunk_size):
             indexer.append(index[i : i + average_chunk_size].tolist())
 
