141a9e1498498a9f0062a41f9b10d560285c98d5
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-08-16 18:03:08 +0200

    Rename

--- dask/array/tests/test_shuffle.py
+++ dask/array/tests/test_shuffle.py
@@ -40,7 +40,7 @@ def test_shuffle(arr, darr, indexer, chunks, other_chunks):
 @pytest.mark.parametrize("tol, chunks", ((1, (3, 2, 3)), (1.4, (5, 3))))
 def test_shuffle_config_tolerance(arr, darr, tol, chunks):
     indexer = [[1, 5, 6], [0, 3], [4, 2, 7]]
-    with dask.config.set({"array.chunksize-tolerance": tol}):
+    with dask.config.set({"array.chunk-size-tolerance": tol}):
         result = darr.shuffle(indexer, axis=1)
     expected = arr[:, [1, 5, 6, 0, 3, 4, 2, 7]]
     assert_eq(result, expected)
