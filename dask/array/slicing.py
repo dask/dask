058f1b69ee3115d63046acf60e537d358462a466
Richard (Rick) Zamora <rzamora217@gmail.com>
2024-08-16 07:52:35 -0700

    Upgrade gpuCI and fix Dask Array failures with "cupy" backend (#11309)

--- dask/array/slicing.py
+++ dask/array/slicing.py
@@ -565,8 +565,9 @@ def take(outname, inname, chunks, index, axis=0):
 
     if not np.isnan(chunks[axis]).any():
         from dask.array._shuffle import _shuffle
+        from dask.array.utils import arange_safe, asarray_safe
 
-        arange = np.arange(np.sum(chunks[axis]))
+        arange = arange_safe(np.sum(chunks[axis]), like=index)
         if len(index) == len(arange) and np.abs(index - arange).sum() == 0:
             # TODO: This should be a real no-op, but the call stack is
             # too deep to do this efficiently for now
@@ -577,7 +578,7 @@ def take(outname, inname, chunks, index, axis=0):
         average_chunk_size = int(sum(chunks[axis]) / len(chunks[axis]))
 
         indexer = []
-        index = np.asarray(index)
+        index = asarray_safe(index, like=index)
         for i in range(0, len(index), average_chunk_size):
             indexer.append(index[i : i + average_chunk_size].tolist())
 
