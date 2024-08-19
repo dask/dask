61c7baa663da42b0169f4d3aeee24e39b6c03904
David Stansby <dstansby@gmail.com>
2024-08-19 10:59:52 +0100

    Explicitly check shape of arrays

--- dask/array/tests/test_overlap.py
+++ dask/array/tests/test_overlap.py
@@ -830,10 +830,13 @@ def test_sliding_window_errors(window_shape, axis):
 
 def test_map_overlap_new_axis():
     arr = da.arange(6, chunks=2)
+    assert arr.shape == (6,)
     assert arr.chunks == ((2, 2, 2),)
 
     actual = arr.map_overlap(lambda x: np.stack([x, x + 0.5]), depth=1, new_axis=[0])
     expected = np.stack([np.arange(6), np.arange(6) + 0.5])
 
     assert actual.chunks == ((1,), (2, 2, 2))
-    assert_eq(expected, actual.compute())
+    actual = actual.compute()
+    assert actual.shape == (2, 6)
+    assert_eq(expected, actual)
