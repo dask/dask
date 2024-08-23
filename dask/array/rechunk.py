c3eb427e3ca18ab88cbebe5d3d2b00004bc2694a
Hendrik Makait <hendrik@makait.com>
2024-08-23 09:07:58 +0200

    Always have threshold

--- dask/array/rechunk.py
+++ dask/array/rechunk.py
@@ -400,6 +400,7 @@ def _choose_rechunk_method(old_chunks, new_chunks, threshold=None):
 
     _old_to_new = old_to_new(old_chunks, new_chunks)
     graph_size = math.prod(sum(len(ins) for ins in axis) for axis in _old_to_new)
+    threshold = threshold or config.get("array.rechunk.threshold")
     graph_size_threshold = _graph_size_threshold(old_chunks, new_chunks, threshold)
     return "tasks" if graph_size < graph_size_threshold else "p2p"
 
