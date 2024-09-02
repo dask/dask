31508dbb8096d016dbe2817e921688993b7cc92f
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-09-02 11:44:22 -0400

    Fix test

--- dask/base.py
+++ dask/base.py
@@ -22,7 +22,6 @@ from operator import getitem
 from typing import Any, Literal, TypeVar
 
 import cloudpickle
-import numpy as np
 from tlz import curry, groupby, identity, merge
 from tlz.functoolz import Compose
 
@@ -1454,7 +1453,7 @@ def _colorize(t):
     >>> _colorize((0, 32, 128))
     '#002080'
     """
-    t = np.array(t[:3], dtype="uint64").tolist()
+    t = t[:3]
     i = sum(v * 256 ** (len(t) - i - 1) for i, v in enumerate(t))
     h = hex(int(i))[2:].upper()
     h = "0" * (6 - len(h)) + h
