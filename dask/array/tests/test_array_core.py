17b1d054ce906db104e48a9d55247f57fdc2219c
Martin Yeo <martin.yeo@metoffice.gov.uk>
2024-10-16 16:46:15 +0100

    Fully exercise broadcast_shapes in type test.

--- dask/array/tests/test_array_core.py
+++ dask/array/tests/test_array_core.py
@@ -10,6 +10,7 @@ import pytest
 
 np = pytest.importorskip("numpy")
 
+import itertools
 import math
 import operator
 import os
@@ -871,7 +872,9 @@ def test_broadcast_shapes():
     assert (3, 4) == broadcast_shapes((3, 1), (1, 4), (4,))
     assert (5, 6, 7, 3, 4) == broadcast_shapes((3, 1), (), (5, 6, 7, 1, 4))
 
-    assert all(isinstance(i, int) for i in broadcast_shapes((2, 5)))
+    assert all(
+        isinstance(i, int) for i in itertools.chain(*broadcast_shapes([(1, 2), (3, 1)]))
+    )
 
     pytest.raises(ValueError, lambda: broadcast_shapes((3,), (3, 4)))
     pytest.raises(ValueError, lambda: broadcast_shapes((2, 3), (2, 3, 1)))
