fb1458a8d330abc6589fa2894d7016e306db35f8
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-11-19 17:35:21 +0100

    Use Alias

--- dask/dataframe/core.py
+++ dask/dataframe/core.py
@@ -8283,14 +8283,14 @@ def new_dd_object(dsk, name, meta, divisions, parent_meta=None):
                 layer.new_axes["j"] = chunks[1][0]
                 layer.output_indices = layer.output_indices + ("j",)
             else:
-                from dask._task_spec import Task
+                from dask._task_spec import Alias, Task
 
                 suffix = (0,) * (len(chunks) - 1)
                 for i in range(len(chunks[0])):
-                    task = layer.pop((name, i))
+                    task = layer.get((name, i))
                     new_key = (name, i) + suffix
                     if isinstance(task, Task):
-                        task = Task(new_key, task)
+                        task = Alias(new_key, task.key)
                     layer[new_key] = task
         return da.Array(dsk, name=name, chunks=chunks, dtype=meta.dtype)
     else:
