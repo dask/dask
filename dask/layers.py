a1eb23240289cf5dbf074ecf28cdd1881693a99f
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-11-21 13:41:07 +0100

    Speed up ArraySliceDep tokenization (#11551)

--- dask/layers.py
+++ dask/layers.py
@@ -110,7 +110,7 @@ class ArrayValuesDep(ArrayBlockwiseDep):
 
 @normalize_token.register(ArraySliceDep)
 def normalize_array_slice_dep(dep):
-    return "ArraySliceDep", dep.chunks, dep.starts
+    return "ArraySliceDep", dep.chunks
 
 
 @normalize_token.register(ArrayBlockIdDep)
