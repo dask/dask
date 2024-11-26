7e32afce6e2d4f3c7edda4ad31347108e7563e5f
David Chudzicki <david@coiled.io>
2024-11-25 22:36:02 -0500

    update Slack text & change 'Ask AI' to 'Ask Slackbot'

--- docs/source/_static/custom.js
+++ docs/source/_static/custom.js
@@ -18,7 +18,10 @@ document.addEventListener("DOMContentLoaded", function () {
   script.setAttribute("runllm-usage-limit-effective-days", 30);
   script.setAttribute("runllm-usage-limit-message", `Hi! You've hit the limit for anonymous questions, but you can join us on Slack in #DaskBot and ask as many questions as you'd like.`);
   script.setAttribute("runllm-brand-logo", "_images/dask_icon.svg");
+  script.setAttribute("runllm-floating-button-text", "Ask DaskBot");
+  script.setAttribute("runllm-join-community-text", "Chat with DaskBot in Slack");
 
   script.async = true;
   document.head.appendChild(script);
 });
+
