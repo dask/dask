fa537961547699b4960638e96471f8436224cf97
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2025-01-10 13:08:02 +0100

    Reroute source of docs for dataframe methods (#11645)

---
+++ dask/dataframe/api.py
@@ -0,0 +1,4 @@
+from __future__ import annotations
+
+from dask.dataframe.dask_expr._groupby import GroupBy, SeriesGroupBy  # noqa: F401
+from dask.dataframe.dask_expr._rolling import Rolling  # noqa: F401
