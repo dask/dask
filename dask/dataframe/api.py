5cf204a1925f15ce737f77c9d8ada18bde3695ab
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2025-01-10 13:44:15 +0100

    Expose downstream utilities for dask.dataframe

--- dask/dataframe/api.py
+++ dask/dataframe/api.py
@@ -1,4 +1,7 @@
 from __future__ import annotations
 
+from dask.dataframe.dask_expr import FrameBase, elemwise, new_collection  # noqa: F401
+from dask.dataframe.dask_expr._expr import _emulate, are_co_aligned  # noqa: F401
 from dask.dataframe.dask_expr._groupby import GroupBy, SeriesGroupBy  # noqa: F401
+from dask.dataframe.dask_expr._reductions import ApplyConcatApply  # noqa: F401
 from dask.dataframe.dask_expr._rolling import Rolling  # noqa: F401
