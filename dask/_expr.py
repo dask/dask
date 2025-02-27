f4cd5ac8be1398fd939231de33d6b16b1bf9e099
fjetter <fjetter@users.noreply.github.com>
2025-02-27 15:07:20 +0100

    change name impl all over

--- dask/_expr.py
+++ dask/_expr.py
@@ -46,9 +46,9 @@ class Expr:
 
     operands: list
 
-    _token: str | None
+    _determ_token: str | None
 
-    def __new__(cls, *args, _token=None, **kwargs):
+    def __new__(cls, *args, _determ_token=None, **kwargs):
         operands = list(args)
         for parameter in cls._parameters[len(operands) :]:
             try:
@@ -58,7 +58,7 @@ class Expr:
         assert not kwargs, kwargs
         inst = object.__new__(cls)
 
-        inst._token = _token
+        inst._determ_token = _determ_token
         inst.operands = [_unpack_collections(o) for o in operands]
         _name = inst._name
         if _name in Expr._instances:
@@ -134,12 +134,14 @@ class Expr:
     @staticmethod
     def _reconstruct(*args):
         typ, *operands, token = args
-        return typ(*operands, _token=token)
+        return typ(*operands, _determ_token=token)
 
     def __reduce__(self):
         if dask.config.get("dask-expr-no-serialize", False):
             raise RuntimeError(f"Serializing a {type(self)} object")
-        return Expr._reconstruct, tuple([type(self)] + self.operands + [self._token])
+        return Expr._reconstruct, tuple(
+            [type(self)] + self.operands + [self.deterministic_token]
+        )
 
     def _depth(self, cache=None):
         """Depth of the expression tree
@@ -469,11 +471,15 @@ class Expr:
     def _funcname(self) -> str:
         return funcname(type(self)).lower()
 
+    @property
+    def deterministic_token(self):
+        if not self._determ_token:
+            self._determ_token = _tokenize_deterministic(*self.operands)
+        return self._determ_token
+
     @functools.cached_property
     def _name(self) -> str:
-        if not self._token:
-            self._token = _tokenize_deterministic(*self.operands)
-        return self._funcname + "-" + self._token
+        return self._funcname + "-" + self.deterministic_token
 
     @property
     def _meta(self):
