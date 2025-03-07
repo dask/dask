ae3d020837b1ffd9d98efc96a65c93e44706fc47
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2025-03-07 12:22:31 +0100

    Fixup

--- dask/array/tests/test_slicing.py
+++ dask/array/tests/test_slicing.py
@@ -398,8 +398,8 @@ def test_slicing_and_chunks():
 def test_slicing_and_unknown_chunks():
     a = da.ones((10, 5), chunks=5)
     a._chunks = ((np.nan, np.nan), (5,))
-    # with pytest.raises(ValueError, match="Array chunk size or shape is unknown"):
-    a[[0, 5]].compute()
+    with pytest.raises(ValueError, match="Array chunk size or shape is unknown"):
+        a[[0, 5]].compute()
 
 
 def test_slicing_identities():
