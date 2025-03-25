bdf192aecd04e78ddcd5f8afa98d1823b74336e9
fjetter <fjetter@users.noreply.github.com>
2025-03-25 10:56:16 +0100

    move to dedicated test file

--- dask/tests/test_expr.py
+++ dask/tests/test_expr.py
@@ -1,11 +1,8 @@
 from __future__ import annotations
 
-import pickle
-
 import pytest
 
-from dask._expr import Expr, HLGExpr
-from dask.tokenize import tokenize
+from dask._expr import Expr
 
 
 class MyExpr(Expr):
@@ -31,24 +28,3 @@ def test_setattr2():
     assert e.bar == 3
     with pytest.raises(AttributeError):
         e.baz = 4
-
-
-def test_hlgexpr():
-    # Ensure tokens are different for different high-level graphs The current
-    # implementation actually ensures that no HLGExpr are tokenizing equally.
-    # Technically, we do not need such a strong guarantee. but tokenizing a full
-    # HLG reliably is tricky and we do not require the reproducibility for
-    # HLGExpr since they do not undergo the same kind of optimization as the
-    # rest of the graph.
-    from dask.highlevelgraph import HighLevelGraph
-
-    dsk = HighLevelGraph.from_collections("x", {"foo": None})
-    dsk2 = HighLevelGraph.from_collections("x", {"bar": None})
-    dsk3 = HighLevelGraph.from_collections("y", {"foo": None})
-    assert tokenize(HLGExpr(dsk)) != tokenize(HLGExpr(dsk2))
-    assert tokenize(HLGExpr(dsk)) != tokenize(HLGExpr(dsk3))
-    assert tokenize(HLGExpr(dsk2)) != tokenize(HLGExpr(dsk3))
-
-    # Roundtrip preserves the tokens
-    for expr in [HLGExpr(dsk), HLGExpr(dsk2), HLGExpr(dsk3)]:
-        assert tokenize(pickle.loads(pickle.dumps(expr))) == tokenize(expr)
