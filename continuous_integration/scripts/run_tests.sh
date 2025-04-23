4f279bbead7812efed9a6c66be2b5684b5b5dbda
fjetter <fjetter@users.noreply.github.com>
2025-04-23 13:42:57 +0200

    typo

--- continuous_integration/scripts/run_tests.sh
+++ continuous_integration/scripts/run_tests.sh
@@ -16,7 +16,7 @@ if [[ $COVERAGE == 'true' ]]; then
     export XTRATESTARGS="--cov=dask --cov-report=xml --junit-xml pytest.xml $XTRATESTARGS"
 fi
 
-echo "py.testdask --runslow $MARKERS $XTRATESTARGS"
+echo "py.test dask --runslow $MARKERS $XTRATESTARGS"
 py.test dask --runslow $MARKERS $XTRATESTARGS
 
 set +e
