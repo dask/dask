1024811c9836ccd3e6187184673f7d7d5fa946ca
N R Navaneet <navaneetnr@gmail.com>
2025-07-13 21:55:41 +0530

    test_dataframe fixes

--- dask/dataframe/tests/test_dataframe.py
+++ dask/dataframe/tests/test_dataframe.py
@@ -3662,7 +3662,7 @@ def test_inplace_operators():
     "idx",
     [
         np.arange(100),
-        sorted(np.random.random(size=100)),
+        sorted(np.random.random(size=100).tolist()),
         pd.date_range("20150101", periods=100),
     ],
 )
