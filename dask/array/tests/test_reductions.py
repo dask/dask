4232085d0427d670dc5516e69634aca4b375aee0
tonyyuyiding <tonyyuyiding@gmail.com>
2025-10-10 11:26:56 +0800

    add tests for different dtypes

--- dask/array/tests/test_reductions.py
+++ dask/array/tests/test_reductions.py
@@ -650,15 +650,16 @@ def test_array_cumreduction_axis(func, use_nan, axis, method):
 
 @pytest.mark.parametrize("func", ["cumsum", "cumprod", "nancumsum", "nancumprod"])
 @pytest.mark.parametrize("method", ["sequential", "blelloch"])
-def test_array_cumreduction_dtype(func, method):
+@pytest.mark.parametrize("target_dtype", [None, int, float])
+def test_array_cumreduction_dtype(func, method, target_dtype):
     np_func = getattr(np, func)
     da_func = getattr(da, func)
 
-    a = np.linspace(0, 1, num=10, dtype=np.float32)
+    a = np.linspace(0, 1, num=10, dtype=float)
     d = da.from_array(a)
 
-    a_r = np_func(a, axis=0, dtype=int)
-    d_r = da_func(d, method=method, axis=0, dtype=int)
+    a_r = np_func(a, axis=0, dtype=target_dtype)
+    d_r = da_func(d, method=method, axis=0, dtype=target_dtype)
 
     assert_eq(a_r, d_r)
 
