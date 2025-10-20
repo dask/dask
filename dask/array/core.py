2be04861a0389e8614cc1441d53e892d9c6c8184
Davis Vann Bennett <davis.v.bennett@gmail.com>
2025-10-20 18:40:34 +0200

    lint

--- dask/array/core.py
+++ dask/array/core.py
@@ -3660,12 +3660,17 @@ def from_array(
         asarray = not hasattr(x, "__array_function__")
 
     previous_chunks = getattr(x, "chunks", None)
-    
-    # As of Zarr 3.x, arrays can have a shards attribute. If present, 
-    # this defines the smallest array region that is safe to write, and 
+
+    # As of Zarr 3.x, arrays can have a shards attribute. If present,
+    # this defines the smallest array region that is safe to write, and
     # thus this is a better default than the chunks attribute.
     # We check for chunks AND shards to be somewhat specific to Zarr 3.x arrays
-    if hasattr(x, "chunks") and hasattr(x, "shards") and (x.shards is not None) and chunks == "auto":
+    if (
+        hasattr(x, "chunks")
+        and hasattr(x, "shards")
+        and (x.shards is not None)
+        and chunks == "auto"
+    ):
         chunks = x.shards
 
     chunks = normalize_chunks(
