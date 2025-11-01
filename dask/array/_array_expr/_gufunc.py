d5e0f4e397a3eb2315682644d9b7a1a00920e19f
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2025-11-01 10:57:07 +0100

    some manual fixes after `run format`

--- dask/array/_array_expr/_gufunc.py
+++ dask/array/_array_expr/_gufunc.py
@@ -439,9 +439,9 @@ def apply_gufunc(
             #### Check if core dimensions consist of only one chunk
             if (dim in core_shapes) and (chunksizes[0][0] < core_shapes[dim]):
                 raise ValueError(
-                    "Core dimension `'{}'` consists of multiple chunks. To fix, rechunk into a single \
-chunk along this dimension or set `allow_rechunk=True`, but beware that this may increase memory usage \
-significantly.".format(dim)
+                    "Core dimension `'{}'` consists of multiple chunks. To fix, rechunk into a single "
+                    "chunk along this dimension or set `allow_rechunk=True`, but beware that this may "
+                    "increase memory usage significantly.".format(dim)
                 )
             #### Check if loop dimensions consist of same chunksizes, when they have sizes > 1
             relevant_chunksizes = list(
