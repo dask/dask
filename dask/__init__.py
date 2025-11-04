9c53e4a2c77e4985bb052a2a62b7f17024c931f8
Jacob Tomlinson <jtomlinson@nvidia.com>
2025-11-04 11:43:35 +0000

    Fix how we expose __version__

--- dask/__init__.py
+++ dask/__init__.py
@@ -3,9 +3,13 @@ from __future__ import annotations
 from dask import config, datasets
 from dask._expr import Expr, HLGExpr, LLGExpr, SingletonExpr
 
-# Backwards compatibility with versioneer
-from dask._version import __commit_id__ as __git_revision__
-from dask._version import __version__
+try:
+    # Backwards compatibility with versioneer
+    from dask._version import __commit_id__ as __git_revision__
+    from dask._version import __version__
+except ImportError:
+    __git_revision__ = "unknown"
+    __version__ = "unknown"
 from dask.base import (
     annotate,
     compute,
