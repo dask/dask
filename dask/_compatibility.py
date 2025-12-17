3836cab551d26129cf3363d86c2d3f5cb8782b7e
Guido Imperiale <crusaderky@gmail.com>
2025-12-17 09:40:08 +0000

    Remove deprecated `dask._compatibility.entry_points` (#12202)

--- dask/_compatibility.py
+++ dask/_compatibility.py
@@ -21,16 +21,6 @@ MACOS = sys.platform == "darwin"
 WINDOWS = sys.platform == "win32"
 
 
-def entry_points(group=None):
-    warnings.warn(
-        "`dask._compatibility.entry_points` has been replaced by `importlib_metadata.entry_points` and will be removed "
-        "in a future version. Please use `importlib_metadata.entry_points` instead.",
-        DeprecationWarning,
-        stacklevel=2,
-    )
-    return importlib_metadata.entry_points(group=group)
-
-
 VERSIONS = {
     "numpy": "1.21.0",
     "pandas": "2.0.0",
