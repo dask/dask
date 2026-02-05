28ca9bcec2a1f26e8ebb0260f1dbd71fe923fc54
crusaderky <crusaderky@gmail.com>
2026-02-05 20:19:19 +0000

    DNM stress-test flaky pandas tests

--- continuous_integration/scripts/run_tests.sh
+++ continuous_integration/scripts/run_tests.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-CMD="python -m pytest dask --runslow"
+CMD="python -m pytest --count=1000 dask/dataframe/io/tests/test_parquet.py::test_filter_nonpartition_columns dask/dataframe/io/tests/test_parquet.py::test_roundtrip_partitioned_pyarrow_dataset dask/dataframe/io/tests/test_parquet.py::test_arrow_partitioning"
 
 if [[ $COVERAGE == 'true' ]]; then
     CMD="$CMD --cov --cov-report=xml --junit-xml=pytest.xml"
