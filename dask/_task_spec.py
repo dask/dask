a240f81cf9e391bdc0d349298a38ff9f4b4a8d29
crusaderky <crusaderky@gmail.com>
2026-02-19 06:57:19 +0000

    Quadratic definition time in xarray.DataArray.to_zarr(compute=False)

--- dask/_task_spec.py
+++ dask/_task_spec.py
@@ -792,7 +792,8 @@ class Task(GraphNode):
         self, subs: dict[KeyType, KeyType | GraphNode], key: KeyType | None = None
     ) -> Task:
         subs_filtered = {
-            k: v for k, v in subs.items() if k in self.dependencies and k != v
+            # subs can be as large as the whole graph; do not iterate over it!
+            k: v for k in (self.dependencies & subs.keys()) if (v := subs[k]) != k
         }
         extras = _extra_args(type(self))  # type: ignore[arg-type]
         extra_kwargs = {
@@ -880,7 +881,8 @@ class NestedContainer(Task, Iterable):
         self, subs: dict[KeyType, KeyType | GraphNode], key: KeyType | None = None
     ) -> NestedContainer:
         subs_filtered = {
-            k: v for k, v in subs.items() if k in self.dependencies and k != v
+            # subs can be as large as the whole graph; do not iterate over it!
+            k: v for k in (self.dependencies & subs.keys()) if (v := subs[k]) != k
         }
         if not subs_filtered:
             return self
@@ -962,7 +964,8 @@ class Dict(NestedContainer, Mapping):
         self, subs: dict[KeyType, KeyType | GraphNode], key: KeyType | None = None
     ) -> Dict:
         subs_filtered = {
-            k: v for k, v in subs.items() if k in self.dependencies and k != v
+            # subs can be as large as the whole graph; do not iterate over it!
+            k: v for k in (self.dependencies & subs.keys()) if (v := subs[k]) != k
         }
         if not subs_filtered:
             return self
