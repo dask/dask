47ab3a2e5a990542ccf2df6d19c8c7e647f6427a
omarwael-24 <omarelmaru@gmail.com>
2026-05-29 21:59:13 +0300

    Adding a type hints for chunks parameter in rechunk function

--- dask/array/rechunk.py
+++ dask/array/rechunk.py
@@ -269,7 +269,7 @@ def _validate_rechunk(old_chunks, new_chunks):
 
 def rechunk(
     x,
-    chunks="auto",
+    chunks: str | tuple[int, ...] | list[int] = "auto",
     threshold=None,
     block_size_limit=None,
     balance=False,
