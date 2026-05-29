b6f88ec154271d909346ab3215d7770a3057d127
omarwael-24 <omarelmaru@gmail.com>
2026-05-29 22:28:08 +0300

    Using Any type for chunks parameter to support dynamic configurations

--- dask/array/rechunk.py
+++ dask/array/rechunk.py
@@ -270,7 +270,7 @@ def _validate_rechunk(old_chunks, new_chunks):
 
 def rechunk(
     x,
-    chunks: str | tuple[int, ...] | list[int] | dict[Any, Any] | int = "auto",
+    chunks: Any = "auto",
     threshold=None,
     block_size_limit=None,
     balance=False,
