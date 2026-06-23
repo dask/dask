4f8480801f10f52a7d8c4d4a9af80afea9afb55f
Akshat86520 <akshatsinghal865@gmail.com>
2026-06-24 01:18:33 +0530

    fix: correct type annotations for lock parameter

--- dask/array/core.py
+++ dask/array/core.py
@@ -3468,12 +3468,12 @@ def _get_chunk_shape(a):
 def from_array(
     x: Any,
     chunks: str | tuple = "auto",
-    name: str | None = None,
+    name: str | bool | None = None,
     lock: bool | SerializableLock = False,
     asarray: bool | None = None,
     fancy: bool = True,
     getitem: Any | None = None,
-    meta: Any | None = None,
+    meta: Any | None = None, 
     inline_array: bool = False,
 ) -> Array:
     """Create dask array from something that looks like an array.
@@ -3689,6 +3689,7 @@ def from_array(
         slices = slices_from_chunks(chunks)
         keys = product([name], *(range(len(bds)) for bds in chunks))
         values = [x[slc] for slc in slices]
+        dsk: HighLevelGraph | dict[tuple[int | str | None, ...], Any]
         dsk = dict(zip(keys, values))
 
     elif is_ndarray and is_single_block:
